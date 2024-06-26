e3390e93da7277327c16f69057e8af2a52feef84
JinSeoHyun <15roa1121@gmail.com>
2024-06-26 18:32:25 +0900

    fix : getter 없던 오류 추가

--- src/main/java/gift/main/dto/SingleProductResponse.java
+++ src/main/java/gift/main/dto/SingleProductResponse.java
@@ -5,6 +5,10 @@ import gift.main.entity.Product;
 public class SingleProductResponse extends Response {
     private Product product;
 
+    public Product getProduct() {
+        return product;
+    }
+
     public SingleProductResponse(int code, String message, Product product) {
         super(code, message);
         this.product = product;
