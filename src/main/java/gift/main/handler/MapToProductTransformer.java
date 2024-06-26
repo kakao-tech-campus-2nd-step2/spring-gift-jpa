a2046ec6eb2a84c6ae0daf9b30f2b1df486bedd3
JinSeoHyun <15roa1121@gmail.com>
2024-06-26 18:34:35 +0900

    fix : 1. 메서드 접근 제어자 수정, 2. id 발급 전략 선택 3.

--- src/main/java/gift/main/handler/MapToProductTransformer.java
+++ src/main/java/gift/main/handler/MapToProductTransformer.java
@@ -1,15 +1,15 @@
 package gift.main.handler;
 
+import gift.main.dto.ProductRequest;
 import gift.main.entity.Product;
 
 import java.util.Map;
 
 public class MapToProductTransformer {
-    public static Product convertMapToProduct(Map<String, Object> map) {
-        Long id = Long.valueOf((int)map.get("id"));
-        String name = (String) map.get("name");
-        int price = (int) map.get("price");
-        String imageUrl = (String) map.get("imageUrl");
+    public static Product convertToProduct(long id, ProductRequest productRequest) {
+        String name = productRequest.name();
+        int price = productRequest.price();
+        String imageUrl = productRequest.imageUrl();
 
         return new Product(id, name, price, imageUrl);
     }
