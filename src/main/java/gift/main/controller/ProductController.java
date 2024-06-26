7200f50ff30ceaeca8e0ac0a2f0ba24b2529ea87
JinSeoHyun <15roa1121@gmail.com>
2024-06-26 18:35:51 +0900

    fix : 1. 메서드 접근 제어자 수정, 2. id 발급 전략 선택 3. 쿼리파라미터 타입형 long으로 수정 4. 불필요한 예외처리 삭제

--- src/main/java/gift/main/controller/ProductController.java
+++ src/main/java/gift/main/controller/ProductController.java
@@ -1,6 +1,7 @@
-package gift.main.product;
+package gift.main.controller;
 
 import gift.main.dto.ListProductResponse;
+import gift.main.dto.ProductRequest;
 import gift.main.dto.Response;
 import gift.main.dto.SingleProductResponse;
 import gift.main.entity.Product;
@@ -11,14 +12,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class ProductController {
     private final Map<Long, Product> producstRepository = new HashMap<>();
+    private final AtomicLong idGenerator = new AtomicLong();
 
     @GetMapping("/products")
     public Response provideProductAll() {
-
         if (producstRepository.isEmpty()) {
             return new Response(200, "현재 모든 물건이 비어있습니다.");
         }
@@ -32,61 +34,41 @@ public class ProductController {
     }
 
     @GetMapping("/product")
-    public Response provideProduct(@RequestParam(value = "id") int idInt) {
-
-        Long id = Long.valueOf(idInt);
-        Product product = producstRepository.get(id);
-
-        if (product == null) {
+    public Response provideProduct(@RequestParam(value = "id") long id) {
+        if (!producstRepository.containsKey(id)) {
             return new Response(400, "해당 ID 값은 없습니다.");
         }
-
-        return new SingleProductResponse(200, "ok", product);
+        System.out.println("producstRepository.get(id) = " + producstRepository.get(id));
+        return new SingleProductResponse(200, "ok", producstRepository.get(id));
 
     }
 
     @PostMapping("/product")
-    public Response AddProduct(@RequestBody HashMap<String, Object> map) {
-        Long id = Long.valueOf((int)map.get("id"));
-        if (producstRepository.containsKey(id)){
-            return new Response(400, "해당 id값은 이미 존재하는 값입니다.");
-        }
-        try {
-            producstRepository.put(id, MapToProductTransformer.convertMapToProduct(map));
-        } catch (NullPointerException e) {
-            return new Response(400, "JSON 형식에 문제가 있습니다.");
-        } catch (Exception e) {
-            return new Response(500, "오류가 발생했습니다.");
-        }
+    public Response addProduct(@RequestBody ProductRequest productRequest) {
+        long id = idGenerator.incrementAndGet();
+        producstRepository.put(id, MapToProductTransformer.convertToProduct(id,productRequest));
         return new Response(200, "정상등록되었습니다.");
     }
 
     @PutMapping("/product")
-    public Response editProduct(@RequestBody HashMap<String, Object> map) {
-        Long id = Long.valueOf((int)map.get("id"));
-        if (!producstRepository.containsKey(id)){
+    public Response updateProduct(@RequestParam(value = "id") long id, @RequestBody ProductRequest productRequest) {
+        if (!producstRepository.containsKey(id)) {
             return new Response(400, "해당 id값은 존재하지 않습니다.");
         }
-        try {
-            producstRepository.replace(id, MapToProductTransformer.convertMapToProduct(map));
-        } catch (NullPointerException e) {
-            return new Response(400, "JSON 형식에 문제가 있습니다.");
-        } catch (Exception e) {
-            return new Response(500, "오류가 발생했습니다.");
-        }
-        return new Response(200, "정상등록되었습니다.");
+        producstRepository.replace((long) id, MapToProductTransformer.convertToProduct(id, productRequest));
+
+        return new Response(200, "정상 수정되었습니다.");
     }
 
     @DeleteMapping("/product")
-    public Response deleteProduct(@RequestParam(value = "id") int idInt) {
-        Long id = Long.valueOf(idInt);
+    public Response deleteProduct(@RequestParam(value = "id") long id) {
         if (!producstRepository.containsKey(id)) {
             return new Response(400, "해당 id값은 존재하지 않습니다.");
         }
         producstRepository.remove(id);
 
-
-        return new Response(200, "삭제가 처리되었습니다.");
+        return new Response(200, "삭제처리되었습니다.");
     }
+
 
 }
