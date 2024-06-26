ffea05de09094a61efe8de6e6822b5415b7726e7
JinSeoHyun <15roa1121@gmail.com>
2024-06-26 14:19:39 +0900

    Feat: dto 작성

---
+++ src/main/java/gift/main/dto/SingleProductResponse.java
@@ -0,0 +1,12 @@
+package gift.main.dto;
+
+import gift.main.entity.Product;
+
+public class SingleProductResponse extends Response {
+    private Product product;
+
+    public SingleProductResponse(int code, String message, Product product) {
+        super(code, message);
+        this.product = product;
+    }
+}
