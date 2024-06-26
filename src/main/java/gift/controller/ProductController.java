7f12a9266c4a3b02f56be56b57913336ddc658cf
young-in <hardworkingini@gmail.com>
2024-06-26 14:03:05 +0900

    [feat] product 전체 조회 기능 구현

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,17 +1,33 @@
 package gift.controller;
 
 import gift.domain.Product;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import gift.dto.CreateProductDto;
+import gift.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
-    // DB가 없으니까, 컬렉션을 사용하여 메모리에 저장
-    private final Map<Long, Product> producsts = new HashMap<>();
 
+    private final ProductService productService;
 
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    // 상품 추가
+    @PostMapping
+    public Product createProduct(@RequestBody CreateProductDto productDto) {
+        try {
+            return productService.createProduct(productDto);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
 }
--- src/main/java/gift/domain/Product.java
+++ src/main/java/gift/domain/Product.java
@@ -5,4 +5,24 @@ public class Product {
     String name;
     String description;
     Integer price;
+
+    public void setId(long andIncrement) {
+        this.id = andIncrement;
+    }
+
+    public void setName(Object name) {
+        this.name = name.toString();
+    }
+
+    public void setPrice(Object price) {
+        this.price = (Integer) price;
+    }
+
+    public void setDescription(Object description) {
+        this.description = description.toString();
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
