e3fc622313f9a516934e19973c760a3484ebc586
JinSeoHyun <15roa1121@gmail.com>
2024-06-28 12:59:43 +0900

    feat : JDBC Dao 를 사용해서 DB 기능 추가

--- src/main/java/gift/main/admin/AdminController.java
+++ src/main/java/gift/main/admin/AdminController.java
@@ -3,12 +3,12 @@ package gift.main.admin;
 import gift.main.dto.ProductRequest;
 import gift.main.entity.Product;
 import gift.main.handler.MapToProductTransformer;
+import gift.main.repository.ProductDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
@@ -17,33 +17,31 @@ import java.util.concurrent.atomic.AtomicLong;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-    private final Map<Long, Product> productsRepository = new HashMap<>();
+    private final ProductDao productDao;
     private final AtomicLong idGenerator = new AtomicLong();
 
-
-//    @PostConstruct
-    public void init() {
-        long id = idGenerator.incrementAndGet();
-        Product product1 = new Product(id, "test", 300, "url");
-        productsRepository.put(id, product1);
+    public AdminController(ProductDao productDao) {
+        this.productDao = productDao;
+        productDao.createProductTable();
     }
+
 
     @GetMapping({"","/"})
     public String adminPage(Model model) {
-        model.addAttribute("products", getProduct());
+        model.addAttribute("products", productDao.selectProductAll());
         return "product";
     }
 
 
     @GetMapping("/product")
     public String findProduct(@RequestParam(value = "id") long id,Model model) {
-        if (!productsRepository.containsKey(id)) {
+        if (productDao.selectProduct(id)==null) {
             model.addAttribute("messages","해당아이디값은 없습니다.");
-            model.addAttribute("products", getProduct());
+            model.addAttribute("products",productDao.selectProductAll());
             return "product";
         }
-        model.addAttribute("seletProduct", productsRepository.get(id));
-        model.addAttribute("products", getProduct());
+        model.addAttribute("seletProduct", productDao.selectProduct(id));
+        model.addAttribute("products", productDao.selectProductAll());
         return "product";
 
     }
@@ -51,40 +49,40 @@ public class AdminController {
     @PostMapping("/product")
     public String addProduct(@ModelAttribute ProductRequest productRequest,Model model) {
         long id = idGenerator.incrementAndGet();
-        productsRepository.put(id, MapToProductTransformer.convertToProduct(id, productRequest));
+        productDao.insertProduct(MapToProductTransformer.convertToProduct(id, productRequest));
 
-        model.addAttribute("products", getProduct());
+        model.addAttribute("products",  productDao.selectProductAll());
         return "product";
     }
+
+    @PutMapping(value = "/product")
+    public String updateProduct(@RequestParam(value = "id") long id, @ModelAttribute ProductRequest productRequest, Model model){
+        productDao.updateProduct(id, MapToProductTransformer.convertToProduct(id, productRequest));
+        model.addAttribute("products", productDao.selectProductAll());
+        return "product";
+    }
+
+    @DeleteMapping("/product")
+    public String deleteProduct(@RequestParam(value = "id") long id, Model model) {
+        productDao.deleteProduct(id);
+        model.addAttribute("products", productDao.selectProductAll());
+        return "product";
+    }
+
+//    private List<Product> getProduct() {
+//        List<Product> productList = new ArrayList<>();
+//        for (Map.Entry<Long, Product> productEntry : productsRepository.entrySet()) {
+//            productList.add(productEntry.getValue());
+//        }
+//
+//        return productList;
+//    }
 
     @GetMapping("/product/edit")
     public String editPage(@RequestParam(value = "id") long id, @ModelAttribute ProductRequest productRequest,Model model) {
         Product product = MapToProductTransformer.convertToProduct(id, productRequest);
         model.addAttribute("product", product);
         return "edit";
-    }
-
-    @PutMapping(value = "/product")
-    public String updateProduct(@RequestParam(value = "id") long id, @ModelAttribute ProductRequest productRequest, Model model){
-        productsRepository.replace(id, MapToProductTransformer.convertToProduct(id, productRequest));
-        model.addAttribute("products", getProduct());
-        return "product";
-    }
-
-    @DeleteMapping("/product")
-    public String deleteProduct(@RequestParam(value = "id") long id, Model model) {
-        productsRepository.remove(id);
-        model.addAttribute("products", getProduct());
-        return "product";
-    }
-
-    private List<Product> getProduct() {
-        List<Product> productList = new ArrayList<>();
-        for (Map.Entry<Long, Product> productEntry : productsRepository.entrySet()) {
-            productList.add(productEntry.getValue());
-        }
-
-        return productList;
     }
 
 }
