f642bd1aec4ed56af1dcfb2108c040019a4ae5dd
LeeTaek2T <137752192+LeeTaek2T@users.noreply.github.com>
2024-07-02 21:56:02 +0900

    충남대 BE_이택_2주차 과제(0단계) (#112)
    * Initial commit
    
    * feat: set up the project
    
    * step1구현
    
    * ReadMe 수정 및 image url추가
    
    * step2
    
    * step3
    
    * feat : prductAdminController에 DB적용 및 step3완성
    
    * ProductContrllerTest에 db적용
    
    * Update src/main/java/gift/ProductAdminController.java
    
    Co-authored-by: Seyun(Marco) / 김세윤 <ksy90101@gmail.com>
    
    * feat : 주석제거
    
    * build : productDTO사용
    
    * feat : schema.sql추가
    
    * feat : 주소 재설정
    
    * feat : RowMapper메서드 생성
    
    * build : template style 분리, Test변경(JDBCTest->SpringBootTest)
    
    * build : contoller-service-repository구조 생성
    
    ---------
    
    Co-authored-by: 박재성(Jason) <wotjd243@naver.com>
    Co-authored-by: test1 <test>
    Co-authored-by: Seyun(Marco) / 김세윤 <ksy90101@gmail.com>

---
+++ src/main/java/gift/ProductController.java
@@ -0,0 +1,64 @@
+package gift;
+
+import jakarta.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+  private final ProductService productService;
+
+  public ProductController(ProductService productService) {
+    this.productService = productService;
+  }
+
+  @GetMapping
+  public List<Product> getAllProducts() {
+    return productService.getAllProducts();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+    Product product = productService.getProductById(id);
+    if (product == null) {
+      return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.ok(product);
+  }
+
+  @PostMapping
+  public Product addProduct(@RequestBody Product product) {
+    return productService.addProduct(product);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Product> updateProduct(@PathVariable Long id,
+    @RequestBody Product updatedProduct) {
+    Product existingProduct = productService.updateProduct(id,updatedProduct);
+    if (existingProduct == null) {
+      return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.ok(updatedProduct);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+    Product existingProduct = productService.deleteProduct(id);
+    if (existingProduct == null) {
+      return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.noContent().build();
+  }
+}
---
+++ src/main/java/gift/ProductService.java
@@ -0,0 +1,50 @@
+package gift;
+
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+@Service
+public class ProductService {
+  private final ProductDao productDao;
+  public ProductService(ProductDao productDao){
+    this.productDao=productDao;
+  }
+
+  public List<Product> getAllProducts() {
+    List<Product> productDtos = productDao.selectAllProducts();
+    return productDtos;
+  }
+
+  public Product getProductById(Long id) {
+    return productDao.selectProduct(id);
+
+  }
+
+  public Product addProduct(Product product) {
+    productDao.insertProduct(product);
+    return product;
+  }
+  public Product updateProduct(Long id, Product updatedProduct) {
+    Product existingProduct = productDao.selectProduct(id);
+    if (existingProduct!=null){
+      updatedProduct.setId(id);
+      productDao.updateProduct(updatedProduct);
+    }
+    return existingProduct;
+  }
+
+  public Product deleteProduct(@PathVariable Long id) {
+    Product existingProduct = productDao.selectProduct(id);
+    if (existingProduct != null) {
+      productDao.deleteProduct(id);
+    }
+    return existingProduct;
+  }
+
+  //테스트를 위한 함수
+  public void dropProductTable(){
+    productDao.dropProductTable();
+  }
+}
