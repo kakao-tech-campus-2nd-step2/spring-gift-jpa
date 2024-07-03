b8ce0e0a04d4fc32c4b94d1b01ad77e5d87e6adb
JinSeoHyun <15roa1121@gmail.com>
2024-07-03 16:09:51 +0900

    feat: 에러 핸들러 추가

--- src/main/java/gift/main/error/ProductExceptionHandler.java
+++ src/main/java/gift/main/error/ProductExceptionHandler.java
@@ -1,2 +1,21 @@
-package gift.main.error;public class ProductExceptionHandler {
+package gift.main.error;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
+
+@ControllerAdvice
+public class ProductExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String HandleWrongNameException(Model model, IllegalArgumentException e) {
+        model.addAttribute("error",e.getMessage());
+        String refererUrl = " http://localhost:8080/spring-gift/admin";
+        model.addAttribute("refererUrl", refererUrl);
+        return "error/error";
+    }
+
 }
--- src/main/java/gift/main/handler/ProductValidator.java
+++ src/main/java/gift/main/handler/ProductValidator.java
@@ -4,16 +4,25 @@ import gift.main.dto.ProductDto;
 import gift.main.dto.ProductRequest;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
 
 public class ProductValidator {
 
     public static ProductDto isValidProductDto(ProductRequest productRequest) {
-        @Pattern(regexp = "^[a-zA-Z0-9( \\[\\]\\+\\-&/\\_]{1,15}$",
-                message = "상품 이름은 공백을 포함하여 최대 15자까지 입력 가능하며, 허용되는 특수 문자는 ( ), [ ], +, -, &, /, _ 입니다.")
+        if (productRequest.name() == null || productRequest.name().length() > 15 ) {
+            throw new IllegalArgumentException("상품명은 공백 포함 1~15자사이여야합니다.");
+        }
+
+        if (!productRequest.name().matches("^[\\w\\s\\(\\)\\[\\]\\+\\-\\&\\/\\_가-힣]+$")){
+            throw new IllegalArgumentException("상품 이름에는 허용되는 특수 문자만 사용할 수 있습니다. 허용되는 특수 문자는 ( ), [ ], +, -, &, /, _ 입니다.");
+        }
+
         String validName = productRequest.name();
         if (validName.contains("카카오")) {
             throw new IllegalArgumentException("카카오라는 이름을 사용시에 관리자에게 문의하세요");
         }
+
+
         @PositiveOrZero(message="가격은 음수일수 없습니다.")
         int validPrice = productRequest.price();
 
