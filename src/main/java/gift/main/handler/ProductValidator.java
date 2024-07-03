22eb7d16b02a2ca8265733e80950a90b126db123
JinSeoHyun <15roa1121@gmail.com>
2024-07-03 16:24:15 +0900

    bug(ProductValidator): 가격 유효성 검사 로직 추가

--- src/main/java/gift/main/handler/ProductValidator.java
+++ src/main/java/gift/main/handler/ProductValidator.java
@@ -2,9 +2,6 @@ package gift.main.handler;
 
 import gift.main.dto.ProductDto;
 import gift.main.dto.ProductRequest;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.PositiveOrZero;
-import javax.validation.constraints.Size;
 
 public class ProductValidator {
 
@@ -17,13 +14,14 @@ public class ProductValidator {
             throw new IllegalArgumentException("상품 이름에는 허용되는 특수 문자만 사용할 수 있습니다. 허용되는 특수 문자는 ( ), [ ], +, -, &, /, _ 입니다.");
         }
 
-        String validName = productRequest.name();
-        if (validName.contains("카카오")) {
+        if (productRequest.name().contains("카카오")) {
             throw new IllegalArgumentException("카카오라는 이름을 사용시에 관리자에게 문의하세요");
         }
+        String validName = productRequest.name();
 
-
-        @PositiveOrZero(message="가격은 음수일수 없습니다.")
+        if(productRequest.price()>=0){
+            throw new IllegalArgumentException("가격은 음수일수 없습니다");
+        }
         int validPrice = productRequest.price();
 
         String validimageUrl = productRequest.imageUrl();
