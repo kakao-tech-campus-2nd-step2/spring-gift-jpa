dad52289c056e2a61958d9e7f5bdd78f098bc9a7
test1 <test>
2024-07-04 03:08:24 +0900

    fix : user테이블 중복으로 인한 오류 -> users로 명명

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,15 @@
 DROP TABLE IF EXISTS product;
-create table product(
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name varchar(255),
-    price bigint,
-    imageUrl varchar(255),
-    primary key (id)
-)
+DROP TABLE IF EXISTS users;
 
-DROP TABLE IF EXISTS user;
-create table user(
-    username varchar(255) PRIMARY KEY,
-    pw varchar(255),
-    accessToken varchar(255)
-)
+CREATE TABLE product (
+                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                         name VARCHAR(255),
+                         price BIGINT,
+                         imageUrl VARCHAR(255)
+);
+
+CREATE TABLE users (
+                      username VARCHAR(255) PRIMARY KEY,
+                      pw VARCHAR(255),
+                      accessToken VARCHAR(255)
+);
