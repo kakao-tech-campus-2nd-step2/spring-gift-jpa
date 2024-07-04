801a58c6a726ec4595f9793cbb8809bd5adcdc10
JinSeoHyun <15roa1121@gmail.com>
2024-07-05 07:49:27 +0900

    fix(MemberService) : != 연산자 고침

--- src/main/java/gift/main/service/MemberService.java
+++ src/main/java/gift/main/service/MemberService.java
@@ -4,7 +4,7 @@ import gift.main.dto.UserDto;
 import gift.main.dto.UserJoinRequest;
 import gift.main.dto.UserLoginRequest;
 import gift.main.entity.User;
-import gift.main.global.validator.UserException;
+import gift.main.global.Exception.UserException;
 import gift.main.repository.UserDao;
 import gift.main.util.AuthUtil;
 import org.springframework.stereotype.Service;
@@ -24,10 +24,17 @@ public class MemberService {
         //유효성 검사해야하는데용~!
         if (userDao.existsUserByEmail(userJoinRequest.email())) {
             throw new UserException("이미 존재하는 이메일 주소입니다.");
+
+
         }
+        System.out.println("userJoinRequest.toString() = " + userJoinRequest.toString());
         UserDto validUser = new UserDto(userJoinRequest) ;
         userDao.insertUser(validUser);
-        return authUtil.createToken(validUser);
+
+        String token = authUtil.createToken(validUser);
+        System.out.println("token = " + token);
+        return token;
+
     }
 
     public String loginUser(UserLoginRequest userLoginRequest) {
@@ -35,8 +42,9 @@ public class MemberService {
         if (!userDao.existsUserByEmail(userLoginRequest.email())) {
             throw new UserException("해당 이메일주소는 존재하지 않습니다.");
         }
+
         User user= userDao.existsUser(userLoginRequest.email(), userLoginRequest.password());
-        if (user!=null) {
+        if (user==null) {
             throw new UserException("비밀번호가 틀렸습니다.");
         }
         return authUtil.createToken(user);
