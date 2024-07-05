0293751ed00f9db0617cfb356ab90e089ae9f7f1
JinSeoHyun <15roa1121@gmail.com>
2024-07-05 16:28:36 +0900

    chore :

--- src/main/java/gift/main/config/WebConfig.java
+++ src/main/java/gift/main/config/WebConfig.java
@@ -1,9 +1,6 @@
 package gift.main.config;
 
 import gift.main.interceptor.AuthInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
