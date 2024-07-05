324adff4a4856037185565d64458ed77b9e778f8
young-in <hardworkingini@gmail.com>
2024-07-05 14:27:56 +0900

    feat(ProductValidation) : 허용되지 않은 특수 문자가 포함된 경우 에러메시지 반환하도록 구현

--- src/main/java/gift/validation/ProductValidation.java
+++ src/main/java/gift/validation/ProductValidation.java
@@ -4,8 +4,13 @@ import gift.dto.CreateProductDto;
 import gift.repository.ProductRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Component
 public class ProductValidation {
+    private static final String ALLOWED_SPECIAL_CHARACTERS = "()[]+\\-&/_.";
+
     public void validateProductDto(CreateProductDto productDto) {
         if (productDto.getName() == null || productDto.getDescription() == null || productDto.getPrice() == null || productDto.getImageUrl() == null) {
             throw new IllegalArgumentException("상품의 이름, 가격, 설명을 모두 입력해야합니다.");
@@ -19,8 +24,15 @@ public class ProductValidation {
     }
 
     public void validateProductName(String name) {
+
         if (name.length() > 15) {
-            throw new IllegalArgumentException("Product name must be 15 characters or less.");
+            throw new IllegalArgumentException("상품 이름은 15자 이하여야 합니다.");
+        }
+
+        Pattern pattern = Pattern.compile("[^" + ALLOWED_SPECIAL_CHARACTERS + "a-zA-Z0-9]");
+        Matcher matcher = pattern.matcher(name);
+        if (matcher.find()) {
+            throw new IllegalArgumentException("상품 이름에는 허용된 특수 문자 (" + ALLOWED_SPECIAL_CHARACTERS + ") 만 사용할 수 있습니다.");
         }
     }
 }
