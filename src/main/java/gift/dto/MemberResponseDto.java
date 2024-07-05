1d5a6dd296103775517f6b050f1308c583f377d0
young-in <hardworkingini@gmail.com>
2024-07-05 16:44:42 +0900

    feat(MemberService) : 회원가입 기능도 성공하면 token 반환하도록 수정

--- src/main/java/gift/dto/MemberResponseDto.java
+++ src/main/java/gift/dto/MemberResponseDto.java
@@ -1,12 +1,9 @@
 package gift.dto;
 
 public class MemberResponseDto {
-    private Long id;
-    private String email;
     private String token;
 
-    public MemberResponseDto(Long id, String email) {
-        this.id = id;
-        this.email = email;
+    public MemberResponseDto(String token) {
+        this.token = token;
     }
 }
--- src/main/java/gift/service/MemberService.java
+++ src/main/java/gift/service/MemberService.java
@@ -4,6 +4,7 @@ import gift.domain.Member;
 import gift.dto.MemberRequestDto;
 import gift.dto.MemberResponseDto;
 import gift.repository.MemberRepository;
+import gift.validation.JwtTokenProvider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,15 +12,17 @@ import org.springframework.stereotype.Service;
 public class MemberService {
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public MemberService(MemberRepository memberRepository, BCryptPasswordEncoder passwordEncoder) {
+    public MemberService(MemberRepository memberRepository, BCryptPasswordEncoder passwordEncoder, JwtTokenProvider jwtTokenProvider) {
         this.memberRepository = memberRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     public MemberResponseDto registerMember(MemberRequestDto requestDto) {
         if (memberRepository.existsByEmail(requestDto.getEmail())) {
-            throw new IllegalArgumentException("Email already exists");
+            throw new IllegalArgumentException("이메일이 이미 존재합니다.");
         }
 
         Member member = new Member();
@@ -27,8 +30,10 @@ public class MemberService {
         member.setPassword(passwordEncoder.encode(requestDto.getPassword()));
         Member savedMember = memberRepository.save(member);
 
-        return new MemberResponseDto(savedMember.getId(), savedMember.getEmail());
+        String token = jwtTokenProvider.generateToken(savedMember);
+        return new MemberResponseDto(token);
     }
+
 
     public Member findByEmail(String email) {
         return memberRepository.findByEmail(email)
