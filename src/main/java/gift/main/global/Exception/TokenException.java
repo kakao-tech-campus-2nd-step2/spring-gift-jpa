b016a93e9e5cb082d894e061a143e0d3c6a4d2dd
JinSeoHyun <15roa1121@gmail.com>
2024-07-05 13:49:06 +0900

    fetch: 커스텀예외 구현

--- src/main/java/gift/main/global/Exception/TokenException.java
+++ src/main/java/gift/main/global/Exception/TokenException.java
@@ -1,7 +1,7 @@
 package gift.main.global.Exception;
 
-public class UserException extends RuntimeException{
-    public UserException(String message) {
+public class TokenException extends RuntimeException{
+    public TokenException(String message) {
         super(message);
     }
 
