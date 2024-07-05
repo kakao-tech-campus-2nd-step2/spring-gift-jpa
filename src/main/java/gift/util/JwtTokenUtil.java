a8b2837501c594e7e96be2ad199d3ed89886d62b
stopmin <geemin0@naver.com>
2024-07-05 15:54:52 +0900

    feat: Token 발급 컨트롤러 및 util 생성

---
+++ src/main/java/gift/util/JwtTokenUtil.java
@@ -0,0 +1,49 @@
+package gift.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenUtil {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    public String generateToken(String username) {
+        System.out.println(jwtSecret);
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+            .setClaims(claims)
+            .setSubject(username)
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
+            .signWith(SignatureAlgorithm.HS256, jwtSecret)
+            .compact();
+    }
+
+    public Claims extractClaims(String token) {
+        return Jwts.parser()
+            .setSigningKey(jwtSecret)
+            .parseClaimsJws(token)
+            .getBody();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+
+    public boolean validateToken(String token, String username) {
+        return (extractUsername(token).equals(username) && !isTokenExpired(token));
+    }
+}
