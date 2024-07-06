646fd1cb19c8e98c4b6ce7b92896471e327311ba
JinSeoHyun <15roa1121@gmail.com>
2024-07-07 03:59:03 +0900

    test : 유저 회원가입 로직 테스트 구현

---
+++ src/test/java/gift/main/service/UserServiceTest.java
@@ -0,0 +1,64 @@
+package gift.main.service;
+
+import gift.main.dto.UserJoinRequest;
+import gift.main.global.Exception.UserException;
+import gift.main.repository.UserDao;
+import gift.main.util.AuthUtil;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootTest
+class UserServiceTest {
+
+    @Autowired
+    private AuthUtil authUtil;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private UserDao userDao;
+
+    private UserService userService;
+
+    //    @BeforeAll // 클래스에 하나
+    @BeforeEach // 메서드마다 하나
+    public void createTabel() {
+        this.userDao = new UserDao(jdbcTemplate);
+        this.userService = new UserService(userDao, authUtil);
+    }
+
+    @AfterEach // 메서드마다 하나
+    public void clearTabel() {
+        jdbcTemplate.execute("DELETE FROM users");
+    }
+
+    @Test
+    public void 이메일로가입() {
+        UserJoinRequest user = new UserJoinRequest("진서현", "jin1228@g.mail", "1234", "user");
+
+        Assertions.assertThatCode(() -> {
+            userService.joinUser(user);
+        }).doesNotThrowAnyException();
+    }
+
+    @Test
+    public void 이미존재하는이메일로가입() {
+        UserJoinRequest user = new UserJoinRequest("진서현", "jin1228@g.mail", "1234", "user");
+        userService.joinUser(user);
+        UserJoinRequest duplicateUser = new UserJoinRequest("진서경", "jin1228@g.mail", "1234", "user");
+
+        Assertions.assertThatThrownBy(() -> {
+                    userService.joinUser(duplicateUser);
+                })
+                .isInstanceOf(UserException.class);
+    }
+
+
+
+}
