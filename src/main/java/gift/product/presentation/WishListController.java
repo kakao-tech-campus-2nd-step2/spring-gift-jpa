ad58872606e3ef6ae4a6e54d18a04098cb6b51e0
stopmin <geemin0@naver.com>
2024-07-08 15:26:17 +0900

    feat: 상품 추가 API 수정(id로 상품 추가하도록 수정)

--- src/main/java/gift/product/presentation/WishListController.java
+++ src/main/java/gift/product/presentation/WishListController.java
@@ -2,20 +2,16 @@ package gift.product.presentation;
 
 
 import gift.product.application.WishListService;
-import gift.product.domain.Product;
 import gift.product.domain.WishList;
 import gift.product.exception.ProductException;
 import gift.util.CommonResponse;
 import gift.util.ErrorCode;
 import gift.util.JwtAuthenticated;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
