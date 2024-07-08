21fa3f0fe8158f6960cc13d1240824aef6ce6004
anacnoda77 <sm1108shin@naver.com>
2024-07-08 18:27:17 +0900

    feat: MemberRepo findByEmail 리턴 타입  Optional<> 설정

--- src/main/java/gift/repository/MemberRepository.java
+++ src/main/java/gift/repository/MemberRepository.java
@@ -1,9 +1,10 @@
 package gift.repository;
 
 import gift.model.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Member findByEmail(String email);
+    Optional<Member> findByEmail(String email);
 }
