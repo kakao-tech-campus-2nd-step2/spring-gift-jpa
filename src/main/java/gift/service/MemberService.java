4dabcf22029c0521127c4894f889f827c6e2e6f3
anacnoda77 <sm1108shin@naver.com>
2024-07-08 18:04:47 +0900

    refactor: MemberService Repo 전환

--- src/main/java/gift/service/MemberService.java
+++ src/main/java/gift/service/MemberService.java
@@ -1,25 +1,35 @@
 package gift.service;
 
 import gift.exception.LoginErrorException;
+import gift.exception.MemberException;
 import gift.model.Member;
 import gift.repository.MemberDao;
+import gift.repository.MemberRepository;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class MemberService {
 
-    private final MemberDao memberDao;
+    private final MemberRepository memberRepository;
 
-    public MemberService(MemberDao memberDao) {
-        this.memberDao = memberDao;
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     public Member join(String email, String password) {
-        return memberDao.insert(new Member(email, password));
+        try {
+            return memberRepository.save(new Member(email, password));
+        } catch (DataIntegrityViolationException e) {
+            throw new MemberException("중복된 이메일의 회원이 이미 존재합니다.");
+        }
+
     }
 
     public Member login(String email, String password) {
-        Member loginedMember = memberDao.getMemberByEmail(email);
+        Member loginedMember = memberRepository.findByEmail(email);
 
         if(!loginedMember.login(email, password)) {
             throw new LoginErrorException();
--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -7,8 +7,10 @@ import gift.repository.ProductRepository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class ProductService {
 
     private final ProductRepository productRepository;
