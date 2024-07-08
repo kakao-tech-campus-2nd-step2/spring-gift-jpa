646488fb8fb872193b608c453a97e97687822d4c
anacnoda77 <sm1108shin@naver.com>
2024-07-08 17:17:51 +0900

    refactor: WishProduct JPA 적용

--- src/main/java/gift/model/WishProduct.java
+++ src/main/java/gift/model/WishProduct.java
@@ -1,20 +1,69 @@
 package gift.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(
+    name = "wish_products",
+    uniqueConstraints = {
+        @UniqueConstraint(
+            name = "constraintName",
+            columnNames = {"member_id", "product_id"}
+        )
+    }
+)
 public class WishProduct {
-    private Long memberId;
-    private Long productId;
 
-    public WishProduct(Long memberId, Long productId) {
-        this.memberId = memberId;
-        this.productId = productId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public WishProduct() {
     }
 
-    public Long getMemberId() {
-        return memberId;
+    public WishProduct(Member member, Product product) {
+        this.member = member;
+        this.product = product;
     }
 
-    public Long getProductId() {
-        return productId;
+    public Long getId() {
+        return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public Member getMember() {
+        return member;
+    }
 }
