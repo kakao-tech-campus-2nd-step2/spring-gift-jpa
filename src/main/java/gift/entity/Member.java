5533dbfddf36c4f5ea6bb4a9bcd47b40341cea5b
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2024-07-08 19:50:10 +0900

    feat : 멤버 entity 추가

---
+++ src/main/java/gift/entity/Member.java
@@ -0,0 +1,61 @@
+package gift.entity;
+
+import gift.dto.MemberDto;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
+
+public class Member {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @Column(nullable = false)
+    private String password;
+
+    private String role;
+    
+    protected Member() {
+
+    }
+
+    public Member(String email, String password, String role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public MemberDto toDto(Member member){
+        return new MemberDto(this.id, this.password, this.email, this.role);
+    }
+}
