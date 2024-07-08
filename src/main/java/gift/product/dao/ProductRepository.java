959f98abfc41d14cc73c56bf70b5b0ef67dabfa5
HSHwan <tnghks2866@gmail.com>
2024-07-08 15:25:09 +0900

    fix: 상품 레포지토리 @Repository 어노테이션 추가

--- src/main/java/gift/product/dao/ProductRepository.java
+++ src/main/java/gift/product/dao/ProductRepository.java
@@ -5,9 +5,11 @@ import io.hypersistence.utils.spring.repository.HibernateRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProductRepository extends
         HibernateRepository<Product>,
         JpaRepository<Product, Long> {
