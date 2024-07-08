a43f98cd6c01b0d3953ce68e7a45cab46084ecd2
wvssm <neoramiregi@gmail.com>
2024-07-09 00:51:11 +0900

    refactor(WishService): Messages 클래스에 정의된 메시지 사용하도록 수정

--- src/main/java/gift/service/WishService.java
+++ src/main/java/gift/service/WishService.java
@@ -1,5 +1,6 @@
 package gift.service;
 
+import gift.constants.Messages;
 import gift.domain.Product;
 import gift.domain.Wish;
 import gift.dto.UserResponseDto;
@@ -19,9 +20,6 @@ public class WishService {
     private final WishRepository wishRepository;
     private final ProductRepository productRepository;
 
-    private final String NOT_FOUND_PRODUCT_BY_NAME_MESSAGE = "해당 이름의 상품이 존재하지 않습니다.";
-    private final String NOT_FOUND_WISH_MESSAGE = "위시가 존재하지 않습니다.";
-
     public WishService(WishRepository wishRepository, ProductRepository productRepository) {
         this.wishRepository = wishRepository;
         this.productRepository = productRepository;
@@ -29,13 +27,13 @@ public class WishService {
 
     public void save(UserResponseDto userResponseDto, WishRequestDto request){
         Product product = productRepository.findByName(request.getProductName())
-                .orElseThrow(() -> new ProductNotFoundException(NOT_FOUND_PRODUCT_BY_NAME_MESSAGE));
+                .orElseThrow(() -> new ProductNotFoundException(Messages.NOT_FOUND_PRODUCT_BY_NAME));
         wishRepository.save(new Wish(userResponseDto.getId(),product.getId(),request.getQuantity()));
     }
 
     public List<WishResponseDto> findByUserEmail(UserResponseDto userResponseDto){
         return wishRepository.findByUserId(userResponseDto.getId())
-                .orElseThrow(() -> new WishNotFoundException(NOT_FOUND_WISH_MESSAGE))
+                .orElseThrow(() -> new WishNotFoundException(Messages.NOT_FOUND_WISH))
                 .stream()
                 .map(this::convertToWishDto)
                 .collect(Collectors.toList());
@@ -43,19 +41,19 @@ public class WishService {
 
     public void delete(UserResponseDto userResponseDto, Long id){
         wishRepository.findByIdAndUserId(id, userResponseDto.getId())
-                .orElseThrow(()-> new WishNotFoundException(NOT_FOUND_WISH_MESSAGE));
+                .orElseThrow(()-> new WishNotFoundException(Messages.NOT_FOUND_WISH));
         wishRepository.delete(id);
     }
 
     public void updateQuantity(UserResponseDto userResponseDto, Long id, WishRequestDto request){
         wishRepository.findByIdAndUserId(id, userResponseDto.getId())
-                .orElseThrow(()-> new WishNotFoundException(NOT_FOUND_WISH_MESSAGE));
+                .orElseThrow(()-> new WishNotFoundException(Messages.NOT_FOUND_WISH));
         wishRepository.updateQuantity(id, request.getQuantity());
     }
 
     private WishResponseDto convertToWishDto(Wish wish) {
         Product product = productRepository.findById(wish.getProductId())
-                .orElseThrow(() -> new ProductNotFoundException(NOT_FOUND_PRODUCT_BY_NAME_MESSAGE));
+                .orElseThrow(() -> new ProductNotFoundException(Messages.NOT_FOUND_PRODUCT_BY_NAME));
         return new WishResponseDto(
                 wish.getId(),
                 wish.getProductId(),
