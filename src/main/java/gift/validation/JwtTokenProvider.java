bd65b97f2f18e3b5bf156196ddf002b9b7994f59
young-in <hardworkingini@gmail.com>
2024-07-08 21:14:40 +0900

    chore(JwtTokenProvider) : 필요없는 주석 삭제

--- src/main/java/gift/validation/JwtTokenProvider.java
+++ src/main/java/gift/validation/JwtTokenProvider.java
@@ -13,12 +13,11 @@ public class JwtTokenProvider {
     private static final String SECRET_KEY = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
 
     public String generateToken(Member member) {
-        // JWT 토큰 생성 로직
         return Jwts.builder()
                 .setSubject(member.getId().toString())
                 .claim("name", member.getEmail())
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 토큰 유효기간 1일
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
                 .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)
                 .compact();
     }
