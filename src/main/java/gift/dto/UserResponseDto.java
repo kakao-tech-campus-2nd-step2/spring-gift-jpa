bc7c77340a1f3be64753cd503552a70ee516e596
wvssm <neoramiregi@gmail.com>
2024-07-09 00:44:04 +0900

    feat(UserResponseDto): 생성자, id getter, setter 추가

--- src/main/java/gift/dto/UserResponseDto.java
+++ src/main/java/gift/dto/UserResponseDto.java
@@ -3,10 +3,24 @@ package gift.dto;
 import gift.domain.User;
 
 public class UserResponseDto {
+    private Long id;
     private String email;
 
     public UserResponseDto(String email) {
         this.email = email;
+    }
+
+    public UserResponseDto(Long id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getEmail() {
@@ -18,6 +32,6 @@ public class UserResponseDto {
     }
 
     public static UserResponseDto from(final User user){
-        return new UserResponseDto(user.getEmail());
+        return new UserResponseDto(user.getId(), user.getEmail());
     }
 }
