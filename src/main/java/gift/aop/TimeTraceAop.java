f9dc89fb8de1e16418993d7348b41483606a2117
Giheon Do <dgh0001@naver.com>
2024-07-08 21:22:20 +0900

    feat: add timeTraceAop

--- src/main/java/gift/aop/TimeTraceAop.java
+++ src/main/java/gift/aop/TimeTraceAop.java
@@ -6,7 +6,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Aspect
-@Component
+//@Component
 public class TimeTraceAop {
 
     @Around("execution(* gift..*(..))")
