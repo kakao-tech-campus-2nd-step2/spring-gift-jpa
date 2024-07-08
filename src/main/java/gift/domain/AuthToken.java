9fb612ffcb00fb789c68a024c33b2ee4b5931546
JJY <ddglackrp@gmail.com>
2024-07-08 22:37:04 +0900

    feat "AuthToken @Entity 추가"
    - record -> class 변경
    - @Entity 추가

--- src/main/java/gift/domain/AuthToken.java
+++ src/main/java/gift/domain/AuthToken.java
@@ -1,3 +1,37 @@
 package gift.domain;
 
-public record AuthToken(String uuid, String email){ }
+import jakarta.persistence.*;
+
+@Entity
+public class AuthToken{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String token;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public AuthToken() {
+    }
+
+    public AuthToken(String token, String email) {
+        this.token = token;
+        this.email = email;
+    }
+}
