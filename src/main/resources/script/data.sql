9a2ec380a2090507ac34e7a84e5d502e54f58279
HSHwan <tnghks2866@gmail.com>
2024-07-08 15:22:49 +0900

    fix: SQL 스크립트 파일 수정

--- src/main/resources/script/data.sql
+++ src/main/resources/script/data.sql
@@ -3,7 +3,7 @@ VALUES ('product1', 4500, 'https://shop.io/product/1/image.jpg'),
        ('product2', 8000, 'https://shop.io/product/2/image.jpg'),
        ('product3', 20000, 'https://shop.io/product/3/image.jpg');
 
-INSERT INTO users(email, password)
+INSERT INTO member(email, password)
 VALUES ('admin@email.com', 'password'),
        ('testuser@gmail.com', 'user123'),
        ('useruser@naver.com', '1q2w3e4r!@#$');
