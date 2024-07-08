f2345b7cb135038a764ca4aa8fa297f74a6e4a3b
anacnoda77 <sm1108shin@naver.com>
2024-07-08 17:18:33 +0900

    feat: WishProductRepository 생성

---
+++ src/main/java/gift/repository/WishProductRepository.java
@@ -0,0 +1,9 @@
+package gift.repository;
+
+import gift.model.WishProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WishProductRepository extends JpaRepository<WishProduct, Long> {
+
+
+}
