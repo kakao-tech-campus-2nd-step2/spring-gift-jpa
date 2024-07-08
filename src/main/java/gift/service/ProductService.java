15d798e48b97b92b1234cedabfed0597a06a8f13
anacnoda77 <sm1108shin@naver.com>
2024-07-08 17:40:27 +0900

    refactor: ProductService Repo 전환

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -1,38 +1,45 @@
 package gift.service;
 
+import gift.exception.ProductException;
 import gift.model.Product;
 import gift.repository.ProductDao;
+import gift.repository.ProductRepository;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
 
-    private final ProductDao productDao;
+    private final ProductRepository productRepository;
 
-    public ProductService(ProductDao productDao) {
-        this.productDao = productDao;
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
 
     public List<Product> getAllProducts() {
-        return productDao.findAll();
+        return productRepository.findAll();
     }
 
     public Product getProduct(Long id) {
-        return productDao.findProductById(id);
+        Optional<Product> product = productRepository.findById(id);
+        if (product.isEmpty()) {
+            throw new ProductException("상품이 존재하지 않습니다.");
+        }
+        return product.get();
     }
 
     public void insertProduct(String name, Integer price, String imageUrl) {
         Product product = new Product(name, price, imageUrl);
-        productDao.insert(product);
+        productRepository.save(product);
     }
 
     public void updateProduct(Long id, String name, Integer price, String imageUrl) {
         Product product = new Product(id, name, price, imageUrl);
-        productDao.update(product);
+        productRepository.save(product);
     }
 
     public void deleteProduct(Long id) {
-        productDao.delete(id);
+        productRepository.deleteById(id);
     }
 }
