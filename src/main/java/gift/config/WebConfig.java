585fb2f1b5d6178e39dd3e6ee20a293e81d0ea81
wvssm <neoramiregi@gmail.com>
2024-07-08 16:09:02 +0900

    feat(WebConfig): LoginUserArgumentResolver 설정 추가

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -1,16 +1,22 @@
 package gift.config;
 
 import gift.interceptor.JwtInterceptor;
+import gift.resolver.LoginUserArgumentResolver;
+import gift.service.UserService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+    private  final UserService userService;
     private final JwtInterceptor jwtInterceptor;
 
-    public WebConfig(JwtInterceptor jwtInterceptor) {
+    public WebConfig(UserService userService, JwtInterceptor jwtInterceptor) {
+        this.userService = userService;
         this.jwtInterceptor = jwtInterceptor;
     }
 
@@ -19,5 +25,10 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(jwtInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/members/register", "/members/login");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginUserArgumentResolver(userService));
     }
 }
