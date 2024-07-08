8ce29162b7c777f73473181d4b4653aa67d6588d
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-08 21:05:20 +0900

    fix : data.sql imageUrl -> image_url

---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO PRODUCTS (name, price, image_url)
+values ('아메리카노',
+        5000,
+        '이미지URL.....');
+
+INSERT INTO PRODUCTS (name, price, image_url)
+values ('카페라떼',
+        6000,
+        '이미지URL2.....');
+
+INSERT INTO PRODUCTS (name, price, image_url)
+values ('바닐라라떼',
+        6000,
+        '이미지URL3.....');
+
+INSERT INTO USERS (email, nickname, password)
+values ('yoonsu0325@gmail.com',
+        'yoonsu0325',
+        'thisIsPassword1234');
