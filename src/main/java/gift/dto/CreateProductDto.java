d51feb003f53410603925125ea50c98633047070
young-in <hardworkingini@gmail.com>
2024-07-08 21:22:09 +0900

    refactor(createProductDto) : 필수값, 최소값 재정리

--- src/main/java/gift/dto/CreateProductDto.java
+++ src/main/java/gift/dto/CreateProductDto.java
@@ -1,5 +1,6 @@
 package gift.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -10,8 +11,12 @@ public class CreateProductDto {
     @Pattern(regexp = "^[a-zA-Z0-9\\s()\\[\\]+\\-&/\\_]*$", message = "허용된 특수 문자는 (, ), [, ], +, -, &, /, _ 입니다.")
     @Pattern(regexp = "^(?!.*카카오).*$", message = "\"카카오\"가 포함된 상품명은 사용할 수 없습니다.")
     String name;
+    @NotBlank
     String description;
+    @NotBlank
+    @Min(0)
     Integer price;
+    @NotBlank
     String imageUrl;
 
     public String getName() {
