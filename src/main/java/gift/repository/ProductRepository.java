3a1e3cedeb563ba5e9027883fd66c257425c322f
noojmogstel <goaudgo@naver.com>
2024-07-08 14:25:02 +0900

    feat(repository): Product Repository 추가

---
+++ src/main/java/gift/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package gift.repository;
+
+import gift.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface ProductRepository extends JpaRepository<Product,Long> {
+
+}
