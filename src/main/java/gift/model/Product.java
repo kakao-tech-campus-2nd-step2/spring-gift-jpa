5d726ea5fbb6d473310197df71601a2dccd0d402
EunKyung Lee <2dmsrud2002@naver.com>
2024-07-08 15:17:56 +0900

    Feat: 상품 클래스 엔티티 매핑
    상품 id는 GenerationType INDENTITY로 자동 증가되도록 설정했고, name은
    not null과 길이가 15인 것을 지정했으며 price와 imageUrl을 모두 not null
    하게 설정했다. 그리고 jpa는 update 쿼리 지원을 하지 않기 때문에 update
    메서드를 만들어주었다.

--- src/main/java/gift/model/Product.java
+++ src/main/java/gift/model/Product.java
@@ -1,24 +1,37 @@
 package gift.model;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import org.hibernate.validator.constraints.Length;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, length = 15)
     private String name;
+    @Column(nullable = false)
     private int price;
+
+    @Column(nullable = false)
     private String imageUrl;
 
     public Product() {
     }
 
-    public Product(Long id, String name, int price, String imageUrl) {
-        this.id = id;
+    public Product(String name, int price, String imageUrl) {
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
+    }
+
+    public Product update(String name, int price, String imageUrl) {
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        return this;
     }
 
     public Long getId() {
