2f6786ab8618d531f5cd47c49ad735e3906c7036
EunKyung Lee <2dmsrud2002@naver.com>
2024-07-08 15:29:58 +0900

    Fix: 엔티티 어노테이션 빼먹음

--- src/main/java/gift/model/Member.java
+++ src/main/java/gift/model/Member.java
@@ -1,10 +1,8 @@
 package gift.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+@Entity
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
