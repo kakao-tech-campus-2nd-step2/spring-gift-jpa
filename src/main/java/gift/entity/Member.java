02681907995d9fff823836dee1cd7efda6fa2330
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2024-07-08 21:13:11 +0900

    chore : 태그 추가

--- src/main/java/gift/entity/Member.java
+++ src/main/java/gift/entity/Member.java
@@ -2,10 +2,14 @@ package gift.entity;
 
 import gift.dto.MemberDto;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="member")
 public class Member {
     
     @Id
