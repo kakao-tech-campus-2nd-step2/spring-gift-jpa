c0fb67daa582e756fcc49a3d5c3501f03bad5ba0
wvssm <neoramiregi@gmail.com>
2024-07-09 00:45:54 +0900

    feat(Messages): 오류메시지 파일 분리

---
+++ src/main/java/gift/constants/Messages.java
@@ -0,0 +1,6 @@
+package gift.constants;
+
+public class Messages {
+    public static final String NOT_FOUND_PRODUCT_BY_NAME = "해당 이름의 상품이 존재하지 않습니다.";
+    public static final String NOT_FOUND_WISH = "위시가 존재하지 않습니다.";
+}
