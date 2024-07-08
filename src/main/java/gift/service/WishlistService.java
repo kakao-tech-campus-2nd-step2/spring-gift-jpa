68c2a4c079b5759d8508095be73a1b5fb629b110
dong_yxxn <g0326kdy@gmail.com>
2024-07-08 20:15:14 +0900

    feat(WishlistService): WishlistController에서 비즈니스로직 분리

--- src/main/java/gift/service/WishlistService.java
+++ src/main/java/gift/service/WishlistService.java
@@ -1,2 +1,25 @@
-package gift.service;public class WishlistService {
+package gift.service;
+
+import gift.model.WishlistItem;
+import gift.repository.WishlistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WishlistService {
+
+    private final WishlistRepository wishlistRepository;
+    @Autowired
+    public WishlistService(WishlistRepository wishlistRepository) {
+        this.wishlistRepository = wishlistRepository;
+    }
+
+    public List<WishlistItem> getWishlistByUserId(Long userId) {
+        return wishlistRepository.findListByUserId(userId);
+    }
+    public List<WishlistItem> saveWishlistItems(List<WishlistItem> wishlistItems) {
+        return wishlistRepository.saveAll(wishlistItems);
+    }
 }
