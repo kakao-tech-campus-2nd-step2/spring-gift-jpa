85317f9b54a4107e257d6b4335de650a2a63edd4
sunandrabbit <sunandrabbit@gamil.com>
2024-07-08 18:00:45 +0900

    feat : 스키마 구조변경에 따른 멤버변수 변경과 Product의 Detail 정보를 담은 inner class 추가

--- src/main/java/gift/wishlist/domain/WishList.java
+++ src/main/java/gift/wishlist/domain/WishList.java
@@ -4,11 +4,15 @@ public class WishList {
 
     private Long id;
     private Long memberId;
-    private String productName;
-    private Long productPrice;
+    private Long productId;
 
     public WishList() {
 
+    }
+
+    public WishList(Long memberId, Long productId) {
+        this.memberId = memberId;
+        this.productId = productId;
     }
 
     public Long getId() {
@@ -27,20 +31,76 @@ public class WishList {
         this.memberId = memberId;
     }
 
-    public String getProductName() {
-        return productName;
+    public Long getProductId() {
+        return productId;
     }
 
-    public void setProductName(String productName) {
-        this.productName = productName;
+    public void setProductId(Long productId) {
+        this.productId = productId;
     }
 
-    public Long getProductPrice() {
-        return productPrice;
+    public static class JoinedWishList{
+
+        private Long id;
+        private Long memberId;
+        private Long productId;
+        private String name;
+        private Integer price;
+        private String imageUrl;
+
+        public JoinedWishList() {
+
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public Long getMemberId() {
+            return memberId;
+        }
+
+        public void setMemberId(Long memberId) {
+            this.memberId = memberId;
+        }
+
+        public Long getProductId() {
+            return productId;
+        }
+
+        public void setProductId(Long productId) {
+            this.productId = productId;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getPrice() {
+            return price;
+        }
+
+        public void setPrice(Integer price) {
+            this.price = price;
+        }
+
+        public String getImageUrl() {
+            return imageUrl;
+        }
+
+        public void setImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+        }
+
     }
 
-    public void setProductPrice(Long productPrice) {
-        this.productPrice = productPrice;
-    }
 
 }
