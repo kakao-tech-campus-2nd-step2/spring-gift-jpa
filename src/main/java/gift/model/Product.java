1c5d6a3ebf8e7f7fd51624b0520fc88c1717c3fd
anacnoda77 <sm1108shin@naver.com>
2024-07-08 14:55:29 +0900

    refactor: Product JPA 적용

--- src/main/java/gift/model/Product.java
+++ src/main/java/gift/model/Product.java
@@ -1,11 +1,23 @@
 package gift.model;
 
-import gift.form.ProductUpdateForm;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private Integer price;
+    @Column(name = "image_url", nullable = false)
     private String imageUrl;
 
     public Product(Long id, String name, Integer price, String imageUrl) {
