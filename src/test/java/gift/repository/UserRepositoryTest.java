b69ba638fcbcbe89c5204f36aa4b917b07eacf51
dong_yxxn <g0326kdy@gmail.com>
2024-07-08 19:45:26 +0900

    feat(UserRepositoryTest): userRepository 메소드 테스트 - testGetIdByEmailPassword()

--- src/test/java/gift/repository/UserRepositoryTest.java
+++ src/test/java/gift/repository/UserRepositoryTest.java
@@ -1,4 +1,29 @@
-import static org.junit.jupiter.api.Assertions.*;
+package gift.repository;
+
+
+import gift.model.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 class UserRepositoryTest {
-  
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testGetIdByEmailPassword(){
+        //given
+        String email = "abc@naver.com";
+        String password = "123";
+        User expected = new User();
+        expected.setEmail(email);
+        expected.setPassword(password);
+        userRepository.save(expected);
+        //when
+        Long actualId = userRepository.getIdByEmailPassword(email, password);
+        //then
+        Assertions.assertThat(actualId).isEqualTo(expected.getId());
+    }
 }
