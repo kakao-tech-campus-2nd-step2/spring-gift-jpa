dda3424745661f87778d5bd7843cb8ebb39aa060
anacnoda77 <sm1108shin@naver.com>
2024-07-08 16:20:46 +0900

    test: MemberRepositoryTest 일부 리팩토링

--- src/test/java/gift/repository/MemberRepositoryTest.java
+++ src/test/java/gift/repository/MemberRepositoryTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import gift.model.Member;
 import gift.model.Role;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -18,18 +19,25 @@ class MemberRepositoryTest {
     Member member = new Member("aaa123@2a.com", "1234");
 
     @Test
+    @DisplayName("Member insert 테스트")
     void insert() {
         assertThat(member.getId()).isNull();
         Member savedMember = memberRepository.save(member);
-        assertThat(savedMember.getId()).isEqualTo(1L);
+        assertThat(savedMember.getId()).isNotNull();
         assertThat(savedMember.getRole()).isEqualTo(Role.ROLE_USER);
+
+        member.setRole(Role.ROLE_ADMIN);
+        Member savedAdminMember = memberRepository.save(member);
+        assertThat(savedMember.getRole()).isEqualTo(Role.ROLE_ADMIN);
     }
 
     @Test
+    @DisplayName("Member findByEmail 테스트")
     void getMemberByEmail() {
-        memberRepository.save(member);
-        Member findMember = memberRepository.findByEmail("aaa123@2a.com");
-        assertThat(findMember.getEmail()).isEqualTo("aaa123@2a.com");
+        Member savedMember = memberRepository.save(member);
+        Member findMember = memberRepository.findByEmail(savedMember.getEmail());
+
+        assertThat(findMember).isEqualTo(savedMember);
     }
 
 
