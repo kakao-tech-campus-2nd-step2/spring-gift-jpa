ccd2f498dab2124bfa0ba3b0ba2f27a446d36c1e
EunKyung Lee <2dmsrud2002@naver.com>
2024-07-08 15:12:16 +0900

    Docs: 기능 목록 작성 - step1

--- src/main/java/gift/service/MemberService.java
+++ src/main/java/gift/service/MemberService.java
@@ -2,26 +2,26 @@ package gift.service;
 
 import gift.exception.user.UserNotFoundException;
 import gift.model.User;
-import gift.repository.UserDao;
+import gift.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-    private final UserDao userDao;
+    private final UserRepository userRepository;
 
-    public UserService(UserDao userDao) {
-        this.userDao = userDao;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     public User getUserByEmail(String email) {
-        User user = userDao.findByEmail(email)
+        User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UserNotFoundException("해당 email의 계정이 존재하지 않습니다."));
         return user;
     }
 
     public User getUserById(Long id) {
-        User user = userDao.findById(id)
+        User user = userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException("해당 email의 계정이 존재하지 않습니다."));
         return user;
     }
