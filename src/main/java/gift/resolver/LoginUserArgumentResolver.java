90c652b15382cb492649e3b8631e0234495beb23
wvssm <neoramiregi@gmail.com>
2024-07-09 00:40:06 +0900

    fix(LoginUserArgumentResolver): 올바르게 Bearer token에서 email 추출하도록 로직 변경

--- src/main/java/gift/resolver/LoginUserArgumentResolver.java
+++ src/main/java/gift/resolver/LoginUserArgumentResolver.java
@@ -1,9 +1,7 @@
 package gift.resolver;
 
 import gift.annotation.LoginUser;
-import gift.domain.User;
-import gift.dto.UserResponseDto;
-import gift.exception.UserNotFoundException;
+import gift.service.JwtUtil;
 import gift.service.UserService;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -13,9 +11,11 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 
 public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
     private final UserService userService;
+    private final JwtUtil jwtUtil;
 
-    public LoginUserArgumentResolver(UserService userService) {
+    public LoginUserArgumentResolver(UserService userService, JwtUtil jwtUtil) {
         this.userService = userService;
+        this.jwtUtil = jwtUtil;
     }
 
     @Override
@@ -25,9 +25,8 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        String email = (String)webRequest.getAttribute("userEmail",NativeWebRequest.SCOPE_REQUEST);
-        System.out.println(email);
-        UserResponseDto userResponseDto = userService.findByEmail(email);
-        return userResponseDto;
+        String authHeader = webRequest.getHeader("Authorization");
+        String userEmail = jwtUtil.extractEmail(authHeader.substring(7));
+        return userService.findByEmail(userEmail);
     }
 }
