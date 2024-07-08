cc9a4d6c5ca5bedb0e1a17ba6f0633486c740694
noojmogstel <goaudgo@naver.com>
2024-07-08 14:18:15 +0900

    feat(entity): Member Entity 추가

---
+++ src/main/java/gift/entity/Member.java
@@ -0,0 +1,21 @@
+package gift.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
