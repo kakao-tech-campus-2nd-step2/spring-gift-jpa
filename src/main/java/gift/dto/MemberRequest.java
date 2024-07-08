edbbd8fdd327301832886d3f1e99d65c77d08947
EunKyung Lee <2dmsrud2002@naver.com>
2024-07-08 15:59:12 +0900

    Refactor: ㅕuser -> memeber로 이름 변경

--- src/main/java/gift/dto/MemberRequest.java
+++ src/main/java/gift/dto/MemberRequest.java
@@ -2,5 +2,5 @@ package gift.dto;
 
 import jakarta.validation.constraints.Email;
 
-public record UserRequest(@Email(message = "이메일 형식의 입력이어야 합니다.") String email, String password) {
+public record MemberRequest(@Email(message = "이메일 형식의 입력이어야 합니다.") String email, String password) {
 }
