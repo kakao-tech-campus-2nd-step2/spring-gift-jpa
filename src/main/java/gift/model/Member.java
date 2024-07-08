a7c02ce633542bd4712e8abe0c07caca23ba0cf1
anacnoda77 <sm1108shin@naver.com>
2024-07-08 14:44:43 +0900

    refactor: Member JPA 적용

--- src/main/java/gift/model/Member.java
+++ src/main/java/gift/model/Member.java
@@ -1,11 +1,23 @@
 package gift.model;
 
-import javax.security.auth.login.CredentialNotFoundException;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "member")
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, unique = true)
     private String email;
+    @Column(nullable = false)
     private String password;
+    @Column(nullable = false)
     private Role role;
 
     public Member() {
