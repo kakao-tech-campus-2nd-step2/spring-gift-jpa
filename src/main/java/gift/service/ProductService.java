3db1087aabe31c7d6973254b41d861d0410d8f38
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-09 22:33:37 +0900

    refactor: 상품 서비스 리팩토링

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -1,6 +1,5 @@
 package gift.service;
 
-import gift.dto.request.ProductRequest;
 import gift.dto.response.AddedProductIdResponse;
 import gift.dto.response.ProductResponse;
 import gift.entity.Product;
@@ -19,15 +18,15 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    public AddedProductIdResponse addProduct(ProductRequest addRequest) {
-        Long addedId = productRepository.save(new Product(addRequest.getName(), addRequest.getPrice(), addRequest.getImageUrl())).getId();
-        return new AddedProductIdResponse(addedId);
+    public AddedProductIdResponse addProduct(String name, int price, String imageUrl) {
+        Long addedProductId = productRepository.save(new Product(name, price, imageUrl)).getId();
+        return new AddedProductIdResponse(addedProductId);
     }
 
     public ProductResponse getProduct(Long productId) {
         return productRepository.findById(productId)
                 .map(product -> new ProductResponse(product.getId(), product.getName(), product.getPrice(), product.getImageUrl()))
-                .get();
+                .orElseThrow(() -> new ProductNotFoundException("Product not found"));
     }
 
     public List<ProductResponse> getProducts() {
@@ -37,26 +36,25 @@ public class ProductService {
                 .toList();
     }
 
-    public void updateProduct(ProductRequest request) {
-        Product product = productRepository.findById(request.getId())
-                .orElseThrow(() -> new ProductNotFoundException("해당 상품 id가 존재하지 않습니다."));
-        product.setName(request.getName());
-        product.setPrice(request.getPrice());
-        product.setImageUrl(request.getImageUrl());
+    public void updateProduct(Long id, String name, int price, String imageUrl) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException("Product not found"));
+
+        product.setName(name);
+        product.setPrice(price);
+        product.setImageUrl(imageUrl);
 
         productRepository.save(product);
     }
 
     public void deleteProduct(Long id) {
-        if (productRepository.findById(id).isEmpty()) {
-            throw new ProductNotFoundException("해당 상품 id가 존재하지 않습니다.");
-        }
-        productRepository.deleteById(id);
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException("Product not found"));
+        productRepository.delete(product);
     }
 
     public void deleteProducts(List<Long> ids) {
         productRepository.deleteAllById(ids);
     }
-
 
 }
