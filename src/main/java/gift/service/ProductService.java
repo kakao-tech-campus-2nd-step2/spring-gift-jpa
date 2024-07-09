11413e1336673ef6da65a243b5e9bd26558db357
HaegyeongKim01 <haegyeong.kim01@gmail.com>
2024-07-09 18:59:20 +0900

    refactor: Product JdbcClient to JPA

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -1,6 +1,6 @@
 package gift.service;
 
-import gift.repository.ProductDao;
+import gift.repository.ProductRepository;
 import gift.vo.Product;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
@@ -12,29 +12,33 @@ import java.util.List;
 @Validated
 public class ProductService {
 
-    private final ProductDao productDao;
+    private final ProductRepository productRepository;
 
-    public ProductService(ProductDao productDao) {
-        this.productDao = productDao;
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
 
     public List<Product> getAllProducts() {
-        return productDao.getProducts();
+        return productRepository.findAll();
     }
 
     public Product getProductById(Long id) {
-        return productDao.getProductById(id);
+        return productRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 상품을 찾을 수 없습니다."));
     }
 
-    public Boolean addProduct(@Valid Product product) {
-        return productDao.addProduct(product);
+    public void addProduct(@Valid Product product) {
+        productRepository.save(product);
     }
 
-    public Boolean updateProduct(@Valid Product product) {
-        return productDao.updateProduct(product);
+    public void updateProduct(@Valid Product product) {
+        productRepository.findById(product.getId())
+                .orElseThrow(() -> new IllegalArgumentException("수정하려는 상품을 찾을 수 없습니다."));
+        productRepository.save(product);
     }
 
-    public Boolean deleteProduct(Long id) {
-        return productDao.deleteProduct(id);
+    public void deleteProduct(Long id) {
+        productRepository.findById(id)
+                        .orElseThrow(() -> new IllegalArgumentException("삭제하려는 상품을 찾을 수 없습니다."));
+        productRepository.deleteById(id);
     }
 }
