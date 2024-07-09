f89ea792ac36fd4012bababa84d1527c0c97bf2c
daolove0323 <daolove0323@gmail.com>
2024-07-10 00:47:53 +0900

    feat: WishRequestDto 클래스 구현

--- src/main/java/gift/controller/wish/WishDto.java
+++ src/main/java/gift/controller/wish/WishDto.java
@@ -2,8 +2,5 @@ package gift.controller.wish;
 
 import gift.domain.Wish;
 
-public record WishDto(Long productId, Long count) {
-    public static WishDto of(Wish wish) {
-        return new WishDto(wish.productId(), wish.count());
-    }
+public record WishDto(Long id, String email, Long productId, Long count) {
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-create table products
+create table product
 (
     id       bigint auto_increment,
     name     varchar(255),
@@ -6,13 +6,13 @@ create table products
     imageUrl varchar(255),
     primary key (id)
 );
-create table members
+create table member
 (
     email varchar(30),
     password varchar(30),
     primary key (email)
 );
-create table wishes
+create table wish
 (
     email          varchar(30),
     productId      bigint,
