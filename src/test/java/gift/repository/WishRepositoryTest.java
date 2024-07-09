b50776105c32b32c957e65899bdebf1ca5848bb2
HaegyeongKim01 <haegyeong.kim01@gmail.com>
2024-07-10 00:59:21 +0900

    test: WishRepository Test 코드 작성

--- src/test/java/gift/repository/WishRepositoryTest.java
+++ src/test/java/gift/repository/WishRepositoryTest.java
@@ -3,32 +3,38 @@ package gift.repository;
 import gift.vo.Member;
 import gift.vo.Product;
 import gift.vo.Wish;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class WishRepositoryTest {
 
     @Autowired
-    private static MemberRepository memberRepository;
-    private static ProductRepository productRepository;
-    private static WishRepository wishRepository;
+    private MemberRepository memberRepository;
 
-    private static Member savedMember;
-    private static Product savedProduct1;
-    private static Product savedProduct2;
+    @Autowired
+    private ProductRepository productRepository;
 
-    @BeforeAll
-    static void setUp() {
+    @Autowired
+    private WishRepository wishRepository;
+
+    private Member savedMember;
+    private Product savedProduct1;
+    private Product savedProduct2;
+
+    @BeforeEach
+    void setUp() {
         Member member = new Member("kookies@google.com", "@123");
         Product product1 = new Product("Ice Americano",
                 4500,
@@ -37,14 +43,15 @@ class WishRepositoryTest {
                 4200,
                 "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg");
 
-        memberRepository.save(member); // Member pre-save
+        savedMember = memberRepository.save(member); // Member pre-save
         savedProduct1 = productRepository.save(product1); // Product pre-save
         savedProduct2 = productRepository.save(product2); // Product pre-save
     }
 
-    @AfterAll
-    static void tearDown() {
-
+    @AfterEach
+    void tearDown() {
+        productRepository.deleteAll();
+        memberRepository.deleteAll();
     }
 
     @Test
@@ -58,6 +65,35 @@ class WishRepositoryTest {
 
         // then
         assertThat(expected.getId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Find Wish By member id (member-pk)")
+    void findWishListByMemberId() {
+        // given
+        wishRepository.save(new Wish(savedMember.getId(), savedProduct1.getId()));
+        wishRepository.save(new Wish(savedMember.getId(), savedProduct2.getId()));
+
+        // when
+        List<Wish> all = wishRepository.findAll();
+
+        // then
+        assertThat(all.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("Delete Wish by id")
+    void deleteWishById() {
+        // given
+        Wish savedWish = wishRepository.save(new Wish(savedMember.getId(), savedProduct1.getId()));
+        Long wishId = savedWish.getId();
+
+        // when
+        wishRepository.deleteById(wishId);
+
+        // then
+        Optional<Wish> deletedWish = wishRepository.findById(wishId);
+        assertThat(deletedWish).isEmpty();
     }
 
 }
