20390818b7e86a18f3290a323602d1e6c9f192c8
Profile-exe <kang67572346@gmail.com>
2024-07-10 00:35:15 +0900

    feat: DTO에서 엔티티로 변환을 위한 toEntity 메서드 추가

--- src/main/java/gift/member/dto/MemberReqDto.java
+++ src/main/java/gift/member/dto/MemberReqDto.java
@@ -1,8 +1,22 @@
 package gift.member.dto;
 
+import gift.member.entity.Member;
+import gift.member.Role;
+
 public record MemberReqDto(
         String name,
         String email,
         String password
 ) {
+
+    public Member toEntity(Role role) {
+
+        return new Member(
+                null,
+                name,
+                email,
+                password,
+                role
+        );
+    }
 }
--- src/main/java/gift/wishlist/dto/WishListReqDto.java
+++ src/main/java/gift/wishlist/dto/WishListReqDto.java
@@ -1,7 +1,19 @@
 package gift.wishlist.dto;
 
+import gift.wishlist.entity.WishList;
+
 public record WishListReqDto(
         Long productId,
         Integer quantity
 ) {
+
+    public WishList toEntity(Long memberId) {
+
+        return new WishList(
+                null,
+                memberId,
+                productId,
+                quantity
+        );
+    }
 }
