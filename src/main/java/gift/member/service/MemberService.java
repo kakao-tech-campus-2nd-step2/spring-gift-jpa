942fbc92ba786e46c95cc02697e576304ebd37c3
sunandrabbit <sunandrabbit@gamil.com>
2024-07-09 16:52:38 +0900

    refactor : Member에 관한 부분을 JPA를 이용한 코드로 리팩토링

--- src/main/java/gift/member/service/MemberService.java
+++ src/main/java/gift/member/service/MemberService.java
@@ -1,11 +1,16 @@
 package gift.member.service;
 
+import gift.member.domain.MemberEntity;
 import gift.member.domain.Token;
 import gift.member.domain.Member;
 import gift.member.error.ForbiddenException;
 import gift.member.repository.MemberRepository;
 import gift.member.util.JwtUtil;
 import gift.member.util.PasswordUtil;
+import gift.product.domain.Product;
+import gift.product.domain.ProductEntity;
+import gift.product.error.AlreadyExistsException;
+import gift.product.error.NotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,18 +25,32 @@ public class MemberService {
     }
 
     public Token register(Member member) {
+        MemberEntity existingMemberEntity = memberRepository.findByEmail(member.getEmail());
+        if (existingMemberEntity != null) {
+            throw new AlreadyExistsException("Alreay Exists Member");
+        }
         member.setPassword(PasswordUtil.encodePassword(member.getPassword()));
-        memberRepository.save(member);
-        return new Token(jwtUtil.generateToken(member));
+        memberRepository.save(dtoToEntity(member));
+
+        MemberEntity memberEntity = memberRepository.findByEmail(member.getEmail());
+        return new Token(jwtUtil.generateToken(entityToDto(memberEntity)));
     }
 
     public Token login(Member member) {
-        Member existingMember = memberRepository.findByEmail(member.getEmail());
-        if (existingMember != null &&
-            PasswordUtil.decodePassword(existingMember.getPassword()).equals(member.getPassword())) {
+        MemberEntity existingMemberEntity = memberRepository.findByEmail(member.getEmail());
+        if (existingMemberEntity != null &&
+            PasswordUtil.decodePassword(existingMemberEntity.getPassword()).equals(member.getPassword())) {
             return new Token(jwtUtil.generateToken(member));
         }
         throw new ForbiddenException("Invalid email or password");
+    }
+
+    private Member entityToDto(MemberEntity memberEntity) {
+        return new Member(memberEntity.getEmail(), memberEntity.getPassword());
+    }
+
+    private MemberEntity dtoToEntity(Member member) {
+        return new MemberEntity(member.getEmail(), member.getPassword());
     }
 
 }
