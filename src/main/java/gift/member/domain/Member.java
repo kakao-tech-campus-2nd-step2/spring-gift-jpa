cbbf698857ad66cd4d3033865bfaa9a434908181
rladbrua0207 <48901587+rladbrua0207@users.noreply.github.com>
2024-07-09 23:50:52 +0900

    feat: Member 엔티티 JPA적용 및 요구사항 변경으로 인한 필드 수정

--- src/main/java/gift/member/domain/Member.java
+++ src/main/java/gift/member/domain/Member.java
@@ -1,12 +1,35 @@
 package gift.member.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
 
+    public Member() {
+    }
+
     public Member(String email, String password) {
+        this(null, email, password);
+    }
+
+    public Member(Long id, String email, String password) {
+        this.id = id;
         this.email = email;
         this.password = password;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getEmail() {
@@ -15,5 +38,10 @@ public class Member {
 
     public String getPassword() {
         return password;
+    }
+
+    public void update(String email, String password) {
+        this.email = email;
+        this.password = password;
     }
 }
