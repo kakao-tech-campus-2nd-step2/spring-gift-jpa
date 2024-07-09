db5f56970bdd9cdd335a70c804045a073753f5c7
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-09 20:24:41 +0900

    feat: 예외 객체를 하나의 Custom 예외 객체에서 메서드로 처리하도록 변경

--- src/main/java/gift/exceptions/CustomException.java
+++ src/main/java/gift/exceptions/CustomException.java
@@ -1,19 +1,41 @@
 package gift.exceptions;
 
+import gift.common.ErrorMessage;
+import org.springframework.http.HttpStatus;
+
 public class CustomException extends RuntimeException{
-    public CustomException(String message) {
+    private final HttpStatus status;
+
+    public CustomException(String message, HttpStatus status) {
         super(message);
+        this.status = status;
     }
 
-    public String invalidNameException() {
-        return getMessage();
+    public HttpStatus getStatus() {
+        return status;
     }
 
-    public String invalidUserException() {
-        return getMessage();
+    public static CustomException invalidNameException() {
+        return new CustomException("카카오는 이름에 포함할 수 없습니다.", HttpStatus.BAD_REQUEST);
     }
 
-    public String userNotFoundException() {
-        return getMessage();
+    public static CustomException redundantEmailException() {
+        return new CustomException(ErrorMessage.emailAlreadyExists, HttpStatus.FORBIDDEN);
+    }
+
+    public static CustomException userNotFoundException() {
+        return new CustomException(ErrorMessage.emailNotExists, HttpStatus.FORBIDDEN);
+    }
+
+    public static CustomException invalidPasswordException() {
+        return new CustomException(ErrorMessage.passwordInvalid, HttpStatus.FORBIDDEN);
+    }
+
+    public static CustomException invalidTokenException() {
+        return new CustomException(ErrorMessage.tokenInvalid, HttpStatus.FORBIDDEN);
+    }
+
+    public static CustomException invalidHeaderException() {
+        return new CustomException(ErrorMessage.headerInvalid, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/gift/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package gift.exceptions;
-
-public class UserNotFoundException  extends RuntimeException{
-    public UserNotFoundException(String message){
-        super(message);
-    }
-}
