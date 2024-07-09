f7da4b5de585756868d96b6c1582b29a2b83b811
wvssm <neoramiregi@gmail.com>
2024-07-09 16:24:05 +0900

    refactor(Member): Member Entity로 맵핑

--- src/main/java/gift/domain/Member.java
+++ src/main/java/gift/domain/Member.java
@@ -1,9 +1,23 @@
 package gift.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="member")
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+    @Column(name = "password", nullable = false)
     private String password;
+
+
+    // JPA가 엔티티 객체를 생성할 때 reflection을 사용하기 때문에 필요함
+    protected Member() {
+    }
 
     public Member(Long id, String email, String password) {
         this.id = id;
