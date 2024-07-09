1fdafc7069d35542924aaaebb3af9a443db8bca4
yoong-saks <wjdghtjd06@gmail.com>
2024-07-10 00:47:12 +0900

    feat: Member -> Entity

--- src/main/java/gift/domain/member/Member.java
+++ src/main/java/gift/domain/member/Member.java
@@ -1,3 +1,51 @@
 package gift.domain.member;
 
-public record Member(Long id, String email, String password, String role) { }
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+public class Member{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ColumnDefault("'user'")
+    private String role;
+
+    public Member(Long id, String email, String password, String role) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Member() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
