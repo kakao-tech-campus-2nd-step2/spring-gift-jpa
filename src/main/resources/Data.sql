9cc63101764c85cc748835b11b77fb875e4d5f1c
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-07-09 16:09:24 +0900

    fix: users의 email이 중복되어서 수정

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -22,11 +22,11 @@ VALUES ('kakao1@kakao.com', '1234', 0);
 INSERT INTO Users (email, password, is_delete)
 VALUES ('kakao2@kakao.com', '123456', 0);
 INSERT INTO Users (email, password, is_delete)
-VALUES ('kakao1@kakao.com', '1234', 0);
+VALUES ('kakao3@kakao.com', '1234', 0);
 INSERT INTO Users (email, password, is_delete)
-VALUES ('kakao2@kakao.com', 'qwer1234', 0);
+VALUES ('kakao4@kakao.com', 'qwer1234', 0);
 INSERT INTO Users (email, password, is_delete)
-VALUES ('kakao3@kakao.com', 'qwer1234', 1);
+VALUES ('kakao5@kakao.com', 'qwer1234', 1);
 
 -- Wish 테이블에 데이터 삽입
 INSERT INTO Wish (product_id, user_id)
