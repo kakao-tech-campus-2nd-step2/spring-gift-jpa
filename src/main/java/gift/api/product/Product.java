42ffbe9c9e757a4fbb17899e57fa0b29d2bb3a1e
Hyewon Seok <hws2008@naver.com>
2024-07-09 16:05:45 +0900

    refactor: change to jpa and map product entity

--- src/main/java/gift/api/product/Product.java
+++ src/main/java/gift/api/product/Product.java
@@ -1,10 +1,40 @@
 package gift.api.product;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Column(length = 15)
     private String name;
+    @NotNull
     private Integer price;
+    @NotNull
     private String imageUrl;
+
+    protected Product() {
+    }
+
+    public Product(ProductRequest productRequest) {
+        this.name = productRequest.getName();
+        this.price = productRequest.getPrice();
+        this.imageUrl = productRequest.getImageUrl();
+    }
+
+    public Product(Long id, ProductRequest productRequest) {
+        this.id = id;
+        this.name = productRequest.getName();
+        this.price = productRequest.getPrice();
+        this.imageUrl = productRequest.getImageUrl();
+    }
 
     public Product(Long id, String name, Integer price, String imageUrl) {
         this.id = id;
--- src/main/java/gift/api/product/ProductDao.java
+++ src/main/java/gift/api/product/ProductDao.java
@@ -1,50 +1,30 @@
 package gift.api.product;
 
-import java.sql.Types;
 import java.util.List;
-import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProductDao {
 
-    private final JdbcClient jdbcClient;
+    private final ProductRepository productRepository;
 
-    public ProductDao(JdbcClient jdbcClient) {
-        this.jdbcClient = jdbcClient;
+    public ProductDao(ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
 
     public List<Product> getAllProducts() {
-        return jdbcClient.sql("select * from product")
-                    .query(Product.class)
-                    .list();
+        return productRepository.findAll();
     }
 
     public Long insert(ProductRequest productRequest) {
-        jdbcClient.sql("insert into product (name, price, imageUrl) values (:name, :price, :imageUrl)")
-            .param("name", productRequest.getName(), Types.VARCHAR)
-            .param("price", productRequest.getPrice(), Types.INTEGER)
-            .param("imageUrl", productRequest.getImageUrl(), Types.VARCHAR)
-            .update();
-        return jdbcClient.sql("select id from product where name = :name")
-            .param("name", productRequest.getName(), Types.VARCHAR)
-            .query(Product.class)
-            .single()
-            .getId();
+        return productRepository.save(new Product(productRequest)).getId();
     }
 
     public void update(long id, ProductRequest productRequest) {
-        jdbcClient.sql("update product set name = :name, price = :price, imageUrl = :imageUrl where id = :id")
-            .param("name", productRequest.getName(), Types.VARCHAR)
-            .param("price", productRequest.getPrice(), Types.INTEGER)
-            .param("imageUrl", productRequest.getImageUrl(), Types.VARCHAR)
-            .param("id", id, Types.BIGINT)
-            .update();
+        productRepository.save(new Product(id, productRequest));
     }
 
     public void delete(long id) {
-        jdbcClient.sql("delete from product where id = :id")
-            .param("id", id)
-            .update();
+        productRepository.deleteById(id);
     }
 }
