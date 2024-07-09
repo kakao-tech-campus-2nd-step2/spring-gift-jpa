32987ff1e6f0e9acf0fbcd52735cf3a729076edf
minjae <minjae4650@gmail.com>
2024-07-10 03:54:15 +0900

    refactor: Simplify WishRepository by extending JpaRepository
    - 기존의 WishRepository 인터페이스를 삭제하고 Spring Data JPA의 JpaRepository를 상속하도록 변경.
    - JpaRepository를 상속함으로써 기본적인 CRUD 및 페이징 기능을 자동으로 제공.

--- src/main/java/gift/repository/WishRepository.java
+++ src/main/java/gift/repository/WishRepository.java
@@ -1,10 +1,11 @@
 package gift.repository;
 
 import gift.model.Wish;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
-public interface WishRepository {
+public interface WishRepository extends JpaRepository<Wish, Long> {
     List<Wish> findByMemberId(Long memberId);
-    Wish save(Wish wish);
-    boolean deleteByIdAndMemberId(Long id, Long memberId);
+    void deleteByIdAndMemberId(Long id, Long memberId);
 }
