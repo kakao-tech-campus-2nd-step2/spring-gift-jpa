8a5bf5c0a568a4b7531fd03a897e8e9ddc4d0b35
JaeBin <201924576@pusan.ac.kr>
2024-07-09 14:49:09 +0900

    feat(global): create CRUDRepository

--- src/main/java/gift/member/repository/MemberRepository.java
+++ src/main/java/gift/member/repository/MemberRepository.java
@@ -1,22 +1,12 @@
 package gift.member.repository;
 
+import gift.global.CRUDRepository;
 import gift.member.domain.Email;
 import gift.member.domain.Member;
 import gift.member.domain.Password;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-    List<Member> findAll();
-
-    Member save(Member member);
-
+public interface MemberRepository extends CRUDRepository<Member, Long> {
     Optional<Member> findByEmailAndPassword(Email email, Password password);
-
-    Optional<Member> findById(Long id);
-
-    void deleteById(Long id);
 }
