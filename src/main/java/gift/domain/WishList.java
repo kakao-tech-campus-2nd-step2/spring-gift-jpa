47f961fd796f32bcac534114224aea3480b3656c
alsswl <alswl4223@naver.com>
2024-07-10 01:34:29 +0900

    refactor : wishlist Mapping 기능 추가

--- src/main/java/gift/domain/WishList.java
+++ src/main/java/gift/domain/WishList.java
@@ -16,9 +16,30 @@ public class WishList {
 
     public WishList() {}
 
+    public WishList(String memberId,Long menuId){
+        this(null, memberId,menuId);
+    }
+
     public WishList(Long id, String memberId, Long menuId){
         this.id = id;
         this.memberId = memberId;
         this.menuId = menuId;
     }
+
+    public Long getMenuId(){
+        return menuId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public static WishList MapWishListRequestToWishList(WishListRequest wishListRequest){
+        return new WishList(wishListRequest.memberId(),wishListRequest.menuId());
+    }
+
+    public static WishListResponse MapWishListToWishListResponse(WishList wishList){
+        return new WishListResponse(wishList.id, wishList.menuId);
+    }
+
 }
