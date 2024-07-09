c4b18f7d358bcd65f1f7a5fef955411049c94fee
sim-mer <gyu2781@gmail.com>
2024-07-09 19:13:51 +0900

    refactor(member.business) : JPA로 변경됨에 따른 로직변경 - 위시리스트 조회 과정 시 바로 연관된 객체로 가져오도록 변경 - 위시리스트 생성 시 연관관계 매핑 레퍼런스 객체로 변경 - 삭제 메서드에 @Transactional 추가

--- src/main/java/gift/member/business/service/WishlistService.java
+++ src/main/java/gift/member/business/service/WishlistService.java
@@ -2,45 +2,44 @@ package gift.member.business.service;
 
 import gift.member.business.dto.WishListDto;
 import gift.member.persistence.entity.Wishlist;
+import gift.member.persistence.repository.MemberRepository;
 import gift.member.persistence.repository.WishlistRepository;
 import gift.member.presentation.dto.WishlistUpdateDto;
-import gift.product.persistence.entity.Product;
 import gift.product.persistence.repository.ProductRepository;
 import java.util.List;
-import java.util.Map;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class WishlistService {
     private final WishlistRepository wishlistRepository;
     private final ProductRepository productRepository;
+    private final MemberRepository memberRepository;
 
     public WishlistService(WishlistRepository wishlistRepository,
-        ProductRepository productRepository) {
+        ProductRepository productRepository, MemberRepository memberRepository) {
         this.wishlistRepository = wishlistRepository;
         this.productRepository = productRepository;
+        this.memberRepository = memberRepository;
     }
 
     public List<WishListDto> getWishLists(Long memberId) {
         var wishLists = wishlistRepository.getWishListByMemberId(memberId);
 
-        var productIds = wishLists.stream()
-            .map(Wishlist::getProductId)
-            .toList();
-
-        Map<Long, Product> products = productRepository.getProductsByIds(productIds);
-
         return wishLists.stream()
             .map(wishlist -> WishListDto.of(
                 wishlist.getId(),
-                products.get(wishlist.getProductId()),
+                wishlist.getProduct(),
                 wishlist.getCount()
             ))
             .toList();
     }
 
     public Long addWishList(Long memberId, Long productId) {
-        var wishList = new Wishlist(productId, memberId, 1);
+        var product = productRepository.getReferencedProduct(productId);
+        var member = memberRepository.getReferencedMember(memberId);
+
+        var wishList = new Wishlist(product, member, 1);
         return wishlistRepository.saveWishList(wishList);
     }
 
@@ -52,6 +51,7 @@ public class WishlistService {
         return wishlistRepository.updateWishlist(wishList);
     }
 
+    @Transactional
     public void deleteWishList(Long memberId, Long productId) {
         wishlistRepository.deleteWishlist(memberId, productId);
     }
