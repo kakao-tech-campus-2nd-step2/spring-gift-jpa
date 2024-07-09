7d66396c247b3c062b6759abc685c3dee2c475f2
suhyeon7497 <nucc1186@gmail.com>
2024-07-09 15:51:02 +0900

    docs: 1단계 요구사항 작성

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # spring-gift-jpa
+
+### 1단계 기능 요구 사항
+- 지금까지 작성한 JdbcTemplate 기반 코드를 JPA로 리팩터링하고 실제 도메인 모델을 어떻게 구성하고 객체와 테이블을 어떻게 매핑해야 하는지 알아본다.
+  - [ ] ProductRepository 생성
+  - [ ] MemberRepository 생성
+  - [ ] WishRepository 생성
+  - [ ] ProductDao에서 ProductRepository로 변경
+    - [ ] validate를 Product로 옮김.
+  - [ ] MemberDao에서 MemberRepository로 변경
+  - [ ] WishDao에서 WishRepository로 변경
+@DataJpaTest를 사용하여 학습 테스트를 해 본다.
--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,2 +1,20 @@
-package gift.repository;public class ProductRespository {
+package gift.repository;
+
+import gift.model.Product;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Override
+    boolean existsById(Long id);
+
+    @Override
+    Optional<Product> findById(Long id);
+
+    @Override
+    Product save(Product product);
+
+    @Override
+    void deleteById(Long id);
 }
