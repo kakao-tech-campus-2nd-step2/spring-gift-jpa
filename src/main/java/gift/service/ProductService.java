5ee560a6fdbb739ec109fb4b04010f879847b20f
rdme0 <we6610@gmail.com>
2024-07-09 23:52:44 +0900

    :recycle: refactor: JPA를 이용한 서비스로 리팩터링

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -1,47 +1,67 @@
 package gift.service;
 
-import gift.entity.Product;
-import gift.exception.BadRequestExceptions.NoSuchProductIdException;
-import gift.dao.ProductDao;
 import gift.dto.ProductDTO;
+import gift.entity.Product;
+import gift.exception.BadRequestExceptions.BadRequestException;
+import gift.exception.BadRequestExceptions.NoSuchProductIdException;
+import gift.exception.InternalServerExceptions.InternalServerException;
+import gift.repository.ProductRepository;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
-    private final ProductDao productDao;
+
     private final ParameterValidator parameterValidator;
+    private final ProductRepository productRepository;
 
     @Autowired
-    public ProductService(ProductDao productDao, ParameterValidator parameterValidator) {
-        this.productDao = productDao;
+    public ProductService(ParameterValidator parameterValidator,
+            ProductRepository productRepository) {
+        this.productRepository = productRepository;
         this.parameterValidator = parameterValidator;
     }
 
     public void addProduct(ProductDTO productDTO) throws RuntimeException {
         try {
-            productDao.insertProduct(new Product(productDTO));
-        } catch (RuntimeException e) {
-            throw new RuntimeException(e.getMessage());
+            productRepository.save(new Product(productDTO));
+        } catch (Exception e) {
+            if(e instanceof IllegalArgumentException || e instanceof DataIntegrityViolationException)
+                throw new BadRequestException("잘못된 값을 입력했습니다.");
+
+            throw new InternalServerException(e.getMessage());
         }
+
     }
 
     public List<ProductDTO> getProductList() {
-        return ProductConverter.convertToProductDTO(productDao.selectProduct());
+        return ProductConverter.convertToProductDTO(productRepository.findAll());
     }
 
-    public void updateProduct(Integer id, ProductDTO productDTO) throws RuntimeException {
+    public void updateProduct(Long id, ProductDTO productDTO) throws RuntimeException {
         parameterValidator.validateParameter(id, productDTO);
-        Product product = new Product(productDTO);
+        Optional<Product> productInDb = productRepository.findById(id);
 
-        if (productDao.updateProduct(product) == 0) {
+        if(!productInDb.isPresent())
             throw new NoSuchProductIdException("id가 %d인 상품은 존재하지 않습니다.".formatted(id));
-        }
+
+        Product productInDB = productInDb.get();
+        Product product = new Product(productDTO); //이따가 검증하는거 해야함
+        productInDB.changeProduct(product);
     }
 
-    public void deleteProduct(Integer id) throws RuntimeException {
-        if(productDao.deleteProduct(id) == 0)
-            throw new NoSuchProductIdException("id가 %d인 상품은 존재하지 않습니다.".formatted(id));
+    public void deleteProduct(Long id) throws RuntimeException {
+        try {
+            productRepository.deleteById(id);
+        } catch (Exception e) {
+            if(e instanceof EmptyResultDataAccessException)
+                throw new NoSuchProductIdException("id가 %d인 상품은 존재하지 않습니다.".formatted(id));
+
+            throw new InternalServerException(e.getMessage());
+        }
     }
 }
