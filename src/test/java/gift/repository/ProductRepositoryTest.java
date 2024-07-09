543c50380a3d05380a20c36b6d5e37d8a151c5f6
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-09 15:32:35 +0900

    test : ProductRepository의 save 기능 test

--- src/test/java/gift/repository/ProductRepositoryTest.java
+++ src/test/java/gift/repository/ProductRepositoryTest.java
@@ -1,2 +1,30 @@
-package gift.repository;public class ProductRepositoryTest {
+package gift.repository;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import gift.domain.Product;
+import gift.repository.product.ProductRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void save() {
+        // given
+        Product product1 = new Product(1L, "아메리카노", 4500, "americano");
+        //when
+        Product savedProduct = productRepository.save(product1);
+        //then
+        Assertions.assertAll(
+            () -> assertThat(savedProduct.getId()).isNotNull(),
+            () -> assertThat(savedProduct.getName()).isEqualTo(product1.getName())
+        );
+    }
 }
