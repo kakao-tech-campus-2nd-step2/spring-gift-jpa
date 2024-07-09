0d53254376fefd4c353b4c544ae3066f0b77a171
EunKyung Lee <2dmsrud2002@naver.com>
2024-07-09 13:58:04 +0900

    Refactor: 가독성을 위해 dto record에 개행 추가

--- src/main/java/gift/dto/LoginRequest.java
+++ src/main/java/gift/dto/LoginRequest.java
@@ -2,5 +2,8 @@ package gift.dto;
 
 import jakarta.validation.constraints.Email;
 
-public record LoginRequest(@Email(message = "이메일 형식의 입력이어야 합니다.") String email, String password) {
+public record LoginRequest(
+        @Email(message = "이메일 형식의 입력이어야 합니다.") String email,
+        String password
+) {
 }
--- src/main/java/gift/dto/WishRequest.java
+++ src/main/java/gift/dto/WishRequest.java
@@ -2,5 +2,7 @@ package gift.dto;
 
 import jakarta.validation.constraints.Min;
 
-public record WishRequest(@Min(value = 1, message = "상품 id는 1 이상이어야 합니다.") Long productId) {
+public record WishRequest(
+        @Min(value = 1, message = "상품 id는 1 이상이어야 합니다.") Long productId
+) {
 }
