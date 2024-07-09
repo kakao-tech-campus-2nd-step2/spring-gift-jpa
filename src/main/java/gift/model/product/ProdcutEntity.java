2274ad8949778bcd5c66559d46cc81998c32b0d5
Jeje <jjh4450jjh4450@gmail.com>
2024-07-10 01:42:27 +0900

    feat(ProductEntity): ProductEntity 구성

---
+++ src/main/java/gift/model/product/ProdcutEntity.java
@@ -0,0 +1,58 @@
+package gift.model.product;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ProdcutEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private long price;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    public ProdcutEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
