a57195ccf21850347904a36c21e1447e9ae26fd9
배민수 <duecost@gmail.com>
2024-07-09 21:06:07 +0900

    fix: AuthService에 트랜잭셔널 어노테이션 명시

--- src/main/java/gift/product/service/AuthService.java
+++ src/main/java/gift/product/service/AuthService.java
@@ -14,8 +14,9 @@ import java.nio.charset.StandardCharsets;
 import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@Service
+@Transactional
 public class AuthService {
 
     private final AuthRepository authRepository;
