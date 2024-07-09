a442b64a4183970a529c371b5e3365eb3e2e8e62
dkswoals <a4445656@gmail.com>
2024-07-09 17:31:17 +0900

    feat: Repository refactoring
    jdbc -> jpa

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO Item (name, price, imgurl)
+INSERT INTO Item (name, price, img_url)
 values ('피자',
         13000,
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwvPdrxYSEP7ZHfy9b3IzZ2yU2HkDh2BjSA_cHkth04jKFwTyQwxA26VlAlw&s');
-INSERT INTO Item (name, price, imgurl)
+INSERT INTO Item (name, price, img_url)
 values ('샐러드',
         20000,
         'https://media.istockphoto.com/id/183752521/ko/%EC%82%AC%EC%A7%84/%EB%B9%84%EB%B9%84%EB%B0%A5.jpg?s=2048x2048&w=is&k=20&c=f5FX83thuFkWNXmo-k7q5zD3xoEdO9oTFBSPEzBxMR0=');
