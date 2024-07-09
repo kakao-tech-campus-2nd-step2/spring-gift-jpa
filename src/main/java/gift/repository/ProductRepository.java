0c1747b26de3778b42e5dfdfdb7685435b6a2499
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-07-09 16:13:19 +0900

    update: Page로 반환하는 경우 Pageable을 파라미터로 사용한다.

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,10 +1,12 @@
 package gift.repository;
 
 import gift.entity.ProductEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-
+    Page<ProductEntity> findAll(Pageable pageable);
 }
