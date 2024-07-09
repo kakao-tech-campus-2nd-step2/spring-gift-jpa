3e49fc163de7aece4f7e17fe46ffa0f462d7673c
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-09 17:41:26 +0900

    test: 토큰서비스 테스트
    -그 외 테스트 정리

--- src/test/java/gift/service/TokenServiceTest.java
+++ src/test/java/gift/service/TokenServiceTest.java
@@ -1,22 +1,53 @@
 package gift.service;
 
-import gift.dto.Token;
+import gift.dto.Member;
+import gift.dto.response.TokenResponse;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-//@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
 class TokenServiceTest {
 
-//    @Autowired
-//    TokenService tokenService;
-//    @Test
-//    void generateToken() {
-//        Token token = tokenService.generateToken(1L);
-//        System.out.println(token.getValue());
-//        Long id = tokenService.getMemberIdByToken(token.getValue());
-//        System.out.println(id);
-//    }
+    private static final Long MEMBER_ID = 1L;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Test
+    void generateToken() {
+        //When
+        TokenResponse token = tokenService.generateToken(MEMBER_ID);
+
+        //Then
+        assertThat(token).isInstanceOf(TokenResponse.class);
+    }
+
+    @Test
+    void getMemberIdByToken() {
+        //Given
+        TokenResponse token = tokenService.generateToken(MEMBER_ID);
+
+        //When
+        Long memberIdByToken = tokenService.getMemberIdByToken(token.getToken());
+
+        //Then
+        assertThat(memberIdByToken).isEqualTo(MEMBER_ID);
+    }
+
+    @Test
+    void isValidateToken() {
+        //Given
+        TokenResponse token = tokenService.generateToken(MEMBER_ID);
+
+        //When
+        boolean isValidate = tokenService.isValidateToken(token.getToken());
+
+        //Then
+        assertThat(isValidate).isTrue();
+    }
 
 }
