6418750118980f2e9acafc2d91e5aea7a3e098b6
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-07-09 10:04:08 +0900

    feat: ProductId와 Id로 삭제하는 Controller 추가

--- src/main/java/gift/wishlist/controller/WishController.java
+++ src/main/java/gift/wishlist/controller/WishController.java
@@ -1,7 +1,6 @@
 package gift.wishlist.controller;
 
 import gift.member.model.Member;
-import gift.member.service.MemberService;
 import gift.wishlist.dto.WishResponse;
 import gift.wishlist.dto.WishRequest;
 import gift.wishlist.service.WishService;
@@ -9,6 +8,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,9 +36,15 @@ public class WishController {
         return ResponseEntity.ok(wishes);
     }
 
-    @DeleteMapping
+    @DeleteMapping("/prooductId/{productId}")
     public ResponseEntity<Void> deleteWishByProductName(Member member, @RequestBody WishRequest request) {
-        wishService.deleteWishByProductName(member.getId(), request.getProductName());
+        wishService.deleteWishByProductId(member.getId(), request.getProductId());
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteWishById(Member member, @PathVariable Long id) {
+        wishService.deleteWishById(id);
         return ResponseEntity.noContent().build();
     }
 
