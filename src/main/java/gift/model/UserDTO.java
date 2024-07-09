eb38077c71d07d9417ac6cf0397906a67b427dfd
Giheon Do <dgh0001@naver.com>
2024-07-09 14:26:43 +0900

    refactor: user, userDto 분리

---
+++ src/main/java/gift/model/UserDTO.java
@@ -0,0 +1,39 @@
+package gift.model;
+
+import gift.util.EmailConstraint;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+public class UserDTO {
+    @EmailConstraint
+    @Length(min = 1, max = 50)
+    @NotNull
+    private String email;
+    @Length(min = 1, max = 50)
+    @NotNull
+    private String password;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public @Length(min = 1, max = 50) @NotNull String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@Length(min = 1, max = 50) @NotNull String email) {
+        this.email = email;
+    }
+
+    public @Length(min = 1, max = 50) @NotNull String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@Length(min = 1, max = 50) @NotNull String password) {
+        this.password = password;
+    }
+}
