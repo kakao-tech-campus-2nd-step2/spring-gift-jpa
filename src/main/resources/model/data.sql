79963841b6d23c6d7a752cb2e3256e73a8a6cfb9
kuma4188 <psb4188@gmail.com>
2024-07-10 01:55:48 +0900

    refactor : UserRepositoryTest 테스트 ㅔ이스 추가

--- src/main/resources/model/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO product (id,name, price, image_url) VALUES (8146027 , '아이스 카페 아메리카노 T', 4500, 'https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg');
-INSERT INTO product (id,name, price, image_url) VALUES (123456 , '아이스 카페 라테 T', 5500, 'https://item.elandrs.com/upload/prd/orgimg/088/2005488088_0000001.jpg?w=750&h=&q=100');
-INSERT INTO product (id,name, price, image_url) VALUES (7891011 , '뜨거운 아이스 아메리카노 T', 6500, 'https://dimg.donga.com/wps/NEWS/IMAGE/2017/02/06/82727038.1.jpg');
--- src/test/java/gift/test/UserRepositoryTest.java
+++ src/test/java/gift/test/UserRepositoryTest.java
@@ -2,26 +2,68 @@ package gift.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import gift.model.SiteUser;
 import gift.repository.UserRepository;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @DataJpaTest
+@TestMethodOrder(OrderAnnotation.class)
 public class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
+
     @Test
-    void save() {
+    @Order(1)
+    @DisplayName("회원가입")
+    void testUserRegistration() {
+        // Given
         SiteUser user = new SiteUser();
         user.setUsername("testuser");
         user.setPassword("password");
         user.setEmail("test@example.com");
+
+        // When
         SiteUser savedUser = userRepository.save(user);
+
+        // Then
         assertThat(savedUser.getId()).isNotNull();
         assertThat(savedUser.getUsername()).isEqualTo("testuser");
+        assertThat(savedUser.getEmail()).isEqualTo("test@example.com");
     }
+
+    @Test
+    @Order(2)
+    @DisplayName("로그인")
+    void testUserLogin() {
+        // Given
+        String username = "testuser";
+        String password = "password";
+        String email = "test@example.com";
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+        userRepository.save(user);
+
+        // When
+        Optional<SiteUser> optionalUser = userRepository.findByUsername(username);
+
+        // Then
+        assertThat(optionalUser).isPresent();
+        SiteUser foundUser = optionalUser.get();
+        assertThat(foundUser.getEmail()).isEqualTo(email);
+
+    }
+
+
 }
