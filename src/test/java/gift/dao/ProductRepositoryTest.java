45a8c190bc2affc32b1cda063a1ae5975fa8b569
Seunghyun Ko <chris00825@naver.com>
2024-07-09 15:20:06 +0900

    test(ProductDao): add update product function test
    기존 등록된 상품의 속성(가격) 변경이 잘 이루어지는지 확인하는 테스트코드 작성

--- src/test/java/gift/dao/ProductRepositoryTest.java
+++ src/test/java/gift/dao/ProductRepositoryTest.java
@@ -2,12 +2,14 @@ package gift.dao;
 
 import gift.product.dao.AdminProductDao;
 import gift.product.model.Product;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DataJpaTest
 public class ProductRepositoryTest {
@@ -25,6 +27,7 @@ public class ProductRepositoryTest {
             "product1.images"
         );
     }
+
     @Test
     void testRegisterProduct() {
         Product registerProduct = productDao.save(product);
@@ -32,6 +35,33 @@ public class ProductRepositoryTest {
         assertThat(registerProduct.getName()).isEqualTo("상품1");
         assertThat(registerProduct.getPrice()).isEqualTo(1500);
         assertThat(registerProduct.getImageUrl()).isEqualTo("product1.images");
+    }
+
+    @Test
+    void testUpdateProduct() {
+        Product registerProduct = productDao.save(product);
+
+        Optional<Product> saveProduct = productDao.findById(registerProduct.getId());
+
+        assertThat(saveProduct).isPresent();
+        assertThat(saveProduct.get().getPrice()).isEqualTo(1500);
+
+        productDao.save(
+            new Product(
+                saveProduct.get().getId(),
+                saveProduct.get().getName(),
+                2000,
+                saveProduct.get().getImageUrl()
+            )
+        );
+
+        Optional<Product> updateProduct = productDao.findById(saveProduct.get().getId());
+
+        assertThat(updateProduct).isPresent();
+        assertAll(
+            () -> assertThat(updateProduct.get().getPrice()).isEqualTo(2000),
+            () -> assertThat(updateProduct.get().getPrice()).isNotEqualTo(1500)
+        );
     }
 
 }
