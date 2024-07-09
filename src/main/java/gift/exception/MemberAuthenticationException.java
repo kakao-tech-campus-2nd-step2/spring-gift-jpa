9df7c8d167942ba6936026d8fea63e04576e7f10
Kangboom <jkl6147@naver.com>
2024-07-09 19:03:45 +0900

    refactor(exception): rename Exception

--- src/main/java/gift/exception/MemberAuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package gift.exception;
-
-public class MemberAuthenticationException extends RuntimeException{
-    public MemberAuthenticationException(String message){
-        super(message);
-    }
-}
