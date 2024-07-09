8d04efdd9d64c5abe36330743dc345c017324489
student-p <paulmoon00@naver.com>
2024-07-09 23:49:03 +0900

    feat: product부분 jpa변경
    product를 앤티티로 변경하고 기존 컨트롤러에 모여있던 코드를 서비스로 분리

---
+++ src/main/java/gift/repository/ProductJpaRepository.java
@@ -0,0 +1,10 @@
+package gift.repository;
+
+import gift.domain.Product;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductJpaRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findByName(String productName);
+}
