cffc5ec815377da3b3978736ae3b5f1fe9f5cc93
xkrwjdals <developjmt@gmail.com>
2024-07-09 22:45:26 +0900

    feat(test): Add WishListRepositoryTest for DataJpaTest

--- src/main/java/gift/service/WishListRepository.java
+++ src/main/java/gift/service/WishListRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+public
 interface WishListRepository extends JpaRepository<WishList, Long> {
 
     Optional<List<WishList>> findByMemberId(long memberId);
