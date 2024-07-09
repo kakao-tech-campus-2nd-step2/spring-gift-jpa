fe7a2cd73b5ff764fe0d2160e6bfb17b84705260
kuma4188 <psb4188@gmail.com>
2024-07-09 15:44:07 +0900

    refactor : product에서 생성자 추가

--- src/main/java/gift/model/Product.java
+++ src/main/java/gift/model/Product.java
@@ -25,6 +25,13 @@ public class Product {
     public Product() {
     }
 
+    public Product(Long id, String name, int price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
     //getter
     public Long getId() {
         return id;
