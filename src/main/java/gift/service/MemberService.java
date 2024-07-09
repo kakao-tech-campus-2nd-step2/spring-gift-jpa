1c8d9ae0dbe533e1ecb15f09a240e724b5160c75
wvssm <neoramiregi@gmail.com>
2024-07-09 19:12:13 +0900

    feat(MemberService): 각 메서드에 transactional 설정

--- src/main/java/gift/service/MemberService.java
+++ src/main/java/gift/service/MemberService.java
@@ -6,6 +6,7 @@ import gift.dto.MemberResponseDto;
 import gift.exception.MemberNotFoundException;
 import gift.repository.MemberRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MemberService {
@@ -15,16 +16,19 @@ public class MemberService {
         this.memberRepository = memberRepository;
     }
 
+    @Transactional
     public void save(MemberRequestDto memberRequestDto){
         memberRepository.save(memberRequestDto.toEntity());
     }
 
+    @Transactional(readOnly = true)
     public boolean findByEmailAndPassword(String email, String password) {
         memberRepository.findByEmailAndPassword(email, password)
                 .orElseThrow(() -> new MemberNotFoundException("해당 정보를 가진 유저가 존재하지 않습니다."));
         return true;
     }
 
+    @Transactional(readOnly = true)
     public MemberResponseDto findByEmail(String email){
         Member member = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new MemberNotFoundException("해당 정보를 가진 유저가 존재하지 않습니다."));
