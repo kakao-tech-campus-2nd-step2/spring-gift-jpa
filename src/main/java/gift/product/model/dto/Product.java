de3619e2e16da028e3df45fd4a01f9ca54612f2a
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-09 15:47:45 +0900

    feat: Product entity 클래스 작성

--- src/main/java/gift/product/model/dto/Product.java
+++ src/main/java/gift/product/model/dto/Product.java
@@ -1,10 +1,25 @@
 package gift.product.model.dto;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Product")
 public class Product {
-    private Long id;        // 선물의 고유 식별자
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
     private String name;    // 선물의 이름
+
+    @Column(nullable = false)
     private int price = 0;      // 선물의 가격
+
+    @Column(name = "image_url")
     private String imageUrl; // 선물 이미지 URL
+
+    @Column(name = "is_active", nullable = false)
     private boolean isActive = true; // 선물의 활성화 상태
 
     public Product(Long id, String name, int price, String imageUrl, Boolean isActive) {
@@ -13,6 +28,10 @@ public class Product {
         this.price = price;
         this.imageUrl = imageUrl;
         this.isActive = isActive;
+    }
+
+    public Product() {
+
     }
 
     public Long getId() {
