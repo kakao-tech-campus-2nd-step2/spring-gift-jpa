06d24bcf013d51ce42e65b7009692261c380251f
daolove0323 <daolove0323@gmail.com>
2024-07-10 00:50:41 +0900

    refac: wishRepository 리팩토링

--- src/main/java/gift/repository/WishRepository.java
+++ src/main/java/gift/repository/WishRepository.java
@@ -1,57 +1,15 @@
 package gift.repository;
 
 import gift.domain.Wish;
-import gift.controller.wish.WishDto;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.dao.IncorrectResultSizeDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class WishRepository {
-    private final JdbcTemplate jdbcTemplate;
-    private final RowMapper<Wish> WISH_ROW_MAPPER = (resultSet, rowNum) -> new Wish(
-        resultSet.getLong("productId"),
-        resultSet.getLong("count")
-    );
-
-    public WishRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public List<Wish> findAll(String email) {
-        String sql = "SELECT * FROM wishes WHERE email = ?";
-        return jdbcTemplate.query(sql, new Object[]{email}, WISH_ROW_MAPPER);
-    }
-
-    public Optional<Wish> find(String email, Long productId) {
-        try {
-            String sql = "SELECT * FROM wishes WHERE email = ? AND productId = ?";
-            return Optional.ofNullable(jdbcTemplate.queryForObject(sql, WISH_ROW_MAPPER,
-                email,
-                productId
-            ));
-        } catch (IncorrectResultSizeDataAccessException ex) {
-            return Optional.empty();
-        }
-    }
-
-    public Wish save(String email, WishDto wish) {
-        String sql = "INSERT INTO wishes (email, productId, count) VALUES (?, ?, ?)";
-        jdbcTemplate.update(sql, email, wish.productId(), wish.count());
-        return find(email, wish.productId()).get();
-    }
-
-    public Wish update(String email, WishDto wish) {
-        String sql = "UPDATE wishes SET count = ? WHERE email = ? AND productId = ?";
-        jdbcTemplate.update(sql, wish.count(), email, wish.productId());
-        return find(email, wish.productId()).get();
-    }
-
-    public void delete(String email, Long productId) {
-        String sql = "DELETE FROM wishes WHERE email = ? AND productId = ?";
-        jdbcTemplate.update(sql, email, productId);
-    }
+public interface WishRepository extends JpaRepository<Wish, Long> {
+    List<Wish> findByEmail(String email);
+    Optional<Wish> findByEmailAndProductId(String email, Long productId);
+    void deleteByEmailAndProductId(String email, Long productId);
 }
