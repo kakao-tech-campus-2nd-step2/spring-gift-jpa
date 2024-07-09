c3eb3efd05112cdf1253cceca12a6965eef402ab
rdme0 <we6610@gmail.com>
2024-07-09 23:11:35 +0900

    :sparkles: feat: ItemRepository 추가

---
+++ src/main/java/gift/repository/ItemRepository.java
@@ -0,0 +1,13 @@
+package gift.repository;
+
+
+import gift.entityForJpa.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    Item findItemById(long id);
+
+}
