6a404f41f0cfa1ebe8747fa312d370ff91d8cf4a
NextrPlue <p.plue1881@gmail.com>
2024-07-09 18:48:49 +0900

    feat(entity): add JPA entity for Wish

--- src/main/java/gift/entity/Wish.java
+++ src/main/java/gift/entity/Wish.java
@@ -1,18 +1,31 @@
 package gift.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "wishlist_items")
 public class Wish {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "member_id", nullable = false)
     private Long memberId;
 
+    @Column(name = "product_id", nullable = false)
     private Long productId;
 
+    @Column(name = "product_number", nullable = false)
     private int productNumber;
 
     public Wish() {}
+
+    public Wish(Long memberId, Long productId, int productNumber) {
+        this.memberId = memberId;
+        this.productId = productId;
+        this.productNumber = productNumber;
+    }
 
     public Wish(Long id, Long memberId, Long productId, int productNumber) {
         this.id = id;
