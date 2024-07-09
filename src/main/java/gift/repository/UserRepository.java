512adc2ae406bbfc2bece9d4528ddaff6b257169
dong_yxxn <g0326kdy@gmail.com>
2024-07-09 14:32:10 +0900

    refactor(UserRepository): query 삭제 - jpa에 맞게 쿼리 삭제 - return 값 Optional<User>로 변경

--- src/main/java/gift/repository/UserRepository.java
+++ src/main/java/gift/repository/UserRepository.java
@@ -1,17 +1,11 @@
 package gift.repository;
 
 import gift.model.User;
-import java.util.List;
-import org.springframework.dao.EmptyResultDataAccessException;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT id FROM User WHERE email = :email AND password = :password")
-    Long getIdByEmailPassword(@Param("email") String email, @Param("password") String password);
+    Optional<User> findByEmailAndPassword(String email, String password);
 }
