c7fcdd049d899dfce7ad0e30d122a613695081b3
momnpa333 <momnpa333@gmail.com>
2024-07-09 20:23:22 +0900

    feat: 안쓰는 import문 삭제

--- src/main/java/gift/repository/WishJpaRepository.java
+++ src/main/java/gift/repository/WishJpaRepository.java
@@ -3,10 +3,8 @@ package gift.repository;
 import gift.model.member.Member;
 import gift.model.product.Product;
 import gift.model.wish.Wish;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
-import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
