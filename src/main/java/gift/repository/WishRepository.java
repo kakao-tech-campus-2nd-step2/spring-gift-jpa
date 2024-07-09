797eb6ae3a63edb90156cdf8c8e1dd2d57996412
rdme0 <we6610@gmail.com>
2024-07-10 03:24:55 +0900

    :recycle: refactor: 반환타입 Optional로 리팩터링

--- src/main/java/gift/repository/WishRepository.java
+++ src/main/java/gift/repository/WishRepository.java
@@ -3,8 +3,10 @@ package gift.repository;
 import gift.entity.Product;
 import gift.entity.Member;
 import gift.entity.Wish;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WishRepository extends JpaRepository<Wish, Long> {
-    Wish findByMemberAndItem(Member member, Product product);
+    Optional<Wish> findByMemberAndProduct(Member member, Product product);
+    void deleteByMemberAndProduct_Id(Member member, Long productId);
 }
