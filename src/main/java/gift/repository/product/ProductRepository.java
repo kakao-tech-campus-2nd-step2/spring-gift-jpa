2754210d8fc00150a60935f268434949c4bfab63
JJY <ddglackrp@gmail.com>
2024-07-09 14:43:12 +0900

    feat "Product Repository fetch join method 추가"

--- src/main/java/gift/repository/product/ProductRepository.java
+++ src/main/java/gift/repository/product/ProductRepository.java
@@ -2,7 +2,18 @@ package gift.repository.product;
 
 import gift.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query(
+            "select p from Product p" +
+                    " join fetch p.wishList" +
+                    " where p.id = :id"
+    )
+    Optional<Product> findProductWithRelation(@Param("id") Long id);
 
 }
