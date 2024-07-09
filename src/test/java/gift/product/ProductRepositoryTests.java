9037af2886f12541ef56c35d6149e0d489397995
Gyumin Bae <bgmsound0528@gmail.com>
2024-07-09 18:05:43 +0900

    test: product repository test 추가

---
+++ src/test/java/gift/product/ProductRepositoryTests.java
@@ -0,0 +1,26 @@
+package gift.product;
+
+import gift.product.infrastructure.persistence.JpaProductRepository;
+import gift.product.infrastructure.persistence.ProductEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class ProductRepositoryTests {
+    @Autowired
+    private JpaProductRepository jpaProductRepository;
+
+    @Test
+    public void saveProduct() {
+        ProductEntity product = new ProductEntity(0L, "test", 100, "test");
+
+        product = jpaProductRepository.save(product);
+
+        assertThat(jpaProductRepository.findById(product.getId())).isPresent();
+    }
+}
