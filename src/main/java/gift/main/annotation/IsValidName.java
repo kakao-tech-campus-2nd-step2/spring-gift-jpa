15ef6987151105607718b1ed024ba6954ced5d39
JinSeoHyun <15roa1121@gmail.com>
2024-07-10 02:37:50 +0900

    chore: 자바 코드컨벤션 지키기

--- src/main/java/gift/main/annotation/IsValidName.java
+++ src/main/java/gift/main/annotation/IsValidName.java
@@ -13,13 +13,16 @@ import java.lang.annotation.RetentionPolicy;
 
 
 @Documented
-@Constraint(validatedBy = ProductNameValidator.class )
-@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.PARAMETER })
+@Constraint(validatedBy = ProductNameValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface IsValidName {
 
     String value() default "";
+
     String message() default "Invalid product name";
+
     Class<?>[] groups() default {};
+
     Class<? extends Payload>[] payload() default {};
 }
--- src/main/java/gift/main/dto/ProductRequest.java
+++ src/main/java/gift/main/dto/ProductRequest.java
@@ -1,11 +1,12 @@
 package gift.main.dto;
-import gift.main.annotation.IsValidName;
 
+import gift.main.annotation.IsValidName;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PositiveOrZero;
 
-public record ProductRequest(@IsValidName String name,
-                             @PositiveOrZero(message = "상품 가격은 음수일 수 없습니다.") int price,
-                             @NotBlank(message = "이미지주소를 등록해주세요.") String imageUrl) {
+public record ProductRequest(
+                @IsValidName String name,
+                @PositiveOrZero(message = "상품 가격은 음수일 수 없습니다.") int price,
+                @NotBlank(message = "이미지주소를 등록해주세요.") String imageUrl) {
 
 }
