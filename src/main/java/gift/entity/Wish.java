50e1a6ce46b26662ae3a0162b64dbb02a8899f1f
noojmogstel <goaudgo@naver.com>
2024-07-09 13:43:33 +0900

    refactor(entity): ManyToOne 연관관계 추가 및 Getter 변경

--- src/main/java/gift/entity/Wish.java
+++ src/main/java/gift/entity/Wish.java
@@ -5,6 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Wish {
@@ -13,30 +16,23 @@ public class Wish {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private Long memberId;
+    @ManyToOne
+    @JoinColumn(name="MEMBER_ID")
+    private Member member;
 
-    @Column(nullable = false)
-    private Long productId;
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product product;
 
     public Wish() {
     }
 
 
-    public Wish(Long memberId, Long productId) {
-        this.memberId=memberId;
-        this.productId=productId;
+    public Long getMemberId(){
+        return member.getId();
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    public Long getProductId() {
-        return productId;
+    public Long getProductId( ) {
+        return  product.getId();
     }
 }
