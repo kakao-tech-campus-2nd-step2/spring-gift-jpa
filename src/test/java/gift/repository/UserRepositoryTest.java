022a35aac815ba10ca9476aaf65bf833a1848737
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-09 19:07:36 +0900

    test : UserRepository의 findbyid 기능 test

--- src/test/java/gift/repository/UserRepositoryTest.java
+++ src/test/java/gift/repository/UserRepositoryTest.java
@@ -2,8 +2,11 @@ package gift.repository;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import gift.domain.Product;
 import gift.domain.User;
 import gift.repository.user.UserRepository;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,8 +28,26 @@ public class UserRepositoryTest {
             () -> assertThat(savedUser.getId()).isNotNull(),
             () -> assertThat(savedUser.getEmail()).isEqualTo(user.getEmail())
         );
-
     }
+
+    @Test
+    void findbyid() {
+        // given
+        Long id = 2L;
+        User user1 = new User(1L, "kakao", "kakao@google.com", "password", "BE");
+        User user2 = new User(2L, "name", "name@google.com", "password", "BE");
+        userRepository.save(user1);
+        userRepository.save(user2);
+        // when
+        Optional<User> findUser = userRepository.findById(id);
+        Long findId = findUser.get().getId();;
+        // then
+        assertThat(findId).isEqualTo(id);
+    }
+
+    
+
+
 
 
 }
