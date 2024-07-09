e5bd6c94c76b43ba2a95793b35d68198cba9e7ef
unknown <dn06023@naver.com>
2024-07-09 14:57:27 +0900

    refactor : jdbcTemplate에서 jpa로 변경

--- src/main/java/gift/Service/ProductService.java
+++ src/main/java/gift/Service/ProductService.java
@@ -2,70 +2,92 @@ package gift.Service;
 
 import gift.Exception.AuthorizedException;
 import gift.Model.Product;
-import gift.Model.ProductDAO;
+import gift.Model.Member;
 import gift.Model.Role;
-import gift.Model.UserInfoDAO;
+import gift.Repository.ProductRepository;
+import gift.Repository.MemberRepository;
 import gift.Token.JwtTokenProvider;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
-    private final ProductDAO productDAO;
+    private final ProductRepository productRepository;
+    private final MemberRepository memberRepository;
     private final JwtTokenProvider jwtTokenProvider;
-    private final UserInfoDAO userInfoDAO;
 
-    public ProductService(ProductDAO productDAO, JwtTokenProvider jwtTokenProvider, UserInfoDAO userInfoDAO){
-        this.productDAO = productDAO;
+    public ProductService(ProductRepository productRepository, MemberRepository memberRepository, JwtTokenProvider jwtTokenProvider){
+        this.productRepository = productRepository;
+        this.memberRepository = memberRepository;
         this.jwtTokenProvider = jwtTokenProvider;
-        this.userInfoDAO = userInfoDAO;
-        productDAO.insertProduct(new Product(1,"a",1, "b"));
     }
 
     public void add(String token, Product product){
         String email = jwtTokenProvider.getEmailFromToken(token);
         String role = jwtTokenProvider.getRoleFromToken(token);
-        if(userInfoDAO.selectUser(email) == null || !role.equals(Role.ADMIN.name())){
+        Optional<Member> memberOptional = memberRepository.findByEmail(email);
+        if(memberOptional.isEmpty()) throw new AuthorizedException();
+
+        Member member = memberOptional.get();
+        if(member.getRole() != Role.ADMIN)
             throw new AuthorizedException();
-        }
-        productDAO.insertProduct(product);
+
+        productRepository.save(product);
     }
 
     public void delete(String token, Long id){
         String email = jwtTokenProvider.getEmailFromToken(token);
         String role = jwtTokenProvider.getRoleFromToken(token);
-        if(userInfoDAO.selectUser(email) == null || !role.equals(Role.ADMIN.name())){
+        Optional<Member> memberOptional = memberRepository.findByEmail(email);
+        if(memberOptional.isEmpty()) throw new AuthorizedException();
+
+        Member member = memberOptional.get();
+        if(member.getRole() != Role.ADMIN)
             throw new AuthorizedException();
-        }
-        productDAO.deleteProduct(id);
+
+        productRepository.deleteById(id);
     }
 
     public void edit(String token, Long id, Product product){
         String email = jwtTokenProvider.getEmailFromToken(token);
         String role = jwtTokenProvider.getRoleFromToken(token);
-        if(userInfoDAO.selectUser(email) == null || !role.equals(Role.ADMIN.name())){
+        Optional<Member> memberOptional = memberRepository.findByEmail(email);
+        if(memberOptional.isEmpty()) throw new AuthorizedException();
+
+        Member member = memberOptional.get();
+        if(member.getRole() != Role.ADMIN)
             throw new AuthorizedException();
-        }
-        productDAO.updateProduct(id, product);
+
+        productRepository.save(product);
     }
 
     public List<Product> getAll(String token){
         String email = jwtTokenProvider.getEmailFromToken(token);
         String role = jwtTokenProvider.getRoleFromToken(token);
-        if(userInfoDAO.selectUser(email) == null || !role.equals(Role.ADMIN.name())){
+        Optional<Member> memberOptional = memberRepository.findByEmail(email);
+        if(memberOptional.isEmpty()) throw new AuthorizedException();
+
+        Member member = memberOptional.get();
+        if(!member.getRole().equals(Role.ADMIN) && !member.getRole().equals(Role.CONSUMER))
             throw new AuthorizedException();
-        }
-        return productDAO.selectAllProduct();
+
+        return productRepository.findAll();
     }
 
-    public Product getById(String token, Long id){
+    public Optional<Product> getById(String token, Long id){
         String email = jwtTokenProvider.getEmailFromToken(token);
         String role = jwtTokenProvider.getRoleFromToken(token);
-        if(userInfoDAO.selectUser(email) == null || !role.equals(Role.ADMIN.name())){
+        Optional<Member> memberOptional = memberRepository.findByEmail(email);
+        if(memberOptional.isEmpty()) throw new AuthorizedException();
+
+        Member member = memberOptional.get();
+        if(!member.getRole().equals(Role.ADMIN) && !member.getRole().equals(Role.CONSUMER))
             throw new AuthorizedException();
-        }
-        return productDAO.selectProduct(id);
+
+        return productRepository.findById(id);
     }
 
 
