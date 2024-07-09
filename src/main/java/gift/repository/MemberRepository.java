d5a97c33a0343181081606ec7a2da4049c0ddbc9
wvssm <neoramiregi@gmail.com>
2024-07-09 16:25:42 +0900

    refactor(MemberRepository): MemberRepository를 JPA repository로 변경

--- src/main/java/gift/repository/MemberRepository.java
+++ src/main/java/gift/repository/MemberRepository.java
@@ -1,11 +1,11 @@
 package gift.repository;
 
 import gift.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface MemberRepository {
-    void save(Member member);
+public interface MemberRepository extends JpaRepository<Long, Member> {
     Optional<Member> findByEmailAndPassword(String email, String password);
     Optional<Member> findByEmail(String email);
 }
