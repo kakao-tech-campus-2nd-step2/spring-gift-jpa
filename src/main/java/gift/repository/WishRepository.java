5113b00b461ef08bc6428893f211e1dcf3d9563f
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-09 20:21:50 +0900

    feat: jpa 레포지토리로 변경

--- src/main/java/gift/repository/WishRepository.java
+++ src/main/java/gift/repository/WishRepository.java
@@ -1,42 +1,16 @@
 package gift.repository;
 
-import gift.dto.WishlistResponseDTO;
-import gift.model.Product;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
+import gift.model.Wish;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 import java.util.List;
 
-@Repository
-public class WishRepository {
-    private final JdbcTemplate jdbcTemplate;
+public interface WishRepository extends JpaRepository<Wish, Long> {
+    @Query("SELECT w.productId FROM Wish w WHERE w.userId = :userId")
+    List<Long> findProductIdsByUserId(@Param("userId") Long userId);
 
-    private static final String selectWishlistSql = "SELECT * FROM PRODUCTS p JOIN WISHLIST w ON p.id = w.product_id JOIN USERS u ON w.user_id = u.id WHERE u.id = ?";
-    private static final String insertWishlistSql = "INSERT INTO WISHLIST (user_id, product_id) VALUES (?, ?)";
-    private static final String deleteWishlistSql = "DELETE FROM WISHLIST WHERE user_id = ? AND product_id = ?";
-
-    public WishRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public void insertWishProduct(Long userId, Long productId) {
-        jdbcTemplate.update(insertWishlistSql, userId, productId);
-    }
-
-    public List<Product> selectWishlist(Long userId) {
-        return jdbcTemplate.query(
-                selectWishlistSql,
-                new Object[]{userId},
-                (resultSet, rowNum) -> new Product(
-                        resultSet.getLong("id"),
-                        resultSet.getString("name"),
-                        resultSet.getInt("price"),
-                        resultSet.getString("imageurl")
-                )
-        );
-    }
-
-    public void deleteWishProduct(Long userId, Long productId) {
-        jdbcTemplate.update(deleteWishlistSql, userId, productId);
-    }
+    void deleteByUserIdAndProductId(Long userId, Long productId);
 }
