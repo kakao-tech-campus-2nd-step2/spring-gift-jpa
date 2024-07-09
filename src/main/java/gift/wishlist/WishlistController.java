4fddedcdcac7d495811aeca3e41c338869308be8
SeoYeongU <westzeroright@gmail.com>
2024-07-09 23:17:16 +0900

    refacotr: Wishlist 도메인에 관해 서비스 계층을 둠

--- src/main/java/gift/wishlist/WishlistController.java
+++ src/main/java/gift/wishlist/WishlistController.java
@@ -6,6 +6,7 @@ import gift.login.LoginMember;
 import gift.logout.TokenValidator;
 import gift.member.Member;
 import gift.product.ProductDao;
+import gift.product.ProductService;
 import java.util.List;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
@@ -22,13 +23,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/wishlist")
 public class WishlistController {
 
-    private final WishlistDao wishlistDao;
-    private final ProductDao productDao;
+    private final WishlistService wishlistService;
+    private final ProductService productService;
     private final TokenValidator tokenValidator;
 
-    public WishlistController(WishlistDao wishlistDao, ProductDao productDao, TokenValidator tokenValidator) {
-        this.wishlistDao = wishlistDao;
-        this.productDao = productDao;
+    public WishlistController(WishlistService wishlistService, ProductService productService, TokenValidator tokenValidator) {
+        this.wishlistService = wishlistService;
+        this.productService = productService;
         this.tokenValidator = tokenValidator;
     }
 
@@ -37,12 +38,12 @@ public class WishlistController {
         throws UnAuthorizationException {
         String token = authHeader.replace("Bearer ", "");
         tokenValidator.validateToken(token);
-        wishlistDao.insertWish(member,request.productId());
+        wishlistService.postWishlist(member,request.productId());
     }
 
     @GetMapping("/admin") // 관리자만 장바구니 테이블을 전체 볼 수 있어야 함 // 수정해야겠다
     public List<Long> getWishlist() {
-        List<Long> wishProducts = wishlistDao.findAllWish();
+        List<Long> wishProducts = wishlistService.getAllWishlist();
         return wishProducts;
     }
 
@@ -52,10 +53,10 @@ public class WishlistController {
         String token = authHeader.replace("Bearer ", "");
         tokenValidator.validateToken(token);
 
-        if (wishlistDao.findProductById(wishId).isEmpty()) {
+        if (wishlistService.getWishlistById(wishId).isEmpty()) {
             throw new InvalidProduct("잘못된 접근입니다");
         }
-        wishlistDao.deleteWish(wishId);
+        wishlistService.deleteWishlist(wishId);
         return ResponseEntity.ok("장바구니에서 제거되었습니다");
     }
 
