a17736a9e16b4c6fee52c29c4f83f706b129bfff
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-09 16:00:05 +0900

    test : ProductRepository의 delete 기능 test

--- src/test/java/gift/repository/ProductRepositoryTest.java
+++ src/test/java/gift/repository/ProductRepositoryTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import gift.domain.Product;
 import gift.repository.product.ProductRepository;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +20,9 @@ class ProductRepositoryTest {
     void save() {
         // given
         Product product1 = new Product(1L, "아메리카노", 4500, "americano");
-        //when
+        // when
         Product savedProduct = productRepository.save(product1);
-        //then
+        // then
         Assertions.assertAll(
             () -> assertThat(savedProduct.getId()).isNotNull(),
             () -> assertThat(savedProduct.getName()).isEqualTo(product1.getName())
@@ -30,10 +31,25 @@ class ProductRepositoryTest {
 
     @Test
     void findbyid() {
-        //given
+        // given
         Long id = 1L;
         productRepository.save(new Product(1L, "아메리카노", 4500, "americano"));
         Long findId = productRepository.findById(id).get().getId();
         assertThat(id).isEqualTo(findId);
+    }
+
+    @Test
+    void deletebyid() {
+        // given
+        Product product1 = new Product(1L, "아메리카노", 4500, "americano");
+        Product product2 = new Product(2L, "가방", 120000, "bag");
+        productRepository.save(product1);
+        productRepository.save(product2);
+
+        // when
+        productRepository.deleteById(1L);
+        List<Product> savedProduct = productRepository.findAll();
+
+        assertThat(savedProduct.size()).isEqualTo(1);
     }
 }
