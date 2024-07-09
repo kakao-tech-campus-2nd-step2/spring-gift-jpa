2eefdab5ec313341d79b7c67297036f0f494f780
sunandrabbit <sunandrabbit@gamil.com>
2024-07-09 18:49:04 +0900

    fix : 기존의 sql.init을 삭제

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO products (name, price, imageUrl) VALUES ('Product 1', 1000, 'http://example.com/image1.jpg');
-INSERT INTO products (name, price, imageUrl) VALUES ('Product 2', 2000, 'http://example.com/image2.jpg');
-INSERT INTO products (name, price, imageUrl) VALUES ('Product 3', 3000, 'http://example.com/image3.jpg');
