8d91a1173d0e7925c38e5c893eff84a3aedc8deb
minjae <minjae4650@gmail.com>
2024-07-09 15:55:27 +0900

    feat: Product 엔티티 클래스에 JPA 어노테이션 추가
    - product 클래스에 JPA 어노테이션을 추가하여 "product" 테이블과 매핑.
    - name, price, imageUrl 필드에 대한 검증 어노테이션 추가.

--- src/main/java/gift/model/Product.java
+++ src/main/java/gift/model/Product.java
@@ -1,19 +1,30 @@
 package gift.model;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
+@Entity
+@Table(name = "product")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull(message = "상품 이름은 필수 입력 값입니다.")
     @Size(max = 15, message = "상품 이름은 최대 15자까지 입력할 수 있습니다.")
     @Pattern(regexp = "^[a-zA-Z0-9가-힣\\(\\)\\[\\]\\+\\-\\&\\/\\_\\s]*$", message = "상품 이름에 허용되지 않는 문자가 포함되어 있습니다.")
+    @Column(nullable = false, length = 15)
     private String name;
 
+    @Column(nullable = false)
     private Integer price;
+
+    @Column(name = "image_url", nullable = false)
     private String imageUrl;
+
+    public Product() {}
 
     public Product(Long id, String name, Integer price, String imageUrl) {
         this.id = id;
