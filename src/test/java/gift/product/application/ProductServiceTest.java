d455dc7a1c1569633604432f5ada89036bfba18b
rladbrua0207 <48901587+rladbrua0207@users.noreply.github.com>
2024-07-09 23:44:05 +0900

    test: ProductService 테스트코드 작성

--- src/test/java/gift/product/application/ProductServiceTest.java
+++ src/test/java/gift/product/application/ProductServiceTest.java
@@ -7,38 +7,46 @@ import gift.product.application.command.ProductUpdateCommand;
 import gift.product.domain.Product;
 import gift.product.domain.ProductRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest
 public class ProductServiceTest {
 
-    @Autowired
+    @InjectMocks
     private ProductService productService;
 
-    @Autowired
+    @Mock
     private ProductRepository productRepository;
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @AfterEach
-    public void endUp() {
-        jdbcTemplate.execute("TRUNCATE TABLE product");
+    public void tearDown() {
+        reset(productRepository);
     }
 
     @Test
     public void 모든_상품_조회_테스트() {
         // Given
-        ProductCreateCommand createCommand = new ProductCreateCommand("Product1", 1000, "http://example.com/image1.jpg");
-        productService.add(createCommand);
+        Product product = new Product("Product1", 1000, "http://example.com/image1.jpg");
+        when(productRepository.findAll()).thenReturn(List.of(product));
 
         // When
         List<ProductResponse> products = productService.findAll();
@@ -46,98 +54,101 @@ public class ProductServiceTest {
         // Then
         assertThat(products).hasSize(1);
         assertThat(products.get(0).name()).isEqualTo("Product1");
+        verify(productRepository, times(1)).findAll();
     }
 
     @Test
     public void 상품_ID로_조회_테스트() {
         // Given
-        ProductCreateCommand createCommand = new ProductCreateCommand("Product1", 1000, "http://example.com/image1.jpg");
-        productService.add(createCommand);
-        Long productId = productRepository.findAll().get(0).getId();
+        Product product = new Product("Product1", 1000, "http://example.com/image1.jpg");
+        when(productRepository.findById(any(Long.class))).thenReturn(Optional.of(product));
 
         // When
-        ProductResponse productResponse = productService.findById(productId);
+        ProductResponse productResponse = productService.findById(1L);
 
         // Then
         assertThat(productResponse.name()).isEqualTo("Product1");
+        verify(productRepository, times(1)).findById(1L);
     }
 
     @Test
     public void 상품_ID로_조회_실패_테스트() {
         // Given
-        Long productId = 1L;
+        when(productRepository.findById(any(Long.class))).thenReturn(Optional.empty());
 
         // When / Then
-        assertThatThrownBy(() -> productService.findById(productId))
+        assertThatThrownBy(() -> productService.findById(1L))
                 .isInstanceOf(NotFoundException.class)
                 .hasMessage("해당 상품이 존재하지 않습니다.");
+        verify(productRepository, times(1)).findById(1L);
     }
 
     @Test
     public void 상품_추가_테스트() {
         // Given
         ProductCreateCommand createCommand = new ProductCreateCommand("Product1", 1000, "http://example.com/image1.jpg");
+        Product product = createCommand.toProduct();
+        when(productRepository.save(any(Product.class))).thenReturn(product);
 
         // When
-        productService.add(createCommand);
+        productService.save(createCommand);
 
         // Then
-        assertThat(productRepository.findAll()).hasSize(1);
-        assertThat(productRepository.findAll().get(0).getName()).isEqualTo("Product1");
+        verify(productRepository, times(1)).save(any(Product.class));
     }
 
     @Test
     public void 상품_업데이트_테스트() {
         // Given
-        ProductCreateCommand createCommand = new ProductCreateCommand("Product1", 1000, "http://example.com/image1.jpg");
-        productService.add(createCommand);
-        Long productId = productRepository.findAll().get(0).getId();
-
-        ProductUpdateCommand updateCommand = new ProductUpdateCommand(productId, "UpdatedProduct", 2000, "http://example.com/image2.jpg");
+        Product product = new Product("Product1", 1000, "http://example.com/image1.jpg");
+        ProductUpdateCommand updateCommand = new ProductUpdateCommand(1L, "UpdatedProduct", 2000, "http://example.com/image2.jpg");
+        when(productRepository.findById(any(Long.class))).thenReturn(Optional.of(product));
 
         // When
         productService.update(updateCommand);
 
         // Then
-        Product updatedProduct = productRepository.findById(productId).get();
-        assertThat(updatedProduct.getName()).isEqualTo("UpdatedProduct");
-        assertThat(updatedProduct.getPrice()).isEqualTo(2000);
-        assertThat(updatedProduct.getImageUrl()).isEqualTo("http://example.com/image2.jpg");
+        verify(productRepository, times(1)).findById(1L);
+        assertThat(product.getName()).isEqualTo("UpdatedProduct");
+        assertThat(product.getPrice()).isEqualTo(2000);
+        assertThat(product.getImageUrl()).isEqualTo("http://example.com/image2.jpg");
     }
 
     @Test
     public void 상품_업데이트_실패_테스트() {
         // Given
-        Long productId = 1L;
-        ProductUpdateCommand updateCommand = new ProductUpdateCommand(productId, "UpdatedProduct", 2000, "http://example.com/image2.jpg");
+        when(productRepository.findById(any(Long.class))).thenReturn(Optional.empty());
+        ProductUpdateCommand updateCommand = new ProductUpdateCommand(1L, "UpdatedProduct", 2000, "http://example.com/image2.jpg");
 
         // When / Then
         assertThatThrownBy(() -> productService.update(updateCommand))
                 .isInstanceOf(NotFoundException.class)
                 .hasMessage("해당 상품이 존재하지 않습니다.");
+        verify(productRepository, times(1)).findById(1L);
     }
 
     @Test
     public void 상품_삭제_테스트() {
         // Given
-        ProductCreateCommand createCommand = new ProductCreateCommand("Product1", 1000, "http://example.com/image1.jpg");
-        productService.add(createCommand);
-        Long productId = productRepository.findAll().get(0).getId();
+        Product product = new Product("Product1", 1000, "http://example.com/image1.jpg");
+        when(productRepository.findById(any(Long.class))).thenReturn(Optional.of(product));
+        doNothing().when(productRepository).delete(any(Product.class));
 
         // When
-        productService.delete(productId);
+        productService.delete(1L);
 
         // Then
-        assertThat(productRepository.findById(productId)).isEmpty();
+        verify(productRepository, times(1)).findById(1L);
+        verify(productRepository, times(1)).delete(product);
     }
 
     @Test
-    void 이름에카카오포함시_상품생성_실패_테스트() throws Exception {
+    void 이름에카카오포함시_상품생성_실패_테스트() {
         // Given
         ProductCreateCommand createCommand = new ProductCreateCommand("카카오가 포함된 이름", 1000, "http://example.com/image1.jpg");
 
         // When / Then
-        assertThatThrownBy(() -> productService.add(createCommand))
+        assertThatThrownBy(() -> productService.save(createCommand))
                 .isInstanceOf(KakaoInNameException.class)
                 .hasMessage("카카오가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다.");
     }
@@ -145,17 +156,13 @@ public class ProductServiceTest {
     @Test
     public void 이름에카카오포함시_상품업데이트_실패_테스트() {
         // Given
-        ProductCreateCommand createCommand = new ProductCreateCommand("Product1", 1000, "http://example.com/image1.jpg");
-        productService.add(createCommand);
-        Long productId = productRepository.findAll().get(0).getId();
-
-        ProductUpdateCommand updateCommand = new ProductUpdateCommand(productId, "카카오가 포함된 이름", 2000, "http://example.com/image2.jpg");
+        Product product = new Product("Product1", 1000, "http://example.com/image1.jpg");
+        when(productRepository.findById(any(Long.class))).thenReturn(Optional.of(product));
+        ProductUpdateCommand updateCommand = new ProductUpdateCommand(1L, "카카오가 포함된 이름", 2000, "http://example.com/image2.jpg");
 
         // When / Then
         assertThatThrownBy(() -> productService.update(updateCommand))
                 .isInstanceOf(KakaoInNameException.class)
                 .hasMessage("카카오가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다.");
-
     }
 }
-
