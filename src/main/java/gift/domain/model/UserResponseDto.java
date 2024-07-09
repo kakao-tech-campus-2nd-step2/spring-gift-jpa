1857b0db6224f149f97f5b35b5718ee9e79b8fdc
J-1ac <tkdwns26@naver.com>
2024-07-09 20:06:18 +0900

    refacotr: UserRepository JPA 방식으로 리팩토링

--- src/main/java/gift/domain/model/UserResponseDto.java
+++ src/main/java/gift/domain/model/UserResponseDto.java
@@ -9,11 +9,6 @@ public class UserResponseDto {
         this.user = user;
         this.token = token;
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ec8458b (refacotr: UserRepository JPA 방식으로 리팩토링)
-
     public User getUser() {
         return user;
     }
@@ -21,9 +16,4 @@ public class UserResponseDto {
     public String getToken() {
         return token;
     }
-<<<<<<< HEAD
-=======
->>>>>>> c87bd7d (refactor: schema.sql 삭제)
-=======
->>>>>>> ec8458b (refacotr: UserRepository JPA 방식으로 리팩토링)
 }
