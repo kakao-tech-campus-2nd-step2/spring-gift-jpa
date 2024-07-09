26860d43c6ea46edc1ec6e0f59fe08303dc46f9a
rdme0 <we6610@gmail.com>
2024-07-10 03:18:57 +0900

    :recycle: refactor: JPA를 이용한 서비스로 리팩터링

--- src/main/java/gift/service/ParameterValidator.java
+++ src/main/java/gift/service/ParameterValidator.java
@@ -1,46 +1,91 @@
 package gift.service;
 
-import gift.dao.ProductDao;
-import gift.dao.UserDao;
+import gift.dto.MemberDTO;
 import gift.dto.ProductDTO;
+import gift.entity.Member;
 import gift.entity.Product;
-import gift.entity.User;
 import gift.exception.BadRequestExceptions.BadRequestException;
 import gift.exception.BadRequestExceptions.DataCorruptionException;
 import gift.exception.BadRequestExceptions.InvalidIdException;
+import gift.exception.BadRequestExceptions.NoSuchProductIdException;
+import gift.exception.BadRequestExceptions.UserNotFoundException;
+import gift.exception.InternalServerExceptions.InternalServerException;
+import gift.repository.MemberRepository;
+import gift.repository.ProductRepository;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ParameterValidator {
-    private final ProductDao productDao;
+
+    private final MemberRepository memberRepository;
+    private final ProductRepository productRepository;
 
     @Autowired
-    public ParameterValidator(ProductDao productDao) {
-        this.productDao = productDao;
+    public ParameterValidator(MemberRepository memberRepository, ProductRepository productRepository) {
+        this.memberRepository = memberRepository;
+        this.productRepository = productRepository;
     }
 
-    public void validateParameter(Integer id, ProductDTO productDTO) throws BadRequestException {
+    public void validateParameter(Long id, ProductDTO productDTO) throws BadRequestException {
         if (!Objects.equals(productDTO.id(), id)) {
             throw new InvalidIdException("올바르지 않은 id입니다.");
         }
     }
-    public void validateParameter(Product product) throws BadRequestException {
-        Product productInDb = productDao.selectOneProduct(product.getId());
-        if(!product.equals(productInDb))
-            throw new DataCorruptionException("클라이언트의 상품 데이터와 DB의 상품 데이터가 일치하지 않습니다.");
+
+
+    public Map<String, Object> validateParameter(
+            MemberDTO memberDTO, ProductDTO productDTO) throws BadRequestException, InternalServerException {
+        Member member = validateMember(memberDTO);
+        Product product = validateProduct(productDTO);
+
+        Map<String, Object> returnMap = new HashMap<String, Object>();
+        returnMap.put("member",member);
+        returnMap.put("product", product);
+
+        return returnMap;
     }
 
-    public void validateParameter(Map<String, Object> userAndWishLists, User user) throws BadRequestException {
-        User userInDb = (User) userAndWishLists.get("user");
-        if(!user.equals(userInDb))
-            throw new DataCorruptionException("클라이언트의 유저 데이터와 DB의 유저 데이터가 일치하지 않습니다.");
+    public Map<String, Object> validateParameter(MemberDTO memberDTO, Long id) throws BadRequestException {
+        Member member = validateMember(memberDTO);
+        Product product = validateProduct(id);
+
+        Map<String, Object> returnMap = new HashMap<String, Object>();
+        returnMap.put("member",member);
+        returnMap.put("product", product);
+
+        return returnMap;
     }
 
-    public void validateParameter(Map<String, Object> userAndWishLists, User user, Product product) throws BadRequestException {
-        validateParameter(userAndWishLists, user);
-        validateParameter(product);
+    private Member validateMember(MemberDTO memberDTO) {
+        Optional<Member> optionalMember = memberRepository.findByEmail(memberDTO.getEmail());
+        if (optionalMember.isEmpty())
+            throw new UserNotFoundException("유저를 찾을 수 없습니다.");
+
+        return optionalMember.get();
     }
+
+    private Product validateProduct(ProductDTO productDTO) {
+        Optional<Product> optionalProduct =
+                productRepository.findByIdAndNameAndPriceAndImageUrl(productDTO.id(),
+                        productDTO.name(),
+                        productDTO.price(), productDTO.imageUrl());
+        if (optionalProduct.isEmpty())
+            throw new BadRequestException("그러한 제품은 없습니다.");
+
+        return optionalProduct.get();
+    }
+
+    private Product validateProduct(Long productId) {
+        Optional<Product> optionalProduct = productRepository.findById(productId);
+        if(optionalProduct.isEmpty())
+            throw new NoSuchProductIdException("id가 %d인 제품은 존재하지 않습니다.".formatted(productId));
+
+        return optionalProduct.get();
+    }
+
 }
