23e7a94a2020a7a080bce51b6e9020b677540d9e
dkswoals <a4445656@gmail.com>
2024-07-09 17:42:18 +0900

    fix: WishList Table 이름 수정

--- src/main/java/gift/model/wishList/WishItem.java
+++ src/main/java/gift/model/wishList/WishItem.java
@@ -5,8 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "wish")
 public class WishItem {
 
     @Id
