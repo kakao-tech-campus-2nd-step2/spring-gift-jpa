03176095d6ec31e6da48165c3c0ae9a4b38d4e58
unknown <dn06023@naver.com>
2024-07-09 15:05:45 +0900

    feat : Member 데이터베이스 테스트 코드 추가

--- src/test/java/gift/MemberRepositoryTest.java
+++ src/test/java/gift/MemberRepositoryTest.java
@@ -1,9 +1,8 @@
 package gift;
 
-import gift.Model.Product;
 import gift.Model.Role;
-import gift.Model.UserInfo;
-import gift.Repository.UserInfoRepository;
+import gift.Model.Member;
+import gift.Repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,14 +11,14 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DataJpaTest
-public class UserInfoRepositoryTest {
+public class MemberRepositoryTest {
     @Autowired
-    private UserInfoRepository userInfoRepository;
+    private MemberRepository memberRepository;
 
     @Test
     void save(){
-        UserInfo expected = new UserInfo("admin","1234", Role.ADMIN);
-        UserInfo actual = userInfoRepository.save(expected);
+        Member expected = new Member("test","1234", Role.ADMIN);
+        Member actual = memberRepository.save(expected);
         assertAll(
                 () -> assertThat(actual.getId()).isNotNull(),
                 () -> assertThat(actual.getEmail()).isEqualTo(expected.getEmail())
@@ -28,11 +27,11 @@ public class UserInfoRepositoryTest {
 
     @Test
     void findByName() {
-        String expectedEmail = "admin";
+        String expectedEmail = "teeeest";
         String expectedPassword = "1234";
         Role expectedRole = Role.ADMIN;
-        userInfoRepository.save(new UserInfo(expectedEmail, expectedPassword, expectedRole));
-        String actual = userInfoRepository.findByEmail(expectedEmail).getEmail();
+        memberRepository.save(new Member(expectedEmail, expectedPassword, expectedRole));
+        String actual = memberRepository.findByEmail(expectedEmail).get().getEmail();
         assertThat(actual).isEqualTo(expectedEmail);
     }
 }
