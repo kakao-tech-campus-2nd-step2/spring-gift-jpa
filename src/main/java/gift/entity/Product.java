916faa218d76359ee642bc53de3cd551467c8d47
rdme0 <we6610@gmail.com>
2024-07-09 23:24:45 +0900

    :recycle: refactor: quantity 필드 제거하고 JPA를 위한 어노테이션 추가

--- src/main/java/gift/entity/Product.java
+++ src/main/java/gift/entity/Product.java
@@ -2,58 +2,72 @@ package gift.entity;
 
 import gift.dto.ProductDTO;
 import gift.exception.BadRequestExceptions.BlankContentException;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import java.util.Objects;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
+@Entity
 public class Product {
 
-    private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private Long id;
 
     @NotBlank(message = "상품 이름을 입력해주세요.")
     @Length(min = 1, max = 15, message = "제품명 길이는 1~15자만 가능합니다.")
     @Pattern(regexp = "[a-zA-Z0-9ㄱ-ㅎ가-힣()\\[\\]+\\-&/_ ]+", message = "( ), [ ], +, -, &, /, _을 제외한 특수문자는 입력할 수 없습니다.")
     @Pattern(regexp = "^((?!카카오).)*$", message = "카카오가 포함된 문구는 담당 MD와 협의한 후에 사용해주시기 바랍니다.")
+    @Column(nullable = false)
+    @Size(max=15)
     private String name;
 
     @NotNull(message = "가격을 입력해주세요")
     @Min(value = 0, message = "가격은 0 이상, 2147483647 이하이여야 합니다.")
     @Max(value = Integer.MAX_VALUE, message = "가격은 0 이상, 2147483647 이하이여야 합니다.")
+    @Column(nullable = false)
     private Integer price;
 
+    @Size(max=255)
+    @Column(nullable = false)
     @NotBlank(message = "이미지 URL을 입력해주세요.")
     @URL(message = "URL 형식이 아닙니다.")
     private String imageUrl;
 
-    private Integer quantity;
-
     public Product() {
     }
 
-    public Product(Integer id, String name, Integer price, String imageUrl) {
-        this(id, name, price, imageUrl, 0);
-    }
-
     public Product(ProductDTO productDTO) {
-        this(productDTO.id(), productDTO.name(), productDTO.price(), productDTO.imageUrl(), 0);
+        this(productDTO.id(), productDTO.name(), productDTO.price(), productDTO.imageUrl());
         blankCatch(productDTO);
     }
 
-    public Product(Integer id, String name, Integer price, String imageUrl, Integer quantity) {
+    public Product(String name, Integer price, String imageUrl) {
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public Product(Long id, String name, Integer price, String imageUrl) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
-        this.quantity = quantity;
     }
 
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
@@ -69,16 +83,20 @@ public class Product {
         return imageUrl;
     }
 
-    public Integer getQuantity() {
-        return quantity;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 
     public ProductDTO convertToProductDTO(Product product) {
-        return new ProductDTO(product.getId(), product.getName(), product.getPrice(), product.getImageUrl(), product.getQuantity());
+        return new ProductDTO(product.getId(), product.getName(), product.getPrice(), product.getImageUrl());
     }
 
     private void blankCatch(ProductDTO productDTO) throws BlankContentException {
@@ -110,7 +128,7 @@ public class Product {
 
     @Override
     public String toString() {
-        return "Product{" +
+        return "Item{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", price=" + price +
