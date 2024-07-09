ecf40225b1fe4bec2ab1f857e421917c548700fd
Kangboom <jkl6147@naver.com>
2024-07-09 19:03:07 +0900

    refactor(domain): remove setter

--- src/main/java/gift/domain/Member.java
+++ src/main/java/gift/domain/Member.java
@@ -15,22 +15,12 @@ public class Member {
         return this.id;
     }
 
-    public void setId(Long id){this.id = id; }
-
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
 }
