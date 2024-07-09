05a3e9b9cecd109a82be1ba99c65a4fd4e58a528
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-09 20:21:33 +0900

    feat: 엔티티로 변경 및 생성

---
+++ src/main/java/gift/model/Wish.java
@@ -0,0 +1,53 @@
+package gift.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "wish")
+public class Wish {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    // 기본 생성자
+    protected Wish() {}
+
+    // 생성자
+    public Wish(Long userId, Long productId) {
+        this.userId = userId;
+        this.productId = productId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long user_id) {
+        this.userId = user_id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+}
