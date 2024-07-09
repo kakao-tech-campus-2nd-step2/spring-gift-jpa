2a21780d05dc40057c8ce11baec7aa0f7486e5f2
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-07-09 14:11:57 +0900

    style: 사용하지 않는 import 삭제

--- src/test/java/gift/member/repository/MemberRepositoryTest.java
+++ src/test/java/gift/member/repository/MemberRepositoryTest.java
@@ -7,10 +7,7 @@ import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @DataJpaTest
 class MemberRepositoryTest {
