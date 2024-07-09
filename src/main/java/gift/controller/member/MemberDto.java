196c89e061ff396e631876946e040972417064b9
daolove0323 <daolove0323@gmail.com>
2024-07-10 00:48:32 +0900

    feat: memberRequestDto 구현

--- src/main/java/gift/controller/member/MemberDto.java
+++ src/main/java/gift/controller/member/MemberDto.java
@@ -2,11 +2,8 @@ package gift.controller.member;
 
 import gift.domain.Member;
 
-public record MemberDto(String email, String password) {
+public record MemberDto(Long id, String email, String password) {
     public static MemberDto of(Member member) {
-        return new MemberDto(member.email(), member.password());
-    }
-    public static Member toMember(MemberDto member) {
-        return new Member(member.email(), member.password());
+        return new MemberDto(member.getId(), member.getEmail(), member.getPassword());
     }
 }
