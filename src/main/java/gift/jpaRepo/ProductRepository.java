94b8586bf251fcfccd862fab21d8a9f3f504abaa
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-09 10:55:26 +0900

    refactor: Product JPR Repository 로 교체

---
+++ src/main/java/gift/jpaRepo/ProductRepository.java
@@ -0,0 +1,8 @@
+package gift.jpaRepo;
+
+import gift.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
