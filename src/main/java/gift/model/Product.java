a99d7581d6672e26aa120dd71747109cf43a3130
suhyeon7497 <nucc1186@gmail.com>
2024-07-09 15:46:41 +0900

    feat: ProductRepository 추가

--- src/main/java/gift/model/Product.java
+++ src/main/java/gift/model/Product.java
@@ -1,11 +1,30 @@
 package gift.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Product")
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+    @Column(name = "price", nullable = false)
     private int price;
+    @Column(name = "image_url", nullable = false)
     private String imageUrl;
+
+    protected Product() {
+
+    }
 
     public Product(Long id, String name, int price, String imageUrl) {
         this.id = id;
