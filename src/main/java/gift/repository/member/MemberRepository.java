93c5fff8c6059167e8da9874e66aac7a7e6a0c7a
JJY <ddglackrp@gmail.com>
2024-07-09 14:43:02 +0900

    feat "Member Repository fetch join method 추가"

--- src/main/java/gift/repository/member/MemberRepository.java
+++ src/main/java/gift/repository/member/MemberRepository.java
@@ -2,6 +2,8 @@ package gift.repository.member;
 
 import gift.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -10,4 +12,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findMemberByEmail(String email);
 
     Optional<Member> findMemberByEmailAndPassword(String email, String password);
+
+    @Query(
+            "select m from Member m" +
+                    " join fetch m.wishList" +
+                    " where m.email=:email "
+    )
+    Optional<Member> findMemberWithRelation(@Param("email") String email);
 }
