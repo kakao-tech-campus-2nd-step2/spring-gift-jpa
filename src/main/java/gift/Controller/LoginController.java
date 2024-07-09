125eda842b55a95ffb894d22ce2f37f86f14f75a
unknown <dn06023@naver.com>
2024-07-09 14:59:18 +0900

    refactor : service에 Autowired 어노테이션 추가

--- src/main/java/gift/Controller/LoginController.java
+++ src/main/java/gift/Controller/LoginController.java
@@ -1,27 +1,17 @@
 package gift.Controller;
 
-import gift.Exception.LoginException;
-import gift.Model.Role;
 import gift.Model.User;
-import gift.Model.UserInfo;
-import gift.Model.UserInfoDAO;
 import gift.Service.UserService;
-import gift.Token.JwtToken;
-import gift.Token.JwtTokenProvider;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
-    private final UserService userService;
-
-    public LoginController(UserService userService){
-        this.userService = userService;
-    }
+    @Autowired
+    private UserService userService;
 
     @PostMapping("/members/register")
     public ResponseEntity<?> register(@RequestBody User user){
