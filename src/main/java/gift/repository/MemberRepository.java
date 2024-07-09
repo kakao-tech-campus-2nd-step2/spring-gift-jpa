e2a74257f7d9a1f86533d0f49a083e4b73f9fd77
wvssm <neoramiregi@gmail.com>
2024-07-09 16:57:33 +0900

    refactor(MemberRepository, ProductRepository, WishRepository): 잘못된 Jpa 설정 변경

--- src/main/java/gift/repository/MemberRepository.java
+++ src/main/java/gift/repository/MemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Long, Member> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmailAndPassword(String email, String password);
     Optional<Member> findByEmail(String email);
 }
--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface ProductRepository extends JpaRepository<Long, Product> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findById(Long id);
     Optional<Product> findByName(String name);
 }
