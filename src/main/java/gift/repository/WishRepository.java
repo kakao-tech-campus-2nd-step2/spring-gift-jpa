42422537637e34a327e03173531e3e34a4ba7d64
HaegyeongKim01 <haegyeong.kim01@gmail.com>
2024-07-09 23:33:42 +0900

    refactor: 사용하지 않는 메소드 삭제

--- src/main/java/gift/repository/WishRepository.java
+++ src/main/java/gift/repository/WishRepository.java
@@ -4,9 +4,7 @@ import gift.vo.Wish;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface WishRepository extends JpaRepository<Wish, Integer> {
     List<Wish> findByMemberId(Long memberId);
-    Optional<Wish> findByMemberIdAndProductId(Long memberId, Long productId);
 }
