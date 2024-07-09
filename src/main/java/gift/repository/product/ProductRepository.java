585cca678a4f50485f398e5b5a5e68f1962806c5
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-09 15:15:23 +0900

    refactor : JpaReopsitory로 변경

--- src/main/java/gift/repository/product/ProductRepository.java
+++ src/main/java/gift/repository/product/ProductRepository.java
@@ -3,24 +3,9 @@ package gift.repository.product;
 import gift.domain.Product;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository {
-    // id에 따른 상품 조회
-    Optional<Product> findById(Long id);
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    // 모든 상품 조회
-    List<Product> findAll();
-
-    // 상품 추가
-    public Product save(Product product);
-
-    // 상품 수정
-    public void update(Long id, Product product);
-
-    // 상품 삭제
-    public void deleteById(Long id);
-
-    // 상품 삭제 시 목록의 id 재정렬
-//    public void orderId();
 
 }
