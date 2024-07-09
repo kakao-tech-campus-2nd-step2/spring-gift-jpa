d8b24c9e0fd58fe7a02451c59d65d782302a7c04
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-09 19:19:07 +0900

    test : WishListRepository의 findbyid 기능 test

--- src/test/java/gift/repository/WishListRepositoryTest.java
+++ src/test/java/gift/repository/WishListRepositoryTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import gift.domain.WishList;
 import gift.repository.wish.WishListRepository;
+import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,21 @@ public class WishListRepositoryTest {
             () -> assertThat(savedWish.getId()).isNotNull(),
             () -> assertThat(savedWish.getQuantity()).isEqualTo(wishList.getQuantity())
         );
+    }
+
+    @Test
+    void findbyid() {
+        // given
+        Long id = 1L;
+        WishList wish1 = new WishList(1L, 1L, 1L, 2);
+        WishList wish2 = new WishList(2L, 1L, 3L, 3);
+        wishListRepository.save(wish1);
+        wishListRepository.save(wish2);
+        // when
+        Optional<WishList> findWish = wishListRepository.findById(id);
+        Long findId = findWish.get().getId();
+        // then
+        assertThat(findId).isEqualTo(id);
     }
 
     
