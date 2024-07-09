e1303fd4baba5495eb74573f09995c521c273d1a
rdme0 <we6610@gmail.com>
2024-07-10 03:20:23 +0900

    :recycle: refactor: JPA를 이용한 DTO에 대응하여 리팩터링

--- src/main/java/gift/service/ProductConverter.java
+++ src/main/java/gift/service/ProductConverter.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 public class ProductConverter {
     public static List<ProductDTO> convertToProductDTO(List<Product> productList) throws BadRequestException {
         return productList.stream().map(product -> {
-            return new ProductDTO(product.getId(), product.getName(), product.getPrice(), product.getImageUrl(), 0);
+            return new ProductDTO(product.getId(), product.getName(), product.getPrice(), product.getImageUrl());
         }).collect(Collectors.toList());
     }
 }
