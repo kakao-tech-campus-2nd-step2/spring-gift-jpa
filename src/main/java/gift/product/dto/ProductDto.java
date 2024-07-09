10e80a07d68c1fc71c7db7db11428632df43676f
ez23re <ez23re@gmail.com>
2024-07-09 14:09:19 +0900

    refactor : 상품명 검증은 값 객체 적용

--- src/main/java/gift/product/dto/ProductDto.java
+++ src/main/java/gift/product/dto/ProductDto.java
@@ -9,9 +9,6 @@ import org.hibernate.validator.constraints.URL;
 public class ProductDto {
     private Long id;
 
-    @NotBlank(message = "상품명을 입력하세요.")
-    @Size(max = 15, message = "상품명은 공백 포함 최대 15자")
-    @Pattern(regexp = "^[\\w\\s()\\[\\]+\\-&/]*$", message = "상품명에 잘못된 문자가 있습니다.")
     private String name;
 
     @Positive(message = "올바른 가격을 입력하세요.")
