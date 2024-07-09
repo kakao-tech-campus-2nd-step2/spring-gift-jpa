753ae4f207638498d7dfbba6adb2bf0816537f65
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-07-09 09:54:06 +0900

    refactor: JPA를 사용을 위한  model -> Entity 수정

--- src/main/java/gift/product/model/Product.java
+++ src/main/java/gift/product/model/Product.java
@@ -1,22 +1,33 @@
 package gift.product.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@Entity
+@Table(name = "product")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @NotBlank(message = "상품의 이름은 필수 항목입니다.")
     @Size(max = 15, message = "상품의 이름은 최대 15자까지 입력할 수 있습니다.")
     private String name;
 
+    @Column(nullable = false)
     @Min(value = 0, message = "상품의 가격은 0보다 크거나 같아야 합니다.")
     private int price;
 
     private String imgUrl;
 
-    public Product(){}
+    public Product() {}
 
     public Product(long id, String name, int price, String imgUrl) {
         this.id=id;
