4741a6f614630776f3818b975a85bcc25c0ddf88
anacnoda77 <sm1108shin@naver.com>
2024-07-09 17:30:10 +0900

    fix: 상품 추가/삭제 시 예외 처리

--- src/main/java/gift/service/WishProductService.java
+++ src/main/java/gift/service/WishProductService.java
@@ -1,10 +1,14 @@
 package gift.service;
 
+import gift.exception.MemberException;
+import gift.exception.ProductException;
 import gift.exception.WishListException;
 import gift.model.Product;
 import gift.model.WishProduct;
+import gift.repository.ProductRepository;
 import gift.repository.WishProductRepository;
 import java.util.List;
+import java.util.NoSuchElementException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,9 +18,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class WishProductService {
 
     private final WishProductRepository wishProductRepository;
+    private final ProductRepository productRepository;
 
-    public WishProductService(WishProductRepository wishProductRepository) {
+    public WishProductService(WishProductRepository wishProductRepository,
+        ProductRepository productRepository) {
         this.wishProductRepository = wishProductRepository;
+        this.productRepository = productRepository;
     }
 
     public List<Product> getMyWishList(Long memberId) {
@@ -24,6 +31,10 @@ public class WishProductService {
     }
 
     public void addMyWish(Long memberId, Long productId) {
+
+        productRepository.findById(productId)
+            .orElseThrow(() -> new ProductException("해당 제품이 존재하지 않습니다."));
+
         try {
             WishProduct wishProduct = new WishProduct(memberId, productId);
             wishProductRepository.save(wishProduct);
@@ -35,11 +46,11 @@ public class WishProductService {
     public void removeMyWish(Long memberId, Long productId) {
         wishProductRepository.findByMemberIdAndProductId(memberId, productId)
             .ifPresentOrElse(wishProductRepository::delete
-            , () -> { throw new WishListException("해당 상품이 위시 리스트에 존재하지 않습니다.");}
+                , () -> {
+                    throw new WishListException("해당 상품이 위시 리스트에 존재하지 않습니다.");
+                }
             );
     }
-
-
 
 
 }
