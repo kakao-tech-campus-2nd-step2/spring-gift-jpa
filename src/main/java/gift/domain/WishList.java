92edeb1a07f37a98f3fcf3ed15462b2e0cca2aa4
alsswl <alswl4223@naver.com>
2024-07-09 19:11:36 +0900

    refactor : wishlist 엔티티 jpa 방식으로 변경

--- src/main/java/gift/domain/WishList.java
+++ src/main/java/gift/domain/WishList.java
@@ -1,9 +1,20 @@
 package gift.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class WishList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String memberId;
+
+    @Column(nullable = false)
     private Long menuId;
+
+    public WishList() {}
 
     public WishList(Long id, String memberId, Long menuId){
         this.id = id;
