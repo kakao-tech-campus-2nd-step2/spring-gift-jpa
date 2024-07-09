7b83af85a0d60334584b40a1a6865ccb633631bb
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-09 11:10:26 +0900

    feat: Member Entity add

---
+++ src/main/java/gift/entity/Member.java
@@ -0,0 +1,38 @@
+package gift.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column(unique = true)
+    @NotNull
+    String email;
+    @NotNull
+    String password;
+
+    public Member(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public Member() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
