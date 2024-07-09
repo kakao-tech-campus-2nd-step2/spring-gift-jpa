718ccf25f2b1314a3c6630d3cb409f6bba0fb978
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-09 13:22:27 +0900

    feat: Wish 관련 예외 클래스추가, 어드바이스컨트롤러 추가

---
+++ src/main/java/gift/exception/WishAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package gift.exception;
+
+public class WishAlreadyExistsException extends RuntimeException {
+
+    public WishAlreadyExistsException(String message) {
+        super(message);
+    }
+
+}
