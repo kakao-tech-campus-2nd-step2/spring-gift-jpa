9d19d980d1ee346d6cbda2474a4b3309363227f0
yoong-saks <wjdghtjd06@gmail.com>
2024-07-10 00:51:17 +0900

    refactor: WishService 알맞게 변경

--- src/main/java/gift/service/wish/WishService.java
+++ src/main/java/gift/service/wish/WishService.java
@@ -3,7 +3,9 @@ package gift.service.wish;
 import gift.domain.wish.Wish;
 import gift.domain.wish.WishRepository;
 import gift.web.dto.WishDto;
+import gift.web.exception.ProductNotFoundException;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,26 +17,27 @@ public class WishService {
     }
 
     public List<WishDto> getWishes(String email) {
-        return List.copyOf(wishRepository.selectAllWishes(email)
-                            .stream()
-                            .map(WishDto::from)
-                            .toList()
-                            );
+        return wishRepository.findAll()
+            .stream()
+            .map(WishDto::from)
+            .collect(Collectors.toList());
     }
 
     public WishDto createWish(String email, WishDto wishDto) {
-        return WishDto.from(wishRepository.insertWish(WishDto.toEntity(wishDto, email)));
+        return WishDto.from(wishRepository.save(WishDto.toEntity(wishDto, email)));
     }
 
     public WishDto updateWish(String email, WishDto wishDto) {
-        wishRepository.getWish(email, wishDto.productId());
+        wishRepository.findByEmailAndProductId(email, wishDto.productId())
+                .orElseThrow(() -> new ProductNotFoundException("위시 제품이 없슴다."));
         Wish newWish = WishDto.toEntity(wishDto, email);
-        wishRepository.updateWish(newWish);
+        wishRepository.save(newWish);
         return WishDto.from(newWish);
     }
 
     public void deleteWish(String email, Long productId) {
-        wishRepository.getWish(email, productId);
-        wishRepository.deleteWish(email, productId);
+        wishRepository.findByEmailAndProductId(email, productId)
+            .orElseThrow(() -> new ProductNotFoundException("위시 제품이 없슴다."));
+        wishRepository.deleteByEmailAndProductId(email, productId);
     }
 }
