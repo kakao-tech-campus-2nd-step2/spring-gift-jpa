18e4e295f64385c57aff5551a8df5e9cf93784cc
alsswl <alswl4223@naver.com>
2024-07-09 19:03:30 +0900

    refactor : menu 엔티티 jpa 방식으로 변경

--- src/main/java/gift/domain/Menu.java
+++ src/main/java/gift/domain/Menu.java
@@ -1,10 +1,23 @@
 package gift.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private int price;
+
+    @Column(nullable = false)
     private String imageUrl;
+
+    public Menu() {}
 
     public Menu(String name, int price, String imageUrl) {
         this(null,name,price,imageUrl);
