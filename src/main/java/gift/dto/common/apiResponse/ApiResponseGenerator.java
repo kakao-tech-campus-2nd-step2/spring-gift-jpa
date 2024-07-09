efd68bc6c6c501a5aeef7e6185899be2ddcb4881
박민규 <pmq0101@naver.com>
2024-07-09 14:43:10 +0900

    remove(ApiResponse): ResponseEntity 상속 떼내기

--- src/main/java/gift/dto/common/apiResponse/ApiResponseGenerator.java
+++ src/main/java/gift/dto/common/apiResponse/ApiResponseGenerator.java
@@ -2,21 +2,22 @@ package gift.dto.common.apiResponse;
 
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 public class ApiResponseGenerator {
-    public static ApiResponse<ApiResponseBody.SuccessBody<Void>> success(HttpStatus status, String message){
-        return new ApiResponse<>(new ApiResponseBody.SuccessBody<>(String.valueOf(status.value()), message, null), status);
+    public static ResponseEntity<ApiResponseBody.SuccessBody<Void>> success(HttpStatus status, String message){
+        return new ResponseEntity<>(new ApiResponseBody.SuccessBody<>(String.valueOf(status.value()), message, null), status);
     }
 
-    public static <D>ApiResponse<ApiResponseBody.SuccessBody<D>> success(HttpStatus status, String message, D data){
-        return new ApiResponse<>(new ApiResponseBody.SuccessBody<>(String.valueOf(status.value()), message, data), status);
+    public static <D>ResponseEntity<ApiResponseBody.SuccessBody<D>> success(HttpStatus status, String message, D data){
+        return new ResponseEntity<>(new ApiResponseBody.SuccessBody<>(String.valueOf(status.value()), message, data), status);
     }
 
-    public static ApiResponse<ApiResponseBody.FailureBody> fail(HttpStatus status, String message){
-        return new ApiResponse<>(new ApiResponseBody.FailureBody(String.valueOf(status), message, null), status);
+    public static ResponseEntity<ApiResponseBody.FailureBody> fail(HttpStatus status, String message){
+        return new ResponseEntity<>(new ApiResponseBody.FailureBody(String.valueOf(status), message, null), status);
     }
     //Todo: 에러 코드 만들기
-    public static ApiResponse<ApiResponseBody.FailureBody> fail(HttpStatus status, String message, String code){
-        return new ApiResponse<>(new ApiResponseBody.FailureBody(String.valueOf(status), message, code), status);
+    public static ResponseEntity<ApiResponseBody.FailureBody> fail(HttpStatus status, String message, String code){
+        return new ResponseEntity<>(new ApiResponseBody.FailureBody(String.valueOf(status), message, code), status);
     }
 }
