81314fd8b10c77bd29e984cebeed80105bdc5ae8
Kangboom <jkl6147@naver.com>
2024-07-09 19:20:20 +0900

    refactor(repository): change return type in save

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,10 +1,15 @@
 package gift.repository;
 
 import gift.domain.Product;
+import gift.dto.ProductRequest;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -37,27 +42,33 @@ public class ProductRepository {
         return jdbcTemplate.query("select * from product", productRowMapper());
     }
 
-    public void save(Product product) {
+    public Product save(ProductRequest productRequest) {
         String sql = "INSERT INTO product (name, price, imageUrl) VALUES (?, ?, ?)";
-        jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl());
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, productRequest.getName());
+            ps.setInt(2, productRequest.getPrice());
+            ps.setString(3, productRequest.getImageUrl());
+            return ps;
+        }, keyHolder);
+
+        return new Product(keyHolder.getKey().longValue(), productRequest.getName(), productRequest.getPrice(),productRequest.getImageUrl());
+
     }
 
-    public void update(Long id, Product product){
+    public Product update(Product product){
         String sql = "UPDATE product SET name = ?, price = ?, imageUrl = ? WHERE id = ?";
-        jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl(), id);
+        jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl(), product.getId());
+
+        return new Product(product.getId(), product.getName(), product.getPrice(), product.getImageUrl());
     }
     public void delete(Long id){
         jdbcTemplate.update("DELETE FROM product WHERE id = ?", id);
     }
 
     private RowMapper<Product> productRowMapper() {
-        return (rs, rowNum) -> {
-            Product product = new Product();
-            product.setId(rs.getLong("id"));
-            product.setName(rs.getString("name"));
-            product.setPrice(rs.getInt("price"));
-            product.setImageUrl(rs.getString("imageUrl"));
-            return product;
-        };
+        return (rs, rowNum) -> new Product(rs.getLong("id"), rs.getString("name"), rs.getInt("price"), rs.getString("imageUrl"));
     }
 }
