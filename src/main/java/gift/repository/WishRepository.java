fe1768026fc6bc28aa3e9de79f51e49daf493d41
wvssm <neoramiregi@gmail.com>
2024-07-09 16:26:15 +0900

    refactor(WishRepository): WishRepository를 JPA repository로 변경

--- src/main/java/gift/repository/WishRepository.java
+++ src/main/java/gift/repository/WishRepository.java
@@ -6,9 +6,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface WishRepository {
-    void save(Wish wish);
     Optional<List<Wish>> findByMemberId(Long memberId);
     Optional<Wish> findByIdAndMemberId(Long id, Long memberId);
-    void delete(Long id);
-    void updateQuantity(Long id, int quantity);
 }
