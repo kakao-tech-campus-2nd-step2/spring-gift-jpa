56baa354fed0ffbdeeaded23cfa666542f3e6915
배민수 <duecost@gmail.com>
2024-07-09 21:40:56 +0900

    test: WishRepository에 대한 테스트 작성

---
+++ src/test/java/gift/WishRepositoryTest.java
@@ -0,0 +1,60 @@
+package gift;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.SoftAssertions.*;
+
+import gift.product.model.Wish;
+import gift.product.repository.WishRepository;
+import java.util.List;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class WishRepositoryTest {
+    @Autowired
+    WishRepository wishRepository;
+
+    @Test
+    void 위시리스트_항목_추가() {
+        Wish wish = new Wish(1L, 1L);
+        Wish insertedWish = wishRepository.save(wish);
+
+        assertSoftly(softly -> {
+            assertThat(insertedWish.getId()).isNotNull();
+            assertThat(insertedWish.getMemberId()).isEqualTo(1L);
+            assertThat(insertedWish.getProductId()).isEqualTo(1L);
+        });
+    }
+
+    @Test
+    void 위시리스트_전체_조회() {
+        wishRepository.save(new Wish(1L, 1L));
+        wishRepository.save(new Wish(1L, 2L));
+
+        List<Wish> wishes = wishRepository.findAllByMemberId(1L);
+
+        assertThat(wishes).hasSize(2);
+    }
+
+    @Test
+    void 위시리스트_조회() {
+        wishRepository.save(new Wish(1L, 1L));
+
+        boolean isPresentWish = wishRepository.findByIdAndMemberId(1L, 1L).isPresent();
+
+        assertThat(isPresentWish).isTrue();
+    }
+
+    @Test
+    void 위시리스트_항목_삭제() {
+        wishRepository.save(new Wish(1L, 1L));
+        wishRepository.deleteById(1L);
+        boolean isPresentWish = wishRepository.findByIdAndMemberId(1L, 1L).isPresent();
+
+        assertThat(isPresentWish).isFalse();
+    }
+}
