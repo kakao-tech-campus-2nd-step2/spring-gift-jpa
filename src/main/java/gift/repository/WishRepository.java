6df617ad4805e50574fd22998139be4ef9d2e597
Kangboom <jkl6147@naver.com>
2024-07-09 19:18:07 +0900

    refactor(repository): change return type in save

--- src/main/java/gift/repository/WishRepository.java
+++ src/main/java/gift/repository/WishRepository.java
@@ -1,9 +1,13 @@
 package gift.repository;
 
-import gift.domain.Product;
 import gift.domain.Wish;
+import gift.dto.WishRequest;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,27 +21,31 @@ public class WishRepository {
     }
 
     private RowMapper<Wish> wishRowMapper(){
-        return (rs, rowNum) -> {
-            Wish wish = new Wish();
-            wish.setId(rs.getLong("id"));
-            wish.setMemberId(rs.getLong("member_id"));
-            wish.setProductId(rs.getLong("product_id"));
-            return wish;
-        };
+        return (rs, rowNum) -> new Wish(rs.getLong("id"), rs.getLong("member_id"), rs.getLong("product_id"));
     }
 
     public List<Wish> findByMemberId(Long memberId) {
-        String sql = "SELECT * FROM Wish WHERE member_id = ?";
+        String sql = "SELECT * FROM wish WHERE member_id = ?";
         return jdbcTemplate.query(sql, wishRowMapper(), memberId);
     }
 
-    public void save(Wish wish) {
-        String sql = "INSERT INTO Wish (member_id, product_id) VALUES (?, ?)";
-        jdbcTemplate.update(sql, wish.getMemberId(), wish.getProductId());
+    public Wish save(WishRequest wishRequest, Long memberId) {
+        String sql = "INSERT INTO wish (member_id, product_id) VALUES (?, ?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setLong(1, memberId);
+            ps.setLong(2, wishRequest.getProductId());
+            return ps;
+        }, keyHolder);
+
+        return new Wish(keyHolder.getKey().longValue(), memberId, wishRequest.getProductId());
     }
 
     public void delete(Long id, Long memberId) {
-        String sql = "DELETE FROM Wish WHERE id = ? AND member_id = ?";
+        String sql = "DELETE FROM wish WHERE id = ? AND member_id = ?";
         jdbcTemplate.update(sql, id, memberId);
     }
 }
