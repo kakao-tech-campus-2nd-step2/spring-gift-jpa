094d1b9177dd339f5aab09911885b8ef7cf01b5e
배민수 <duecost@gmail.com>
2024-07-09 21:40:21 +0900

    test: AuthRepository에 대한 테스트 작성

---
+++ src/test/java/gift/AuthRepositoryTest.java
@@ -0,0 +1,57 @@
+package gift;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import gift.product.model.Member;
+import gift.product.repository.AuthRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class AuthRepositoryTest {
+    @Autowired
+    AuthRepository authRepository;
+
+    @Test
+    void 회원가입() {
+        Member member = new Member("test@test.com", "test");
+        Member registeredMember = authRepository.save(member);
+
+        assertSoftly(softly -> {
+            assertThat(registeredMember.getId()).isNotNull();
+            assertThat(registeredMember.getEmail()).isEqualTo("test@test.com");
+            assertThat(registeredMember.getPassword()).isEqualTo("test");
+        });
+    }
+
+    @Test
+    void 이메일로_회원_찾기() {
+        Member member = new Member("test@test.com", "test");
+        Member registeredMember = authRepository.save(member);
+
+        Member findedMember = authRepository.findByEmail(registeredMember.getEmail());
+
+        assertThat(findedMember).isNotNull();
+    }
+
+    @Test
+    void 특정_이메일을_가진_회원_존재_여부() {
+        Member member = new Member("test@test.com", "test");
+        Member registeredMember = authRepository.save(member);
+
+        boolean isMemberPresent = authRepository.existsByEmail(registeredMember.getEmail());
+        assertThat(isMemberPresent).isTrue();
+    }
+
+    @Test
+    void 특정_ID를_가진_회원_존재_여부() {
+        Member member = new Member("test@test.com", "test");
+        Member registeredMember = authRepository.save(member);
+
+        boolean isMemberPresent = authRepository.existsById(registeredMember.getId());
+        assertThat(isMemberPresent).isTrue();
+    }
+}
