a223833001ad0612353d0f8ea14bb30cf362590a
Dalsungmin <cnww123@naver.com>
2024-07-10 19:46:13 +0900

    Feat: Wishlist 인터페이스와 연결
    인터페이스와 연결하면서 메서드들 인터페이스에 맞게 수정

--- src/main/java/gift/service/WishlistService.java
+++ src/main/java/gift/service/WishlistService.java
@@ -3,25 +3,27 @@ package gift.service;
 import gift.model.WishList;
 import gift.repository.WishlistRepository;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WishlistService {
     private final WishlistRepository wishlistRepository;
 
+    @Autowired
     public WishlistService(WishlistRepository wishlistRepository) {
         this.wishlistRepository = wishlistRepository;
     }
 
     public WishList addProduct(WishList product) {
-        return wishlistRepository.addProduct(product);
+        return wishlistRepository.save(product);
     }
 
     public List<WishList> getProductsByMemberId(Long memberId) {
-        return wishlistRepository.getProductsByMemberId(memberId);
+        return wishlistRepository.findByMemberId(memberId);
     }
 
-    public void deleteItem(Long id) {
-        wishlistRepository.deleteItem(id);
+    public void deleteById(Long productId) {
+        wishlistRepository.deleteById(productId);
     }
 }
