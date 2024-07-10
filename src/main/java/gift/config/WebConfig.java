cfd2cf641c7b60fbbf71e5a6daf35b0957479850
minjae <minjae4650@gmail.com>
2024-07-10 15:52:34 +0900

    refactor: Move WebConfig to config package
    - WebConfig 클래스를 component 패키지에서 config 패키지로 이동.
    - 패키지 구조를 개선하여 설정 관련 클래스를 명확하게 구분.

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -1,5 +1,6 @@
-package gift.component;
+package gift.config;
 
+import gift.component.LoginMemberArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
