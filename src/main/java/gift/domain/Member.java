3c2ecb70acbd9ffc967c6a6cbfd0b85f0680ffee
ariimo <159447466+ariimo@users.noreply.github.com>
2024-07-10 21:55:16 +0900

    refactor: Member Entity

--- src/main/java/gift/domain/Member.java
+++ src/main/java/gift/domain/Member.java
@@ -1,10 +1,28 @@
 package gift.domain;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "members")
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberId;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String password;
     private String role;
+
+    public Member(){}
 
     public Member(Long memberId, String email, String password, String role) {
         this.memberId = memberId;
@@ -31,5 +49,17 @@ public class Member {
 
     public String getPassword() {
         return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
