11ca062ccedc3a75cfe855c1c6c8bc420da9adc8
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2024-07-10 15:05:48 +0900

    fix : entity에 유효성 검사 삭제

--- src/main/java/gift/entity/Product.java
+++ src/main/java/gift/entity/Product.java
@@ -17,11 +17,10 @@ public class Product{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Size(max = 15, message = "Name is too long!")
-    @Pattern(regexp = "^[a-zA-Z0-9 ()\\[\\]+\\-\\&\\/\\_가-힣]*$", message = "Name has invalid character")
-    @Pattern(regexp = "^(?!.*카카오).*$", message = "'카카오'가 포함된 문구는 담당 MD와 협의한 경우에만 사용 가능합니다")
     private String name;
+
     private int price;
+    
     private String imageUrl;
 
     public Product() {
