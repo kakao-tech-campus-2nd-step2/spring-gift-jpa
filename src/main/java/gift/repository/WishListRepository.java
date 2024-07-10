57987ec08028e172abe06c425a125e82baa0fb74
humpose <yjpx416@gamil.com>
2024-07-10 15:53:16 +0900

    Feat: optional 추가

--- src/main/java/gift/repository/WishListRepository.java
+++ src/main/java/gift/repository/WishListRepository.java
@@ -2,10 +2,11 @@ package gift.repository;
 
 import gift.model.User;
 import gift.model.WishList;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WishListRepository extends JpaRepository<WishList, Long> {
-    WishList findByUser(User user);
+    Optional<WishList> findByUser(User user);
 }
