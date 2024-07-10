3c7fa21e41e40f15ce3edcd36ee206bb32e963f4
humpose <yjpx416@gamil.com>
2024-07-10 15:54:20 +0900

    Refactor: 안 쓰는 import삭제

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,12 +1,7 @@
 package gift.repository;
 
-import gift.model.Name;
 import gift.model.Product;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
--- src/main/java/gift/repository/UserRepository.java
+++ src/main/java/gift/repository/UserRepository.java
@@ -3,7 +3,9 @@ package gift.repository;
 
 import gift.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 }
