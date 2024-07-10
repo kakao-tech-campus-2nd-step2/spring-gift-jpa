cffee64d0a6239bcbb9b07a45e36f507c48eeae0
suno-boy <tnsgh3691@gmail.com>
2024-07-10 15:36:55 +0900

    refactor: Config단순화

--- src/main/java/gift/Config/WebConfig.java
+++ src/main/java/gift/Config/WebConfig.java
@@ -1,5 +1,6 @@
 package gift.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,16 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private final InterceptorOfToken interceptorOfToken;
-
-    public WebConfig(InterceptorOfToken interceptorOfToken) {
-        this.interceptorOfToken = interceptorOfToken;
-    }
+    @Autowired
+    private InterceptorOfToken interceptorOfToken;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(interceptorOfToken)
-                .addPathPatterns("/**") // 인터셉터가 적용될 경로 패턴을 설정
-                .excludePathPatterns("/user/**"); // 인터셉처 적용 안 해도 되는 경로 패턴 설정
+        registry.addInterceptor(interceptorOfToken).addPathPatterns("/**");
     }
 }
