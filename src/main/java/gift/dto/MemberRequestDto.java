f99506ff993cc105eebeed6ed9dd382fb5b28c34
wvssm <neoramiregi@gmail.com>
2024-07-10 16:10:03 +0900

    feat(MemberRequestDto): id field 추가

--- src/main/java/gift/dto/MemberRequestDto.java
+++ src/main/java/gift/dto/MemberRequestDto.java
@@ -3,12 +3,20 @@ package gift.dto;
 import gift.domain.Member;
 
 public class MemberRequestDto {
+    private Long id;
     private String email;
     private String password;
+
+    public MemberRequestDto(Long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
 
     public Member toEntity(){
         return new Member(this.getEmail(), this.getPassword());
     }
+    public Long getId() { return id; }
 
     public String getPassword() {
         return password;
