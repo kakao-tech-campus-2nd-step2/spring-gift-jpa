d6d279026ee36b3478078ecddb741ff04fac8d41
Awhn <ahnshwan@outlook.com>
2024-07-10 15:31:57 +0900

    fix : repository 패키지 추가

--- src/main/java/gift/repository/MemberRepository.java
+++ src/main/java/gift/repository/MemberRepository.java
@@ -1,12 +1,11 @@
-package gift.domain;
+package gift.repository;
 
+import gift.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByToken(String token);
     Member findById(int id);
     int searchIdByToken(String token);
-    String searchTokenByToken(String token);
-    String searchTokenByEmailAndPassword(String email, String password);
+    Boolean existsByToken(String token);
     Member save(Member member);
 }
--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,5 +1,6 @@
-package gift.domain;
+package gift.repository;
 
+import gift.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
