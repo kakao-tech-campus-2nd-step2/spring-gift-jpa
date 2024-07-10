a1cb313cb74c2256c1cf0ab0c9034392cb87b916
suno-boy <tnsgh3691@gmail.com>
2024-07-10 11:28:27 +0900

    chore: jakarta 의존성 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,16 +6,15 @@ CREATE TABLE Product (
 );
 
 CREATE TABLE IF NOT EXISTS users (
-                                     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                        email VARCHAR(255) NOT NULL UNIQUE,
+                        password VARCHAR(255) NOT NULL
     );
 
-CREATE TABLE wishes
-(
-    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
-    user_id    BIGINT,
-    product_id BIGINT,
-    FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (product_id) REFERENCES product (id)
+CREATE TABLE wishes (
+                        id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+                        user_id    BIGINT,
+                        product_id BIGINT,
+                        FOREIGN KEY (user_id) REFERENCES users (id),
+                        FOREIGN KEY (product_id) REFERENCES product (id)
 );
