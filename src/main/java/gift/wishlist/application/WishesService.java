b159339d4e0fb1352dea9c3ee41b065945f4dc0f
HSHwan <tnghks2866@gmail.com>
2024-07-10 15:33:04 +0900

    refactor: Wish 엔티티 FK 관련 기능 리팩토링

--- src/main/java/gift/wishlist/application/WishesService.java
+++ src/main/java/gift/wishlist/application/WishesService.java
@@ -1,6 +1,8 @@
 package gift.wishlist.application;
 
 import gift.error.WishAlreadyExistsException;
+import gift.member.dao.MemberRepository;
+import gift.member.entity.Member;
 import gift.product.dao.ProductRepository;
 import gift.product.entity.Product;
 import gift.wishlist.dao.WishesRepository;
@@ -14,40 +16,49 @@ import java.util.NoSuchElementException;
 public class WishesService {
 
     private final WishesRepository wishesRepository;
+    private final MemberRepository memberRepository;
     private final ProductRepository productRepository;
 
-    public WishesService(WishesRepository wishesRepository, ProductRepository productRepository) {
+    public WishesService(WishesRepository wishesRepository,
+                         MemberRepository memberRepository,
+                         ProductRepository productRepository) {
         this.wishesRepository = wishesRepository;
+        this.memberRepository = memberRepository;
         this.productRepository = productRepository;
     }
 
-    public void addProductToWishlist(Wish wish) {
-        productRepository.findById(wish.getProductId())
-                .orElseThrow(() -> new NoSuchElementException("해당 상품은 존재하지 않습니다."));
-        if (wishesRepository.existsByMemberIdAndProductId(
-                wish.getMemberId(), wish.getProductId())) {
-            throw new WishAlreadyExistsException();
-        }
+    public void addProductToWishlist(Long memberId, Long productId) {
+        wishesRepository.findByMember_IdAndProduct_Id(memberId, productId)
+                .ifPresent(wish -> {
+                    throw new WishAlreadyExistsException();
+                });
 
-        wishesRepository.save(wish);
+        wishesRepository.save(createWish(memberId, productId));
     }
 
-    public void removeProductFromWishlist(Wish wish) {
-        if (!wishesRepository.existsByMemberIdAndProductId(
-                wish.getMemberId(), wish.getProductId())) {
-            throw new NoSuchElementException("해당 상품은 위시 리스트에 존재하지 않습니다.");
-        }
+    public void removeProductFromWishlist(Long memberId, Long productId) {
+        Wish wish = wishesRepository.findByMember_IdAndProduct_Id(memberId, productId)
+                .orElseThrow(() -> new NoSuchElementException("해당 위시는 존재하지 않습니다."));
 
         wishesRepository.delete(wish);
     }
 
     public List<Product> getWishlistOfMember(Long memberId) {
-        List<Long> productIdList = wishesRepository.findByMemberId(memberId)
-                                                   .stream()
-                                                   .map(Wish::getProductId)
-                                                   .toList();
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new NoSuchElementException("해당 회원 계정은 존재하지 않습니다."))
+                .getWishList()
+                .stream()
+                .map(Wish::getProduct)
+                .toList();
+    }
 
-        return productRepository.findByIdIn(productIdList);
+    private Wish createWish(Long memberId, Long productId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new NoSuchElementException("해당 회원 계정은 존재하지 않습니다."));
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new NoSuchElementException("해당 상품은 존재하지 않습니다."));
+
+        return new Wish(member, product);
     }
 
 }
