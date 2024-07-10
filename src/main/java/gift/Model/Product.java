eff867e10b31b8ca45787ad4092ff4b644b458b4
Woojae Jeong <woo6388@naver.com>
2024-07-11 02:21:18 +0900

    style : Product의 name 길이를 varchar 15가 되게끔 수정

--- src/main/java/gift/Model/Product.java
+++ src/main/java/gift/Model/Product.java
@@ -7,7 +7,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
-    @Column(nullable = false)
+    @Column(nullable = false,length = 15)
     private String name;
     @Column(nullable = false)
     private int price;
