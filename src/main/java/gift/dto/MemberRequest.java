850a0141868de4b4d012fdc94a8ee82dd1e1d09b
EunKyung Lee <2dmsrud2002@naver.com>
2024-07-10 14:32:31 +0900

    Refactor: dto 개행 및 valid 추가

--- src/main/java/gift/dto/MemberRequest.java
+++ src/main/java/gift/dto/MemberRequest.java
@@ -1,9 +1,16 @@
 package gift.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 public record MemberRequest(
-        @Email(message = "이메일 형식의 입력이어야 합니다.") String email,
+        @Email(message = "이메일 형식의 입력이어야 합니다.")
+        @NotNull
+        @NotEmpty
+        String email,
+        @NotNull
+        @NotEmpty
         String password
 ) {
 }
--- src/main/java/gift/dto/WishRequest.java
+++ src/main/java/gift/dto/WishRequest.java
@@ -1,8 +1,11 @@
 package gift.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 public record WishRequest(
-        @Min(value = 1, message = "상품 id는 1 이상이어야 합니다.") Long productId
+        @Min(value = 1, message = "상품 id는 1 이상이어야 합니다.")
+        @NotNull
+        Long productId
 ) {
 }
