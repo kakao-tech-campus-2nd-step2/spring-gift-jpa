3cbb10c665c9134cf3bcec65186057e7ec6a09e2
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-10 15:44:51 +0900

    refactor : Valid 관련 어노테이션 삭제 및 @Column 추가

--- src/main/java/gift/domain/Product.java
+++ src/main/java/gift/domain/Product.java
@@ -1,5 +1,6 @@
 package gift.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,16 +17,7 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank(message = "이름은 필수 입력값입니다.")
-    @Size(max = 15, message = "이름의 최대 글자수는 15입니다.")
-    @Pattern(
-        regexp = "^[가-힣a-zA-Z0-9\\(\\)\\[\\]\\+\\-\\&\\/\\_\\s]*$",
-        message = "상품 이름은 최대 15자, 한글과 영문, 그리고 특수기호([],(),+,-,&,/,_)만 사용 가능합니다!"
-    )
-    @Pattern(
-        regexp = "(?!.*카카오).*",
-        message = "'카카오'가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다!"
-    )
+    @Column(nullable = false)
     private String name;
     private int price;
     private String imageUrl;
