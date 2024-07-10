e54359287339470fcaefc44707bb7ab1ed9db602
unknown <dn06023@naver.com>
2024-07-10 14:30:15 +0900

    refactor : conflict 해결 시 생겼던 오류 수정

---
+++ src/main/java/gift/Exception/ProductNotFoundException.java
@@ -0,0 +1,2 @@
+package gift.Exception;public class ProductNotFoundException {
+}
--- src/main/java/gift/Model/UserInfo.java
+++
@@ -1,6 +0,0 @@
-package gift.Model;
-
-import org.springframework.context.annotation.Bean;
-
-public record UserInfo(String email, String password, Role role) {
-}
