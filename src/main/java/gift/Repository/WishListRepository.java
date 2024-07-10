532e44fe0c8c303515c7ad804a04904de970beb9
Woojae Jeong <woo6388@naver.com>
2024-07-11 04:16:22 +0900

    feat : WishListRepository를 JPA화

--- src/main/java/gift/Repository/WishListRepository.java
+++ src/main/java/gift/Repository/WishListRepository.java
@@ -1,50 +1,29 @@
 package gift.Repository;
 
-import gift.Model.RequestWishListDTO;
 import gift.Model.ResponseWishListDTO;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Repository;
+import gift.Model.WishList;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-@Repository
-public class WishListRepository {
-    private final JdbcTemplate jdbctemplate;
+public interface WishListRepository extends JpaRepository<WishList, Long> {
+    @Query("SELECT new gift.Model.ResponseWishListDTO(p.name, w.count) " +
+            "FROM WishList w INNER JOIN Product p ON w.productId = p.id " +
+            "WHERE w.email = :email")
+    List<ResponseWishListDTO> findWishListsByEmail(@Param("email")String email);
 
+    @Transactional
+    @Modifying
+    @Query("UPDATE WishList w SET w.count = :count " +
+            "WHERE w.email = :email AND w.productId = :productId")
+    void updateWishListCount(@Param("count")int count, @Param("email")String email, @Param("productId")Long productId);
 
-    public WishListRepository(JdbcTemplate jdbctemplate) {
-        this.jdbctemplate = jdbctemplate;
-    }
-
-    public void insertWishList(String email, RequestWishListDTO requestWishListDTO) {
-        var sql = "insert into WishLists (email, productId, count) values (?,?,?)";
-        jdbctemplate.update(sql, email, requestWishListDTO.getProductId(), requestWishListDTO.getCount());
-    }
-
-    public List<ResponseWishListDTO> selectWishList(String email) {
-        var sql = "select name, count from wishLists INNER JOIN products on wishlists.productId = products.id where email= ?";
-        return jdbctemplate.query(sql, new Object[]{email}, responseWishListDTORowMapper());
-    }
-
-    public List<ResponseWishListDTO> updateWishList(String email, RequestWishListDTO requestWishListDTO) {
-        var sql = "UPDATE wishLists SET count = ? WHERE email = ? AND productId = ?";
-        jdbctemplate.update(sql, requestWishListDTO.getCount(), email, requestWishListDTO.getProductId());
-        return selectWishList(email);
-    }
-
-    public List<ResponseWishListDTO> deleteWishList(String email, RequestWishListDTO requestWishListDTO) {
-        var sql = "DELETE FROM wishLists WHERE email = ? AND productId = ?";
-        jdbctemplate.update(sql, email, requestWishListDTO.getProductId());
-        return selectWishList(email);
-    }
-
-    private RowMapper<ResponseWishListDTO> responseWishListDTORowMapper() {
-        return (rs, rowNum) -> new ResponseWishListDTO(
-                rs.getString("name"),
-                rs.getInt("count")
-        );
-    }
-
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM WishList w WHERE w.email = :email AND w.productId = :productId")
+    void deleteWishListByEmailAndProductId(@Param("email")String eamil, @Param("productId")Long productId);
 }
-
