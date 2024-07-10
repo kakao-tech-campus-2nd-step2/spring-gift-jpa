85d7f97bc3c424f166e2531dc925d10e72a9f4c1
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-07-11 01:59:59 +0900

    refactor: request에 validation 추가

--- src/main/java/gift/product/dto/ProductRequest.java
+++ src/main/java/gift/product/dto/ProductRequest.java
@@ -1,11 +1,18 @@
 package gift.product.dto;
 
 import gift.product.model.Product;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class ProductRequest {
     private long id;
+
+    @NotBlank(message = "상품의 이름은 필수 항목입니다.")
+    @Size(max = 15, message = "상품의 이름은 최대 15자까지 입력할 수 있습니다.")
     private String name;
 
+    @Min(value = 0, message = "상품의 가격은 0보다 크거나 같아야 합니다.")
     private int price;
 
     private String imgUrl;
