f3f34970117d764272a625d009c8cd82e7f9d028
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-10 14:28:05 +0900

    feat : 길이 제한 @Column의 length를 사용하여 15자까지만 입력할 수 있도록 제한합니다.

--- src/main/java/gift/model/Product.java
+++ src/main/java/gift/model/Product.java
@@ -18,7 +18,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", length = 15, nullable = false)
     @NotBlank(message = "상품 이름은 최소 1자 이상이어야 합니다.")
     @Size(max = 15, message = "상품 이름은 공백 포함 최대 15자까지 입력할 수 있습니다.")
     @Pattern(regexp = "^[a-zA-Z0-9가-힣ㄱ-ㅎㅏ-ㅣ\\s\\(\\)\\[\\]\\+\\-\\&\\/\\_]*$", message = "상품 이름에 (), [], +, -, &, /, _ 외 특수 문자는 사용할 수 없습니다.")
