0e46517cc1b1f7d84cfc4dc69e1f61d4c85612fc
suno-boy <tnsgh3691@gmail.com>
2024-07-10 11:30:05 +0900

    feat: UserEntity 구현
    - 기존의 Entity라고 이름만 갖던 UserEntity를 어노테이션 붙여서, JPA에 의해 자동 테이블 생성기능을 하게 수정.

--- src/main/java/gift/Entity/UserEntity.java
+++ src/main/java/gift/Entity/UserEntity.java
@@ -1,7 +1,15 @@
 package gift.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class UserEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String email;
     private String password;
@@ -10,6 +18,7 @@ public class UserEntity {
         this.email = email;
         this.password = password;
     }
+
     public UserEntity(){}
 
     public Long getId() {
