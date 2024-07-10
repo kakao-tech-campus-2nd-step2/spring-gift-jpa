3bff418f0459f5c0cf4970b28f765cb57f033382
wvssm <neoramiregi@gmail.com>
2024-07-10 16:12:39 +0900

    refactor(WishService): memberResponseDto로 Member 정보를 받거나, memberId를 전달받도록 수정

--- src/main/java/gift/service/WishService.java
+++ src/main/java/gift/service/WishService.java
@@ -4,10 +4,7 @@ import gift.constants.Messages;
 import gift.domain.Member;
 import gift.domain.Product;
 import gift.domain.Wish;
-import gift.dto.MemberResponseDto;
-import gift.dto.ProductResponseDto;
-import gift.dto.WishRequestDto;
-import gift.dto.WishResponseDto;
+import gift.dto.*;
 import gift.exception.WishNotFoundException;
 import gift.repository.WishRepository;
 import org.springframework.stereotype.Service;
@@ -26,18 +23,16 @@ public class WishService {
     }
 
     @Transactional
-    public void save(MemberResponseDto memberResponseDto, WishRequestDto request){
-        Member member = new Member(memberResponseDto.getId(),memberResponseDto.getEmail(),memberResponseDto.getPassword());
-
-        ProductResponseDto productDto = productService.findByName(request.getProductName());
+    public void save(MemberRequestDto memberRequestDto, WishRequestDto wishRequestDto){
+        ProductResponseDto productDto = productService.findByName(wishRequestDto.getProductName());
         Product product = new Product(productDto.getId(), productDto.getName(), productDto.getPrice(), productDto.getImageUrl());
 
-        wishRepository.save(new Wish(member,product,request.getQuantity()));
+        wishRepository.save(new Wish(memberRequestDto.toEntity(),product,wishRequestDto.getQuantity()));
     }
 
     @Transactional(readOnly = true)
-    public List<WishResponseDto> findByMemberEmail(MemberResponseDto memberResponseDto){
-        return wishRepository.findByMemberId(memberResponseDto.getId())
+    public List<WishResponseDto> getMemberWishListByMemberId(Long memberId){
+        return wishRepository.findByMemberId(memberId)
                 .orElseThrow(() -> new WishNotFoundException(Messages.NOT_FOUND_WISH))
                 .stream()
                 .map(WishResponseDto::from)
@@ -46,15 +41,15 @@ public class WishService {
     }
 
     @Transactional
-    public void deleteById(MemberResponseDto memberResponseDto, Long id){
-        wishRepository.findByIdAndMemberId(id, memberResponseDto.getId())
+    public void deleteWishByMemberIdAndWishId(Long memberId, Long id){
+        wishRepository.findByIdAndMemberId(id, memberId)
                 .orElseThrow(()-> new WishNotFoundException(Messages.NOT_FOUND_WISH));
         wishRepository.deleteById(id);
     }
 
     @Transactional
-    public void updateQuantity(MemberResponseDto memberResponseDto, Long id, WishRequestDto request){
-        Wish existingWish = wishRepository.findByIdAndMemberId(id, memberResponseDto.getId())
+    public void updateQuantityByMemberIdAndWishId(Long memberId, Long id, WishRequestDto request){
+        Wish existingWish = wishRepository.findByIdAndMemberId(id, memberId)
                 .orElseThrow(()-> new WishNotFoundException(Messages.NOT_FOUND_WISH));
 
         Wish updatedWish = new Wish(existingWish.getId(),existingWish.getMember(),existingWish.getProduct(),request.getQuantity());
