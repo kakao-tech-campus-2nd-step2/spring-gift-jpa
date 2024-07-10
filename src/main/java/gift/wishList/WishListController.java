c6608705ce00eb0067996f0fcee9444070be2619
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-11 00:55:49 +0900

    fix : WishList Controller 리턴 타입 변경

--- src/main/java/gift/wishList/WishListController.java
+++ src/main/java/gift/wishList/WishListController.java
@@ -4,78 +4,52 @@ import gift.annotation.LoginUser;
 import gift.product.Product;
 import gift.product.ProductRepository;
 import gift.user.User;
-import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/wishes")
 public class WishListController {
 
-    private final WishListRepository wishListRepository;
-    private final ProductRepository productRepository;
+    private final WishListService wishListService;
 
-    public WishListController(WishListRepository wishListRepository, ProductRepository productRepository) {
-        this.wishListRepository = wishListRepository;
-        this.productRepository = productRepository;
+    public WishListController(WishListService wishListService) {
+        this.wishListService = wishListService;
     }
 
     //생성
     @PostMapping
     @Transactional
-    public ResponseEntity<?> addWishes(@LoginUser User user, @RequestBody WishListDTO wishListDTO) {
-        Product product = productRepository.findById(wishListDTO.getProductID()).orElseThrow();
-        WishList wishList = new WishList(wishListDTO.getCount());
-        user.addWishList(wishList);
-        product.addWishList(wishList);
-        wishListRepository.save(wishList);
-        return ResponseEntity.ok(new WishListDTO(wishList));
+    public WishListDTO addWishes(@LoginUser User user, @RequestBody WishListDTO wishListDTO) {
+        return wishListService.addWish(wishListDTO, user);
     }
 
     //조회(userid)
     @GetMapping
     @Transactional
-    public ResponseEntity<?> getWishesByUserID(@LoginUser User user) {
-        List<WishList> wishLists = wishListRepository.findByUser(user);
-        List<WishListDTO> wishListDTOS = new ArrayList<>();
-        for (WishList wishList : wishLists) {
-            wishListDTOS.add(new WishListDTO(wishList));
-        }
-        return ResponseEntity.ok(wishListDTOS);
+    public List<WishListDTO> getWishesByUser(@LoginUser User user) {
+        return wishListService.findByUser(user);
     }
 
     //수정(count) -> 0이면 삭제
     @PutMapping("/{id}")
     @Transactional
-    public ResponseEntity<?> updateWishesCount(@PathVariable long id, @RequestBody CountDTO count) {
+    public WishListDTO updateWishesCount(@PathVariable long id, @RequestBody CountDTO count) {
         if (count.count == 0) {
-            WishList wishList = wishListRepository.findById(id).orElseThrow();
-            wishList.getUser().removeWishList(wishList);
-            wishList.getProduct().removeWishList(wishList);
-            wishListRepository.deleteById(id);
-
-            return ResponseEntity.ok(null);
+            wishListService.deleteByID(id);
+            return null;
         }
-        WishList wishList = wishListRepository.findById(id).orElseThrow();
-        wishList.setCount(count.getCount());
-
-        return ResponseEntity.ok(new WishListDTO(wishList));
+        return wishListService.updateCount(count, id);
     }
 
     //삭제(id)
     @DeleteMapping("/{id}")
     @Transactional
-    public ResponseEntity<?> deleteWishes(@PathVariable long id) {
-        WishList wishList = wishListRepository.findById(id).orElseThrow();
-        wishList.getUser().removeWishList(wishList);
-        wishList.getProduct().removeWishList(wishList);
-        wishListRepository.deleteById(id);
-
-        return ResponseEntity.ok(null);
+    public WishListDTO deleteWishes(@PathVariable long id) {
+        wishListService.deleteByID(id);
+        return null;
     }
 
 }
