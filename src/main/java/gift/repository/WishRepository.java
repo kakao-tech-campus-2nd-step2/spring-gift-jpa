8d36c317359d404f8b99662c3b9ca5d620fa93b3
minju26 <mjalswn26@gmail.com>
2024-07-10 14:32:04 +0900

    feat: JpaRepository 정의

---
+++ src/main/java/gift/repository/WishRepository.java
@@ -0,0 +1,12 @@
+package gift.repository;
+
+import gift.domain.Member;
+import gift.domain.Product;
+import gift.domain.Wish;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface WishRepository extends JpaRepository<Wish, Long> {
+    List<Wish> findWishByMember(Member member);
+}
