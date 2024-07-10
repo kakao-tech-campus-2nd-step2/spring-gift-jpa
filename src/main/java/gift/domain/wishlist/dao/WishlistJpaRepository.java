1af53f38242986740a916a0ec6357166f9eafa39
hyunseo Jeong <wjdgustj2877@naver.com>
2024-07-10 14:24:06 +0900

    refactor: JPA 외래키 조회 방식 JPQL 이용하여 수정

--- src/main/java/gift/domain/wishlist/dao/WishlistJpaRepository.java
+++ src/main/java/gift/domain/wishlist/dao/WishlistJpaRepository.java
@@ -3,9 +3,13 @@ package gift.domain.wishlist.dao;
 import gift.domain.wishlist.entity.WishItem;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WishlistJpaRepository extends JpaRepository<WishItem, Long> {
-    List<WishItem> findAllByUser_id(Long userId);
+
+    @Query("select w from WishItem w where w.userId = :userId")
+    List<WishItem> findAllByUserId(@Param("userId") Long userId);
 }
