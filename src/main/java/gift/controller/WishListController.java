0fd198dd1a1ac4f9921f7188abd63e023b1a3050
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-07-10 14:10:04 +0900

    fix: conflict 해결

--- src/main/java/gift/controller/WishListController.java
+++ src/main/java/gift/controller/WishListController.java
@@ -31,48 +31,27 @@ public class WishListController {
     }
 
     @GetMapping("")
-<<<<<<< HEAD
     public ResponseEntity<WishListResponse> getAllWishList(@LoginUser LoginUserRequest user) {
         WishListResponse responses = wishService.findAllWish(user.id());
-=======
-    public ResponseEntity<WishListResponse> getAllWishList(@LoginUser User user) {
-        WishListResponse responses = wishService.findAllWish(user.getId());
->>>>>>> 74cf6b2bfb66a871340d58a87b34a502f4ead750
         return ResponseEntity.ok().body(responses);
     }
 
     @PostMapping("")
-<<<<<<< HEAD
     public ResponseEntity<String> addWishProduct(@LoginUser LoginUserRequest user, @Valid @RequestBody WishRequest wishRequest) {
         wishService.addWistList(user.id(), wishRequest);
-=======
-    public ResponseEntity<String> addWishProduct(@LoginUser User user, @Valid @RequestBody WishRequest wishRequest) {
-        wishService.addWistList(user.getId(), wishRequest);
->>>>>>> 74cf6b2bfb66a871340d58a87b34a502f4ead750
         return ResponseEntity.ok().body("위시리스트에 상품이 추가되었습니다.");
     }
 
     @PatchMapping("")
-<<<<<<< HEAD
     public ResponseEntity<String> updateWishProduct(@LoginUser LoginUserRequest user,
         @Valid @RequestBody WishRequest wishRequest) {
         wishService.updateWishList(user.id(), wishRequest);
-=======
-    public ResponseEntity<String> updateWishProduct(@LoginUser User user,
-        @Valid @RequestBody WishRequest wishRequest) {
-        wishService.updateWishList(user.getId(), wishRequest);
->>>>>>> 74cf6b2bfb66a871340d58a87b34a502f4ead750
         return ResponseEntity.ok().body("위시리스트에 상품이 수정되었습니다.");
     }
 
     @DeleteMapping("")
-<<<<<<< HEAD
     public ResponseEntity<String> deleteWishProduct(@LoginUser LoginUserRequest user, @RequestBody WishDeleteRequest wishDeleteRequest) {
         wishService.deleteWishList(user.id(), wishDeleteRequest.productId());
-=======
-    public ResponseEntity<String> deleteWishProduct(@LoginUser User user, @RequestBody WishDeleteRequest wishDeleteRequest) {
-        wishService.deleteWishList(user.getId(), wishDeleteRequest.productId());
->>>>>>> 74cf6b2bfb66a871340d58a87b34a502f4ead750
         return ResponseEntity.ok().body("위시리스트에서 상품이 삭제되었습니다.");
     }
 }
