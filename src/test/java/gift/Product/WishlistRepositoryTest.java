f40c8f6c8c3e51bf16724fd0c60fbeddf62513d4
Dalsungmin <cnww123@naver.com>
2024-07-10 20:05:46 +0900

    Feat: 위시리스트 상품 추가 기능 테스트
    위시리스트에 사용자ID, 상품ID를 입력받아 상품을 추가하는 메서드 테스트

---
+++ src/test/java/gift/Product/WishlistRepositoryTest.java
@@ -0,0 +1,27 @@
+package gift.Product;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import gift.model.WishList;
+import gift.repository.WishlistRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class WishlistRepositoryTest {
+    @Autowired
+    private WishlistRepository wishlistRepository;
+
+    @Test
+    void save() {
+        WishList expected = new WishList(111L, 111L);
+        WishList actual = wishlistRepository.save(expected);
+
+        assertThat(actual.getId()).isNotNull();
+        assertThat(actual.getMemberId()).isEqualTo(expected.getMemberId());
+        assertThat(actual.getProductId()).isEqualTo(expected.getProductId());
+    }
+
+}
