b4c3997528f76b23c112b6ca7e3edd29ad9c9845
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-07-10 15:58:29 +0900

    remove: 사용되지 않는 필드의 제거
    사용되지 않는 필드의 제거

--- src/main/java/gift/model/BaseEntity.java
+++ src/main/java/gift/model/BaseEntity.java
@@ -1,26 +1,13 @@
 package gift.model;
 
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
-
-@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
-
-    @CreatedDate
-    private LocalDateTime createdDate;
-
-    @LastModifiedDate
-    private LocalDateTime modifiedDate;
 }
--- src/main/java/gift/repository/BaseRepository.java
+++ src/main/java/gift/repository/BaseRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
 public interface BaseRepository<T, ID> extends JpaRepository<T, ID> {
-
     default T findByIdOrThrow(ID id) {
         return findById(id)
                 .orElseThrow(() -> new NotFoundElementException("존재하지 않는 리소스에 대한 접근입니다."));
