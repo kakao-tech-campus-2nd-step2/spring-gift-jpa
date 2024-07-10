63825a6213eea8daee92ceafc68570974a78c824
Woojae Jeong <woo6388@naver.com>
2024-07-11 02:54:22 +0900

    feat : MemberRepository JPA화

---
+++ src/main/java/gift/Repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package gift.Repository;
+
+import gift.Model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByEmail(String email);
+}
