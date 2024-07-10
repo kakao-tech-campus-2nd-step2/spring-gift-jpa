d9e5b96b194c0b6224e0e4db85d3e881af7f3da1
suno-boy <tnsgh3691@gmail.com>
2024-07-10 15:39:23 +0900

    refactor: @Autowired로 의존성 자동주입 및 코드 단순화

--- src/main/java/gift/Config/InterceptorOfToken.java
+++ src/main/java/gift/Config/InterceptorOfToken.java
@@ -1,59 +1,27 @@
 package gift.Config;
 
-import gift.Service.TokenService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import gift.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class InterceptorOfToken implements HandlerInterceptor {
 
-    private final TokenService tokenService;
-
-    public InterceptorOfToken(TokenService tokenService) {
-        this.tokenService = tokenService;
-    }
+    @Autowired
+    private UserService userService;
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
-                             Object handler) throws Exception {
-        final String authorizationHeader = request.getHeader("Authorization");
-
-        if (isInvalidAuthorizationHeader(authorizationHeader)) {
-            setUnauthorizedResponse(response);
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = request.getHeader("Authorization");
+        if (token != null && userService.validateToken(token)) {
+            return true;
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return false;
         }
-
-        String token = extractToken(authorizationHeader);
-        String email = tokenService.getEmailFromToken(token);
-
-        if (isInvalidToken(token, email)) {
-            setUnauthorizedResponse(response);
-            return false;
-        }
-
-        setRequestAttributes(request, token);
-        return true;
-    }
-
-    private boolean isInvalidAuthorizationHeader(String authorizationHeader) {
-        return authorizationHeader == null || !authorizationHeader.startsWith("Bearer ");
-    }
-
-    private String extractToken(String authorizationHeader) {
-        return authorizationHeader.substring(7);
-    }
-
-    private boolean isInvalidToken(String token, String email) {
-        return email == null || !tokenService.validateToken(token, email);
-    }
-
-    private void setUnauthorizedResponse(HttpServletResponse response) {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-    }
-
-    private void setRequestAttributes(HttpServletRequest request, String token) {
-        request.setAttribute("token", token);
     }
 }
