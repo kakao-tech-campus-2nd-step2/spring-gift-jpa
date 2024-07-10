0a3c3fe56d9ebcdf1eba7ed7cb2caa81fbc1c73f
ariimo <159447466+ariimo@users.noreply.github.com>
2024-07-10 21:53:37 +0900

    refactor: Product Entity

--- src/main/java/gift/domain/Product.java
+++ src/main/java/gift/domain/Product.java
@@ -1,10 +1,28 @@
 package gift.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private double price;
+
     private String imageUrl;
+
+    public Product() {}
 
     public Product(Long id, String name, double price, String imageUrl) {
         this.id = id;
@@ -13,23 +31,31 @@ public class Product {
         this.imageUrl = imageUrl;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public Long getId() {
         return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public double getPrice() {
         return price;
     }
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
     public String getImageUrl() {
         return imageUrl;
+    }
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 }
