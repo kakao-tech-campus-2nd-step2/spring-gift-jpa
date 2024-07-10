5a3cf2b34836b5d9359a8e103810bd4f70da10cf
suno-boy <tnsgh3691@gmail.com>
2024-07-10 19:42:08 +0900

    feat: UserRepository에 findbyEmail Optimal반환형으로 메서드 구현

--- src/main/java/gift/Repository/UserRepository.java
+++ src/main/java/gift/Repository/UserRepository.java
@@ -4,7 +4,9 @@ import gift.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    // 필요한 경우 커스텀 쿼리 메서드 추가 가능
+    Optional<UserEntity> findByEmail(String email);
 }
