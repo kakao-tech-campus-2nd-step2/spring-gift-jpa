68274f75cd6c142271d4c6f08491257f2a5297b1
Hyewon Seok <hws2008@naver.com>
2024-07-10 21:44:24 +0900

    refactor: separate business logic from WishController

--- src/main/java/gift/api/wishlist/WishController.java
+++ src/main/java/gift/api/wishlist/WishController.java
@@ -1,8 +1,6 @@
 package gift.api.wishlist;
 
 import gift.global.LoginMember;
-import jakarta.persistence.EntityManager;
-import jakarta.transaction.Transactional;
 import java.net.URI;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
@@ -18,40 +16,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/wishes")
 public class WishController {
 
-    private final EntityManager entityManager;
-    private final WishRepository wishRepository;
+    private final WishService wishService;
 
-    public WishController(EntityManager entityManager, WishRepository wishRepository) {
-        this.entityManager = entityManager;
-        this.wishRepository = wishRepository;
+    public WishController(WishService wishService) {
+        this.wishService = wishService;
     }
 
     @GetMapping()
     public ResponseEntity<List<Wish>> getItems(@LoginMember Long memberId) {
-        return ResponseEntity.ok().body(wishRepository.findByMemberId(memberId));
+        return ResponseEntity.ok().body(wishService.getItems(memberId));
     }
 
     @PostMapping()
     public ResponseEntity<Void> add(@RequestBody WishRequest wishRequest, @LoginMember Long memberId) {
-        wishRepository.save(new Wish(memberId, wishRequest.productId(), wishRequest.quantity()));
+        wishService.add(memberId, wishRequest);
         return ResponseEntity.created(URI.create("/api/wishes/" + memberId)).build();
     }
 
-    @Transactional
     @PutMapping()
     public ResponseEntity<Void> update(@RequestBody WishRequest wishRequest, @LoginMember Long memberId) {
+        wishService.update(memberId, wishRequest);
         if (wishRequest.quantity() == 0) {
-            wishRepository.deleteByMemberIdAndProductId(memberId, wishRequest.productId());
             return ResponseEntity.noContent().build();
         }
-        Wish wish = entityManager.find(Wish.class, new WishId(memberId, wishRequest.productId()));
-        wish.setQuantity(wishRequest.quantity());
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping()
     public ResponseEntity<Void> delete(@RequestBody WishRequest wishRequest, @LoginMember Long memberId) {
-        wishRepository.deleteByMemberIdAndProductId(memberId, wishRequest.productId());
+        wishService.delete(memberId, wishRequest);
         return ResponseEntity.noContent().build();
     }
 }
