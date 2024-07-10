13b51bc5eaf68d1ca523ec9d3b2387158fd09733
wvssm <neoramiregi@gmail.com>
2024-07-10 16:05:47 +0900

    feat(Messages): 멤버 조회 실패시 나타나는 메시지 추가

--- src/main/java/gift/constants/Messages.java
+++ src/main/java/gift/constants/Messages.java
@@ -4,4 +4,5 @@ public class Messages {
     public static final String NOT_FOUND_PRODUCT_BY_NAME = "해당 이름의 상품이 존재하지 않습니다.";
     public static final String NOT_FOUND_WISH = "위시가 존재하지 않습니다.";
     public static final String NOT_FOUND_PRODUCT_BY_ID= "해당 ID의 상품이 존재하지 않습니다.";
+    public static final String NOT_FOUND_MEMBER = "해당 정보를 가진 회원이 존재하지 않습니다.";
 }
