d393b2630100c04a6822e5f0d5b0ade608435593
Dalsungmin <cnww123@naver.com>
2024-07-10 19:48:57 +0900

    Feat: Product 서비스와 연결
    서비스와 연결하면서 자료형 엔티티로 변환하고 html 링크로 반환하던 리턴 값을 엔티티로 변경

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -5,6 +5,10 @@ import gift.exception.ProductNotFoundException;
 import gift.model.Product;
 import gift.service.ProductService;
 import jakarta.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -20,38 +24,41 @@ public class ProductController {
     }
 
     @GetMapping
-    public String getProducts(Model model) {
-        model.addAttribute("products", productService.getAllProducts());
-        model.addAttribute("product", new Product());
-        return "product-list";
+    public ResponseEntity<List<Product>> getProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
     }
 
     @PostMapping("/add")
+    public ResponseEntity<Product> addProduct(@RequestBody @Valid Product product, RedirectAttributes redirectAttributes) {
         Product addedProduct = productService.addProduct(product);
+        return ResponseEntity.ok(addedProduct);
     }
 
     //@PostMapping("/{id}")
 
+    public ResponseEntity<Product> updateProduct(@Valid @PathVariable Long id, @RequestBody Product product, RedirectAttributes redirectAttributes) {
         productService.updateProduct(id, product);
+        return ResponseEntity.ok(product);
     }
 
     @PostMapping("/delete/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable("id") Long id) {
         productService.deleteProduct(id);
+        return ResponseEntity.ok().body("delete complete!");
     }
 
     @GetMapping("/view/{id}")
+    public ResponseEntity<Product> getProductDetails(@PathVariable("id") Long id, Model model, RedirectAttributes redirectAttributes) {
         Optional<Product> product = productService.getProductById(id);
         model.addAttribute("product", product);
+        return product.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
 
     }
 
     @GetMapping("/{id}")
-    @ResponseBody
-    public Product getProductById(@PathVariable("id") Long id) {
-        try {
-            return productService.getProductById(id);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Product not found: " + e.getMessage());
-        }
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Optional<Product> product = productService.getProductById(id);
+        return product.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
