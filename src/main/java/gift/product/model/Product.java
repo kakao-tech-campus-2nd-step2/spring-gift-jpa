3f30588d8722cb1087d8db4f4937f9937bda3513
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-07-11 01:57:26 +0900

    refactor: validation 삭제

--- src/main/java/gift/product/model/Product.java
+++ src/main/java/gift/product/model/Product.java
@@ -7,9 +7,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "product")
@@ -18,12 +15,9 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @NotBlank(message = "상품의 이름은 필수 항목입니다.")
-    @Size(max = 15, message = "상품의 이름은 최대 15자까지 입력할 수 있습니다.")
     private String name;
 
     @Column(nullable = false)
-    @Min(value = 0, message = "상품의 가격은 0보다 크거나 같아야 합니다.")
     private int price;
 
     @Column(nullable = false)
