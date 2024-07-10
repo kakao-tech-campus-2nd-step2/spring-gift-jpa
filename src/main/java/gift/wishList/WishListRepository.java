e152069a026fe28774e79566af8e7e3d0f8dbc3f
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-10 11:20:05 +0900

    fix : WishList Repository findByUserID -> findByUser

--- src/main/java/gift/wishList/WishListRepository.java
+++ src/main/java/gift/wishList/WishListRepository.java
@@ -1,5 +1,6 @@
 package gift.wishList;
 
+import gift.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface WishListRepository extends JpaRepository<WishList, Long> {
-    public List<WishList> findByUserID(Long userID);
+    public List<WishList> findByUser(User user);
 }
