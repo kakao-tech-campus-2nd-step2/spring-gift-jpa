60d1fff5c4f0e2202ec6246f81cdb3f135cb2797
suno-boy <tnsgh3691@gmail.com>
2024-07-10 19:04:57 +0900

    refactor: ProductRepository에 findByname메서드 추가

--- src/main/java/gift/Repository/ProductRepository.java
+++ src/main/java/gift/Repository/ProductRepository.java
@@ -4,7 +4,9 @@ import gift.Entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-    // 필요한 경우 커스텀 쿼리 메서드 추가 가능
+    Optional<ProductEntity> findByName(String name);
 }
