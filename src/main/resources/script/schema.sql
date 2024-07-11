c0fe5a51845206faeb96407c9775b651ebeeb716
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-12 04:33:19 +0900

    feat: 초기 데이터 sql 스크립트 수정

--- src/main/resources/script/schema.sql
+++ src/main/resources/script/schema.sql
@@ -3,24 +3,32 @@ CREATE TABLE product (
                           name VARCHAR(255) NOT NULL,
                           price INT NOT NULL CHECK (price >= 0),
                           image_url TEXT,
-                          is_active BOOLEAN DEFAULT TRUE
+                          user_id BIGINT NOT NULL,
+                          is_active BOOLEAN NOT NULL DEFAULT true,
+                          reg_date TIMESTAMP NOT NULL DEFAULT NOW(),
+                          mod_date TIMESTAMP NOT NULL DEFAULT NOW(),
+                          FOREIGN KEY (user_id) REFERENCES app_user(id)
 )engine=InnoDB;
 
 CREATE TABLE app_user (
                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
                       email VARCHAR(255) NOT NULL UNIQUE,
                       password VARCHAR(100) NOT NULL,
-                      is_active BOOLEAN NOT NULL DEFAULT TRUE,
+                      is_active BOOLEAN NOT NULL DEFAULT true,
                       role VARCHAR(50) NOT NULL DEFAULT 'USER',
-                      salt VARCHAR(255)
+                      salt VARCHAR(255),
+                      reg_date TIMESTAMP NOT NULL DEFAULT NOW(),
+                      mod_date TIMESTAMP NOT NULL DEFAULT NOW()
 )engine=InnoDB;
 
 CREATE TABLE wish (
                     id BIGINT AUTO_INCREMENT PRIMARY KEY,
                     user_id BIGINT NOT NULL,
                     product_id BIGINT NOT NULL,
-                    quantity INT DEFAULT 1,
+                    quantity INT NOT NULL DEFAULT 1,
                     is_active BOOLEAN DEFAULT TRUE,
+                    reg_date TIMESTAMP NOT NULL DEFAULT NOW(),
+                    mod_date TIMESTAMP NOT NULL DEFAULT NOW(),
                     FOREIGN KEY (user_id) REFERENCES AppUser(id),
                     FOREIGN KEY (product_id) REFERENCES Product(id)
 )engine=InnoDB;
