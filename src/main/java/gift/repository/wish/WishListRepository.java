0e5c7e6e4bd520b5207909b39eaab7b81e324fa6
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-11 14:49:47 +0900

    refactor : Jpa 메서드 추가

--- src/main/java/gift/repository/wish/WishListRepository.java
+++ src/main/java/gift/repository/wish/WishListRepository.java
@@ -1,11 +1,18 @@
 package gift.repository.wish;
 
-import gift.domain.WishList;
+import gift.model.wishlist.WishList;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WishListRepository extends JpaRepository<WishList, Long> {
+    List<WishList> findAllByUserId(Long userId);
+    Optional<WishList> findByIdAndUserId(Long wishId, Long userId);
+    Optional<WishList> findByUserIdAndAndProductId(Long userId, Long ProductId);
 
+
+    void deleteByUserIdAndAndProductId(Long userId, Long productId);
 }
 
