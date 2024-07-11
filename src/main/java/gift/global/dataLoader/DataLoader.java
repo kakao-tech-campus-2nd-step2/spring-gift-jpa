94daee0093e7ff887b40b1e2c895e3a5f37b0040
donghyuun <bomul0524@gmail.com>
2024-07-12 04:31:23 +0900

    refactor: 초기 데이터 입력 방식 변경 (CommandRunner -> @PostConstruct)

--- src/main/java/gift/global/dataLoader/DataLoader.java
+++ src/main/java/gift/global/dataLoader/DataLoader.java
@@ -1,31 +1,32 @@
-package gift.global.commandLineRunner;
+package gift.global.dataLoader;
 
 import gift.domain.cart.CartItem;
 import gift.domain.cart.JpaCartItemRepository;
 import gift.domain.product.JpaProductRepository;
 import gift.domain.product.Product;
-import gift.domain.user.User;
 import gift.domain.user.JpaUserRepository;
-import org.springframework.boot.CommandLineRunner;
+import gift.domain.user.User;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 @Profile("!test") // 테스트 환경에서는 동작하지 않도록
-public class DataLoader implements CommandLineRunner {
+public class DataLoader {
 
     private final JpaProductRepository jpaProductRepository;
     private final JpaUserRepository jpaUserRepository;
-
     private final JpaCartItemRepository jpaCartItemRepository;
+    @Autowired
     public DataLoader(JpaProductRepository jpaProductRepository, JpaUserRepository jpaUserRepository, JpaCartItemRepository jpaCartItemRepository) {
         this.jpaProductRepository = jpaProductRepository;
         this.jpaUserRepository = jpaUserRepository;
         this.jpaCartItemRepository = jpaCartItemRepository;
     }
 
-    @Override
-    public void run(String... args) throws Exception {
+    @PostConstruct
+    public void init() {
         // Product
         Product americano = new Product("아이스 아메리카노 T", 4500, "https://example.com/image.jpg");
         Product cafuchino = new Product("아이스 카푸치노 M", 4700, "https://example.com/image.jpg");
