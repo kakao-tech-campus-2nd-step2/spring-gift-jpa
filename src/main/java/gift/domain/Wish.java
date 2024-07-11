a1a5bb65c73fd41bb20e0e41051df858d9cda398
wvssm <neoramiregi@gmail.com>
2024-07-11 22:32:18 +0900

    feat(Wish): OneToMany 양방향 관계 설정 - Lazy Fetch 적용 - 테스트를 위해 equals, hashCode 함수 상속

--- src/main/java/gift/domain/Wish.java
+++ src/main/java/gift/domain/Wish.java
@@ -2,6 +2,8 @@ package gift.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name="wish")
 public class Wish {
@@ -12,12 +14,26 @@ public class Wish {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
     private Product product;
 
     @Column(name = "quantity", nullable = false)
     private int quantity;
+
+    // 테스트 시 유용
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Wish wish = (Wish) o;
+        return quantity == wish.quantity && Objects.equals(id, wish.id) && Objects.equals(member, wish.member) && Objects.equals(product, wish.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, member, product, quantity);
+    }
 
     protected Wish () {
     }
@@ -49,5 +65,15 @@ public class Wish {
 
     public int getQuantity() {
         return quantity;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+        member.getWishes().add(this);
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+        product.getWishes().add(this);
     }
 }
