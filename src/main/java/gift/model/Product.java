63bc1e31a9a1385595d71b9cd2eb0f2a460619b0
suhyeon7497 <nucc1186@gmail.com>
2024-07-11 20:43:05 +0900

    refactor: 의미 없는 어노테이션 제거

--- src/main/java/gift/model/Product.java
+++ src/main/java/gift/model/Product.java
@@ -7,10 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "product")
 public class Product {
 
     @Id
@@ -19,7 +17,7 @@ public class Product {
     @Column(name = "name", nullable = false)
     private String name;
     @Column(name = "price", nullable = false)
-    private int price;
+    private Integer price;
     @Column(name = "image_url", nullable = false)
     private String imageUrl;
 
@@ -46,7 +44,7 @@ public class Product {
         return name;
     }
 
-    public int getPrice() {
+    public Integer getPrice() {
         return price;
     }
 
--- src/main/java/gift/model/Wish.java
+++ src/main/java/gift/model/Wish.java
@@ -7,10 +7,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "wish")
 public class Wish {
 
     @Id
@@ -27,7 +25,6 @@ public class Wish {
     private Integer count;
 
     protected Wish() {
-
     }
 
     public Wish(Integer count) {
