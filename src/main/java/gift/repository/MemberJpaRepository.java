a9ceae2afadd89f3b09a810f0a8b6b378a06fef8
나제법 <skwpqjq@gmail.com>
2024-07-11 15:38:34 +0900

    feat: MemberJpaRepository.java 추가

---
+++ src/main/java/gift/repository/MemberJpaRepository.java
@@ -0,0 +1,14 @@
+package gift.repository;
+
+import gift.domain.Member;
+import gift.domain.vo.Email;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberJpaRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(Email email);
+
+}
