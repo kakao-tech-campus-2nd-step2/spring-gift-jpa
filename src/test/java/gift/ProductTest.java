9bd53efadb9cea604cc2edabb4cfe53b93b68a30
J-1ac <tkdwns26@naver.com>
2024-07-11 17:30:42 +0900

    style: Product, User 테스트 파일명 변경

--- src/test/java/gift/ProductTest.java
+++ src/test/java/gift/ProductTest.java
@@ -10,7 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-public class ProductRepositoryTest {
+public class ProductTest {
 
     @Autowired
     private TestEntityManager entityManager;
--- src/test/java/gift/UserTest.java
+++ src/test/java/gift/UserTest.java
@@ -10,7 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-public class UserRepositoryTest {
+public class UserTest {
 
     @Autowired
     private TestEntityManager entityManager;
