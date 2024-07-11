b7db89bbc3e4ad771785d85d1093ab8415997df4
신예준 <dpwns1826@gmail.com>
2024-07-11 17:42:21 +0900

    feat: Entity 추가

--- src/main/java/gift/domain/WishList/WishList.java
+++ src/main/java/gift/domain/WishList/WishList.java
@@ -1,12 +1,42 @@
 package gift.domain.WishList;
 
-import gift.domain.product.Product;
-import java.util.ArrayList;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-public record WishList(
-    long id,
-    long userId,
-    ArrayList<Product> products
-) {
+@Entity
+public class WishList {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private Long productId;
+
+    public WishList() {
+        
+    }
+
+    public WishList(Long memberId, long productId) {
+        this.memberId = memberId;
+        this.productId = productId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
 }
