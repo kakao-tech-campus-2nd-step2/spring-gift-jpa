2d0ffd9f9a101d86130372a391ad3ce4ba0d642d
bokyeong <wntjdals0412@gmail.com>
2024-07-12 03:12:05 +0900

    Refactor: solver에서 UserDetail DTO 반환

--- src/main/java/gift/util/JwtUtil.java
+++ src/main/java/gift/util/JwtUtil.java
@@ -1,7 +1,5 @@
 package gift.util;
 
-import gift.authentication.UserDetails;
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,34 +11,21 @@ import java.util.Map;
 
 @Component
 public class JwtUtil {
-    @Value ("${jwt.secret}")
+    @Value("${jwt.secret}")
     private String secretKey;
 
     @Value("${jwt.expiredMs}")
     private Long expirationTime;
 
-    public String generateToken(Long id,String email) {
-        Map<String,Object> claims = new HashMap<>();
-        claims.put("id",id);
-        claims.put("email",email);
+    public String generateToken(Long id, String email) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", id);
+        claims.put("email", email);
         return Jwts.builder()
-                .setSubject(id.toString())
-                .setSubject(email)
+                .setClaims(claims)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
-    }
-
-    public UserDetails getUserDetail(String token){
-        Claims claims = Jwts.parser()
-                .setSigningKey(secretKey).build()
-                .parseClaimsJws(token)
-                .getBody();
-        Long id = claims.get("id", Long.class);
-        String email = claims.get("email", String.class);
-        return new UserDetails(id,email);
-
-        //이 부분을 resolver..?
     }
 }
