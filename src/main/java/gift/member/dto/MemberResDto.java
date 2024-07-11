2e47a5d82bcd90bfd76197ff404ee75222f64175
Profile-exe <kang67572346@gmail.com>
2024-07-11 10:26:13 +0900

    feat(MemberResDto): 회원 응답에 위시 리스트 목록 추가

--- src/main/java/gift/member/dto/MemberResDto.java
+++ src/main/java/gift/member/dto/MemberResDto.java
@@ -1,13 +1,18 @@
 package gift.member.dto;
 
 import gift.member.entity.Member;
+import gift.wishlist.dto.WishListResDto;
+import java.util.List;
 
 public record MemberResDto(
         Long id,
-        String email
+        String email,
+        List<WishListResDto> wishLists
 ) {
 
     public MemberResDto(Member member) {
-        this(member.getId(), member.getEmail());
+        this(member.getId(), member.getEmail(), member.getWishLists().stream()
+                .map(WishListResDto::new)
+                .toList());
     }
 }
