eaa20cfa9583d803d60a22bd883d00acfb992ecc
test1 <test>
2024-07-11 18:06:23 +0900

    feat : 필요없는 함수 제거

--- src/main/java/gift/Repository/UserDao.java
+++ src/main/java/gift/Repository/UserDao.java
@@ -5,6 +5,5 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDao extends JpaRepository<UserEntity, Long> {
-
   Optional<UserEntity> findByEmail(String email);
 }
