a8f70c34da41e0b239f8f255ba9f8e2f49d849af
Kiwoong Kim <gw150311@naver.com>
2024-07-12 03:09:29 +0900

    feat(WishlistRepository): Jdbc Template -> JPA 리팩토링
    - removeWishlist 로직 예외 추가

--- src/main/java/gift/repository/WishlistRepository.java
+++ src/main/java/gift/repository/WishlistRepository.java
@@ -1,40 +1,15 @@
 package gift.repository;
 
+import gift.DTO.Wishlist;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class WishlistRepository {
+public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
 
-    private JdbcTemplate jdbcTemplate;
+    Optional<Wishlist> findByEmailAndProductId(String email, Long productId);
 
-    private static final String SELECT_ALL_PRODUCTS_BY_EMAIL_SQL
-        = "SELECT product_id FROM wishlist WHERE email = ?";
-    private static final String INSERT_PRODUCT_TO_WISHLIST_SQL
-        = "INSERT INTO wishlist (email, product_id) VALUES (?, ?)";
-    private static final String DELETE_PRODUCT_FROM_WISHLIST_SQL
-        = "DELETE FROM wishlist WHERE email = ? AND product_id = ?";
-
-    @Autowired
-    public WishlistRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public List<Long> findProductIdsByUserEmail(String email) {
-        return jdbcTemplate.query(
-            SELECT_ALL_PRODUCTS_BY_EMAIL_SQL,
-            new Object[]{email},
-            (rs, rowNum) -> rs.getLong("product_id")
-        );
-    }
-
-    public void addProductToWishlist(String email, Long productId) {
-        jdbcTemplate.update(INSERT_PRODUCT_TO_WISHLIST_SQL, email, productId);
-    }
-
-    public void removeProductFromWishlist(String email, Long productId) {
-        jdbcTemplate.update(DELETE_PRODUCT_FROM_WISHLIST_SQL, email, productId);
-    }
+    List<Wishlist> findByEmail(String email);
 }
