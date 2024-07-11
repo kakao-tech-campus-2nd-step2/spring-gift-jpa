7826d9a6222b2fdee99cc9c23fe53af9b789a9dd
ariimo <159447466+ariimo@users.noreply.github.com>
2024-07-11 16:45:42 +0900

    refactor: Product Entity 수정

--- src/main/java/gift/domain/Product.java
+++ src/main/java/gift/domain/Product.java
@@ -8,18 +8,19 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "products")
+@Table(name = "product")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 15)
     private String name;
 
     @Column(nullable = false)
     private double price;
 
+    @Column(nullable = false)
     private String imageUrl;
 
     public Product() {}
