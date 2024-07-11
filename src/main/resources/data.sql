c530f6a37ea5a9a41138c220056fc7e91a988638
seoyoung-dev <seoyoung.dev@gmail.com>
2024-07-11 23:18:35 +0900

    feat: 초기 정보 저장

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into members (email, password) values ('testuser@gmail.com', 'test123');
+insert into products (name, price, image_url) values ('1번쨰 상품', 1000, 'http://1');
+insert into products (name, price, image_url) values ('2번쨰 상품', 2000, 'http://2');
+insert into products (name, price, image_url) values ('3번쨰 상품', 1000, 'http://3');
