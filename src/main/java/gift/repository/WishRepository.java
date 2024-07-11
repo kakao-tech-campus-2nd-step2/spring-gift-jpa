1dff1335b5b3cc381258800a8f91319f5a06a5ce
rdme0 <we6610@gmail.com>
2024-07-11 21:03:51 +0900

    :sparkles: feat: 페이지네이션 구현

--- src/main/java/gift/repository/WishRepository.java
+++ src/main/java/gift/repository/WishRepository.java
@@ -4,10 +4,13 @@ import gift.entity.Product;
 import gift.entity.Member;
 import gift.entity.Wish;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WishRepository extends JpaRepository<Wish, Long> {
     Optional<Wish> findByMemberAndProduct(Member member, Product product);
     void deleteByMemberAndProduct_Id(Member member, Long productId);
     void deleteByProduct_Id(Long productId);
+    Page<Wish> findByMember(Member member, Pageable pageable);
 }
