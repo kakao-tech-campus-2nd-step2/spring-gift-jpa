172104e9103e6474027bb3c10dcbfeae26d5446b
bokyeong <wntjdals0412@gmail.com>
2024-07-12 03:16:33 +0900

    Feat: Wish JPA 적용

--- src/main/java/gift/service/WishService.java
+++ src/main/java/gift/service/WishService.java
@@ -2,10 +2,14 @@ package gift.service;
 
 import gift.domain.member.MemberRepository;
 import gift.domain.product.ProductRepository;
+import gift.domain.wish.Wish;
 import gift.domain.wish.WishRepository;
 import gift.dto.WishAddRequestDto;
 import gift.dto.WishResponseDto;
+import gift.exception.CustomException;
+import gift.exception.ErrorCode;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,33 +25,38 @@ public class WishService {
         this.productRepository = productRepository;
     }
 
+    @Transactional
     public void addWish(Long memberId, WishAddRequestDto request) {
         checkProductValidation(request.getProductId());
-        wishRepository.addWish(memberId, request);
+        Wish wish = wishRepository.findDistinctByMemberIdAndProductId(memberId, request.getProductId())
+                .map(w -> {
+                    w.setQuantity(w.getQuantity() + request.getQuantity());
+                    return w;
+                })
+                .orElseGet(() -> new Wish(memberId, request.getProductId(), request.getQuantity()));
+
+        wishRepository.save(wish);
     }
 
     public List<WishResponseDto> getAllWishes(Long id) {
         checkMemberValidation(id);
-        return wishRepository.getAllWishes(id)
-                .stream()
-                .map(WishResponseDto::new)
-                .toList();
+        return wishRepository.findAllByMemberId(id).stream().map(WishResponseDto::new).toList();
     }
 
+    @Transactional
     public void deleteWish(Long memberId, Long productId) {
         checkMemberValidation(memberId);
         checkProductValidation(productId);
-        wishRepository.deleteWish(memberId,productId);
+        wishRepository.deleteByMemberIdAndProductId(memberId, productId);
     }
 
     private void checkProductValidation(Long productId) {
-        if (productRepository.isNotExistProductId(productId)) {
-            throw new IllegalArgumentException("유효하지 않은 상품 정보입니다.");
-        }
+        productRepository.findById(productId)
+                .orElseThrow(() -> new CustomException(ErrorCode.INVALID_PRODUCT));
     }
 
     private void checkMemberValidation(Long id) {
-        if (memberRepository.isNotExistMemberById(id)) {
+        if (!memberRepository.existsById(id)) {
             throw new IllegalArgumentException("유효하지 않은 회원 정보입니다.");
         }
     }
