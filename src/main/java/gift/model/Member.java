60bb5b5639488720c0fe5c0c1a4deb3c3ffa4a95
Dalsungmin <cnww123@naver.com>
2024-07-11 22:50:13 +0900

    Feat: OneToMany 구현을 위해 위시리스트와 연결
    멤버ID와 위시리스트의 OneToMany 구현을 위해 어노테이션 추가

--- src/main/java/gift/model/Member.java
+++ src/main/java/gift/model/Member.java
@@ -1,12 +1,16 @@
 package gift.model;
 
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "member_table")
@@ -20,6 +24,10 @@ public class Member {
 
     @Column(nullable = false)
     private String password;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<WishList> wishlists = new ArrayList<>();
+
 
 
     public Member() {
