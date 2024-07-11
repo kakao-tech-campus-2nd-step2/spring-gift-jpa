16fa46f6014ef0db7679a142caca78c13cef29ac
donghyuun <bomul0524@gmail.com>
2024-07-11 22:02:42 +0900

    test(CastItem): 테스트 코드 리팩토링

--- src/test/java/gift/JpaDataTest/CartItemRepositoryTest.java
+++ src/test/java/gift/JpaDataTest/CartItemRepositoryTest.java
@@ -10,6 +10,7 @@ import gift.domain.user.User;
 import gift.domain.user.JpaUserRepository;
 import java.util.List;
 import jdk.jfr.Description;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,23 +29,32 @@ public class CartItemRepositoryTest {
     @Autowired
     private JpaProductRepository productRepository;
 
-    @Test
-    @Description("장바구니 조회")
-    void getCartItems() {
-        // given
+    private User user;
+    private Product product1;
+    private Product product2;
+
+    @BeforeEach
+    void setUp() {
         User user = new User("minji@example.com", "password1");
         User savedUser = userRepository.saveAndFlush(user);
+        this.user = savedUser;
 
         Product product1 = new Product("아이스 아메리카노 T", 4500, "https://example.com/image.jpg");
         Product product2 = new Product("아이스 카페모카 M", 4700, "https://example.com/image.jpg");
+        Product savedProduct1 = productRepository.save(product1);
+        Product savedProduct2 = productRepository.save(product2);
+        this.product1 = savedProduct1;
+        this.product2 = savedProduct2;
+    }
 
-        productRepository.saveAndFlush(product1);
-        productRepository.saveAndFlush(product2);
 
+    @Test
+    @Description("장바구니 조회")
+    void getCartItems() {
+        // when
         cartItemRepository.save(new CartItem(user, product1));
         cartItemRepository.save(new CartItem(user, product2));
-        // when
-        List<CartItem> cartItems = cartItemRepository.findAllByUserId(savedUser.getId());
+        List<CartItem> cartItems = cartItemRepository.findAllByUserId(user.getId());
         // then
         assertThat(cartItems.size()).isEqualTo(2);
     }
@@ -52,39 +62,23 @@ public class CartItemRepositoryTest {
     @Test
     @Description("장바구니 추가")
     void addCartItem() {
-        // given
-        User user = new User("minji@example.com", "password1");
-        User savedUser = userRepository.saveAndFlush(user);
-
-        Product product = new Product("아이스 아메리카노 T", 4500, "https://example.com/image.jpg");
-        Product savedProduct = productRepository.saveAndFlush(product);
-
-        cartItemRepository.save(new CartItem(user, product));
         // when
-        List<CartItem> cartItems = cartItemRepository.findAllByUser(savedUser);
+        cartItemRepository.save(new CartItem(user, product1));
+        List<CartItem> cartItems = cartItemRepository.findAllByUser(user);
         // then
         assertThat(cartItems.size()).isEqualTo(1);
         CartItem cartItem = cartItems.get(0);
-        System.out.println("cartItem.getUser() = " + cartItem.getUser());
-        System.out.println("savedUser = " + savedUser);
-        assertThat(cartItem.getUser()).isEqualTo(savedUser);
-        assertThat(cartItem.getProduct()).isEqualTo(savedProduct);
+        assertThat(cartItem.getUser()).isEqualTo(user);
+        assertThat(cartItem.getProduct()).isEqualTo(product1);
     }
 
     @Test
     @Description("장바구니 삭제")
     void deleteCartItem() {
-        // given
-        User user = new User("minji@example.com", "password1");
-        User savedUser = userRepository.saveAndFlush(user);
-
-        Product product = new Product("아이스 아메리카노 T", 4500, "https://example.com/image.jpg");
-        Product savedProduct = productRepository.saveAndFlush(product);
-
-        cartItemRepository.save(new CartItem(savedUser, savedProduct));
         // when
-        cartItemRepository.deleteByUserIdAndProductId(savedUser.getId(), product.getId());
-        List<CartItem> cartItems = cartItemRepository.findAllByUserId(savedUser.getId());
+        cartItemRepository.save(new CartItem(user, product1));
+        cartItemRepository.deleteByUserIdAndProductId(user.getId(), product1.getId());
+        List<CartItem> cartItems = cartItemRepository.findAllByUserId(user.getId());
         // then
         assertThat(cartItems.size()).isEqualTo(0);
 
