861afdaaa4a93a769b6827ec11e113fa052658ee
Kangboom <jkl6147@naver.com>
2024-07-11 15:14:25 +0900

    feat(entity): add MemberEntity

---
+++ src/main/java/gift/entity/MemberEntity.java
@@ -0,0 +1,48 @@
+package gift.entity;
+
+import gift.domain.Member;
+import gift.service.MemberService;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name ="member")
+public class MemberEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column
+    private String password;
+
+    public MemberEntity(){}
+
+    public MemberEntity(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Member toMember(){
+        return new Member(this.id, this.email, this.password);
+    }
+}
