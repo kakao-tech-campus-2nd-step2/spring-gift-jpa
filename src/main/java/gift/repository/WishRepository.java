3f7d12318e76cb25e946bf1fdee0f025d9d55130
wvssm <neoramiregi@gmail.com>
2024-07-12 03:50:21 +0900

    refactor(WishRepository): Optional 삭제

--- src/main/java/gift/repository/WishRepository.java
+++ src/main/java/gift/repository/WishRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface WishRepository extends JpaRepository<Wish, Long> {
-    Optional<List<Wish>> findByMemberId(Long memberId);
+    List<Wish> findByMemberId(Long memberId);
     Optional<Wish> findByIdAndMemberId(Long id, Long memberId);
 }
