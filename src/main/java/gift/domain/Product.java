50faa6a6e1c9e71d7613e83376c38ca0a42afd32
박민규 <pmq0101@naver.com>
2024-07-11 20:11:11 +0900

    feat(Product): length= 15 추가

--- src/main/java/gift/domain/Product.java
+++ src/main/java/gift/domain/Product.java
@@ -14,7 +14,7 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
+    @Column(nullable = false, length = 15)
     private String name;
     @Column(nullable = false)
     private int price;
