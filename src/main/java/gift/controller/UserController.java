d0ba5e11e6f744eaffca9e4ed65e3ab5a87e5277
Giheon Do <dgh0001@naver.com>
2024-07-11 09:55:15 +0900

    refactor: UserController에서 UserService로 로직 분리

--- src/main/java/gift/controller/UserController.java
+++ src/main/java/gift/controller/UserController.java
@@ -4,6 +4,7 @@ import gift.model.AccessTokenDO;
 import gift.model.User;
 import gift.model.UserDTO;
 import gift.repository.UserRepository;
+import gift.service.UserService;
 import gift.util.UserUtility;
 import io.jsonwebtoken.Claims;
 import jakarta.validation.Valid;
@@ -21,11 +22,13 @@ public class UserController {
 
     private final UserRepository userRepository;
     private final UserUtility userUtility;
+    private final UserService userService;
 
     @Autowired
-    public UserController(UserRepository userRepository, UserUtility userUtility) {
+    public UserController(UserRepository userRepository, UserUtility userUtility, UserService userService) {
         this.userRepository = userRepository;
         this.userUtility = userUtility;
+        this.userService = userService;
     }
 
     @PostMapping("/signup")
@@ -37,26 +40,8 @@ public class UserController {
 
     @PostMapping("/login")
     public ResponseEntity<Object> login(@RequestBody @Valid UserDTO user) {
-        Optional<User> result = userRepository.findByEmail(user.getEmail());
-        if (!result.isPresent())
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Email does not exist");
-        User foundUser = result.get();
-        if (!user.getPassword().equals(foundUser.getPassword()))
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Password does not match");
-        String accessToken = userUtility.makeAccessToken(foundUser);
+        String accessToken = userService.login(user);
         return ResponseEntity.ok().body(userUtility.accessTokenToObject(accessToken));
-    }
-
-    @PostMapping("/authenticate")
-    public ResponseEntity<Object> authenticate(@RequestBody AccessTokenDO accessToken) {
-        Claims claims = userUtility.tokenParser(accessToken.getAccessToken());
-        if (claims == null) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid token");
-        String email = claims.get("email", String.class);
-        if (email == null) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid token");
-        Optional<User> result = userRepository.findByEmail(email);
-        if (!result.isPresent())
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Email does not exist");
-        return ResponseEntity.ok().body(userUtility.emailToObject(email));
     }
 
     @ExceptionHandler(ResponseStatusException.class)
---
+++ src/main/java/gift/service/UserService.java
@@ -0,0 +1,35 @@
+package gift.service;
+
+import gift.model.User;
+import gift.model.UserDTO;
+import gift.repository.UserRepository;
+import gift.util.UserUtility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+    private UserUtility userUtility;
+
+    @Autowired
+    public UserService(UserRepository userRepository, UserUtility userUtility) {
+        this.userRepository = userRepository;
+        this.userUtility = userUtility;
+    }
+
+    public String login(UserDTO user){
+        Optional<User> result = userRepository.findByEmail(user.getEmail());
+        if (!result.isPresent())
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Email does not exist");
+        User foundUser = result.get();
+        if (!user.getPassword().equals(foundUser.getPassword()))
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Password does not match");
+        return userUtility.makeAccessToken(foundUser);
+    }
+}
