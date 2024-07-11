2844c18bcda21885111c6f7649ef4bc51761773c
peacefullyquietly <pjh73015@gmail.com>
2024-07-11 16:13:17 +0900

    refactor : MemberRepository 클래스 수정에 따른 변경 사항

--- src/main/java/gift/service/MemberService.java
+++ src/main/java/gift/service/MemberService.java
@@ -1,26 +1,23 @@
 package gift.service;
 
+import gift.entity.MemberEntity;
 import gift.model.Member;
 import gift.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-
 public class MemberService {
 
     private final MemberRepository memberRepository;
-    private final JwtUtil jwtUtil;
 
     @Autowired
-    public MemberService(MemberRepository memberRepository, JwtUtil jwtUtil) {
+    public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
-        this.jwtUtil = jwtUtil;
     }
 
-
-    public Member authenticateToken(Member member) {
-        Member foundMember = memberRepository.findByEmail(member.getEmail());
+    public MemberEntity authenticateToken(Member member) {
+        MemberEntity foundMember = memberRepository.findByEmail(member.getEmail());
 
         if (foundMember == null || !member.getPassword().equals(foundMember.getPassword())) {
             return null;
@@ -34,12 +31,13 @@ public class MemberService {
             return MemberServiceStatus.EMAIL_ALREADY_EXISTS;
         }
 
-        memberRepository.save(member);
+        // DTO to Entity
+        MemberEntity memberEntity = new MemberEntity(member.getEmail(), member.getPassword());
+        memberRepository.save(memberEntity);
         return MemberServiceStatus.SUCCESS;
     }
 
     public boolean existsByEmail(String email) {
-        Member member = memberRepository.findByEmail(email);
-        return member != null;
+        return memberRepository.existsByEmail(email);
     }
 }
