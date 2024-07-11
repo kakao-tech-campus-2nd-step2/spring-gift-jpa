c2d65b09de17ed4ed8360b88540501b86dc51e51
배민수 <duecost@gmail.com>
2024-07-11 17:25:31 +0900

    test: WishRepository 테스트 코드 수정

--- src/test/java/gift/WishRepositoryTest.java
+++ src/test/java/gift/WishRepositoryTest.java
@@ -3,7 +3,9 @@ package gift;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
+import gift.product.model.Member;
 import gift.product.model.Wish;
+import gift.product.repository.AuthRepository;
 import gift.product.repository.WishRepository;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -16,42 +18,53 @@ class WishRepositoryTest {
     @Autowired
     WishRepository wishRepository;
 
+    @Autowired
+    AuthRepository authRepository;
+
     @Test
     void 위시리스트_항목_추가() {
-        Wish wish = new Wish(1L, 1L);
+        Member member = new Member("test@test.com", "1234");
+        member = authRepository.save(member);
+        Wish wish = new Wish(member, 1L);
         Wish insertedWish = wishRepository.save(wish);
 
         assertSoftly(softly -> {
             assertThat(insertedWish.getId()).isNotNull();
-            assertThat(insertedWish.getMemberId()).isEqualTo(1L);
+            assertThat(insertedWish.getMember().getId()).isNotNull();
             assertThat(insertedWish.getProductId()).isEqualTo(1L);
         });
     }
 
     @Test
     void 위시리스트_전체_조회() {
-        wishRepository.save(new Wish(1L, 1L));
-        wishRepository.save(new Wish(1L, 2L));
+        Member member = new Member("test@test.com", "1234");
+        member = authRepository.save(member);
+        wishRepository.save(new Wish(member, 1L));
+        wishRepository.save(new Wish(member, 2L));
 
-        List<Wish> wishes = wishRepository.findAllByMemberId(1L);
+        List<Wish> wishes = wishRepository.findAllByMemberId(member.getId());
 
         assertThat(wishes).hasSize(2);
     }
 
     @Test
     void 위시리스트_조회() {
-        wishRepository.save(new Wish(1L, 1L));
+        Member member = new Member("test@test.com", "1234");
+        member = authRepository.save(member);
+        wishRepository.save(new Wish(member, 1L));
 
-        boolean isPresentWish = wishRepository.findByIdAndMemberId(1L, 1L).isPresent();
+        boolean isPresentWish = wishRepository.findByIdAndMemberId(1L, member.getId()).isPresent();
 
         assertThat(isPresentWish).isTrue();
     }
 
     @Test
     void 위시리스트_항목_삭제() {
-        wishRepository.save(new Wish(1L, 1L));
-        wishRepository.deleteById(1L);
-        boolean isPresentWish = wishRepository.findByIdAndMemberId(1L, 1L).isPresent();
+        Member member = new Member("test@test.com", "1234");
+        member = authRepository.save(member);
+        Wish wish = wishRepository.save(new Wish(member, 1L));
+        wishRepository.deleteById(wish.getId());
+        boolean isPresentWish = wishRepository.findByIdAndMemberId(wish.getId(), member.getId()).isPresent();
 
         assertThat(isPresentWish).isFalse();
     }
