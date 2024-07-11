ba91daad7633363c156065ae91d22e240ccf0326
peacefullyquietly <pjh73015@gmail.com>
2024-07-11 16:09:01 +0900

    refactor : 기존 jdbc에서 JPA 사용으로 변경됨에 따른 수정 사항

--- src/main/java/gift/repository/WishListRepository.java
+++ src/main/java/gift/repository/WishListRepository.java
@@ -1,37 +1,10 @@
 package gift.repository;
 
-import gift.model.Product;
-import gift.model.WishList;
+import gift.entity.WishListEntity;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-public class WishListRepository {
-    @Autowired
-    JdbcTemplate jdbcTemplate;
-
-    public List<Long> readWishList(Long userId) {
-        String sql = "SELECT product_id FROM wishlist WHERE user_id = ?";
-        return jdbcTemplate.queryForList(sql, Long.class, userId);
-    }
-
-
-    public void addProductToWishList(Long userId, Long productId) {
-        String sql = "INSERT INTO wishlist (product_id, user_id) VALUES (?, ?)";
-        jdbcTemplate.update(sql, productId, userId);
-    }
-
-    public void removeWishList(Long userId) {
-        String sql = "DELETE FROM wishlist WHERE user_id = ?";
-        jdbcTemplate.update(sql, userId);
-    }
-
-    public void removeProductFromWishList(Long userId, Long productId) {
-        String sql = "DELETE FROM wishlist WHERE user_id = ? AND product_id = ?";
-        jdbcTemplate.update(sql, userId, productId);
-    }
-
+public interface WishListRepository extends JpaRepository<WishListEntity, Long> {
+    List<WishListEntity> findByUserId(Long userId);
+    WishListEntity findByUserIdAndProductId(Long userId, Long productId);
 }
