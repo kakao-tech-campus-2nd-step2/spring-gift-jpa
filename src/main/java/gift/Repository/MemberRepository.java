b748152ac042076baacc5ecd48c3662dcaddf108
unknown <dn06023@naver.com>
2024-07-11 10:57:39 +0900

    refactor : 엔티티 클래스의 이름 변경

--- src/main/java/gift/Repository/MemberRepository.java
+++ src/main/java/gift/Repository/MemberRepository.java
@@ -1,12 +1,12 @@
 package gift.Repository;
 
-import gift.Model.Member;
+import gift.Model.Entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByEmail(String email);
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    Optional<MemberEntity> findByEmail(String email);
 }
