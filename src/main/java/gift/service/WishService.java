7594158e6eadb61dcb851527c99746e1b4ab887d
youcaslte03 <luckthb325@naver.com>
2024-07-12 00:45:19 +0900

    refactor:WishService 수정

--- src/main/java/gift/service/WishService.java
+++ src/main/java/gift/service/WishService.java
@@ -1,6 +1,10 @@
 package gift.service;
 
+import gift.entity.Member;
+import gift.entity.Product;
 import gift.entity.Wish;
+import gift.repository.MemberRepository;
+import gift.repository.ProductRepository;
 import gift.repository.WishRepository;
 import java.util.List;
 import java.util.Optional;
@@ -9,10 +13,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class WishService {
     private final WishRepository wishRepository;
+    private final MemberRepository memberRepository;
+    private final ProductRepository productRepository;
 
 
-    public WishService(WishRepository wishRepository) {
+    public WishService(WishRepository wishRepository, MemberRepository memberRepository, ProductRepository productRepository) {
         this.wishRepository = wishRepository;
+        this.memberRepository = memberRepository;
+        this.productRepository = productRepository;
 
     }
 
@@ -20,26 +28,32 @@ public class WishService {
         return wishRepository.findByMemberId(memberId);
     }
 
-    public boolean addWishlist(Wish wish){
-        Optional<Wish> Wishlist = wishRepository.findByMemberIdAndProductId(wish.getMemberId(), wish.getProductId());
-        if(Wishlist.isPresent()){
+    public boolean addWishlist(Long memberId, Long productId){
+        Optional<Member> member = memberRepository.findById(memberId);
+        Optional<Product> product = productRepository.findById(productId);
+
+        if(member.isEmpty() || product.isEmpty()){
             return false;
         }
+        Wish wish = new Wish(member.get(), product.get());
         wishRepository.save(wish);
         return true;
     }
 
-    public boolean updateWishlistItem(Wish wish){
-        Optional<Wish> Wishlist = wishRepository.findByMemberIdAndProductId(wish.getMemberId(), wish.getProductId());
-        if(Wishlist.isPresent()){
+    public boolean updateWishlist(Long memberId, Long productId){
+        Optional<Member> member = memberRepository.findById(memberId);
+        Optional<Product> product = productRepository.findById(productId);
+
+        if(member.isEmpty() || product.isEmpty()){
             return false;
         }
+        Wish wish = new Wish(member.get(), product.get());
         wishRepository.save(wish);
         return true;
     }
 
-    public boolean deleteWishlist(Long userId, Long productId){
-        Optional<Wish> wish = wishRepository.findByMemberIdAndProductId(userId, productId);
+    public boolean deleteWishlist(Long memberId, Long productId){
+        Optional<Wish> wish = wishRepository.findByMemberIdAndProductId(memberId, productId);
         if (wish.isPresent()){
             wishRepository.delete(wish.get());
             return true;
