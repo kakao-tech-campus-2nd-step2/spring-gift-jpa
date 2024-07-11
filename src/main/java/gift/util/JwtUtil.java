067a31197beb1829a3ff754f006e95e541fee175
신예준 <dpwns1826@gmail.com>
2024-07-11 15:09:10 +0900

    feat: JWT 만료 시간 설정

--- src/main/java/gift/util/JwtUtil.java
+++ src/main/java/gift/util/JwtUtil.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -16,9 +17,14 @@ public class JwtUtil {
     @Value("${jwt.secret-key}")
     private String secretKey;
 
+    @Value("${jwt.access-expiration}")
+    private Long accessExpiration;
+
     public String generateToken(Member member) {
         return Jwts.builder().subject(String.valueOf(member.id()))
-            .claim("name", member.email())
+            .claim("email", member.email())
+            .issuedAt(new Date())
+            .expiration(new Date(System.currentTimeMillis() + accessExpiration))
             .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
             .compact();
     }
@@ -29,7 +35,7 @@ public class JwtUtil {
                 .verifyWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
                 .build()
                 .parseSignedClaims(token).getPayload();
-            return new MemberResponse(Long.parseLong(claims.getSubject()), claims.get("name", String.class));
+            return new MemberResponse(Long.parseLong(claims.getSubject()), claims.get("email", String.class));
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "유효하지 않은 인증 정보입니다");
         }
