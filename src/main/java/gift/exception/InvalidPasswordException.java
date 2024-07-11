b6217fcb89adaca886e61299f84557309379f5a5
bokyeong <wntjdals0412@gmail.com>
2024-07-12 03:08:05 +0900

    Feat: CustomException error 및 핸들러

--- src/main/java/gift/exception/InvalidPasswordException.java
+++ src/main/java/gift/exception/InvalidPasswordException.java
@@ -1,7 +1,7 @@
 package gift.exception;
 
-public class InvalidPasswordException extends RuntimeException{
-    public InvalidPasswordException(String message){
+public class InvalidPasswordException extends RuntimeException {
+    public InvalidPasswordException(String message) {
         super(message);
     }
 }
