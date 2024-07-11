5c0c6b5ca26d737cf591af7c088cc6ddf1f69566
seolbin96 <jsb3038@jnu.ac.kr>
2024-07-12 00:43:30 +0900

    feat : data,schema 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,10 @@
-INSERT INTO products (name, price, imageUrl) VALUES ('Sample Product', 1000, 'https://via.placeholder.com/150');
+DELETE FROM wishlist;
+DELETE FROM products;
+DELETE FROM users;
+
+INSERT INTO products (name, price, image_url) VALUES ('Sample Product', 1000, 'https://via.placeholder.com/150');
 INSERT INTO users (email, password) VALUES ('test@example.com', 'password123');
-INSERT INTO wishlist (userId, productId)
+INSERT INTO wishlist (user_id, product_id)
 SELECT u.email, p.id
 FROM users u, products p
 WHERE u.email = 'test@example.com' AND p.name = 'Sample Product';
