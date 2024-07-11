620fd0705e4a74be52a01cf5e277bf0aa5c2a545
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-12 04:43:59 +0900

    feat: 상품 정호 관련 wish 개수 조회 repository 기능  구현

--- src/main/java/gift/product/model/ProductRepository.java
+++ src/main/java/gift/product/model/ProductRepository.java
@@ -4,12 +4,21 @@ import gift.product.model.dto.Product;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findByIdAndIsActiveTrue(Long id);
 
-    // isActive가 true인 모든 제품 찾기
     List<Product> findAllByIsActiveTrue();
+
+    // 특정 ID의 Product와 관련 Wish 개수 조회
+    @Query("SELECT p, COUNT(w) FROM Product p LEFT JOIN Wish w ON w.product = p WHERE p.id = :id AND p.isActive = true GROUP BY p")
+    Optional<Object[]> findProductByIdWithWishCount(@Param("id") Long id);
+
+    // 모든 상품의 상품 정보와 Wish 개수 조회
+    @Query("SELECT p, COUNT(w) FROM Product p LEFT JOIN Wish w ON w.product = p WHERE p.isActive = true GROUP BY p")
+    List<Object[]> findAllActiveProductsWithWishCount();
 }
