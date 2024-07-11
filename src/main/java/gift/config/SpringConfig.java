48930e772088b4981f49bc6ef3922d1eb5d467eb
Giheon Do <dgh0001@naver.com>
2024-07-11 22:47:10 +0900

    refactor: 스프링 빈 등록 방식 컴포넌트 스캔 방식으로 변경

--- src/main/java/gift/config/SpringConfig.java
+++ src/main/java/gift/config/SpringConfig.java
@@ -1,39 +1,11 @@
 package gift.config;
 
-import gift.controller.AdminController;
-import gift.repository.*;
-import jakarta.persistence.EntityManager;
-import org.springframework.context.annotation.Bean;
+//import gift.controller.AdminController;
+
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
-
-    private final EntityManager em;
-
-    public SpringConfig(EntityManager em) {
-        this.em = em;
-    }
-
-    @Bean
-    public AdminController adminController() {
-        return new AdminController(productRepository());
-    }
-
-    @Bean
-    public ProductRepository productRepository() {
-        return new JpaProductRepository(em);
-    }
-
-    @Bean
-    public UserRepository userRepository() {
-        return new JpaUserRepository(em);
-    }
-
-    @Bean
-    public WishlistRepository wishlistRepository() {
-        return new JpaWishlistRepository(em);
-    }
 
 //    @Bean
 //    public TimeTraceAop timeTraceAop() {
