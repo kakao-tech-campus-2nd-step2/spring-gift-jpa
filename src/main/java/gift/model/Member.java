22563054b061a39ebb76f3e2c37a5a5164625101
ajy4304 <ajy4304@naver.com>
2024-07-11 16:05:10 +0900

    refactor(repository): JPA로 리팩토링

--- src/main/java/gift/model/Member.java
+++ src/main/java/gift/model/Member.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
--- src/main/java/gift/repository/WishListRepository.java
+++ src/main/java/gift/repository/WishListRepository.java
@@ -1,53 +1,12 @@
 package gift.repository;
 
 import gift.model.WishList;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.sql.DataSource;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
-public class WishListRepository {
-    private final JdbcTemplate jdbcTemplate;
-
-    private final RowMapper<WishList> wishListRowMapper = (rs, rowNum) -> {
-        WishList wishList = new WishList();
-        wishList.setUserEmail(rs.getString("user_email"));
-        wishList.setProductId(rs.getLong("product_id"));
-        return wishList;
-    };
-
-    public WishListRepository(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
-
-    public List<WishList> findByUserEmail(String email) {
-        return jdbcTemplate.query("SELECT * FROM wishlist WHERE user_email = ?", wishListRowMapper, email);
-    }
-
-    public Optional<WishList> findByUserEmailAndProductId(String email, Long productId) {
-        List<WishList> results = jdbcTemplate.query("SELECT * FROM wishlist WHERE user_email = ? AND product_id = ?", wishListRowMapper, email, productId);
-        return results.stream().findFirst();
-    }
-
-    public Optional<WishList> findById(Long id) {
-        List<WishList> results = jdbcTemplate.query("SELECT * FROM wishlist WHERE id = ?", wishListRowMapper, id);
-        return results.stream().findFirst();
-    }
-
-    public void save(WishList wishList) {
-        jdbcTemplate.update("INSERT INTO wishlist (user_email, product_id) VALUES (?, ?)", wishList.getUserEmail(), wishList.getProductId());
-    }
-
-    public void deleteById(Long id) {
-        jdbcTemplate.update("DELETE FROM wishlist WHERE id = ?", id);
-    }
-
-    public void deleteByUserEmailAndProductId(String email, Long productId) {
-        jdbcTemplate.update("DELETE FROM wishlist WHERE user_email = ? AND product_id = ?", email, productId);
-    }
-
+public interface WishListRepository extends JpaRepository<WishList, Long> {
+    List<WishList> findByUser_Email(String email); // userEmail에서 user.email로 변경. 언더바가 . 역할을 하나봐요...
+    Optional<WishList> findByUser_EmailAndProductId(String email, Long productId);
 }
