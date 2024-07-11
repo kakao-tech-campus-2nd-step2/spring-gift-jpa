0cca942fc0e9ac47c62c85767f6ac23a70e0d857
rbm0524 <rbm0524@naver.com>
2024-07-11 22:34:01 +0900

    fix : Pathvariable 삭제, Entity인 Users를 사용하는 것으로 수정

--- src/main/java/gift/Controller/WishListController.java
+++ src/main/java/gift/Controller/WishListController.java
@@ -1,7 +1,8 @@
 package gift.Controller;
 
 import gift.Annotation.LoginMemberResolver;
-import gift.Model.User;
+import gift.Entity.Users;
+import gift.Entity.Wishlist;
 import gift.Model.WishListItem;
 import gift.Service.WishListService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class WishListController {
@@ -21,30 +23,37 @@ public class WishListController {
     }
 
     @GetMapping("/wishlist")
-    public String getWishlist(@LoginMemberResolver User user, Model model) {
-        List<WishListItem> wishlist = wishlistService.getWishlist(user.getId());
+    public String getWishlist(@LoginMemberResolver Users user, Model model) {
+        List<Wishlist> wishlist = wishlistService.getWishlist(user.getId());
         model.addAttribute("wishlist", wishlist);
         return "wishlist";
     }
 
     @PostMapping("/wishlist/add")
-    public String addWishlistItem(@LoginMemberResolver User user, @RequestBody WishListItem wishListItem) {
+    public String addWishlistItem(@LoginMemberResolver Users user, @RequestBody WishListItem wishListItem) {
         if(user == null) {
             return "redirect:/login";
         }
+
         wishListItem.setUserId(user.getId());
         wishlistService.addWishlistItem(wishListItem);
         return "redirect:/products";
     }
 
-    @PostMapping("/wishlist/remove/{productId}")
-    public String removeWishlistItem(@LoginMemberResolver User user, @RequestBody WishListItem wishListItem) {
+    @PostMapping("/wishlist/remove")
+    public String removeWishlistItem(@LoginMemberResolver Users user, @RequestBody WishListItem wishListItem) {
         if(user == null) {
             return "redirect:/login";
         }
 
+        Optional<Wishlist> wishlistOptional = wishlistService.getWishlist(user.getId()).stream()
+                .filter(wishlist -> wishlist.getProductId() == wishListItem.getProductId())
+                .findFirst();
         wishListItem.setUserId(user.getId());
-        wishlistService.removeWishlistItem(wishListItem);
+
+        wishlistService.removeWishlistItem(wishListItem, wishlistOptional.get());
+
         return "redirect:/wishlist";
     }
+
 }
