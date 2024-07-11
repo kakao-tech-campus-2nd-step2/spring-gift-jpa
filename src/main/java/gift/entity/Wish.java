6eba4c394942ae1df4c9454da00cea6d1c1d0dc5
us4c0d3 <jwseok312@gmail.com>
2024-07-12 00:51:34 +0900

    feat(Product): constraints 이름 추가

--- src/main/java/gift/entity/Wish.java
+++ src/main/java/gift/entity/Wish.java
@@ -2,6 +2,7 @@ package gift.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -56,11 +57,13 @@ public class Wish {
     private Integer quantity;
 
     @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false,
+        foreignKey = @ForeignKey(name = "fk_wishes_user_id_ref_users_id"))
     private User user;
 
     @ManyToOne
-    @JoinColumn(name = "product_id", nullable = false)
+    @JoinColumn(name = "product_id", nullable = false,
+        foreignKey = @ForeignKey(name = "fk_wishes_product_id_ref_products_id"))
     private Product product;
 
     protected Wish() {
