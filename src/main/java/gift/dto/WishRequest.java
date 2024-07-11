e65da5e0d5de2e84c8957ca9c34b4ff826d3d549
Kangboom <jkl6147@naver.com>
2024-07-11 15:18:06 +0900

    feat(dto): add toWishEntity

--- src/main/java/gift/dto/WishRequest.java
+++ src/main/java/gift/dto/WishRequest.java
@@ -1,11 +1,27 @@
 package gift.dto;
 
+import gift.entity.WishEntity;
+
 public class WishRequest {
     private Long productId;
 
+    private Long memberId;
+
     public WishRequest(){}
-    public WishRequest(Long productId){this.productId = productId;}
+    public WishRequest(Long memberId, Long productId){
+        this.memberId = memberId;
+        this.productId = productId;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
     public Long getProductId() {
         return productId;
+    }
+
+    public WishEntity toWishEntity(){
+        return new WishEntity(this.memberId, this.productId);
     }
 }
