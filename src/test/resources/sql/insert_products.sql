b69a206c614ee0207a8eec6a7ee34c17ec1bc4ee
us4c0d3 <jwseok312@gmail.com>
2024-07-12 00:19:50 +0900

    refactor(Wish): 테스트 더미 데이터 삽입을 파일 로드 형식으로 변경

--- src/test/resources/sql/insert_products.sql
+++ src/test/resources/sql/insert_products.sql
@@ -1,5 +1,4 @@
 DELETE FROM products;
-ALTER TABLE products ALTER COLUMN id RESTART WITH 1;
 
 INSERT INTO products (name, price, image_url) VALUES ('Product A', 1000, 'http://example.com/images/product_a.jpg');
 INSERT INTO products (name, price, image_url) VALUES ('Product B', 2000, 'http://example.com/images/product_b.jpg');
