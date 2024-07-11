4b6e513cc98ad68b25c615557310fa7756c96bbc
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-11 14:50:18 +0900

    refactor : DTO 생성으로 인한 수정

--- src/main/java/gift/service/WishListService.java
+++ src/main/java/gift/service/WishListService.java
@@ -1,37 +1,54 @@
 package gift.service;
 
-import gift.domain.WishList;
+import gift.model.product.Product;
+import gift.model.user.User;
+import gift.model.wishlist.WishList;
+import gift.model.wishlist.WishRequest;
+import gift.model.wishlist.WishResponse;
+import gift.repository.product.ProductRepository;
+import gift.repository.user.UserRepository;
 import gift.repository.wish.WishListRepository;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WishListService {
     private final WishListRepository wishListRepository;
-    public WishListService(WishListRepository wishListRepository) {
+    private final ProductRepository productRepository;
+    private final UserRepository userRepository;
+
+    public WishListService(WishListRepository wishListRepository,
+        ProductRepository productRepository,
+        UserRepository userRepository) {
         this.wishListRepository = wishListRepository;
+        this.productRepository = productRepository;
+        this.userRepository = userRepository;
     }
 
-    public Optional<WishList> getWishListByUserId(Long userId) {
-        return wishListRepository.findById(userId);
+    public List<WishResponse> getWishListByUserId(Long userId) {
+        return wishListRepository.findAllByUserId(userId).stream()
+            .map(wishList -> WishResponse.from(wishList, wishList.getProduct()))
+            .collect(Collectors.toList());
     }
 
-    public void addWishList(Long userId, Long productId, int quantity) {
-        WishList wishList = new WishList(userId, productId, quantity);
-        wishListRepository.save(wishList);
+    public WishResponse addWishList(Long userId, WishRequest wishRequest) {
+        User user = userRepository.findById(userId).orElseThrow();
+        Product product = productRepository.findById(wishRequest.productId()).orElseThrow();
+        WishList wishList = wishRequest.toEntity(user, product);
+        return WishResponse.from(wishListRepository.save(wishList), product);
+
     }
 
-    public void updateProductQuantity(Long id, int quantity) {
-        Optional<WishList> existingWish = wishListRepository.findById(id);
-        WishList wish = existingWish.get();
-        wish.setQuantity(quantity);
-
-        wishListRepository.save(wish);
+    public WishResponse updateProductQuantity(Long wishId, Long userId, int quantity) {
+        WishList wishList = wishListRepository.findByIdAndUserId(wishId, userId).orElseThrow();
+        wishList.setQuantity(quantity);
+        return WishResponse.from(wishListRepository.save(wishList), wishList.getProduct());
     }
 
 
-    public void removeWishList(Long productId) {
-        wishListRepository.deleteById(productId);
+    public void removeWishList(Long userId, Long productId) {
+        wishListRepository.deleteByUserIdAndAndProductId(userId, productId);
     }
 }
