1936ca7293bf4733d31e741ca657b9df396a70ed
gitjiho <jiho99322@gmail.com>
2024-07-11 18:26:35 +0900

    fix: 테스트용으로 바꿔놓은 인터셉터 url 롤백

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -21,8 +21,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(tokenInterceptor)
-            .addPathPatterns("/s/**")
-            .excludePathPatterns("/**");
+            .addPathPatterns("/**")
+            .excludePathPatterns("/users/**");
     }
 
     @Override
