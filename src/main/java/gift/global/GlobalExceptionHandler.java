6c36c76688d6023c3b8738240d7b04c5dcf65b4f
Hyewon Seok <hws2008@naver.com>
2024-07-11 23:57:13 +0900

    fix: respond error message in json format

--- src/main/java/gift/global/GlobalExceptionHandler.java
+++ src/main/java/gift/global/GlobalExceptionHandler.java
@@ -10,19 +10,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException e) {
-        return ResponseEntity.badRequest().body(e.getBindingResult()
-                                                .getFieldError()
-                                                .getDefaultMessage());
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
+        return ResponseEntity.badRequest().body(ErrorResponse.of(e.getBindingResult()
+                                                                .getFieldError()
+                                                                .getDefaultMessage()));
     }
 
     @ExceptionHandler(GlobalException.class)
-    public ResponseEntity<String> handleGlobalException(GlobalException e) {
-        return new ResponseEntity<>(e.getMessage(), e.getHttpStatus());
+    public ResponseEntity<ErrorResponse> handleGlobalException(GlobalException e) {
+        return new ResponseEntity<>(ErrorResponse.of(e.getMessage()), e.getHttpStatus());
     }
 
     @ExceptionHandler(UnsupportedOperationException.class)
-    public ResponseEntity<String> handleUnsupportedOperationException(UnsupportedOperationException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<ErrorResponse> handleUnsupportedOperationException(UnsupportedOperationException e) {
+        return ResponseEntity.badRequest().body(ErrorResponse.of(e.getMessage()));
     }
 }
