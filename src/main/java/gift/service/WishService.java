5499f31de5f1e2681ca03173911b34b33a8cfbec
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-11 14:53:53 +0900

    feat: Wish 엔티티 필드의 수정에 따른 코드 개선

--- src/main/java/gift/service/WishService.java
+++ src/main/java/gift/service/WishService.java
@@ -2,27 +2,36 @@ package gift.service;
 
 import gift.dto.WishRequestDTO;
 import gift.dto.WishResponseDTO;
+import gift.model.Product;
+import gift.model.User;
 import gift.model.Wish;
 import gift.repository.ProductRepository;
+import gift.repository.UserRepository;
 import gift.repository.WishRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class WishService {
     private final WishRepository wishRepository;
+    private final UserRepository userRepository;
     private final ProductRepository productRepository;
 
-    public WishService(WishRepository wishRepository, ProductRepository productRepository){
+    public WishService(WishRepository wishRepository, UserRepository userRepository, ProductRepository productRepository){
 
         this.wishRepository = wishRepository;
+        this.userRepository = userRepository;
         this.productRepository = productRepository;
 
     }
 
-    public void addWishProduct(WishRequestDTO wishRequestDTO) {
-        Wish wish = new Wish(wishRequestDTO.userId(), wishRequestDTO.productId());
+    public void addWishProduct(Long userId, WishRequestDTO wishRequestDTO) {
+        User user = userRepository.findById(userId).orElse(null);
+        Product product = productRepository.findById(wishRequestDTO.productId()).orElse(null);
+
+        Wish wish = new Wish(user, product);
         wishRepository.save(wish);
     }
 
@@ -32,7 +41,11 @@ public class WishService {
         return new WishResponseDTO(userId, productRepository.findAllById(productsId));
     }
 
+    @Transactional
     public void deleteWishProduct(Long userId, Long productId) {
-        wishRepository.deleteByUserIdAndProductId(userId, productId);
+        User user = userRepository.findById(userId).orElse(null);
+        Product product = productRepository.findById(productId).orElse(null);
+
+        wishRepository.deleteByUserAndProduct(user, product);
     }
 }
