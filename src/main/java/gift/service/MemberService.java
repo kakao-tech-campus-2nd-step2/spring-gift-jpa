6f0b82edbfe0d10c3ebc01ce9a39a9277ccadd2d
Kangboom <jkl6147@naver.com>
2024-07-11 15:20:18 +0900

    refactor(service): refactor MemberService

--- src/main/java/gift/service/MemberService.java
+++ src/main/java/gift/service/MemberService.java
@@ -2,8 +2,12 @@ package gift.service;
 
 import gift.domain.Member;
 import gift.dto.MemberRequest;
+import gift.entity.MemberEntity;
 import gift.repository.MemberRepository;
 import gift.util.JwtUtil;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,31 +19,36 @@ public class MemberService {
         this.memberRepository = memberRepository;
         this.jwtUtil = jwtUtil;
     }
-
+    public List<Member> findAllMember(){
+        return memberRepository.findAll().stream().map(MemberEntity::toMember).collect(Collectors.toList());
+    }
     public String register(MemberRequest memberRequest) {
 
-        Member member = memberRepository.save(memberRequest);
+        MemberEntity memberEntity = memberRepository.save(memberRequest.toMemberEntity());
 
-        return jwtUtil.generateToken(member);
+        return jwtUtil.generateToken(memberEntity.toMember());
     }
 
     public String login(MemberRequest memberRequest) {
-        Member member = memberRepository.findByEmail(memberRequest.getEmail());
-        if (member != null && member.getPassword().equals(memberRequest.getPassword())) {
+        MemberEntity memberEntity = memberRepository.findByEmail(memberRequest.getEmail()).orElseThrow(()->new EntityNotFoundException("not found Entity"));
+        Member member = memberEntity.toMember();
+
+        if (member.getPassword().equals(memberRequest.getPassword())) {
             return jwtUtil.generateToken(member);
         }
         return null;
     }
 
     public void deleteMember(Long id){
-        memberRepository.delete(id);
+        MemberEntity memberEntity = memberRepository.findById(id).orElseThrow(()->new EntityNotFoundException("not found Entity"));
+        memberRepository.delete(memberEntity);
     }
 
     public Member getMemberFromToken(String token) {
         String email = jwtUtil.getEmailFromToken(token);
 
         if(email != null) {
-            return memberRepository.findByEmail(email);
+            return memberRepository.findByEmail(email).orElseThrow(()->new EntityNotFoundException("not found Entity")).toMember();
         }
         return null;
     }
