b1c4297a9675034c30d2e0cb2133dcfbeb3551a3
gitjiho <jiho99322@gmail.com>
2024-07-11 14:28:27 +0900

    remove: 사용하지 않는 import 삭제

--- src/test/java/gift/repository/UserRepositoryTest.java
+++ src/test/java/gift/repository/UserRepositoryTest.java
@@ -6,8 +6,6 @@ import gift.entity.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
