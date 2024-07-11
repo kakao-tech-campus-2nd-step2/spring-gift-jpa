edf49883415bb027e819acf50394671f4ee6e477
seojiu <seojiu5598@gmail.com>
2024-07-11 15:47:06 +0900

    test(WishListRepositoryTests.java): 위시리스트 저장 후 사이즈 체크, 위시리스트 저장하고 삭제 후 비었는지 체크

--- src/test/java/gift/repositoryTest/WishListRepositoryTests.java
+++ src/test/java/gift/repositoryTest/WishListRepositoryTests.java
@@ -1,4 +1,51 @@
 package gift.repositoryTest;
 
+import gift.model.Member;
+import gift.model.Product;
+import gift.model.WishList;
+import gift.model.MemberRepository;
+import gift.model.ProductRepository;
+import gift.model.WishListRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 public class WishListRepositoryTests {
+
+    @Autowired
+    private WishListRepository wishListRepository;
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void testSaveAndFindWishList() {
+        Member member = new Member(null, "jiu@gmail.com", "password123", null);
+        memberRepository.save(member);
+        Product product = new Product(null, "지우", 1000, "http://example.com/image.jpg");
+        productRepository.save(product);
+        WishList wishList = new WishList(null, member.getId(), product.getId());
+        wishListRepository.save(wishList);
+
+        List<WishList> foundWishLists = wishListRepository.findByMemberId(member.getId());
+        assertThat(foundWishLists).hasSize(1);
+    }
+
+    @Test
+    void testRemoveProductFromWishList() {
+        Member member = new Member(null, "jiu@gmail.com", "password123", null);
+        memberRepository.save(member);
+        Product product = new Product(null, "지우", 1000, "http://example.com/image.jpg");
+        productRepository.save(product);
+        WishList wishList = new WishList(null, member.getId(), product.getId());
+        wishListRepository.save(wishList);
+
+        wishListRepository.deleteByMemberIdAndProductId(member.getId(), product.getId());
+        List<WishList> foundWishLists = wishListRepository.findByMemberId(member.getId());
+        assertThat(foundWishLists).isEmpty();
+    }
 }
