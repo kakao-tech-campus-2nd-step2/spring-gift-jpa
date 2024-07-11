9af0046649902270dab66f66d5b3411be42b0f6f
EunKyung Lee <2dmsrud2002@naver.com>
2024-07-11 14:51:27 +0900

    Feat: 로그인 / 회원가입 페이지 생성 및 js
    index.html과 나누기 위해 login.html 파일을 만들었고 페이지 이동 시킬
    때를 위해서 ViewController를 생성해서 /login시에 login.html 파일로
    이동하게 만들었다. 또한, /wish 일 때 wish.html 로 이동시키도록 만들었다.
    회원가입 이후, 로그인에 성공하면 localStorage에 token이 들어간다.

---
+++ src/main/resources/static/js/auth.js
@@ -0,0 +1,39 @@
+function register() {
+    const email = document.querySelector('#username').value;
+    const password = document.querySelector('#password').value;
+    fetch('/members/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    }).then(response => {
+        if (response.status === 200) {
+            alert("회원가입이 완료되었습니다.");
+        } else {
+            alert("회원가입이 실패했습니다.");
+        }
+    });
+}
+
+function login() {
+    const email = document.querySelector('#username').value;
+    const password = document.querySelector('#password').value;
+
+    fetch('/members/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    }).then(response => response.json())
+        .then(data => {
+            if (data.token) {
+                localStorage.setItem('authToken', data.token);
+                alert("로그인에 성공했습니다.");
+                window.location.href = '/';
+            } else {
+                alert("로그인에 실패했습니다.");
+            }
+        });
+}
