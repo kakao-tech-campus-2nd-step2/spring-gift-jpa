372080d8a0ee9ffbc56347683c4be8c01e67eedb
sunandrabbit <sunandrabbit@gamil.com>
2024-07-11 11:45:52 +0900

    feat : DB 초기화 데이터 주입

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO products (name, price, image_url) VALUES ('Sample Product 1', 1000, 'http://example.com/image1.jpg');
+INSERT INTO products (name, price, image_url) VALUES ('Sample Product 2', 2000, 'http://example.com/image2.jpg');
+INSERT INTO products (name, price, image_url) VALUES ('Sample Product 3', 3000, 'http://example.com/image3.jpg');
