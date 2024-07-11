ab885d23d057d0a50eafc53566ea83f1ff504522
dkswoals <a4445656@gmail.com>
2024-07-11 17:52:21 +0900

    feat: WishListController 리펙토링
    getWishList 반환값 List<WishItemDTO> -> List<WishListResponse>
    deleteFromWishList wishId로만 삭제 가능하도록 변경

--- src/main/java/gift/controller/WishListController.java
+++ src/main/java/gift/controller/WishListController.java
@@ -1,6 +1,6 @@
 package gift.controller;
 
-import gift.model.wishList.WishItemDto;
+import gift.model.wishList.WishListResponse;
 import gift.service.WishListService;
 import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,23 +12,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WishListController {
+
     private final WishListService wishListService;
+
     public WishListController(WishListService wishListService) {
         this.wishListService = wishListService;
     }
 
     @GetMapping("/wishes")
-    public List<WishItemDto> getWishList(@RequestAttribute("userId") Long userId) {
-        return wishListService.getList(userId);
+    public List<WishListResponse> getWishList(@RequestAttribute("userId") Long userId) {
+        return wishListService.getWishList(userId);
     }
 
     @PostMapping("/wishes/{id}")
-    public void addToWishList(@PathVariable("id") Long itemId ,@RequestAttribute("userId") Long userId) {
-        wishListService.addToWishList(userId,itemId);
+    public void addToWishList(@PathVariable("id") Long itemId,
+        @RequestAttribute("userId") Long userId) {
+        wishListService.addToWishList(userId, itemId);
     }
 
     @DeleteMapping("/wishes/{id}")
-    public void deleteFromWishList(@PathVariable("id") Long itemId, @RequestAttribute("userId") Long userId) {
-        wishListService.deleteFromWishList(userId,itemId);
+    public void deleteFromWishList(@PathVariable("id") Long id,
+        @RequestAttribute("userId") Long userId) {
+        wishListService.deleteFromWishList(id);
     }
 }
