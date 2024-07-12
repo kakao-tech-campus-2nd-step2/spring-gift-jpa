c8365990dc17d5c7f53f8afa4467beb2456028cd
humpose <yjpx416@gamil.com>
2024-07-12 14:50:46 +0900

    Feat: 페이지네이션 기능

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,10 +1,13 @@
 package gift.repository;
 
 import gift.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Product findByName_Name(String name);
+    Page<Product> findAll(Pageable pageable);  // 추가
 }
