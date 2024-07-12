39d5b9355fd271204f3249a36f9644ffee6b2c8b
gitjiho <jiho99322@gmail.com>
2024-07-12 14:42:56 +0900

    feat: JPA repository에 Page타입의 find JPQL 추가

--- src/main/java/gift/repository/WishRepository.java
+++ src/main/java/gift/repository/WishRepository.java
@@ -2,8 +2,9 @@ package gift.repository;
 
 import gift.entity.Wish;
 import jakarta.transaction.Transactional;
-import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,7 +13,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface WishRepository extends JpaRepository<Wish, Long> {
 
-    List<Wish> findByUserId(Long userId);
+    Page<Wish> findByUserId(Long userId, Pageable pageable);
 
     Optional<Wish> findByUserIdAndId(Long userId, Long wishId);
 
