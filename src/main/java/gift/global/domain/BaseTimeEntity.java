a87ec27c5cab93225e84f1b41402c91a23ca74bb
sim-mer <gyu2781@gmail.com>
2024-07-12 22:15:21 +0900

    feat(BaseTimeEntity) : 생성, 수정시간 관리가 필요한 엔티티에 상속되는 슈퍼클래스 작성

---
+++ src/main/java/gift/global/domain/BaseTimeEntity.java
@@ -0,0 +1,28 @@
+package gift.global.domain;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+}
