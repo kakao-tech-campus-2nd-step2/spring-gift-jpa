3c2b02be232663f031eec94fa5f1fb8acc3d4a2f
mingjuu <tisu79@naver.com>
2024-07-12 18:17:39 +0900

    feat : 서비스 계층에서 페이지네이션을 처리하기 위한 서비스구현

---
+++ src/main/java/gift/service/ProductService.java
@@ -0,0 +1,21 @@
+package gift.service;
+
+import gift.domain.Product;
+import gift.repository.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Page<Product> getProducts(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
+}
