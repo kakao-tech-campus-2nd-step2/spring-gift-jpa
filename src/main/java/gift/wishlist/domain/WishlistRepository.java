e39a44e332b4ea5cee6e1ac30c529456fa26e8a2
rladbrua0207 <48901587+rladbrua0207@users.noreply.github.com>
2024-07-12 15:33:26 +0900

    feat: Wishlist 페이지네이션 적용

--- src/main/java/gift/wishlist/domain/WishlistRepository.java
+++ src/main/java/gift/wishlist/domain/WishlistRepository.java
@@ -1,14 +1,17 @@
 package gift.wishlist.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
-    List<Wishlist> findByMemberId(Long memberId);
+    Page<Wishlist> findAllByMemberId(Long memberId, Pageable pageable);
+
+    Page<Wishlist> findAllByProductId(Long productId, Pageable pageable);
 
     void deleteAllByMemberId(Long memberId);
+
     void deleteAllByProductId(Long productId);
 }
