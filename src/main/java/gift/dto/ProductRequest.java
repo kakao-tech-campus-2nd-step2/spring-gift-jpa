53d49c0bcd769a15f13f8932d7d46b3e58e4bef3
EunKyung Lee <2dmsrud2002@naver.com>
2024-07-12 14:45:18 +0900

    Refactor: NotBlank가 더 큰 개념이어서 수정함

--- src/main/java/gift/dto/ProductRequest.java
+++ src/main/java/gift/dto/ProductRequest.java
@@ -1,11 +1,11 @@
 package gift.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 
 public record ProductRequest(Long id,
-                             @NotNull(message = "상품 이름이 옳지 않습니다.")
+                             @NotBlank(message = "상품 이름이 옳지 않습니다.")
                              @Length(min = 1, max = 15, message = "상품 이름은 15자 이내 여야 합니다.")
                              @Pattern(regexp = "[a-zA-Z0-9가-힣\\(\\)\\[\\]\\-+&_\\/\\s]+", message = "상품 이름에는 (), [], -, +, &, _, /, 공백을 제외한 특수 문자를 사용할 수 없습니다.")
                              @Pattern(regexp = "^(?!.*카카오).*$", message = "상품 이름에 '카카오' 가 포함 되어 있습니다. 담당 MD와 협의가 필요합니다.")
