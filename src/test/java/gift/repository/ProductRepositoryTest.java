c92b13db4f65f06b045e4a8551de17936863226b
donghyun kim <gimdonghyun5434@gmail.com>
2024-07-12 17:11:44 +0900

    refactor : test fixture 사용하도록 리팩토링

--- src/test/java/gift/repository/ProductRepositoryTest.java
+++ src/test/java/gift/repository/ProductRepositoryTest.java
@@ -1,9 +1,10 @@
 package gift.repository;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import gift.domain.Product;
+import gift.repository.fixture.ProductFixture;
 import jakarta.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
@@ -23,7 +24,7 @@ class ProductRepositoryTest {
     @DisplayName("Create Test")
     void save() {
         // given
-        Product expected = new Product("test",100,"http://example");
+        Product expected = ProductFixture.createProduct("test",100,"test.url");
 
         // when
         Product actual = productRepository.save(expected);
@@ -41,21 +42,18 @@ class ProductRepositoryTest {
     @DisplayName("Read By Id Test")
     void findById() {
         // given
-        String expectedName = "test";
-        int expectedPrice = 100;
-        String expectedImageUrl = "http://example";
-        Product expected = new Product(expectedName, expectedPrice, expectedImageUrl);
-        Product savedProduct = productRepository.save(expected);
+        Product expected = ProductFixture.createProduct("test",100,"test.url");
+        productRepository.save(expected);
 
         // when
-        Product findProduct = productRepository.findById(savedProduct.getId()).get();
+        Product actual = productRepository.findById(expected.getId()).get();
 
         // then
         assertAll(
-            () -> assertThat(findProduct.getId()).isNotNull(),
-            () -> assertThat(findProduct.getName()).isEqualTo(expectedName),
-            () -> assertThat(findProduct.getPrice()).isEqualTo(expectedPrice),
-            () -> assertThat(findProduct.getImageUrl()).isEqualTo(expectedImageUrl)
+            () -> assertThat(actual.getId()).isNotNull(),
+            () -> assertThat(actual.getName()).isEqualTo(expected.getName()),
+            () -> assertThat(actual.getPrice()).isEqualTo(expected.getPrice()),
+            () -> assertThat(actual.getImageUrl()).isEqualTo(expected.getImageUrl())
         );
     }
 
@@ -63,18 +61,15 @@ class ProductRepositoryTest {
     @DisplayName("Read All Test")
     void findAll() {
         // given
-        Product product1 = productRepository.save(new Product("상품1", 1000, "http://product1"));
-        Product product2 = productRepository.save(new Product("상품2", 2000, "http://product2"));
+        productRepository.save(ProductFixture.createProduct("test1",100,"test1.url"));
+        productRepository.save(ProductFixture.createProduct("test2",200,"test2.url"));
 
         // when
-        List<Product> findProducts = productRepository.findAll();
+        List<Product> actuals = productRepository.findAll();
 
         // then
         assertAll(
-            () -> assertThat(findProducts.size()).isEqualTo(2),
-            () -> assertThat(findProducts.get(0).getName()).isEqualTo(product1.getName()),
-            () -> assertThat(findProducts.get(0).getPrice()).isEqualTo(product1.getPrice()),
-            () -> assertThat(findProducts.get(0).getImageUrl()).isEqualTo(product1.getImageUrl())
+            () -> assertThat(actuals.size()).isEqualTo(2)
         );
     }
 
@@ -82,14 +77,13 @@ class ProductRepositoryTest {
     @DisplayName("Delete By Id Test")
     void deleteById() {
         // given
-        Product savedProduct = productRepository.save(new Product("test",100,"url"));
-        Long expectedId = savedProduct.getId();
+        Product expected = ProductFixture.createProduct("test",100,"test.url");
+        productRepository.save(expected);
         // when
-        productRepository.deleteById(savedProduct.getId());
+        productRepository.deleteById(expected.getId());
+        Optional<Product> actual = productRepository.findById(expected.getId());
         // then
-        assertAll(
-            () -> assertThat(productRepository.findById(expectedId)).isEqualTo(Optional.empty())
-        );
+        assertThat(actual).isEmpty();
     }
 
 }
