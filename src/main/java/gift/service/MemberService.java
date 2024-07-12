0989c9c54c1e8cc2337a56bcca214efe95b93256
나제법 <skwpqjq@gmail.com>
2024-07-12 14:08:27 +0900

    refactor: MemberService.java MemberJpaRepository 사용하도록 변경

--- src/main/java/gift/service/MemberService.java
+++ src/main/java/gift/service/MemberService.java
@@ -4,7 +4,7 @@ import gift.authentication.token.JwtProvider;
 import gift.authentication.token.Token;
 import gift.domain.Member;
 import gift.domain.vo.Email;
-import gift.repository.MemberRepository;
+import gift.repository.MemberJpaRepository;
 import gift.web.dto.request.LoginRequest;
 import gift.web.dto.request.member.CreateMemberRequest;
 import gift.web.dto.response.LoginResponse;
@@ -12,25 +12,29 @@ import gift.web.dto.response.member.CreateMemberResponse;
 import gift.web.dto.response.member.ReadMemberResponse;
 import gift.web.validation.exception.IncorrectEmailException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class MemberService {
 
-    private final MemberRepository memberRepository;
+    private final MemberJpaRepository memberJpaRepository;
     private final JwtProvider jwtProvider;
 
-    public MemberService(MemberRepository memberRepository, JwtProvider jwtProvider) {
-        this.memberRepository = memberRepository;
+    public MemberService(MemberJpaRepository memberJpaRepository,
+        JwtProvider jwtProvider) {
+        this.memberJpaRepository = memberJpaRepository;
         this.jwtProvider = jwtProvider;
     }
 
+    @Transactional
     public CreateMemberResponse createMember(CreateMemberRequest request) {
         Member member = request.toEntity();
-        return new CreateMemberResponse(memberRepository.save(member));
+        return CreateMemberResponse.fromEntity(memberJpaRepository.save(member));
     }
 
     public ReadMemberResponse readMember(Long id) {
-        Member member = memberRepository.findById(id)
+        Member member = memberJpaRepository.findById(id)
             .orElseThrow(() -> new IllegalArgumentException("회원을 찾을 수 없습니다. id: " + id));
 
         return ReadMemberResponse.fromEntity(member);
@@ -38,7 +42,7 @@ public class MemberService {
 
     public LoginResponse login(LoginRequest request) {
         Email email = Email.from(request.getEmail());
-        Member member = memberRepository.findByEmail(email).orElseThrow(IncorrectEmailException::new);
+        Member member = memberJpaRepository.findByEmail(email).orElseThrow(IncorrectEmailException::new);
 
         member.matchPassword(request.getPassword());
 
