3162c12f34b6499a8a029f46856cc8c160e3fef6
peacefullyquietly <pjh73015@gmail.com>
2024-07-13 04:17:26 +0900

    chore : 사용하지 않는 어노테이션 제거

--- src/test/java/gift/repository/ProductRepositoryTest.java
+++ src/test/java/gift/repository/ProductRepositoryTest.java
@@ -5,13 +5,11 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
-@ActiveProfiles("test")
 class ProductRepositoryTest {
 
     @Autowired
