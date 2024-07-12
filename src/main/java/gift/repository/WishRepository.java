f327bbc1fd369449eee03a71c42de6c900e465da
wvssm <neoramiregi@gmail.com>
2024-07-12 14:23:00 +0900

    feat(WishRepository): findByMemberId 메서드 추가

--- src/main/java/gift/repository/WishRepository.java
+++ src/main/java/gift/repository/WishRepository.java
@@ -2,6 +2,8 @@ package gift.repository;
 
 import gift.domain.Product;
 import gift.domain.Wish;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +12,5 @@ import java.util.Optional;
 public interface WishRepository extends JpaRepository<Wish, Long> {
     List<Wish> findByMemberId(Long memberId);
     Optional<Wish> findByIdAndMemberId(Long id, Long memberId);
+    Page<Wish> findByMemberId(Long memberId, Pageable pageable);
 }
