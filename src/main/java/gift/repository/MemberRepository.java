b5add722fea9301ae16c9aeb13d7b80d6cd72b4f
나제법 <skwpqjq@gmail.com>
2024-07-12 14:12:09 +0900

    rename: xxxJpaRepository 이름 변경
    - Jpa 키워드 삭제

--- src/main/java/gift/repository/MemberRepository.java
+++ src/main/java/gift/repository/MemberRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberJpaRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(Email email);
 
--- src/main/java/gift/repository/WishProductRepository.java
+++ src/main/java/gift/repository/WishProductRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WishProductJpaRepository extends JpaRepository<WishProduct, Long> {
+public interface WishProductRepository extends JpaRepository<WishProduct, Long> {
 
     List<WishProduct> findByMemberId(Long memberId);
 
