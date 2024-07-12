9051a56a13b01d894d2eebfd170d76362f1bebaf
suhyeon7497 <nucc1186@gmail.com>
2024-07-12 15:16:21 +0900

    feat: wish pagenation

--- src/main/java/gift/service/WishService.java
+++ src/main/java/gift/service/WishService.java
@@ -8,11 +8,17 @@ import gift.model.dto.WishResponseDto;
 import gift.repository.ProductRepository;
 import gift.repository.WishRepository;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class WishService {
+    private static final int PAGE_SIZE = 10;
 
     private final WishRepository wishRepository;
     private final ProductRepository productRepository;
@@ -23,18 +29,19 @@ public class WishService {
     }
 
     @Transactional(readOnly = true)
-    public List<WishResponseDto> getWishList(LoginMemberDto loginMemberDto) {
-        return wishRepository.findAllByMemberId(loginMemberDto.getId())
-            .stream()
-            .map(WishResponseDto::from)
-            .toList();
+    public List<WishResponseDto> getWishList(LoginMemberDto loginMemberDto, int pageNo, String criteria) {
+        Pageable pageable = PageRequest.of(pageNo, PAGE_SIZE, Sort.by(Direction.DESC, criteria));
+        Page<WishResponseDto> page = wishRepository.findAllByMemberId(loginMemberDto.getId(),
+            pageable).map(WishResponseDto::from);
+        return page.getContent();
     }
 
     @Transactional
     public void addProductToWishList(WishRequestDto wishRequestDto, LoginMemberDto loginMemberDto) {
         Product product = productRepository.findById(wishRequestDto.getProductId())
             .orElseThrow(() -> new IllegalArgumentException("Wish 값이 잘못되었습니다."));
-        wishRepository.save(new Wish(loginMemberDto.toEntity(), product, wishRequestDto.getCount()));
+        wishRepository.save(
+            new Wish(loginMemberDto.toEntity(), product, wishRequestDto.getCount()));
     }
 
     @Transactional
