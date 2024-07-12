f031b6c117f0f24587b97c7d529738a6ccf07a85
hyunseo Jeong <wjdgustj2877@naver.com>
2024-07-12 14:16:20 +0900

    feat: 상품 삭제 시 해당 위시리스트 아이템 삭제 기능 구현

--- src/main/java/gift/domain/product/service/ProductService.java
+++ src/main/java/gift/domain/product/service/ProductService.java
@@ -3,6 +3,8 @@ package gift.domain.product.service;
 import gift.domain.product.dao.ProductJpaRepository;
 import gift.domain.product.dto.ProductDto;
 import gift.domain.product.entity.Product;
+import gift.domain.wishlist.dao.WishlistJpaRepository;
+import gift.domain.wishlist.service.WishlistService;
 import gift.exception.InvalidProductInfoException;
 import java.util.List;
 import org.springframework.stereotype.Service;
@@ -11,9 +13,12 @@ import org.springframework.stereotype.Service;
 public class ProductService {
 
     private final ProductJpaRepository productJpaRepository;
+    private final WishlistService wishlistService;
 
-    public ProductService(ProductJpaRepository productJpaRepository) {
+    public ProductService(ProductJpaRepository productJpaRepository,
+        WishlistService wishlistService) {
         this.productJpaRepository = productJpaRepository;
+        this.wishlistService = wishlistService;
     }
 
     public Product create(ProductDto productDto) {
@@ -42,6 +47,7 @@ public class ProductService {
         Product product = productJpaRepository.findById(productId)
             .orElseThrow(() -> new InvalidProductInfoException("error.invalid.product.id"));
 
+        wishlistService.deleteAllByProductId(productId);
         productJpaRepository.delete(product);
     }
 }
