d6b6d736d7d9f094c4b973f897342646e74ce9dd
sunandrabbit <sunandrabbit@gamil.com>
2024-07-12 14:38:39 +0900

    fix : 초기화 비밀번호를 encoding하여 삽입

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,9 +3,9 @@ INSERT INTO products (name, price, image_url) VALUES ('Sample2', 2000, 'http://i
 INSERT INTO products (name, price, image_url) VALUES ('Sample3', 3000, 'http://image3.jpg');
 
 INSERT INTO members (email, password)
-VALUES ('admin1@kakao.com', '1111');
+VALUES ('admin1@kakao.com', 'MTExMQ=='); --원래 비밀번호 1111
 INSERT INTO members (email, password)
-VALUES ('admin2@kakao.com', '2222');
+VALUES ('admin2@kakao.com', 'MjIyMg=='); --원래 비밀번호 2222
 
 INSERT INTO wish_lists (product_id, member_id) VALUES (1, 1);
 INSERT INTO wish_lists (product_id, member_id) VALUES (2, 1);
