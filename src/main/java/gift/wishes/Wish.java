9b826d796d303bc7d7b2b6cb6d0e1ce202b49583
doyooon <ian661087@gmail.com>
2024-07-12 17:11:40 +0900

    feat Wish.java: 객체간의 연관 관계를 고려하여 Wish Entity 작성

---
+++ src/main/java/gift/wishes/Wish.java
@@ -0,0 +1,58 @@
+package gift.wishes;
+
+import gift.member.Member;
+import gift.product.Product;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Wish {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn
+    private Product product;
+
+    private Long quantity;
+
+    public Wish() {
+    }
+
+    public Wish(Member member, Product product, Long quantity){
+        this.member = member;
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    void updateQuantity(Long quantity){
+        this.quantity = quantity;
+    }
+
+}
