71478003be16253fe50ec026c3ffa14af5926d0a
나제법 <skwpqjq@gmail.com>
2024-07-12 14:00:44 +0900

    feat: UpdateWishProductRequest.java 기본 생성자 추가

--- src/main/java/gift/web/dto/request/wishproduct/UpdateWishProductRequest.java
+++ src/main/java/gift/web/dto/request/wishproduct/UpdateWishProductRequest.java
@@ -1,22 +1,17 @@
 package gift.web.dto.request.wishproduct;
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 
 public class UpdateWishProductRequest {
 
-    @NotNull
-    private Long wishProductId;
     @Min(1)
     private Integer quantity;
 
-    public UpdateWishProductRequest(Long wishProductId, Integer quantity) {
-        this.wishProductId = wishProductId;
-        this.quantity = quantity;
+    private UpdateWishProductRequest() {
     }
 
-    public Long getWishProductId() {
-        return wishProductId;
+    public UpdateWishProductRequest(Integer quantity) {
+        this.quantity = quantity;
     }
 
     public Integer getQuantity() {
