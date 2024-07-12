e4d585d1dca7515b4239164dbde54cdb488390e5
ariimo <159447466+ariimo@users.noreply.github.com>
2024-07-12 17:34:25 +0900

    refactor: WishService 수정

--- src/main/java/gift/services/WishService.java
+++ src/main/java/gift/services/WishService.java
@@ -1,17 +1,26 @@
 package gift.services;
 
 
+import gift.classes.Exceptions.ProductException;
+import gift.domain.Member;
+import gift.domain.Product;
 import gift.domain.Wish;
+import gift.dto.MemberDto;
 import gift.dto.WishDto;
+import gift.repositories.MemberRepository;
+import gift.repositories.ProductRepository;
 import gift.repositories.WishRepository;
 import jakarta.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WishService {
     private final WishRepository wishRepository;
+    private ProductRepository productRepository;
+    private MemberRepository memberRepository;
 
     public WishService(WishRepository wishRepository) {
         this.wishRepository = wishRepository;
@@ -23,19 +32,29 @@ public class WishService {
         List<Wish> wishList = wishRepository.findAllByMemberId(memberId);
         return wishList.stream()
             .map(wish -> new WishDto(
-                wish.getMemberId(),
-                wish.getProductId()
+                wish.getMember().getId(),
+                wish.getProduct().getId()
             ))
             .collect(Collectors.toList());
     }
 
 //    Wish 추가
     @Transactional
-    public void addWish(Long memberId, Long productId){
-        Wish wish = new Wish();
-        wish.setMemberId(memberId);
-        wish.setProductId(productId);
-        wishRepository.save(wish);
+    public void addWish(MemberDto memberDto, Long productId){
+        Member member = memberRepository.findByEmail(memberDto.getEmail());
+        Optional<Product> optionalProduct = productRepository.findById(productId);
+
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+
+            Wish wish = new Wish();
+            wish.setMember(member);
+            wish.setProduct(product);
+            wishRepository.save(wish);
+        } else {
+            // 제품이 존재하지 않는 경우에 대한 예외 처리
+            throw new ProductException("Product with ID " + productId + " not found");
+        }
     }
 
 //    Wish 삭제
