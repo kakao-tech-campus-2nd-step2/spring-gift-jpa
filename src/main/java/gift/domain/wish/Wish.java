b80815235ff85c03e22dcf7f43e0be6ba9d9d1c3
bokyeong <wntjdals0412@gmail.com>
2024-07-12 15:57:11 +0900

    Feat: wish에 연관 관계 추가 member, product 참조

--- src/main/java/gift/domain/wish/Wish.java
+++ src/main/java/gift/domain/wish/Wish.java
@@ -1,32 +1,30 @@
 package gift.domain.wish;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import gift.domain.member.Member;
+import gift.domain.product.Product;
+import jakarta.persistence.*;
 
 @Entity
 public class Wish {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long memberId;
-    private Long productId;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
     private Integer quantity;
 
     public Wish() {
     }
 
-    public Wish(Long id, Long memberId, Long productId, Integer quantity) {
-        this.id = id;
-        this.memberId = memberId;
-        this.productId = productId;
-        this.quantity = quantity;
-    }
-
-    public Wish(Long memberId, Long productId, Integer quantity) {
-        this.memberId = memberId;
-        this.productId = productId;
+    public Wish(Member member, Product product, Integer quantity) {
+        this.member = member;
+        this.product = product;
         this.quantity = quantity;
     }
 
@@ -34,12 +32,12 @@ public class Wish {
         return id;
     }
 
-    public Long getMemberId() {
-        return memberId;
+    public Member getMember() {
+        return member;
     }
 
-    public Long getProductId() {
-        return productId;
+    public Product getProduct() {
+        return product;
     }
 
     public Integer getQuantity() {
