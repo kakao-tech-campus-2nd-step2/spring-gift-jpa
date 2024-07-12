1c58909880c2a196e34407425f45be85247cfd35
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-12 14:36:47 +0900

    feat : Product Controller /products/pages로 페이지네이션  + 서비스로 분리

--- src/main/java/gift/product/ProductController.java
+++ src/main/java/gift/product/ProductController.java
@@ -1,6 +1,8 @@
 package gift.product;
 
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
@@ -8,25 +10,23 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.Optional;
-
 @Controller
 @RequestMapping("/manager")
 public class ProductController {
-    private final ProductRepository productRepository;
+    private final ProductService productService;
 
-    public ProductController(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    public ProductController(ProductService productService) {
+        ;
+        this.productService = productService;
     }
 
     @PostMapping("/products/add")
-    public String addProduct(@Valid @ModelAttribute("newProduct") ProductDTO newProduct, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+    public String addProduct(@Valid @ModelAttribute("newProduct") ProductDTO newProduct, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         if (bindingResult.hasErrors()) {
             return "AddProduct";
         }
 
-        Product product = new Product(newProduct.getName(), newProduct.getPrice(), newProduct.getImageUrl());
-        productRepository.save(product);
+        Product product = productService.insertNewProduct(newProduct);
         redirectAttributes.addAttribute("id", product.getId());
 
         return "redirect:/manager/products/{id}";
@@ -34,58 +34,51 @@ public class ProductController {
 
     @PutMapping("/products/update/{id}")
     @Transactional
-    public String updateProduct(@PathVariable Long id, @Valid @ModelAttribute("product") ProductDTO product, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+    public String updateProduct(@PathVariable Long id, @Valid @ModelAttribute("product") ProductDTO product, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         if (bindingResult.hasErrors()) {
             return "UpdateProduct";
         }
-
-        Optional<Product> productFound = productRepository.findById(id);
-        productFound.ifPresent(product1 -> {
-            product1.setName(product.getName());
-            product1.setPrice(product.getPrice());
-            product1.setImageUrl(product.getImageUrl());
-        });
+        productService.updateProduct(id, product);
 
         redirectAttributes.addAttribute("id", id);
         return "redirect:/manager/products/{id}";
     }
 
     @DeleteMapping("/products/delete/{id}")
-    public String deleteProduct(@PathVariable Long id){
-        Optional<Product> product = productRepository.findById(id);
-
-        if(product.isPresent()){
-            productRepository.deleteById(id);
-        }
+    public String deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
         return "redirect:/manager/products";
     }
 
+    @GetMapping("/products/pages")
+    public ResponseEntity<Page<Product>> getProductsPage(@RequestParam(required = false, defaultValue = "0", value = "page") int page,
+                                                         @RequestParam(required = false, defaultValue = "10", value = "size") int size) {
+
+        return ResponseEntity.ok(productService.getProductPages(page, size));
+
+    }
+
     @GetMapping("/products")
-    public String getProductsView(Model model){
-        model.addAttribute("products", productRepository.findAll());
+    public String getProductsView(Model model) {
+        model.addAttribute("products", productService.findAllProducts());
         return "ManageProduct";
     }
 
     @GetMapping("/products/add")
-    public String addProductView(Model model){
-        model.addAttribute("newProduct", new Product());
+    public String addProductView(Model model) {
+        model.addAttribute("newProduct", new ProductDTO());
         return "AddProduct";
     }
 
     @GetMapping("/products/update/{id}")
-    public String updateProductView(@PathVariable Long id, Model model){
-        Optional<Product> product = productRepository.findById(id);
-        if(product.isPresent()){
-            ProductDTO productDTO = new ProductDTO(product.get());
-            model.addAttribute("product",productDTO);
-        }
+    public String updateProduct(@PathVariable Long id, Model model) {
+        model.addAttribute(productService.findByID(id));
         return "UpdateProduct";
     }
 
     @GetMapping("/products/{id}")
     public String getProduct(@PathVariable long id, Model model) {
-        Optional<Product> product = productRepository.findById(id);
-        product.ifPresent(value -> model.addAttribute("product", value));
+        model.addAttribute(productService.findByID(id));
         return "ProductInfo";
     }
 }
