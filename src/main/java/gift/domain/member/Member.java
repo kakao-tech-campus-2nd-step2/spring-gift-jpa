6466416b8c55d38081587c8bd70736eea6022e9b
bokyeong <wntjdals0412@gmail.com>
2024-07-12 16:37:42 +0900

    Feat: 멤버 위시리스트 양방향 매핑 설정

--- src/main/java/gift/domain/member/Member.java
+++ src/main/java/gift/domain/member/Member.java
@@ -1,6 +1,12 @@
 package gift.domain.member;
 
+import gift.domain.wish.Wish;
+import gift.exception.CustomException;
+import gift.exception.ErrorCode;
 import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -13,6 +19,9 @@ public class Member {
     private String name;
     private String password;
     private int role;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Wish> wishList = new ArrayList<>();
 
     public Member(String email, String name, String password, int role) {
         this.email = email;
@@ -44,6 +53,10 @@ public class Member {
         return name;
     }
 
+    public List<Wish> getWishList() {
+        return wishList;
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
@@ -54,5 +67,17 @@ public class Member {
 
     public boolean isMatch(String password) {
         return password.equals(this.password);
+    }
+
+    public void addWish(Wish wish) {
+        wishList.add(wish);
+    }
+
+    public void deleteWish(Long productId) {
+        Wish target = this.wishList.stream()
+                .filter(w -> w.getProduct().getId().equals(productId))
+                .findFirst()
+                .orElseThrow(() -> new CustomException(ErrorCode.INVALID_PRODUCT));
+        wishList.remove(target);
     }
 }
