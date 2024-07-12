1ad1f60724f25b655ab19b26f7b1af057e59e2d5
박민규 <pmq0101@naver.com>
2024-07-12 13:59:02 +0900

    feat(GlobalExceptionHandler): NoSuchElement 예외 처리 및 GlobalExceptionHandler 파일 위치 변경

--- src/main/java/gift/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/controller/exception/GlobalExceptionHandler.java
@@ -1,7 +1,8 @@
-package gift.controller;
+package gift.controller.exception;
 
 import gift.dto.common.apiResponse.ApiResponseBody.FailureBody;
 import gift.dto.common.apiResponse.ApiResponseGenerator;
+import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -17,5 +18,11 @@ public class GlobalExceptionHandler {
         FieldError fieldError = e.getBindingResult().getFieldErrors().getFirst();
         return ApiResponseGenerator.fail(HttpStatus.BAD_REQUEST,
             fieldError.getField() + " : " + fieldError.getDefaultMessage());
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<FailureBody> handleNoSuchElementException(NoSuchElementException e) {
+        return ApiResponseGenerator.fail(HttpStatus.NOT_FOUND, e.getMessage(),
+            String.valueOf(HttpStatus.NOT_FOUND.value()));
     }
 }
