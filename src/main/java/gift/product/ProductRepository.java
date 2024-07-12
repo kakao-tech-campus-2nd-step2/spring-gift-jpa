4264566ecc119639e864d60e30e22cc7db047a43
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-12 14:38:23 +0900

    feat : Product Repository findAll 추가

--- src/main/java/gift/product/ProductRepository.java
+++ src/main/java/gift/product/ProductRepository.java
@@ -1,8 +1,11 @@
 package gift.product;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Pageable;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    public Page<Product> findAll(Pageable pageable);
 }
