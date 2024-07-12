e1a9bf73034e530e8df5e1e5b65a268e4d6aecff
peacefullyquietly <pjh73015@gmail.com>
2024-07-13 04:16:23 +0900

    refactor : 연관관계 매핑에 따른 수정 사항

--- src/main/java/gift/service/WishListService.java
+++ src/main/java/gift/service/WishListService.java
@@ -1,45 +1,72 @@
 package gift.service;
 
+import gift.domain.ProductDTO;
+import gift.entity.MemberEntity;
+import gift.entity.ProductEntity;
 import gift.entity.WishListEntity;
 import gift.domain.WishListDTO;
 import gift.repository.WishListRepository;
 import java.util.Optional;
+import gift.repository.MemberRepository;
+import gift.repository.ProductRepository;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class WishListService {
+    private final WishListRepository wishListRepository;
+    private final ProductRepository productRepository;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    private WishListRepository wishListRepository;
-
-    private WishListDTO toWishListDTO(WishListEntity wishListEntity) {
-        return new WishListDTO(wishListEntity.getProductId(), wishListEntity.getUserId());
+    public WishListService(WishListRepository wishListRepository,
+        WishListRepository wishListRepository1, ProductRepository productRepository, MemberRepository memberRepository) {
+        this.wishListRepository = wishListRepository1;
+        this.productRepository = productRepository;
+        this.memberRepository = memberRepository;
     }
 
+    private WishListDTO toWishListDTO(WishListEntity wishListEntity) {
+        return new WishListDTO(wishListEntity.getProductEntity().getId(), wishListEntity.getUserEntity().getId());
+    }
+
+    private WishListEntity dtoToEntity(Long userId, ProductDTO product) throws Exception {
+        MemberEntity memberEntity = memberRepository.findById(userId)
+            .orElseThrow(() -> new Exception("유저가 존재하지 않습니다."));
+
+        ProductEntity productEntity = productRepository.findById(product.getId())
+            .orElseThrow(() -> new Exception("상품이 존재하지 않습니다."));
+
+        return new WishListEntity(productEntity, memberEntity);
+    }
+
+
     public List<WishListDTO> readWishList(Long userId) {
-        List<WishListEntity> wishListEntities = wishListRepository.findByUserId(userId);
+        List<WishListEntity> wishListEntities = wishListRepository.findByUserEntity_Id(userId);
         return wishListEntities.stream()
             .map(this::toWishListDTO)
             .collect(Collectors.toList());
 
     }
 
-    public void addProductToWishList(Long userId, Long productId) {
-        WishListEntity wishListEntity = new WishListEntity(productId, userId);
-        wishListRepository.save(wishListEntity);
+    @Transactional
+    public void addProductToWishList(Long userId, ProductDTO product) throws Exception {
+        wishListRepository.save(dtoToEntity(userId, product));
     }
 
+    @Transactional
     public void removeWishList(Long userId) {
-        List<WishListEntity> wishListEntities = wishListRepository.findByUserId(userId);
+        List<WishListEntity> wishListEntities = wishListRepository.findByUserEntity_Id(userId);
         wishListRepository.deleteAll(wishListEntities);
     }
 
+    @Transactional
     public void removeProductFromWishList(Long userId, Long productId) {
-        Optional<WishListEntity> wishListEntityOpt = wishListRepository.findByUserIdAndProductId(userId, productId);
+        Optional<WishListEntity> wishListEntityOpt = wishListRepository.findByUserEntity_IdAndProductEntity_Id(userId, productId);
         wishListEntityOpt.ifPresent(wishListRepository::delete);
     }
 
