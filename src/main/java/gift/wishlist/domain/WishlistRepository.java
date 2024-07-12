f5700040f8f1bcb2776d833b64468edbba64c654
rladbrua0207 <48901587+rladbrua0207@users.noreply.github.com>
2024-07-12 16:00:46 +0900

    perf: Wishlist 삭제 시 성능 개선

--- src/main/java/gift/wishlist/domain/WishlistRepository.java
+++ src/main/java/gift/wishlist/domain/WishlistRepository.java
@@ -3,15 +3,23 @@ package gift.wishlist.domain;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
+
     Page<Wishlist> findAllByMemberId(Long memberId, Pageable pageable);
 
     Page<Wishlist> findAllByProductId(Long productId, Pageable pageable);
 
-    void deleteAllByMemberId(Long memberId);
+    @Modifying
+    @Query("DELETE FROM Wishlist w WHERE w.member.id = :memberId")
+    void deleteAllByMemberId(@Param("memberId") Long memberId);
 
-    void deleteAllByProductId(Long productId);
+    @Modifying
+    @Query("DELETE FROM Wishlist w WHERE w.product.id = :productId")
+    void deleteAllByProductId(@Param("productId") Long productId);
 }
