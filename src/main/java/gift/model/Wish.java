1c469f15a73f46cff7d14bc28cb1d001231dc834
Kim Sanghae <pelikan@pusan.ac.kr>
2024-07-13 02:37:32 +0900

    fix: git conflict

--- src/main/java/gift/model/Wish.java
+++ src/main/java/gift/model/Wish.java
@@ -1,5 +1,16 @@
 package gift.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "wish")
 public class Wish {
     private Long id;
     private Long memberId;
--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,7 +1,8 @@
 package gift.repository;
 
 import gift.model.Product;
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface ProductRepository {
     List<Product> findAll();
