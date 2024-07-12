113b3511653c7feed3c640ad3268453f4ec6cd0d
test1 <test>
2024-07-12 14:56:42 +0900

    fix : N+1문제 해결

--- src/main/java/gift/Repository/WishListRepository.java
+++ src/main/java/gift/Repository/WishListRepository.java
@@ -3,10 +3,13 @@ package gift.Repository;
 import gift.DTO.WishList;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface WishListRepository extends JpaRepository<WishList, Long> {
 
+  @Query("SELECT wl from WishList wl join fetch wl.member")
   List<WishList> findByMemberId(Long memberId);
 
+  @Query("SELECT wl from WishList wl join fetch wl.product")
   List<WishList> findByProductId(Long productId);
 }
