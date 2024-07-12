52c032232e499aff80cee2cdaa73d33bce79a29c
Kim Sanghae <pelikan@pusan.ac.kr>
2024-07-13 04:02:22 +0900

    feat: 관계 매핑 추가

--- src/main/java/gift/model/Wish.java
+++ src/main/java/gift/model/Wish.java
@@ -4,27 +4,43 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
-import java.util.UUID;
+import java.util.Objects;
 
 @Entity
+@Table(name = "wish")
 public class Wish {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
-    private Long memberId;
-
-    @NotNull
     private String productName;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
 
     public Wish() {
     }
 
-    public Wish(Long memberId, String productName) {
-        this.memberId = memberId;
+    public Wish(Member member, String productName) {
+        this.member = member;
         this.productName = productName;
+    }
+
+    public Wish(Product product, Member member) {
+        this.productName = product.getName();
+        this.product = product;
+        this.member = member;
     }
 
     public Long getId() {
@@ -36,11 +52,11 @@ public class Wish {
     }
 
     public Long getMemberId() {
-        return memberId;
+        return this.member.getId();
     }
 
     public void setMemberId(Long memberId) {
-        this.memberId = memberId;
+        this.member.setId(memberId);
     }
 
     public String getProductName() {
@@ -50,4 +66,5 @@ public class Wish {
     public void setProductName(String productName) {
         this.productName = productName;
     }
+
 }
