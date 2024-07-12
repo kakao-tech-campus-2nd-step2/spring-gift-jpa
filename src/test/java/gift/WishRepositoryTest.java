0adaa017e5db0f787e23aa643ec9aecd086f247a
youcaslte03 <luckthb325@naver.com>
2024-07-12 15:43:30 +0900

    feat:PagingTest추가

--- src/test/java/gift/WishRepositoryTest.java
+++ src/test/java/gift/WishRepositoryTest.java
@@ -11,6 +11,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -61,7 +65,7 @@ public class WishRepositoryTest {
 
 
     @Test
-    void save() {
+    public void save() {
         Member member = new Member("admin@gmail.com", "password");
         memberRepository.save(member);
 
@@ -79,7 +83,7 @@ public class WishRepositoryTest {
 
 
     @Test
-    void findByMemberId() {
+    public void findByMemberId() {
         Member member = new Member("admin@gmail.com", "password");
         memberRepository.save(member);
 
@@ -95,4 +99,30 @@ public class WishRepositoryTest {
         List<Wish> wishlists = wishRepository.findByMemberId(member.getId());
         assertThat(wishlists).hasSize(2);
     }
+
+    @Test
+    public void findByMemberIdPagingTest(){
+        Member member = new Member("admin@gmail.com", "password");
+        memberRepository.save(member);
+        Long memberId = member.getId();
+
+        for(int i=0; i<50; i++){
+            Product product = new Product("name"+i,1000*i, i+".com");
+            productRepository.save(product);
+
+            Wish wish = new Wish(member, product);
+            wishRepository.save(wish);
+        }
+
+        Pageable pageable = PageRequest.of(0,10);
+        Page<Wish> page = wishRepository.findByMemberId(memberId, pageable);
+
+        assertThat(page.getTotalElements()).isEqualTo(50);
+        assertThat(page.getTotalPages()).isEqualTo(5);
+        assertThat(page.getContent().size()).isEqualTo(10);
+
+    }
+
+
+
 }
