4706b72b5367a2e8c33e42a681770132c4512d65
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-12 15:03:39 +0900

    refactor : Paging 기능 추가

--- src/main/java/gift/controller/WishListController.java
+++ src/main/java/gift/controller/WishListController.java
@@ -9,7 +9,9 @@ import gift.service.WishListService;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,7 +31,12 @@ public class WishListController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<WishResponse>> getWishList(@LoginMember LoginUserDTO member, Pageable pageable) {
+    public ResponseEntity<Page<WishResponse>> getWishList(@LoginMember LoginUserDTO member,
+    @RequestParam(defaultValue = "0") int page,
+    @RequestParam(defaultValue = "5") int size,
+    @RequestParam(defaultValue = "id,asc") String[] sort) {
+        Sort.Direction direction = Sort.Direction.fromString(sort[1]);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sort[0]));
         Page<WishResponse> wishLists = wishListService.getWishListByUserId(member.getId(), pageable);
         return ResponseEntity.ok(wishLists);
     }
