e5d17a8bfb0722caefe54f14dd5156f25b47be22
JinSeoHyun <15roa1121@gmail.com>
2024-07-13 04:07:23 +0900

    fix : 관리자 상품 관련 버그 고침 1. html 페이지에서 삭제,수정  요청 주소가 /{id}가 아닌 쿼리 파라미터 형식이라 변경 2. 컨트롤러 단에서 삭제 수정 요청 주소를 쿼리파라미터로 받고 있는 부분 변경 3. html 페이지 내에서 조회 로직 내에서 사용하는 변수명이 달라 생기는 에러 수정

--- src/main/java/gift/main/controller/AdminProductController.java
+++ src/main/java/gift/main/controller/AdminProductController.java
@@ -1,6 +1,5 @@
 package gift.main.controller;
 
-import gift.main.annotation.AdminCheck;
 import gift.main.annotation.SessionUser;
 import gift.main.dto.ProductRequest;
 import gift.main.dto.ProductResponce;
@@ -10,46 +9,39 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
-@RequestMapping("/admin")
-public class ProductController {
+@RequestMapping("/admin/product")
+public class AdminProductController {
     private final ProductService productService;
 
-    public ProductController(ProductService productService) {
+    public AdminProductController(ProductService productService) {
         this.productService = productService;
     }
 
-    @GetMapping("/products")
-    public ResponseEntity<?> getProducts(@SessionUser UserVo sessionUserVo) {
-        List<ProductResponce> products = productService.getProducts();
-        return ResponseEntity.ok(products);
-    }
-
-    @GetMapping("/product/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<?> findProduct(@PathVariable(name = "id") Long id, @SessionUser UserVo sessionUserVo) {
         ProductResponce product = productService.getProduct(id);
         return ResponseEntity.ok(product);
     }
 
-    @PostMapping("/product")
+    @PostMapping
     public ResponseEntity<String> addProduct(@Valid @RequestBody ProductRequest productRequest, @SessionUser UserVo sessionUserVo) {
         productService.addProduct(productRequest, sessionUserVo);
         return ResponseEntity.ok("Product added successfully");
     }
 
-    @PutMapping("/product")
+    @PutMapping("/{id}")
     public ResponseEntity<?> updateProduct(
-            @RequestParam(value = "id") long id,
+            @PathVariable(value = "id") long id,
             @Valid @RequestBody ProductRequest productRequest,
             @SessionUser UserVo sessionUserVo) {
-
+        System.out.println("productRequest = " + productRequest);
         productService.updateProduct(id, productRequest);
         return ResponseEntity.ok("Product added successfully");
     }
 
-    @DeleteMapping("/product/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteProduct(@PathVariable(name = "id") Long id) {
         productService.deleteProduct(id);
         return ResponseEntity.ok("Product deleted successfully");
