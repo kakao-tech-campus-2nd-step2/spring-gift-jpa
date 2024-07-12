22446ea66435c53e8b0bc37cafe0aa5d2f6847c2
박민규 <pmq0101@naver.com>
2024-07-12 15:37:17 +0900

    refactor(WishResponseDTO): Wish Entity 변경 -> WishResponseDTO 변경

--- src/main/java/gift/dto/responseDTO/WishResponseDTO.java
+++ src/main/java/gift/dto/responseDTO/WishResponseDTO.java
@@ -4,7 +4,7 @@ import gift.domain.Wish;
 
 public record WishResponseDTO(Long id, Long userId, Long productId, Integer count) {
     public static WishResponseDTO of(Wish wish) {
-        return new WishResponseDTO(wish.getId(), wish.getUserId(), wish.getProductId(),
+        return new WishResponseDTO(wish.getId(), wish.getUser().getId(), wish.getProduct().getId(),
             wish.getCount());
     }
 }
