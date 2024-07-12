46ced4c2ed1c28dc25f94061836a049f33fb168b
Kangboom <jkl6147@naver.com>
2024-07-13 02:59:19 +0900

    feat(repository): add Paging

--- src/main/java/gift/repository/WishRepository.java
+++ src/main/java/gift/repository/WishRepository.java
@@ -2,6 +2,8 @@ package gift.repository;
 
 import gift.entity.MemberEntity;
 import gift.entity.WishEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +12,6 @@ import java.util.List;
 @Repository
 public interface WishRepository extends JpaRepository<WishEntity, Long> {
 
-    List<WishEntity> findAllByMemberEntity(MemberEntity memberEntity);
+    Page<WishEntity> findAllByMemberEntity(MemberEntity memberEntity, Pageable pageable);
 }
 
