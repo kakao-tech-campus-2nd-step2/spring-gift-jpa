6d30f22657ec281d8ad5cbb12ebfd4bffd45edba
Lucerna00 <pjs3859811@naver.com>
2024-07-12 22:26:52 +0900

    fix(WishedProduct.java): apply lazy loading

--- src/main/java/gift/domain/WishedProduct.java
+++ src/main/java/gift/domain/WishedProduct.java
@@ -3,6 +3,7 @@ package gift.domain;
 import gift.dto.WishedProductDTO;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -16,11 +17,11 @@ public class WishedProduct {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_email", nullable = false)
     private Member member;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
