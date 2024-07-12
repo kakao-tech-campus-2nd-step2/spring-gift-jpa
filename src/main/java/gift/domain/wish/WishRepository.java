b2465724857e691eca2872f852388cfecf11e37b
yoong-saks <wjdghtjd06@gmail.com>
2024-07-12 16:12:19 +0900

    refactor: 사용하지않는 import 삭제

--- src/main/java/gift/domain/wish/WishRepository.java
+++ src/main/java/gift/domain/wish/WishRepository.java
@@ -1,12 +1,8 @@
 package gift.domain.wish;
 
-import gift.web.exception.ProductNotFoundException;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
