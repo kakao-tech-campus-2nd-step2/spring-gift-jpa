16af29a3d018f531a663fbd4b13938c41146960c
sunandrabbit <sunandrabbit@gamil.com>
2024-07-12 14:47:24 +0900

    fix : id가 전달되지 않아 null로인한 오류가 발생하던 것을 수정

--- src/main/java/gift/service/MemberService.java
+++ src/main/java/gift/service/MemberService.java
@@ -39,11 +39,10 @@ public class MemberService {
 
     @Transactional
     public Token login(Member member) {
-        Optional<MemberEntity> existingMemberEntity = memberRepository.findByEmail(
-            member.getEmail());
+        Optional<MemberEntity> existingMemberEntity = memberRepository.findByEmail(member.getEmail());
         if (existingMemberEntity.isPresent() &&
             PasswordUtil.matches(member.getPassword(), existingMemberEntity.get().getPassword())) {
-            return new Token(jwtUtil.generateToken(member));
+            return new Token(jwtUtil.generateToken(entityToDto(existingMemberEntity)));
         }
         throw new ForbiddenException("Invalid email or password");
     }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
-INSERT INTO products (name, price, image_url) VALUES ('Sample1', 1000, 'http://image1.jpg');
-INSERT INTO products (name, price, image_url) VALUES ('Sample2', 2000, 'http://image2.jpg');
-INSERT INTO products (name, price, image_url) VALUES ('Sample3', 3000, 'http://image3.jpg');
+INSERT INTO products (id, name, price, image_url) VALUES (1, 'Sample1', 1000, 'http://image1.jpg');
+INSERT INTO products (id, name, price, image_url) VALUES (2, 'Sample2', 2000, 'http://image2.jpg');
+INSERT INTO products (id, name, price, image_url) VALUES (3, 'Sample3', 3000, 'http://image3.jpg');
 
-INSERT INTO members (email, password)
-VALUES ('admin1@kakao.com', 'MTExMQ=='); --원래 비밀번호 1111
-INSERT INTO members (email, password)
-VALUES ('admin2@kakao.com', 'MjIyMg=='); --원래 비밀번호 2222
+INSERT INTO members (id, email, password)
+VALUES (1, 'admin1@kakao.com', 'MTExMQ=='); --원래 비밀번호 1111
+INSERT INTO members (id, email, password)
+VALUES (2, 'admin2@kakao.com', 'MjIyMg=='); --원래 비밀번호 2222
 
-INSERT INTO wish_lists (product_id, member_id) VALUES (1, 1);
-INSERT INTO wish_lists (product_id, member_id) VALUES (2, 1);
-INSERT INTO wish_lists (product_id, member_id) VALUES (1, 2);
-INSERT INTO wish_lists (product_id, member_id) VALUES (3, 2);
+INSERT INTO wish_lists (id, product_id, member_id) VALUES (1, 1, 1);
+INSERT INTO wish_lists (id, product_id, member_id) VALUES (2, 2, 1);
+INSERT INTO wish_lists (id, product_id, member_id) VALUES (3, 1, 2);
+INSERT INTO wish_lists (id, product_id, member_id) VALUES (4, 3, 2);
