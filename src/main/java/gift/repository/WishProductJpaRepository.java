31718ac2c095fe5f08f329462b23366ef884323f
나제법 <skwpqjq@gmail.com>
2024-07-12 14:05:32 +0900

    feat: WishProductJpaRepository.java 추가

---
+++ src/main/java/gift/repository/WishProductJpaRepository.java
@@ -0,0 +1,15 @@
+package gift.repository;
+
+import gift.domain.WishProduct;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WishProductJpaRepository extends JpaRepository<WishProduct, Long> {
+
+    List<WishProduct> findByMemberId(Long memberId);
+
+    Optional<WishProduct> findByMemberIdAndProductId(Long memberId, Long productId);
+}
