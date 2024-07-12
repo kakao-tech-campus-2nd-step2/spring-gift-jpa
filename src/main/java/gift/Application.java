f7f89708b41bf640bdb8f5256a8fcd08c4fa949f
gitjiho <jiho99322@gmail.com>
2024-07-12 15:15:31 +0900

    feat: product 더미데이터 30개 추가

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,12 +1,33 @@
 package gift;
 
+import gift.entity.Product;
+import gift.repository.ProductRepository;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
+    @Autowired
+    private ProductRepository productRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner initializer() {
+        return args -> {
+            List<Product> products = new ArrayList<>();
+            for (int i = 1; i <= 30; i++) {
+                products.add(new Product("Product " + i, i * 100, "img" + i + ".jpg"));
+            }
+            productRepository.saveAll(products);
+        };
     }
 }
