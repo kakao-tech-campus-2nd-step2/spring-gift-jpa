0dbdbb90eeefbaa9725d973333c86b117b30ca93
jinhwa-park <parkjinhwa3914@gmail.com>
2024-07-12 17:52:02 +0900

    feat: refactor JwtAuthenticationFilter, add ProductRequest and ProductResponse, GlobalExceptionHandler

---
+++ src/main/java/gift/dto/ProductResponse.java
@@ -0,0 +1,40 @@
+package gift.dto;
+
+public class ProductResponse {
+    private Long id;
+    private String name;
+    private Integer price;
+    private String imageUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
