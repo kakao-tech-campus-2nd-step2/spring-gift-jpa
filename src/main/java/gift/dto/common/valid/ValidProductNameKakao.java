69b215aa2da27ab36e18ef84352ce7cbf9cf9b82
박민규 <pmq0101@naver.com>
2024-07-12 14:24:37 +0900

    feat(Valid): ProductValid 주석 제거 후 @Deprecated 추가

--- src/main/java/gift/dto/common/valid/ValidProductNameKakao.java
+++ src/main/java/gift/dto/common/valid/ValidProductNameKakao.java
@@ -1,7 +1,20 @@
 package gift.dto.common.valid;
 
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
 /*
-쓰지 않는 어노테이션
+ * @deprecated Replaced by ValidProductName
+ */
+@Deprecated
 @Target({FIELD, TYPE})
 @Retention(RUNTIME)
 @Constraint(validatedBy = ProductNameKakaoValidator.class)
@@ -9,10 +22,9 @@ package gift.dto.common.valid;
 public @interface ValidProductNameKakao {
     String message() default "\"카카오\"가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다";
 
-    Class<?>[] groups() default { };
+    Class<?>[] groups() default {};
 
-    Class<? extends Payload>[] payload() default { };
+    Class<? extends Payload>[] payload() default {};
 }
 
 
- */
