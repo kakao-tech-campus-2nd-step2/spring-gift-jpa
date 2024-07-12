6356ca2ed199f1b8b2c283dddc849b40ef7e4d57
cussle <cussle@kakao.com>
2024-07-13 00:48:01 +0900

    fix(ApiExceptionHandler): 중복 코드 제거

--- src/main/java/gift/exception/ApiExceptionHandler.java
+++ src/main/java/gift/exception/ApiExceptionHandler.java
@@ -28,13 +28,4 @@ public class ApiExceptionHandler {
         }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
-            errors.put(error.getField(), error.getDefaultMessage());
-        }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
-    }
 }
