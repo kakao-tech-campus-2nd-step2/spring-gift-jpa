09a5d067e404c73291f09156694716c124338c6a
JinSeoHyun <15roa1121@gmail.com>
2024-07-12 13:44:00 +0900

    fix: Aspect 도입 고민 중으로 해당 로직 삭제

--- src/main/java/gift/main/aop/AdminAspect.java
+++ src/main/java/gift/main/aop/AdminAspect.java
@@ -7,22 +7,17 @@ import gift.main.dto.UserVo;
 import gift.main.entity.Role;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
-
+/*
+도입 유지 고민...
+ */
 
 @Component
 @Aspect
 public class AdminAspect {
 
-    
-    @Pointcut("execution(* gift.main.controller.ProductController.*(..))")
-    public void cut() {
-
-    }
-
-    @Before("cut()")
+//    @Before("execution(* gift.main.controller.ProductController.*(..))")
     public void checkAdmin(@SessionUser UserVo userVo) {
         System.out.println("실행된다.");
         if (!Role.ADMIN.equals(userVo.getRole())) {
