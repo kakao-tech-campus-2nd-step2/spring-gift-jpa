b2dae2121bf3ef3c6f878cd945a61b727bae6cda
mingjuu <tisu79@naver.com>
2024-07-12 18:16:45 +0900

    feat : 페이지네이션을 위한 메서드 추가

--- src/main/java/gift/repository/WishRepository.java
+++ src/main/java/gift/repository/WishRepository.java
@@ -3,12 +3,15 @@ package gift.repository;
 import gift.domain.Member;
 import gift.domain.Product;
 import gift.domain.Wish;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 public interface WishRepository extends JpaRepository<Wish, Long> {
     List<Wish> findByMember(Member member);
     boolean existsByMemberAndProduct(Member member, Product product);
     void deleteByMemberAndProduct(Member member, Product product);
+    Page<Wish> findByMember(Member member, Pageable pageable);
 }
