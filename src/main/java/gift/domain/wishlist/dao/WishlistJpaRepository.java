d7b0ea9dfecfdd3022bbd63c75feeb77609ae557
hyunseo Jeong <wjdgustj2877@naver.com>
2024-07-12 14:28:22 +0900

    fix: JPQL delete 쿼리에 `@Modifying` 추가

--- src/main/java/gift/domain/wishlist/dao/WishlistJpaRepository.java
+++ src/main/java/gift/domain/wishlist/dao/WishlistJpaRepository.java
@@ -3,6 +3,7 @@ package gift.domain.wishlist.dao;
 import gift.domain.wishlist.entity.WishItem;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,8 +15,10 @@ public interface WishlistJpaRepository extends JpaRepository<WishItem, Long> {
     List<WishItem> findAllByUserId(@Param("userId") Long userId);
 
     @Query("delete from WishItem w where w.user.id = :userId")
+    @Modifying
     void deleteAllByUserId(@Param("userId") Long userId);
 
     @Query("delete from WishItem w where w.product.id = :productId")
+    @Modifying
     void deleteAllByProductId(@Param("productId") Long productId);
 }
