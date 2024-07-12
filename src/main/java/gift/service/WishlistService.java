cbe2ff4ac355d9115bbb8da1f00af73e3475d36d
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-12 15:38:17 +0900

    feat : WishlistService 수정 MemberRepository를 사용하지 않고 MemberService를 사용하도록 변경하였습니다.

--- src/main/java/gift/service/WishlistService.java
+++ src/main/java/gift/service/WishlistService.java
@@ -4,29 +4,28 @@ import gift.model.Member;
 import gift.model.Product;
 import gift.model.Wishlist;
 import gift.repository.MemberRepository;
-import gift.repository.ProductRepository;
 import gift.repository.WishlistRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class WishlistService {
 
     private final WishlistRepository wishlistRepository;
-    private final MemberRepository memberRepository;
     private final ProductService productService;
+    private final MemberService memberService;
 
-    public WishlistService(WishlistRepository wishlistRepository, ProductService productService, MemberRepository memberRepository) {
+    public WishlistService(WishlistRepository wishlistRepository, ProductService productService,
+        MemberService memberService) {
         this.wishlistRepository = wishlistRepository;
         this.productService = productService;
-        this.memberRepository = memberRepository;
+        this.memberService = memberService;
     }
 
     public List<Product> getWishlist(String email) {
-        Member member = memberRepository.findByEmail(email);
+        Member member = memberService.findMemberByEmail(email);
         List<Wishlist> wishlistItems = wishlistRepository.findByMember(member);
         return wishlistItems.stream()
             .map(Wishlist::getProduct)
@@ -35,7 +34,7 @@ public class WishlistService {
 
     @Transactional
     public void addWishlist(String email, Long productId) {
-        Member member = memberRepository.findByEmail(email);
+        Member member = memberService.findMemberByEmail(email);
         Product product = productService.findProductsById(productId);
         Wishlist wishlist = new Wishlist(null, member, product);
         wishlistRepository.save(wishlist);
@@ -43,7 +42,7 @@ public class WishlistService {
 
     @Transactional
     public void removeWishlist(String email, Long productId) {
-        Member member = memberRepository.findByEmail(email);
+        Member member = memberService.findMemberByEmail(email);
         Product product = productService.findProductsById(productId);
         wishlistRepository.deleteByMemberAndProduct(member, product);
     }
