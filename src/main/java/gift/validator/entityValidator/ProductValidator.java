472f5e671fd96745f17596c6a2871dbb0af1ce6a
rdme0 <we6610@gmail.com>
2024-07-13 23:36:15 +0900

    :recycle: refactor: Product 클래스의 검증 어노테이션 분리

---
+++ src/main/java/gift/validator/entityValidator/ProductValidator.java
@@ -0,0 +1,59 @@
+package gift.validator.entityValidator;
+
+import gift.dto.ProductDTO;
+import gift.exception.BadRequestExceptions.BadRequestException;
+import gift.exception.BadRequestExceptions.BlankContentException;
+import java.util.regex.Pattern;
+
+public class ProductValidator {
+
+    private static final Pattern NAME_PATTERN = Pattern.compile(
+            "[a-zA-Z0-9ㄱ-ㅎ가-힣()\\[\\]+\\-&/_ ]+"
+    );
+    private static final Pattern NAME_EXCLUDE_PATTERN = Pattern.compile(
+            "^((?!카카오).)*$"
+    );
+
+    private static final Pattern URL_PATTERN = Pattern.compile(
+            "^((http(s)?)://)?(www.)?([a-zA-Z0-9]+)\\.[a-z]+([a-zA-z0-9.?#]+)?"
+    );
+
+    public void validate(ProductDTO productDTO) {
+        validateName(productDTO.name());
+        validatePrice(productDTO.price());
+        validateImageUrl(productDTO.imageUrl());
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new BlankContentException("상품 이름을 입력해주세요.");
+        }
+        if (name.length() > 15) {
+            throw new BadRequestException("제품명 길이는 1~15자만 가능합니다.");
+        }
+        if (!NAME_PATTERN.matcher(name).matches()) {
+            throw new BadRequestException("( ), [ ], +, -, &, /, _을 제외한 특수문자는 입력할 수 없습니다.");
+        }
+        if (!NAME_EXCLUDE_PATTERN.matcher(name).matches()) {
+            throw new BadRequestException("카카오가 포함된 문구는 담당 MD와 협의한 후에 사용해주시기 바랍니다.");
+        }
+    }
+
+    private void validatePrice(Integer price) {
+        if (price == null) {
+            throw new BlankContentException("가격을 입력해주세요.");
+        }
+        if (price < 0) {
+            throw new BadRequestException("가격은 0 이상, 2147483647 이하이여야 합니다.");
+        }
+    }
+
+    private void validateImageUrl(String imageUrl) {
+        if (imageUrl == null || imageUrl.isBlank()) {
+            throw new BlankContentException("이미지 URL을 입력해주세요.");
+        }
+        if(!URL_PATTERN.matcher(imageUrl).matches()) {
+            throw new BadRequestException("URL 형식이 아닙니다.");
+        }
+    }
+}
