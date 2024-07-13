40d7d94056fad6032eaa4dc92e74af9dc631360d
Kim Sanghae <pelikan@pusan.ac.kr>
2024-07-13 14:37:47 +0900

    feat: productRepository 페이지 기능 추가

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package gift.repository;
 import gift.model.Product;
 import java.util.Optional;
 import javax.swing.text.html.Option;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ import org.springframework.stereotype.Repository;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Optional<Product> findByName(String name);
+
+    Page<Product> findAll(Pageable pageable);
 }
