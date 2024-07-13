2bdf6302fe44d8bd98ceead2d5bd339a8854dec4
kangwlgns <kangji0615@gmail.com>
2024-07-14 05:15:12 +0900

    fix: fix minor bugs

--- src/main/java/gift/wishlist/repository/WishListRepository.java
+++ src/main/java/gift/wishlist/repository/WishListRepository.java
@@ -11,8 +11,6 @@ public interface WishListRepository extends JpaRepository<WishList, WishListId> 
     // 페이지로 가져오는 함수
     Page<WishList> findByWishListIdUserUserId(Long userId, Pageable pageable);
 
-    Page<WishList> findByPrice(int price);
-
     // 다 가져오는 함수
     List<WishList> findByWishListIdUserUserId(Long userId);
 }
