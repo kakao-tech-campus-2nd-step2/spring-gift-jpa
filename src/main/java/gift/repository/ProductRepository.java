7b802d1b2f29067f30cc75a85f55c9c5bbd5b585
young-in <hardworkingini@gmail.com>
2024-07-13 23:43:50 +0900

    feat(ProductRepository) : findall()에  pageable 메서드 추가

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,15 +1,16 @@
 package gift.repository;
 
 import gift.domain.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findAll();
+    Page<Product> findAll(Pageable pageable);
     Optional<Product> findById(Long id);
     Product save(Product product);
     void delete(Product product);
