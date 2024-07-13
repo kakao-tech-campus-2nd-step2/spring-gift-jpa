bc7768a92b6354ece028b8ff70a11215a6c8f17b
Dalsungmin <cnww123@naver.com>
2024-07-14 02:35:47 +0900

    Feat: 레포지토리단 매개변수 수정
    pageable까지 받아서 리스트가 아닌 Page형으로 반환

--- src/main/java/gift/repository/WishlistRepository.java
+++ src/main/java/gift/repository/WishlistRepository.java
@@ -1,6 +1,8 @@
 package gift.repository;
 import gift.model.Member;
 import gift.model.WishList;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +10,7 @@ import java.util.List;
 
 @Repository
 public interface WishlistRepository extends JpaRepository<WishList, Long> {
-    List<WishList> findByMember(Member member);
+    Page<WishList> findByMember(Member member, Pageable pageable);
 
     //void deleteById(Long productId);
 }
