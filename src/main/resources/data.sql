b4c7e780a5aa7149ce9f9aa7d57c01577f2d81fe
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-13 21:42:40 +0900

    fix: 테스트를 위해 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,18 @@
 INSERT INTO products (name, price, image_url) VALUES ('coffee', 4500, 'www.coffee.com');
 INSERT INTO products (name, price, image_url) VALUES ('cake', 6500, 'www.cake.com');
 INSERT INTO products (name, price, image_url) VALUES ('bread', 3000, 'www.bread.com');
+INSERT INTO products (name, price, image_url) VALUES ('cookie', 2500, 'www.cookie.com');
+INSERT INTO products (name, price, image_url) VALUES ('iceTea', 3000, 'www.iceTea.com');
+INSERT INTO products (name, price, image_url) VALUES ('cledor', 2500, 'www.cledor.com');
+INSERT INTO products (name, price, image_url) VALUES ('asinayo', 1500, 'www.asinayo.com');
 
 INSERT INTO users (email, password, role) VALUES ('admin@admin', 'admin', 'admin');
 INSERT INTO users (email, password, role) VALUES ('user@user', 'user', 'user');
 
 INSERT INTO wish (user_id, product_id) VALUES (2, 1);
+INSERT INTO wish (user_id, product_id) VALUES (2, 2);
+INSERT INTO wish (user_id, product_id) VALUES (2, 3);
+INSERT INTO wish (user_id, product_id) VALUES (2, 4);
+INSERT INTO wish (user_id, product_id) VALUES (2, 5);
+INSERT INTO wish (user_id, product_id) VALUES (2, 6);
+INSERT INTO wish (user_id, product_id) VALUES (2, 7);
