a5c6d1d6b7a5c304fced923961d83def902b354e
JinSeoHyun <15roa1121@gmail.com>
2024-07-14 02:12:12 +0900

    fetch : 물건리스트  페이지로 볼 수 있게 변경
    [변경 사항]
    1. ProductController : Page 객체를 반환하는 getProductPage 메서드 추가
    2. ProductService:  Page 객체를 반환하는 getProductPage 메서드 추가
    3. ProductRepository : Page 객체를반환할 수 있도록 메서드 추가
    4. memberProduct.html : get 요청 변경
    
    ---

--- src/main/java/gift/main/controller/ProductController.java
+++ src/main/java/gift/main/controller/ProductController.java
@@ -2,11 +2,14 @@ package gift.main.controller;
 
 import gift.main.dto.ProductResponce;
 import gift.main.service.ProductService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ProductController {
@@ -20,5 +23,11 @@ public class ProductController {
     public ResponseEntity<?> getProducts() {
         List<ProductResponce> products = productService.getProducts();
         return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/productpage/{pageNum}")
+    public ResponseEntity<?> getProductPage(@PathVariable(name = "pageNum")Optional<Integer> pageNum) {
+        Page<ProductResponce> productPage = productService.getProductPage(pageNum.orElse(0));
+        return ResponseEntity.ok(productPage);
     }
 }
--- src/main/java/gift/main/repository/ProductRepository.java
+++ src/main/java/gift/main/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package gift.main.repository;
 
 import gift.main.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,7 @@ import java.util.Optional;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findById(Long id);
+
+    Page<Product> findAll(Pageable pageable);
+
 }
