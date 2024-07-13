a3cb222f1be957d819751553750be6d5b22c55f8
amm0124 <amm012450@gmail.com>
2024-07-13 18:41:19 +0900

    FEAT : wish paging 컨트롤러, 서비스 구현

--- src/main/java/gift/service/WishService.java
+++ src/main/java/gift/service/WishService.java
@@ -4,7 +4,6 @@ import gift.dto.TokenLoginRequestDTO;
 import gift.dto.WishResponseDTO;
 import gift.entity.Member;
 import gift.entity.Product;
-import gift.entity.ProductInfo;
 import gift.entity.Wish;
 import gift.exceptionhandler.WishException;
 import gift.repository.MemberRepository;
@@ -12,6 +11,10 @@ import gift.repository.ProductRepository;
 import gift.repository.WishRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -81,26 +84,51 @@ public class WishService {
 
     }
 
-    public WishResponseDTO getWishesByMemberId(TokenLoginRequestDTO tokenLoginRequestDTO) {
+
+    public List<WishResponseDTO> WishToDto(List<Wish> wishlist){
+        List<WishResponseDTO> wishResponseDtoList = new ArrayList<>();
+        for(Wish wish : wishlist){
+            Product product = wish.getProduct();
+            Member member = wish.getMember();
+            WishResponseDTO wishResponseDTO = new WishResponseDTO(
+                    member.getEmail(),
+                    product.getName(),
+                    product.getImageUrl(),
+                    wish.getCount(),
+                    product.getPrice()
+            );
+            wishResponseDtoList.add(wishResponseDTO);
+        }
+        return wishResponseDtoList;
+    }
+
+    public List<WishResponseDTO> getWishesByMemberId(TokenLoginRequestDTO tokenLoginRequestDTO) {
         String email = tokenLoginRequestDTO.getEmail();
         Long memberId = findByEmail(email);
         List<Wish> wishlist = wishRepository.findByMemberId(memberId);
-        List<ProductInfo> productInfo = new ArrayList<>();
-
-        for (Wish wish : wishlist) {
-            Integer count = wish.getCount();
-            Product product = wish.getProduct();
-            productInfo.add(new ProductInfo(product.getName(),
-                    product.getPrice(),
-                    product.getImageUrl(),
-                    count
-                    )
-            );
-        }
-
-        return new WishResponseDTO(email, productInfo);
+        return WishToDto(wishlist);
     }
 
+
+    public Page<WishResponseDTO> paging(Pageable pageable){
+
+        int page = pageable.getPageNumber() - 1;
+        int pageSize = pageable.getPageSize();
+        Sort sort = Sort.by(Sort.Direction.DESC, "id");
+        Page<Wish> wishesPage = wishRepository.findAll(PageRequest.of(page, pageSize, sort));
+
+        Page<WishResponseDTO> wishResponseDTOs = wishesPage.map(wish -> {
+            return new WishResponseDTO(
+                    wish.getMember().getEmail(),
+                    wish.getProduct().getName(),
+                    wish.getProduct().getImageUrl(),
+                    wish.getCount(),
+                    wish.getProduct().getPrice()
+            );
+        });
+
+        return wishResponseDTOs;
+    }
 
 
 }
