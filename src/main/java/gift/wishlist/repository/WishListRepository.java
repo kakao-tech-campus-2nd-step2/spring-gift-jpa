77a55e84abc4fd94b9bef76ea5060f718b32fcf1
kangwlgns <kangji0615@gmail.com>
2024-07-13 20:52:32 +0900

    feat: add a find method

--- src/main/java/gift/wishlist/repository/WishListRepository.java
+++ src/main/java/gift/wishlist/repository/WishListRepository.java
@@ -1,10 +1,10 @@
 package gift.wishlist.repository;
 
-import gift.product.entity.Product;
 import gift.wishlist.entity.WishList;
 import gift.wishlist.model.WishListId;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WishListRepository extends JpaRepository<WishList, WishListId> {
+    List<WishList> findByWishListIdUserUserId(Long userId);
 }
