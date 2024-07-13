d31ff83dcf15a0fc97fd10ceb68446e56ead7d63
dong_yxxn <g0326kdy@gmail.com>
2024-07-13 19:27:11 +0900

    refactor(WishlistService): wishlist 추가 메소드 수정 - 단순히 saveAll에서 상품 추가일 때에는 amount만 올라가도록 설정함

--- src/main/java/gift/service/WishlistService.java
+++ src/main/java/gift/service/WishlistService.java
@@ -2,12 +2,17 @@ package gift.service;
 
 import gift.model.WishlistItem;
 import gift.repository.WishlistRepository;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class WishlistService {
@@ -21,7 +26,38 @@ public class WishlistService {
     public Page<WishlistItem> getWishlistByUserId(Long userId, Pageable pageable) {
         return wishlistRepository.findListByUserId(userId, pageable);
     }
-    public List<WishlistItem> saveWishlistItems(List<WishlistItem> wishlistItems) {
-        return wishlistRepository.saveAll(wishlistItems);
+    public List<WishlistItem> saveWishlistItemsWithUserId(Long userId, List<WishlistItem> wishlistItems) {
+        List<WishlistItem> userWishlistItems = wishlistRepository.findListByUserId(userId);
+        if(!userWishlistItems.isEmpty()){
+            Map<Long, WishlistItem> userWishlistMap = convertListToMap(userWishlistItems);
+            userWishlistItems = mergeWishlist(wishlistItems, userWishlistMap);
+        }else{
+            userWishlistItems = wishlistItems;
+        }
+        return wishlistRepository.saveAll(userWishlistItems);
     }
+
+    private static Map<Long, WishlistItem> convertListToMap(List<WishlistItem> wishlistItems) {
+        Map<Long, WishlistItem> userWishlistMap = new HashMap<>();
+        for(WishlistItem wishlistItem : wishlistItems){
+            userWishlistMap.put(wishlistItem.getProduct().getId(), wishlistItem);
+        }
+        return userWishlistMap;
+    }
+
+    private static List<WishlistItem> mergeWishlist(List<WishlistItem> wishlistItems,
+        Map<Long, WishlistItem> userWishlistMap) {
+        for (WishlistItem wishlistItem : wishlistItems) {
+            Long productId = wishlistItem.getProduct().getId();
+            if (userWishlistMap.containsKey(productId)) {
+                WishlistItem existingItem = userWishlistMap.get(productId);
+                existingItem.setAmount(existingItem.getAmount() + wishlistItem.getAmount());
+                userWishlistMap.put(productId, existingItem);
+            } else {
+                userWishlistMap.put(productId, wishlistItem);
+            }
+        }
+        return new ArrayList<>(userWishlistMap.values());
+    }
+
 }
