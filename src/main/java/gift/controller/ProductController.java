a25fc7e20e04e6a815a74e7187b2be748b2fac51
EunKyung Lee <2dmsrud2002@naver.com>
2024-07-13 21:08:27 +0900

    Refactor: pageableDefault 값 추가

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -6,6 +6,7 @@ import gift.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,7 +28,7 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public ResponseEntity<Page<Product>> getAllProducts(Pageable pageable) {
+    public ResponseEntity<Page<Product>> getAllProducts(@PageableDefault(page = 0, size = 5) Pageable pageable) {
         Page<Product> productsList = productService.getAllProducts(pageable);
         return ResponseEntity.ok().body(productsList);
     }
--- src/main/java/gift/controller/WishController.java
+++ src/main/java/gift/controller/WishController.java
@@ -8,6 +8,7 @@ import gift.service.WishService;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,7 +29,7 @@ public class WishController {
     }
 
     @GetMapping("/wishes")
-    public ResponseEntity<Page<Product>> getAllWishProductsByMember(@LoginMember Member member, Pageable pageable) {
+    public ResponseEntity<Page<Product>> getAllWishProductsByMember(@LoginMember Member member, @PageableDefault(page = 0, size = 5) Pageable pageable) {
         Page<Product> allProducts = wishService.getAllWishProductsByMember(member, pageable);
         return ResponseEntity.ok().body(allProducts);
     }
