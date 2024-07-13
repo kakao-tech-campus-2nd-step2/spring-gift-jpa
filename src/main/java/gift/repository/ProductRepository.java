1212d749ebf701eea9b1c840285006be40604dd5
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-13 22:52:57 +0900

    fix: Page 객체를 반환하도록 수정

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,10 +1,18 @@
 package gift.repository;
 
 import gift.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 import java.util.List;
 
 public  interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findAllById(Iterable<Long> productsId);
+    Page<Product> findAll(Pageable pageable);
+
+    @Query("SELECT p FROM Product p WHERE p.id IN :ids")
+    Page<Product> findAllById(@Param("ids") List<Long> productsId, Pageable pageable);
 }
