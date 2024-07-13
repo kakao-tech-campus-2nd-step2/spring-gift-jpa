2d5ba8d5d628b286ea467ed97d8eff06aaa81c29
Lucerna00 <pjs3859811@naver.com>
2024-07-13 20:49:55 +0900

    feat(data.sql): add product dummy data

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -4,13 +4,10 @@ import gift.domain.Product;
 import gift.repository.ProductRepository;
 import gift.dto.ProductDTO;
 import gift.exception.NoSuchProductException;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
