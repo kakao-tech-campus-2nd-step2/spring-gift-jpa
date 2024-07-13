4a49421112d88c090cd9f76affad29fa4ba32d7e
Giheon Do <dgh0001@naver.com>
2024-07-13 21:29:52 +0900

    refactor: findByEmail 리턴 자료형 변경(pagination 기능 이동)

--- src/test/java/gift/repository/WishlistRepositoryTest.java
+++ src/test/java/gift/repository/WishlistRepositoryTest.java
@@ -1,7 +1,5 @@
 package gift.repository;
 
-import gift.entity.Product;
-import gift.entity.ProductDTO;
 import gift.entity.Wishlist;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,49 +11,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class WishlistRepositoryTest {
 
     @Autowired
-    private ProductRepository productRepository;
-    @Autowired
     private WishlistRepository wishlistRepository;
 
     @Test
     public void save() {
         // given
         String testEmail = "test@naver.com";
-        ProductDTO product1 = new ProductDTO("test1", 123, "test1.com");
-        ProductDTO product2 = new ProductDTO("test2", 456, "test2.com");
-        Product saved1 = productRepository.save(new Product(product1));
-        Product saved2 = productRepository.save(new Product(product2));
 
         Wishlist wishlist = wishlistRepository.save(new Wishlist(testEmail));
 
         // when
-        wishlist.addProduct(saved1);
-        wishlist.addProduct(saved2);
         Wishlist expect = wishlistRepository.save(wishlist);
 
         // then
-        assertThat(expect.getProducts().size()).isEqualTo(2);
+        assertThat(expect.getEmail()).isEqualTo(testEmail);
     }
 
     @Test
     public void find() {
         // given
         String testEmail = "test@naver.com";
-        ProductDTO product1 = new ProductDTO("test1", 123, "test1.com");
-        ProductDTO product2 = new ProductDTO("test2", 456, "test2.com");
-        Product saved1 = productRepository.save(new Product(product1));
-        Product saved2 = productRepository.save(new Product(product2));
-
         Wishlist wishlist = wishlistRepository.save(new Wishlist(testEmail));
 
         // when
-        wishlist.addProduct(saved1);
-        wishlist.addProduct(saved2);
         wishlistRepository.save(wishlist);
-
         Wishlist expect = wishlistRepository.findByEmail(testEmail).get();
 
         // then
-        assertThat(expect.getProducts().size()).isEqualTo(2);
+        assertThat(expect.getEmail()).isEqualTo(testEmail);
     }
 }
