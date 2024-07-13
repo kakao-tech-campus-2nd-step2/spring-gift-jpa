dbf7cddb2c6fde720dd3968fa0732e251064f51a
xkrwjdals <developjmt@gmail.com>
2024-07-13 22:14:13 +0900

    feat: Modify ErrorCode Correctly

--- src/main/java/gift/exception/ErrorCode.java
+++ src/main/java/gift/exception/ErrorCode.java
@@ -1,9 +1,11 @@
 package gift.exception;
 
 public enum ErrorCode {
-    MEMBER_NOT_FOUND("해당 사용자를 데이터 베이스에서 찾을 수 없습니다.\nmemberID: %d"),
-    PRODUCT_NOT_FOUND("해당 상품 데이터 베이스에서 찾을 수 없습니다.\nproductName: $s"),
-    WISHLIST_NOT_FOUND("$s 사용자의 위시 리스트에서 $s 을(를) 찾을 수 없습니다.");
+    MEMBER_NOT_FOUND("해당 사용자를 데이터 베이스에서 찾을 수 없습니다.\nEmail: %s\nPassword: %s"),
+
+    MEMBER_ID_NOT_FOUND("해당 사용자를 데이터 베이스에서 찾을 수 없습니다.\nMember ID: %d"),
+    PRODUCT_NOT_FOUND("해당 상품 데이터 베이스에서 찾을 수 없습니다.\nProduct ID: %d"),
+    WISHLIST_NOT_FOUND("Member ID = %d 사용자의 위시 리스트에서 Product Id = %d 을(를) 찾을 수 없습니다.");
 
     private final String messageTemplate;
 
