255a56580d1a6bdd9fd16a6c128bae0cc3a274e1
rdme0 <we6610@gmail.com>
2024-07-14 02:36:22 +0900

    :white_check_mark: refactor: 엔티티 리팩터링에 의한 테스트 코드 리팩터링

--- src/test/java/gift/repository/WishRepositoryTest.java
+++ src/test/java/gift/repository/WishRepositoryTest.java
@@ -60,40 +60,38 @@ class WishRepositoryTest {
     @Test
     @DisplayName("위시리스트 추가")
     void save(){
-        Wish wish = member.addWish(product);
+        Wish wish = new Wish(member, product, 1);
         Wish actualWish = wishRepository.save(wish);
 
         assertThat(actualWish.getProduct()).isEqualTo(product);
         assertThat(actualWish.getMember()).isEqualTo(member);
         assertThat(actualWish.getQuantity()).isEqualTo(1);
 
-        member.addWish(product);
+        wish.incrementQuantity();
+        wishRepository.flush();
 
-        assertThat(actualWish.getQuantity()).isEqualTo(2);
+        assertThat(wishRepository.count()).isEqualTo(1);
     }
 
     @Test
     @DisplayName("위시리스트 개수 수정")
     void updateQuantity(){
-        Wish wish = member.addWish(product);
-        wish.setQuantity(3);
+        Wish wish = new Wish(member, product, 1);
+        wishRepository.save(wish);
+        assertThat(wishRepository.findByMemberAndProduct(member, product).get().getQuantity()).isEqualTo(1);
+        wish.changeQuantity(3);
+        wishRepository.flush();
 
-        Wish actualWish = wishRepository.save(wish); //Wish 영속화
-        assertThat(actualWish.getQuantity()).isEqualTo(3);
-
-        wish.setQuantity(200);
-
-        assertThat(actualWish.getQuantity()).isEqualTo(200);
-
+        assertThat(wishRepository.findByMemberAndProduct(member, product).get().getQuantity()).isEqualTo(3);
     }
 
     @Test
     @DisplayName("위시리스트 Member & 상품 ID로 제거")
     void delete1(){
-        Wish wish = member.addWish(product);
+        Wish wish = new Wish(member, product, 1);
         wishRepository.save(wish);
-
         assertThat(wishRepository.count()).isEqualTo(1);
+
         wishRepository.deleteByMemberAndProduct_Id(member, product.getId());
         assertThat(wishRepository.count()).isEqualTo(0);
     }
@@ -101,9 +99,9 @@ class WishRepositoryTest {
     @Test
     @DisplayName("위시리스트 상품 ID로 제거")
     void delete2(){
-        Wish wish = member.addWish(product);
+        Wish wish = new Wish(member, product, 1);
         wishRepository.save(wish);
-        Wish wish2 = member2.addWish(product);
+        Wish wish2 = new Wish(member2, product, 1);
         wishRepository.save(wish2);
 
 
