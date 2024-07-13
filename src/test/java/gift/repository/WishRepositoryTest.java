a3845a107051f36c88be3559861ad765d7f442ac
rdme0 <we6610@gmail.com>
2024-07-14 04:53:01 +0900

    :fire: remove: Transactional 어노테이션 제거

--- src/test/java/gift/repository/WishRepositoryTest.java
+++ src/test/java/gift/repository/WishRepositoryTest.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
-@Transactional
 class WishRepositoryTest {
 
     @Autowired
