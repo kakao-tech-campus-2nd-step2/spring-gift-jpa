6c3e95ddfe8d724c8b0bcc173a8261ca4b3aacb0
ariimo <159447466+ariimo@users.noreply.github.com>
2024-07-13 22:55:00 +0900

    feat: PageController에 Pagination 구현

--- src/main/java/gift/controller/PageController.java
+++ src/main/java/gift/controller/PageController.java
@@ -7,6 +7,11 @@ import gift.services.ProductService;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
@@ -24,9 +29,23 @@ public class PageController {
     }
 
     @GetMapping("/") // 주소 매핑
-    public String indexPageGet(Model model) {
-        List<ProductDto> products = productService.getAllProducts();
+    public String indexPageGet(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.DESC) Pageable pageable) {
+        Page<ProductDto> products = productService.getAllProducts(pageable);
+        int totalPages = products.getTotalPages();
+
+        // 총 페이지 수가 0일 때 1로 설정
+        if (totalPages == 0) {
+            totalPages = 1;
+        }
+        int nowPage = products.getPageable().getPageNumber() + 1;
+        int startPage = Math.max(nowPage - 4, 1);
+        int endPage = Math.min(nowPage + 5,totalPages);
+
         model.addAttribute("products", products);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
         return "index";
     }
 
