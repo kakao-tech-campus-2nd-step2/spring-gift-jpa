db37747628ed7bec7dbe7308753fb35f2851adb2
rdme0 <we6610@gmail.com>
2024-07-14 01:34:11 +0900

    :recycle: refactor: converter 엔티티 -> 별도 객체로 분리

--- src/main/java/gift/util/converter/MemberConverter.java
+++ src/main/java/gift/util/converter/MemberConverter.java
@@ -1,4 +1,4 @@
-package gift.service;
+package gift.util.converter;
 
 import gift.dto.MemberDTO;
 import gift.entity.Member;
@@ -8,5 +8,9 @@ public class MemberConverter {
     public static MemberDTO convertToMemberDTO(Member member) {
         return new MemberDTO(member.getEmail(), member.getPassword(), member.getName(),
                 member.getRole(), member.getWishList());
+    }
+
+    public static Member convertToMember(MemberDTO memberDTO) {
+        return new Member(memberDTO.getEmail(), memberDTO.getPassword(), memberDTO.getName(), memberDTO.getRole(), memberDTO.getWishList());
     }
 }
