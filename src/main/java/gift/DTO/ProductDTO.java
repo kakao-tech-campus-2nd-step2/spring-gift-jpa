969e45cc4ec9fa1b4dd5b6a1c26fe08da7d794e7
suno-boy <tnsgh3691@gmail.com>
2024-07-13 19:30:12 +0900

    feat: ProductDTO, WishDTO 구현
    - Entity를 보호하기 위해 사용할 DTO 구현

---
+++ src/main/java/gift/DTO/ProductDTO.java
@@ -0,0 +1,77 @@
+package gift.DTO;
+
+import gift.Entity.ProductEntity;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import java.util.List;
+
+public class ProductDTO {
+
+    private Long id;
+
+    @Size(max = 15, message = "상품 이름은 공백을 포함하여 최대 15자까지 입력할 수 있습니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣 ()\\[\\]+\\-\\&/_]*$", message = "(),[],+,-,&,/,_를 제외한 특수 문자 사용은 불가능합니다.")
+    @Pattern(regexp = "^(?!.*카카오).*$", message = "카카오가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다.")
+    private String name;
+    private int price;
+    private String imageUrl;
+
+    private List<WishDTO> wishes;
+
+    public ProductDTO() {}
+
+    public ProductDTO(ProductEntity entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.price = entity.getPrice();
+        this.imageUrl = entity.getImageUrl();
+    }
+
+    public ProductDTO(Long id, String name, int price, String imageUrl, List<WishDTO> wishes) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.wishes = wishes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public List<WishDTO> getWishes() {
+        return wishes;
+    }
+
+    public void setWishes(List<WishDTO> wishes) {
+        this.wishes = wishes;
+    }
+}
---
+++ src/main/java/gift/DTO/WishDTO.java
@@ -0,0 +1,30 @@
+package gift.DTO;
+
+public class WishDTO {
+
+    private Long id;
+    private String comment;
+
+    public WishDTO() {}
+
+    public WishDTO(Long id, String comment) {
+        this.id = id;
+        this.comment = comment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
