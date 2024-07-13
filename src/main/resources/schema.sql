47fcc8240692a4d2e795fb118cda585874563742
cussle <cussle@kakao.com>
2024-07-14 00:57:33 +0900

    refactor: 7/13 코드리뷰 반영(schema.sql 생성)

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,23 @@
+-- 상품(PRODUCTS) 스키마
+CREATE TABLE IF NOT EXISTS product (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price INT NOT NULL,
+    image_url VARCHAR(255) NOT NULL
+);
+
+-- 회원(MEMBERS) 스키마
+CREATE TABLE IF NOT EXISTS member (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+-- 위시 리스트(WISHLISTS) 스키마
+CREATE TABLE IF NOT EXISTS wish (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    member_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member(id),
+    FOREIGN KEY (product_id) REFERENCES product(id)
+);
