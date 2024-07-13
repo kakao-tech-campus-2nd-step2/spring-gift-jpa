50f439c2557901ccf4d0b3d439af444f13bb906d
rdme0 <we6610@gmail.com>
2024-07-14 02:15:50 +0900

    :recycle: refactor: setter 메서드 최소화 및 기본생성자 접근제어자 변경

--- src/main/java/gift/entity/Member.java
+++ src/main/java/gift/entity/Member.java
@@ -1,7 +1,5 @@
 package gift.entity;
 
-import gift.dto.MemberDTO;
-import gift.dto.WishListDTO;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,7 +9,6 @@ import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -44,7 +41,7 @@ public class Member {
     @OneToMany(mappedBy = "member")
     private List<Wish> wishList;
 
-    public Member() {
+    protected Member() {
     }
 
     public Member(String email, String password, String name, String role, List<Wish> wishList) {
@@ -55,93 +52,23 @@ public class Member {
         this.wishList = wishList;
     }
 
-    public Member(MemberDTO memberDTO) {
-        this.email = memberDTO.getEmail();
-        this.password = memberDTO.getPassword();
-        this.name = memberDTO.getName();
-        this.role = memberDTO.getRole();
-        this.wishList = memberDTO.getWishList();
-    }
-
     public String getEmail() {
         return email;
     }
-
-    private void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPassword() {
         return password;
     }
-
-    private void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getName() {
         return name;
     }
-
-    private void setName(String name) {
-        this.name = name;
-    }
-
     public String getRole() {
         return role;
     }
-
-    private void setRole(String role) {
-        this.role = role;
-    }
-
     public List<Wish> getWishList() {
         return wishList;
     }
-
-    private void setWishList(List<Wish> wishList) {
-        this.wishList = wishList;
-    }
-
     public Long getId() {
         return id;
-    }
-
-    public void changeMemberInfo(Member member) {
-        setEmail(member.getEmail());
-        setPassword(member.getPassword());
-        setName(member.getName());
-        setRole(member.getRole());
-        setWishList(member.getWishList());
-    }
-
-
-    public Wish addWish(Product product) {
-        for (Wish wish : wishList) {
-            if (wish.getProduct().equals(product)) {
-                wish.incrementQuantity();
-                return wish;
-            }
-        }
-        wishList.add(new Wish(this, product, 1));
-        return wishList.getLast();
-    }
-
-    public boolean removeWish(Product product) {
-        for (Wish wish : wishList) {
-            if (wish.getProduct().equals(product)) {
-                return wishList.remove(wish);
-            }
-        }
-        return false;
-    }
-
-    public WishListDTO convertToWishListDTO() {
-        List<Wish> wishListForView = new ArrayList<>(this.getWishList());
-        for (Wish wish : wishListForView) {
-            wish.setMember(null); // 개인정보 보호
-        }
-        return new WishListDTO(wishListForView);
     }
 
     @Override
@@ -162,17 +89,5 @@ public class Member {
     @Override
     public int hashCode() {
         return Objects.hash(email, password, name, role);
-    }
-
-    @Override
-    public String toString() {
-        return "Member{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", name='" + name + '\'' +
-                ", role='" + role + '\'' +
-                ", wishList=" + wishList +
-                '}';
     }
 }
