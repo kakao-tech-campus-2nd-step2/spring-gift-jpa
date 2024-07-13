13b8e2af21ede0bda2cf48b68e5e1c2a5d6f7b41
kangwlgns <kangji0615@gmail.com>
2024-07-14 05:03:52 +0900

    refactor: change repository for page

--- src/main/java/gift/product/repository/ProductRepository.java
+++ src/main/java/gift/product/repository/ProductRepository.java
@@ -2,9 +2,12 @@ package gift.product.repository;
 
 import gift.product.entity.Product;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findAll(Pageable pageable);
     List<Product> findByName(String name);
     boolean existsByName(String name);
 }
--- src/main/java/gift/wishlist/repository/WishListRepository.java
+++ src/main/java/gift/wishlist/repository/WishListRepository.java
@@ -3,8 +3,16 @@ package gift.wishlist.repository;
 import gift.wishlist.entity.WishList;
 import gift.wishlist.model.WishListId;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WishListRepository extends JpaRepository<WishList, WishListId> {
+    // 페이지로 가져오는 함수
+    Page<WishList> findByWishListIdUserUserId(Long userId, Pageable pageable);
+
+    Page<WishList> findByPrice(int price);
+
+    // 다 가져오는 함수
     List<WishList> findByWishListIdUserUserId(Long userId);
 }
