9667a83b626325de363741f8d2e8339ba7815a4a
young-in <hardworkingini@gmail.com>
2024-07-13 23:55:58 +0900

    feat(main.html) : 전체 상품 조회 html 수정 (페이지 나누고, 정렬)

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -6,7 +6,9 @@ import gift.dto.UpdateProductDto;
 import gift.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,7 +32,12 @@ public class ProductController {
 
     // 전체 상품 조회
     @GetMapping
-    public ResponseEntity<Page<Product>> getAllProducts(Pageable pageable) {
+    public ResponseEntity<Page<Product>> getAllProducts(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "name") String sort,
+            @RequestParam(defaultValue = "asc") String direction) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.fromString(direction), sort));
         Page<Product> allProducts = productService.getAllProducts(pageable);
         if (allProducts.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
