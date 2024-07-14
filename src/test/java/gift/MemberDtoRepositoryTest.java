efeced067e28390a05a69004ffcde4bb9756bd29
rbm0524 <rbm0524@naver.com>
2024-07-14 17:00:23 +0900

    fix : User였던 것을 Member로 이름 변경

---
+++ src/test/java/gift/MemberDtoRepositoryTest.java
@@ -0,0 +1,83 @@
+package gift;
+
+import gift.Entity.Member;
+import gift.Mapper.Mapper;
+import gift.Model.MemberDto;
+import gift.Repository.MemberJpaRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+public class MemberDtoRepositoryTest {
+
+    @Autowired
+    private MemberJpaRepository memberJpaRepository;
+    @Autowired
+    private Mapper mapper;
+
+    @Test
+    public void testGetAllUsers() {
+        MemberDto memberDto1 = new MemberDto(1L, "1234@naver.com", "1234", "1234", false);
+        MemberDto memberDto2 = new MemberDto(2L, "5678@naver.com", "5678", "5678", false);
+
+        Member member1 = mapper.memberDtoToEntity(memberDto1);
+        Member member2 = mapper.memberDtoToEntity(memberDto2);
+
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        assertThat(memberJpaRepository.findAll()).isNotEmpty();
+    }
+
+    @Test
+    public void testGetUserById() {
+        MemberDto memberDto1 = new MemberDto(1L, "1234@naver.com", "1234", "1234", false);
+        Member member1 = mapper.memberDtoToEntity(memberDto1);
+        memberJpaRepository.save(member1);
+
+        Optional<Member> foundUserOptional = memberJpaRepository.findById(member1.getId());
+        assertThat(foundUserOptional).isNotNull();
+
+        foundUserOptional.ifPresent(foundUser -> {
+            assertThat(foundUser.getEmail()).isEqualTo(member1.getEmail());
+            assertThat(foundUser.getPassword()).isEqualTo(member1.getPassword());
+        });
+
+    }
+
+    @Test
+    public void testSaveUser() {
+        MemberDto memberDto1 = new MemberDto(1L, "1234@naver.com", "1234", "1234", false);
+        Member member1 = mapper.memberDtoToEntity(memberDto1);
+        Member savedMember = Optional.of(memberJpaRepository.save(member1)).get();
+        assertThat(savedMember.getEmail()).isEqualTo(member1.getEmail());
+        assertThat(savedMember.getPassword()).isEqualTo(member1.getPassword());
+
+    }
+
+    @Test
+    public void testDeleteUser() {
+        MemberDto memberDto1 = new MemberDto(1L, "1234@naver.com", "1234", "1234", false);
+        Member member1 = mapper.memberDtoToEntity(memberDto1);
+        memberJpaRepository.save(member1);
+
+        memberJpaRepository.delete(member1);
+
+        assertThat(memberJpaRepository.findById(member1.getId())).isEmpty();
+    }
+
+    @Test
+    public void testUpdateUser() {
+        MemberDto memberDto1 = new MemberDto(1L, "1234@naver.com", "1234", "1234", false);
+        Member member1 = mapper.memberDtoToEntity(memberDto1);
+        memberJpaRepository.save(member1);
+    }
+
+
+}
