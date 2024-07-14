3c21b00d9446cabb841f5fdaeb839c5b0d9f8799
momnpa333 <momnpa333@gmail.com>
2024-07-15 00:31:37 +0900

    feat: converter 제작 및 config 추가

--- src/main/java/gift/global/WebConfig.java
+++ src/main/java/gift/global/WebConfig.java
@@ -3,8 +3,10 @@ package gift.global;
 import gift.global.auth.interceptor.AuthenticationInterceptor;
 import gift.global.auth.interceptor.AuthorizationInterceptor;
 import gift.global.auth.resolver.LoginInfoArgumentResolver;
+import gift.global.converter.StringToSearchTypeConverter;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,13 +16,16 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final AuthorizationInterceptor authorizationInterceptor;
     private final AuthenticationInterceptor authenticationInterceptor;
+    private final StringToSearchTypeConverter stringToSearchTypeConverter;
 
     public WebConfig(
         AuthorizationInterceptor authorizationInterceptor,
-        AuthenticationInterceptor authenticationInterceptor
+        AuthenticationInterceptor authenticationInterceptor,
+        StringToSearchTypeConverter stringToSearchTypeConverter
     ) {
         this.authorizationInterceptor = authorizationInterceptor;
         this.authenticationInterceptor = authenticationInterceptor;
+        this.stringToSearchTypeConverter = stringToSearchTypeConverter;
     }
 
     @Override
@@ -34,5 +39,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginInfoArgumentResolver());
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(stringToSearchTypeConverter);
     }
 }
---
+++ src/main/java/gift/global/converter/StringToSearchTypeConverter.java
@@ -0,0 +1,18 @@
+package gift.global.converter;
+
+import gift.model.product.SearchType;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StringToSearchTypeConverter implements Converter<String, SearchType> {
+
+    @Override
+    public SearchType convert(String source) {
+        try {
+            return SearchType.fromDescription(source);
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException("Invalid search type: " + source);
+        }
+    }
+}
