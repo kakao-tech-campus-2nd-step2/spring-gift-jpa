ef60282efddb019d1bb9b779dbd1ea6cc7d2b40a
나제법 <skwpqjq@gmail.com>
2024-07-14 17:31:48 +0900

    feat: Application.java 실행 시 local 프로필 적용

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -2,10 +2,13 @@ package gift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setAdditionalProfiles("local");
+        app.run(args);
     }
 }
