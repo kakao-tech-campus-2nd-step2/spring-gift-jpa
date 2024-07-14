4a39405a7f245995b6df7f3d3d54d83ef4381263
ajy4304 <ajy4304@naver.com>
2024-07-14 16:34:16 +0900

    refactor(WishListRepository): 사용하지 않는 메서드 제거

--- src/main/java/gift/repository/WishListRepository.java
+++ src/main/java/gift/repository/WishListRepository.java
@@ -9,5 +9,4 @@ import java.util.Optional;
 
 public interface WishListRepository extends JpaRepository<WishList, Long> {
     List<WishList> findByMember(Member member);
-    Optional<WishList> findByMemberAndProduct(Member member, Product product);
 }
