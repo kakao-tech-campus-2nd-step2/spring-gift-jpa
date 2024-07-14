a19bbd8341a651188f05c4bba88dd7546c69c471
NextrPlue <p.plue1881@gmail.com>
2024-07-14 19:54:15 +0900

    feat(service): add pagination support for fetching wishes by member ID

--- src/main/java/gift/repository/WishlistRepository.java
+++ src/main/java/gift/repository/WishlistRepository.java
@@ -1,6 +1,8 @@
 package gift.repository;
 
 import gift.entity.Wish;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +11,5 @@ import java.util.Optional;
 public interface WishlistRepository extends JpaRepository<Wish, Long> {
     Optional<Wish> findByMemberIdAndProductId(Long memberId, Long productId);
     List<Wish> findByMemberId(Long memberId);
+    Page<Wish> findByMemberId(Long memberId, PageRequest pageRequest);
 }
