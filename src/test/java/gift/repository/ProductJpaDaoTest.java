e08e7caef180e353fdc98af96e0561059e3cf668
5win <94297900+5win@users.noreply.github.com>
2024-07-14 23:55:16 +0900

    fix: Fix test code to use table truncate - 삽입한 샘플 데이터와의 충돌을 해결.

--- src/test/java/gift/repository/ProductJpaDaoTest.java
+++ src/test/java/gift/repository/ProductJpaDaoTest.java
@@ -8,8 +8,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 @DataJpaTest
+@Sql("/sql/truncateIdentity.sql")
 class ProductJpaDaoTest {
 
     @Autowired
