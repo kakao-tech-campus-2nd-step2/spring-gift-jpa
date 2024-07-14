3c887de2227870690b6407240c17188168612816
HaegyeongKim01 <haegyeong.kim01@gmail.com>
2024-07-14 21:02:55 +0900

    feat: 상품 목록 페이지네이션

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -5,11 +5,14 @@ import gift.dto.ProductDto;
 import gift.dto.ProductUpdateDto;
 import jakarta.validation.Valid;
 import gift.vo.Product;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/products")
@@ -22,12 +25,25 @@ public class ProductController {
     }
 
     /**
-     * 상품 조회 - 전체
-     * @return 전체 상품 목록
+     * 상품 조회 -전체 (페이징)
+     * 페이지 번호와 페이지 크기를 사용하여 페이징된 상품 목록을 조회한다.
+     * @param pageNumber 요청 시 페이지 번호 지정, 기본값 0
+     * @param pageSize 요청 시 페이지 크기 지정, 기본값 10
+     * @return 페이징된 상품 목록, 관련 메타데이터 포함한 응답 객체 반환
      */
     @GetMapping()
-    public List<Product> getAllProducts() {
-        return service.getAllProducts();
+    public ResponseEntity<Map<String, Object>> getAllProducts(
+            @RequestParam(defaultValue = "0") int pageNumber,
+            @RequestParam(defaultValue = "5") int pageSize) {
+        Page<Product> allProductsPaged = service.getAllProducts(pageNumber, pageSize);
+        System.out.println("pageNum:" +pageNumber);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("content", allProductsPaged.getContent());
+        response.put("totalPages", allProductsPaged.getTotalPages());
+        response.put("currentPageNumber", allProductsPaged.getNumber());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     /**
