c39b9d8128938a932a30db8f5797a496307c1e30
rdme0 <we6610@gmail.com>
2024-07-14 14:58:26 +0900

    :recycle: refactor: 메서드 이름 스네이크 케이스 -> 카멜 케이스 리팩터링

--- src/main/java/gift/repository/WishRepository.java
+++ src/main/java/gift/repository/WishRepository.java
@@ -11,6 +11,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface WishRepository extends JpaRepository<Wish, Long> {
     Optional<Wish> findByMemberAndProduct(Member member, Product product);
     void deleteByMemberAndProductId(Member member, Long productId);
-    void deleteByProduct_Id(Long productId);
+    void deleteByProductId(Long productId);
     Page<Wish> findByMember(Member member, Pageable pageable);
 }
