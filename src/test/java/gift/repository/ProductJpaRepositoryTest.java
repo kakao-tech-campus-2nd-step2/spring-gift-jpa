3f0403f7ec4d9a50de9ffb6fe559b5a87c8a23c9
momnpa333 <momnpa333@gmail.com>
2024-07-15 00:54:48 +0900

    test: 상품 가격 정렬 조회 테스트 추가

--- src/test/java/gift/repository/ProductJpaRepositoryTest.java
+++ src/test/java/gift/repository/ProductJpaRepositoryTest.java
@@ -5,10 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 import gift.model.product.Product;
 import gift.repository.product.ProductRepository;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 @DataJpaTest
 public class ProductJpaRepositoryTest {
@@ -18,7 +21,7 @@ public class ProductJpaRepositoryTest {
 
     @Test
     @DisplayName("상품 저장")
-    public void save() {
+    void save() {
         // given
         Product product = new Product(1L, "product1", 1000, "product1.jpg");
         // when
@@ -35,7 +38,7 @@ public class ProductJpaRepositoryTest {
 
     @Test
     @DisplayName("상품 삭제")
-    public void delete() {
+    void delete() {
         // given
         Product product = new Product(1L, "product1", 1000, "product1.jpg");
         productRepository.save(product);
@@ -47,7 +50,7 @@ public class ProductJpaRepositoryTest {
 
     @Test
     @DisplayName("상품 수정")
-    public void update() {
+    void update() {
         // given
         Product product = new Product(1L, "product1", 1000, "product1.jpg");
         productRepository.save(product);
@@ -66,7 +69,7 @@ public class ProductJpaRepositoryTest {
 
     @Test
     @DisplayName("상품 조회")
-    public void findById() {
+    void findById() {
         // given
         Product product = new Product(1L, "product1", 1000, "product1.jpg");
         productRepository.save(product);
@@ -78,6 +81,47 @@ public class ProductJpaRepositoryTest {
             () -> assertThat(findProduct.getPrice()).isEqualTo(1000),
             () -> assertThat(findProduct.getImageUrl()).isEqualTo("product1.jpg")
         );
+    }
+
+    @Test
+    @DisplayName("상품 가격 정렬 조회")
+    public void testFindAllOrderByPrice() {
+        //given
+        Product product1 = new Product(1L, "product1", 300, "product1.jpg");
+        Product product2 = new Product(2L, "product2", 200, "product2.jpg");
+        Product product3 = new Product(3L, "product3", 100, "product3.jpg");
+        productRepository.save(product1);
+        productRepository.save(product2);
+        productRepository.save(product3);
+
+        //when
+        Page<Product> result = productRepository.findAllOrderByPrice(PageRequest.of(0, 10));
+
+        //then
+        List<Product> products = result.getContent();
+        assertThat(products.get(0).getPrice()).isEqualTo(100);
+        assertThat(products.get(1).getPrice()).isEqualTo(200);
+        assertThat(products.get(2).getPrice()).isEqualTo(300);
+    }
+
+    @Test
+    @DisplayName("상품 이름으로 조회")
+    public void testFindByNameContaining() {
+        //given
+        Product product1 = new Product(1L, "sampleProduct1", 300, "product1.jpg");
+        Product product2 = new Product(2L, "product2", 200, "product2.jpg");
+        Product product3 = new Product(3L, "sampleProduct3", 100, "product3.jpg");
+        productRepository.save(product1);
+        productRepository.save(product2);
+        productRepository.save(product3);
+
+        //when
+        Page<Product> result = productRepository.findByNameContaining("sample",
+            PageRequest.of(0, 10));
+
+        //then
+        List<Product> products = result.getContent();
+        assertThat(products.size()).isEqualTo(2);
     }
 
 }
