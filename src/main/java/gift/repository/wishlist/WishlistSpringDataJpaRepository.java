e0b2038870b863b9bd3f39a16bd925d295bc7f2b
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-07-14 16:03:40 +0900

    feat: WishlistSpringDataJpaRepository 수정

--- src/main/java/gift/repository/wishlist/WishlistSpringDataJpaRepository.java
+++ src/main/java/gift/repository/wishlist/WishlistSpringDataJpaRepository.java
@@ -1,11 +1,14 @@
 package gift.repository.wishlist;
 
 import gift.domain.WishlistItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface WishlistSpringDataJpaRepository extends JpaRepository<WishlistItem, Long> {
     List<WishlistItem> findByMemberId(Long memberId);
+    Page<WishlistItem> findByMemberId(Long memberId, Pageable pageable);
     void deleteByMemberIdAndProductId(Long memberId, Long productId);
 }
