9d7bcfc2c0001722a1c65e97c3f597fbece80f98
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-14 14:42:00 +0900

    feat : WishlistRepository 수정 Wishlist의 String memberEmail과 Long productId가 Member와 Product로 변경되었으므로 WishlistRepository도 수정해줍니다.

--- src/main/java/gift/repository/WishlistRepository.java
+++ src/main/java/gift/repository/WishlistRepository.java
@@ -1,5 +1,7 @@
 package gift.repository;
 
+import gift.model.Member;
+import gift.model.Product;
 import gift.model.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,7 +11,6 @@ import java.util.List;
 @Repository
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
 
-    List<Wishlist> findByMemberEmail(String email);
-
-    void deleteByMemberEmailAndProductId(String email, Long productId);
+    List<Wishlist> findByMember(Member member);
+    void deleteByMemberAndProduct(Member member, Product product);
 }
