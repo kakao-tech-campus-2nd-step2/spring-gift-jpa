458463f0de397a79bfa4dc1e411b0629b553f336
yoong-saks <wjdghtjd06@gmail.com>
2024-07-14 20:44:49 +0900

    feat: ProductMapper 생성

---
+++ src/main/java/gift/mapper/ProductMapper.java
@@ -0,0 +1,21 @@
+package gift.mapper;
+
+import gift.domain.product.Product;
+import gift.web.dto.ProductDto;
+
+public class ProductMapper {
+
+    public ProductDto toDto(Product product) {
+        return new ProductDto(product.getId(),
+            product.getName(),
+            product.getPrice(),
+            product.getImageUrl());
+    }
+
+    public Product toEntity(ProductDto productDto) {
+        return new Product(productDto.name(),
+            productDto.price(),
+            productDto.imageUrl()
+        );
+    }
+}
