728d4fe1e733ba243c4583ac0814810f959bd461
bokyeong <wntjdals0412@gmail.com>
2024-07-15 03:50:27 +0900

    Feat: 에러코드 추가

--- src/main/java/gift/exception/ErrorCode.java
+++ src/main/java/gift/exception/ErrorCode.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
     INVALID_PRODUCT(HttpStatus.BAD_REQUEST, "유효하지 않은 상품정보입니다."),
-    INVALID_USER(HttpStatus.BAD_REQUEST, "유효하지 않은 멤버정보입니다.");
-
+    INVALID_MEMBER(HttpStatus.BAD_REQUEST, "유효하지 않은 멤버정보입니다."),
+    INVALID_WISH(HttpStatus.BAD_REQUEST, "유효하지 않은 위시정보입니다.");
     private final HttpStatus error;
 
     private final String message;
