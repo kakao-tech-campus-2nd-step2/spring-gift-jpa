40b8179991e61211b585c59765b7e672b93fa3be
HyeJiJUN <jhj910421@gmail.com>
2024-07-14 15:26:37 +0900

    rename: make DTO 래folder and move all DTO

---
+++ src/main/java/gift/DTO/LogInMemberDTO.java
@@ -0,0 +1,19 @@
+package gift.DTO;
+
+public class LogInMemberDTO {
+    private String email;
+    private String password;
+
+    public LogInMemberDTO(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
