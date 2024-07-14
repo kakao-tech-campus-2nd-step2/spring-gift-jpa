fc7305341a1dec1ea357f10535fecb25fde99493
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-14 19:19:49 +0900

    refactor : @PageableDefault 사용

--- src/main/java/gift/controller/WishListController.java
+++ src/main/java/gift/controller/WishListController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,11 +33,7 @@ public class WishListController {
 
     @GetMapping
     public ResponseEntity<Page<WishResponse>> getWishList(@LoginMember LoginUserDTO member,
-    @RequestParam(defaultValue = "0") int page,
-    @RequestParam(defaultValue = "5") int size,
-    @RequestParam(defaultValue = "id,asc") String[] sort) {
-        Sort.Direction direction = Sort.Direction.fromString(sort[1]);
-        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sort[0]));
+    @PageableDefault(size = 10, sort =  "name") Pageable pageable) {
         Page<WishResponse> wishLists = wishListService.getWishListByUserId(member.getId(), pageable);
         return ResponseEntity.ok(wishLists);
     }
