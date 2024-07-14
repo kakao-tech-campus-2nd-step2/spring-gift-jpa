bdd6e885be8367d12caf61e8fefe7400f758a7f3
나제법 <skwpqjq@gmail.com>
2024-07-14 17:33:35 +0900

    style: Application.java 사용하지 않는 import 문 제거

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -2,10 +2,10 @@ package gift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
         app.setAdditionalProfiles("local");
