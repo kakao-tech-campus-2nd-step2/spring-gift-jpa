0b029dd931b3622bdfb2b4db220130f9b5026314
bokyeong <wntjdals0412@gmail.com>
2024-07-15 03:51:25 +0900

    Refactor: MemberRegisterResponseDto 레코드로 변환

--- src/main/java/gift/dto/MemberRegisterResponseDto.java
+++ src/main/java/gift/dto/MemberRegisterResponseDto.java
@@ -1,25 +1,4 @@
 package gift.dto;
 
-public class MemberRegisterResponseDto {
-    private final Long id;
-    private final String email;
-    private final String name;
-
-    public MemberRegisterResponseDto(Long id, String email, String name) {
-        this.id = id;
-        this.email = email;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getName() {
-        return name;
-    }
+public record MemberRegisterResponseDto(long id, String email, String name) {
 }
