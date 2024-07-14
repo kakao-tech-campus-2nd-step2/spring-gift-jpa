fcaa7508eb3835246f96f4cbb8375bbd67026fa3
bokyeong <wntjdals0412@gmail.com>
2024-07-15 03:30:34 +0900

    Refactor: member 양방향 매핑 삭제 member 내의 wishList 제거

--- src/main/java/gift/domain/member/Member.java
+++ src/main/java/gift/domain/member/Member.java
@@ -1,19 +1,13 @@
 package gift.domain.member;
 
-import gift.domain.wish.Wish;
-import gift.exception.CustomException;
-import gift.exception.ErrorCode;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
     @Column(unique = true)
     @NotNull
@@ -25,9 +19,6 @@ public class Member {
     @NotNull
     private String password;
     private int role;
-
-    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Wish> wishList = new ArrayList<>();
 
     public Member(String email, String name, String password, int role) {
         this.email = email;
@@ -51,23 +42,7 @@ public class Member {
         return name;
     }
 
-    public List<Wish> getWishList() {
-        return wishList;
-    }
-
     public boolean isMatch(String password) {
         return password.equals(this.password);
-    }
-
-    public void addWish(Wish wish) {
-        wishList.add(wish);
-    }
-
-    public void deleteWish(Long productId) {
-        Wish target = this.wishList.stream()
-                .filter(w -> w.getProduct().getId().equals(productId))
-                .findFirst()
-                .orElseThrow(() -> new CustomException(ErrorCode.INVALID_PRODUCT));
-        wishList.remove(target);
     }
 }
