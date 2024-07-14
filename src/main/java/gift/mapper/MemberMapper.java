4c8cd03e3583d8050953c04bb3d9a856d736e83d
yoong-saks <wjdghtjd06@gmail.com>
2024-07-14 20:17:50 +0900

    feat: MemberMapper 생성

---
+++ src/main/java/gift/mapper/MemberMapper.java
@@ -0,0 +1,15 @@
+package gift.mapper;
+
+import gift.domain.member.Member;
+import gift.web.dto.MemberDto;
+
+public class MemberMapper {
+    public MemberDto toDto(Member member) {
+        return new MemberDto(member.getEmail(),
+                            member.getPassword());
+    }
+
+    public Member toEntity(MemberDto memberDto) {
+        return new Member(memberDto.email(), memberDto.password());
+    }
+}
--- src/main/java/gift/web/dto/MemberDto.java
+++ src/main/java/gift/web/dto/MemberDto.java
@@ -3,12 +3,4 @@ package gift.web.dto;
 import gift.domain.member.Member;
 
 public record MemberDto(String email,
-                        String password) {
-    public static MemberDto from(Member member) {
-        return new MemberDto(member.getEmail(), member.getPassword());
-    }
-
-    public static Member toEntity(MemberDto memberDto) {
-        return new Member(null, memberDto.email(), memberDto.password(), null);
-    }
-}
+                        String password) { }
