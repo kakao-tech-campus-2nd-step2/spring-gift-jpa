000658a87a7c5e429b9b73b99ff5f53eb2559b07
rbm0524 <rbm0524@naver.com>
2024-07-14 17:06:55 +0900

    fix : ProductDto를 Controller에 보내는 것으로 수정

--- src/main/java/gift/Service/ProductService.java
+++ src/main/java/gift/Service/ProductService.java
@@ -1,53 +1,70 @@
 package gift.Service;
 
-import gift.Entity.Products;
-import gift.Model.Product;
+import gift.Entity.Product;
+import gift.Mapper.Mapper;
+import gift.Model.ProductDto;
 import gift.Repository.ProductJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @Qualifier("productService")
 public class ProductService {
 
     private final ProductJpaRepository productJpaRepository;
+    private final Mapper mapper;
 
     @Autowired
-    public ProductService(ProductJpaRepository productJpaRepository) {
+    public ProductService(ProductJpaRepository productJpaRepository, Mapper mapper) {
         this.productJpaRepository = productJpaRepository;
+        this.mapper = mapper;
     }
 
-    public List<Products> getAllProducts() {
-        return productJpaRepository.findByisDeletedFalse();
+    public List<ProductDto> getAllProducts() {
+        List<Product> products = productJpaRepository.findByisDeletedFalse();
+        List<ProductDto> productsToDto = products.stream()
+                .map(mapper::productToDto)
+                .collect(Collectors.toList());
+        return productsToDto;
     }
 
-    public Optional<Products> getProductById(Long id) {
-        return productJpaRepository.findById(id);
+    public Page<ProductDto> getAllProductsByPage(Pageable pageable) {
+        Page<Product> products = productJpaRepository.findByisDeletedFalse(pageable);
+        return products.map(mapper::productToDto);
     }
 
-    public void saveProduct(Product product) {
-        Products products = Products.createProducts(product);
-        productJpaRepository.save(products);
+    public Optional<ProductDto> getProductById(Long id) {
+        Optional<Product> product = productJpaRepository.findById(id);
+        return product.map(mapper::productToDto);
 
     }
 
-    public void updateProduct(Product productDetails) {
-        Products products = Products.createProducts(productDetails);
-        productJpaRepository.save(products);
+    public void saveProduct(ProductDto productDto) {
+        Product product = mapper.productDtoToEntity(productDto);
+        productJpaRepository.save(product);
+
+    }
+
+    public void updateProduct(ProductDto productDtoDetails) {
+        Product product = mapper.productDtoToEntity(productDtoDetails);
+        productJpaRepository.save(product);
     }
 
     public void deleteProduct(Long id) {
         // 제품 ID로 제품을 찾습니다.
-        Optional<Products> productOptional = productJpaRepository.findById(id);
+        Optional<Product> productOptional = productJpaRepository.findById(id);
         if (productOptional.isPresent()) {
             // 제품이 존재하면, isDeleted를 true로 설정합니다.
-            Products products = productOptional.get();
-            products.setDeleted(true);
+            Product product = productOptional.get();
+            product.setDeleted(true);
             // 변경된 상태를 저장합니다.
-            productJpaRepository.save(products);
+            productJpaRepository.save(product);
         }
 
     }
