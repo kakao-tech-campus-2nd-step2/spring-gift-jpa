931035aac3c316dbdb484cf0cc2e78df6c7fc91c
박민규 <pmq0101@naver.com>
2024-07-14 12:49:57 +0900

    refactor(JpaWishRepository): jpql로 변경

--- src/main/java/gift/repository/JpaWishRepository.java
+++ src/main/java/gift/repository/JpaWishRepository.java
@@ -3,9 +3,11 @@ package gift.repository;
 import gift.domain.Wish;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JpaWishRepository extends JpaRepository<Wish, Long> {
+    @Query("SELECT w FROM Wish w JOIN FETCH w.product p WHERE w.user.id = :userId")
     List<Wish> findAllByUserId(Long userId);
 }
