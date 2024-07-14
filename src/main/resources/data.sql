b84621af66ef9372003f00bc0cfc4dadd1d5cd39
dkswoals <a4445656@gmail.com>
2024-07-14 14:15:33 +0900

    chore: 더미 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,37 @@
 INSERT INTO Item (name, price, img_url)
-values ('ItemA',
+values ('Item1',
         1000,
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwvPdrxYSEP7ZHfy9b3IzZ2yU2HkDh2BjSA_cHkth04jKFwTyQwxA26VlAlw&s');
 INSERT INTO Item (name, price, img_url)
-values ('ItemB',
+values ('Item2',
         2000,
         'https://media.istockphoto.com/id/183752521/ko/%EC%82%AC%EC%A7%84/%EB%B9%84%EB%B9%84%EB%B0%A5.jpg?s=2048x2048&w=is&k=20&c=f5FX83thuFkWNXmo-k7q5zD3xoEdO9oTFBSPEzBxMR0=');
 INSERT INTO Item (name, price, img_url)
-values ('ItemC',
+values ('Item3',
+        3000,
+        'https://media.istockphoto.com/id/183752521/ko/%EC%82%AC%EC%A7%84/%EB%B9%84%EB%B9%84%EB%B0%A5.jpg?s=2048x2048&w=is&k=20&c=f5FX83thuFkWNXmo-k7q5zD3xoEdO9oTFBSPEzBxMR0=');
+INSERT INTO Item (name, price, img_url)
+values ('Item4',
+        1000,
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwvPdrxYSEP7ZHfy9b3IzZ2yU2HkDh2BjSA_cHkth04jKFwTyQwxA26VlAlw&s');
+INSERT INTO Item (name, price, img_url)
+values ('Item5',
+        2000,
+        'https://media.istockphoto.com/id/183752521/ko/%EC%82%AC%EC%A7%84/%EB%B9%84%EB%B9%84%EB%B0%A5.jpg?s=2048x2048&w=is&k=20&c=f5FX83thuFkWNXmo-k7q5zD3xoEdO9oTFBSPEzBxMR0=');
+INSERT INTO Item (name, price, img_url)
+values ('Item6',
+        3000,
+        'https://media.istockphoto.com/id/183752521/ko/%EC%82%AC%EC%A7%84/%EB%B9%84%EB%B9%84%EB%B0%A5.jpg?s=2048x2048&w=is&k=20&c=f5FX83thuFkWNXmo-k7q5zD3xoEdO9oTFBSPEzBxMR0=');
+INSERT INTO Item (name, price, img_url)
+values ('Item7',
+        1000,
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwvPdrxYSEP7ZHfy9b3IzZ2yU2HkDh2BjSA_cHkth04jKFwTyQwxA26VlAlw&s');
+INSERT INTO Item (name, price, img_url)
+values ('Item8',
+        2000,
+        'https://media.istockphoto.com/id/183752521/ko/%EC%82%AC%EC%A7%84/%EB%B9%84%EB%B9%84%EB%B0%A5.jpg?s=2048x2048&w=is&k=20&c=f5FX83thuFkWNXmo-k7q5zD3xoEdO9oTFBSPEzBxMR0=');
+INSERT INTO Item (name, price, img_url)
+values ('Item9',
         3000,
         'https://media.istockphoto.com/id/183752521/ko/%EC%82%AC%EC%A7%84/%EB%B9%84%EB%B9%84%EB%B0%A5.jpg?s=2048x2048&w=is&k=20&c=f5FX83thuFkWNXmo-k7q5zD3xoEdO9oTFBSPEzBxMR0=');
 
