1935914e13d41a3a83849b69617d9cbbbb556193
dkswoals <a4445656@gmail.com>
2024-07-14 14:13:28 +0900

    feat: item, wishList repository findAll 반환값 Page<>로 변경

--- src/main/java/gift/repository/WishListRepository.java
+++ src/main/java/gift/repository/WishListRepository.java
@@ -1,7 +1,8 @@
 package gift.repository;
 
 import gift.model.wishList.WishItem;
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface WishListRepository extends JpaRepository<WishItem, Long> {
 
-    @Query("select w from WishItem w join fetch w.item")
-    List<WishItem> findAllByUserId(Long userId);
+    @Query("select w from WishItem w join fetch w.item order by w.id desc")
+    Page<WishItem> findAllByUserId(Long userId, Pageable pageable);
+
 }
