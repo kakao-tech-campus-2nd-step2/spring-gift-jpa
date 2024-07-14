cf77695d0442508c3704b8b4149fb89d29642dc5
jinhwa-park <parkjinhwa3914@gmail.com>
2024-07-14 17:38:06 +0900

    fix: save current status while troubleshooting build issues

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -2,7 +2,13 @@ package gift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "gift.repository")
+@EntityScan(basePackages = "gift.entity")
+@ComponentScan(basePackages = "gift")
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
