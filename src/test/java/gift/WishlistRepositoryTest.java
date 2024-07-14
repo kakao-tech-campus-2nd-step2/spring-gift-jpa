30bd0134481466a8c42efb4b232ca3b94a7a55cc
rbm0524 <rbm0524@naver.com>
2024-07-14 17:22:01 +0900

    fix : BeforeEach로 mapper를 수동 생성 및 주입

--- src/test/java/gift/WishlistRepositoryTest.java
+++ src/test/java/gift/WishlistRepositoryTest.java
@@ -10,8 +10,13 @@ import gift.Model.WishlistDto;
 import gift.Repository.ProductJpaRepository;
 import gift.Repository.MemberJpaRepository;
 import gift.Repository.WishlistJpaRepository;
+import gift.Service.MemberService;
+import gift.Service.ProductService;
+import gift.Service.WishlistService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -30,14 +35,32 @@ public class WishlistRepositoryTest {
     private MemberJpaRepository memberJpaRepository;
     @Autowired
     private ProductJpaRepository productJpaRepository;
-    @Autowired
+
     private Mapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        // 필요한 서비스의 목 객체 생성
+        ProductService productService = Mockito.mock(ProductService.class);
+        MemberService memberService = Mockito.mock(MemberService.class);
+        WishlistService wishListService = Mockito.mock(WishlistService.class);
+
+        MemberDto mockMemberDto = new MemberDto(1L, "test@test.com", "testName", "testPassword", false);
+        ProductDto mockProductDto = new ProductDto(1L, "testProduct", 1000, "testUrl", false);
+
+        // 목 객체에 대한 반환 값 설정
+        Mockito.when(memberService.findByUserId(Mockito.anyLong())).thenReturn(Optional.of(mockMemberDto));
+        Mockito.when(productService.getProductById(Mockito.anyLong())).thenReturn(Optional.of(mockProductDto));
+
+        // Mapper 인스턴스 수동 생성 및 주입
+        mapper = new Mapper(productService, memberService, wishListService);
+    }
 
     @Test
     public void testGetWishlist() {
         MemberDto memberDto1 = new MemberDto(1L, "1234@naver.com", "1234", "1234", false);
         Member member1 = mapper.memberDtoToEntity(memberDto1);
-        Member savedMember = memberJpaRepository.save(member1);
+        memberJpaRepository.save(member1);
 
         WishlistDto wishlistDto = new WishlistDto(1L, 1L, 1, 0, "test", 1000);
         Wishlist wishlist = mapper.wishlistDtoToEntity(wishlistDto);
