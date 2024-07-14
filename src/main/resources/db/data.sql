3fc196791537f01d7fd288f42b965a20df6bf70d
student-p <paulmoon00@naver.com>
2024-07-14 22:20:54 +0900

    feat: 사전 데이터 등록
    테스트 편의를 위해 사전 데이터 등록

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,9 +1,18 @@
-INSERT INTO products_tb (name, price, imageUrl) VALUES ('AAA', 1000, 'aoeitwi');
-INSERT INTO products_tb (name, price, imageUrl) VALUES ('BBB', 2000, 'woitjewt');
-INSERT INTO products_tb (name, price, imageUrl) VALUES ('CCC', 3000, 'ghleag');
+INSERT INTO product_tb (name, price, image_url) VALUES ('a1', 1000, 'aoeitwi');
+INSERT INTO product_tb (name, price, image_url) VALUES ('a2', 2000, 'woitjewt');
+INSERT INTO product_tb (name, price, image_url) VALUES ('a3', 3000, 'ghleag');
+INSERT INTO product_tb (name, price, image_url) VALUES ('a4', 1000, 'aoeitwi');
+INSERT INTO product_tb (name, price, image_url) VALUES ('a5', 2000, 'woitjewt');
+INSERT INTO product_tb (name, price, image_url) VALUES ('a6', 3000, 'ghleag');
+INSERT INTO product_tb (name, price, image_url) VALUES ('a7', 1000, 'aoeitwi');
+INSERT INTO product_tb (name, price, image_url) VALUES ('a8', 2000, 'woitjewt');
+INSERT INTO product_tb (name, price, image_url) VALUES ('a9', 3000, 'ghleag');
 
-INSERT INTO user_tb (email, password, accessToken) values ('asdf', '1234', '_asdf');
+INSERT INTO user_tb (email, password, access_token) values ('asdf@gmail.com', '1234', 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhc2RmQGdtYWlsLmNvbSJ9.wRzdf9FJyjWqf_n65eXE6ZiDKatUiOZ0h-Ll8MxDdcQ');
 
-INSERT INTO wishlist_tb (user_id, product_id, quantity) VALUES(1, 1, 2);
-INSERT INTO wishlist_tb (user_id, product_id, quantity) VALUES(1, 2, 3);
-INSERT INTO wishlist_tb (user_id, product_id, quantity) VALUES(1, 3, 4);
+INSERT INTO wish_tb (user_id, product_name, quantity) VALUES(1, 'a1', 2);
+INSERT INTO wish_tb (user_id, product_name, quantity) VALUES(1, 'a2', 3);
+INSERT INTO wish_tb (user_id, product_name, quantity) VALUES(1, 'a3', 4);
+INSERT INTO wish_tb (user_id, product_name, quantity) VALUES(1, 'a4', 2);
+INSERT INTO wish_tb (user_id, product_name, quantity) VALUES(1, 'a5', 3);
+INSERT INTO wish_tb (user_id, product_name, quantity) VALUES(1, 'a6', 4);
