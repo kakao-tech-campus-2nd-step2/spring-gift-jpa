8457b9871aaefb95292930e0fac312a6c89537b9
bokyeong <wntjdals0412@gmail.com>
2024-07-14 22:05:49 +0900

    Refactor: repository 어노테이션 삭제

--- src/main/java/gift/domain/member/MemberRepository.java
+++ src/main/java/gift/domain/member/MemberRepository.java
@@ -1,10 +1,8 @@
 package gift.domain.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
