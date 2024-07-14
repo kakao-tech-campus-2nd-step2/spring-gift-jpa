e2b2d73f2c58577d8c642e0a25b8df76c9103d69
BOMIN LYU <rbm0524@naver.com>
2024-07-15 00:59:11 +0900

    fix : conflict 해결하면서 이전 버전으로 돌아갔던 클래스들 복원

--- src/main/java/gift/Service/WishlistService.java
+++ src/main/java/gift/Service/WishlistService.java
@@ -1,10 +1,14 @@
 package gift.Service;
 
+import gift.Entity.Member;
 import gift.Entity.Wishlist;
 import gift.Mapper.Mapper;
+import gift.Model.MemberDto;
 import gift.Model.WishlistDto;
 import gift.Repository.WishlistJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,23 +16,29 @@ import java.util.List;
 @Service
 public class WishlistService {
 
-    private final WishlistJpaRepository wishListJpaRepository;
+    private final WishlistJpaRepository wishlistJpaRepository;
     private final Mapper mapper;
 
     @Autowired
     public WishlistService(WishlistJpaRepository wishlistJpaRepository, Mapper mapper) {
-        this.wishListJpaRepository = wishlistJpaRepository;
+        this.wishlistJpaRepository = wishlistJpaRepository;
         this.mapper = mapper;
     }
 
     public List<Wishlist> getWishlist(long userId) {
-        return wishListJpaRepository.findByIdUserId(userId);
+        return wishlistJpaRepository.findByIdUserId(userId);
+    }
+
+    public Page<WishlistDto> getWishlistByPage(MemberDto memberDto, Pageable pageable) {
+        Member member = mapper.memberDtoToEntity(memberDto);
+        Page<Wishlist> wishlist = wishlistJpaRepository.findByMember(member, pageable);
+        return wishlist.map(mapper::wishlistToDto);
     }
 
     public void addWishlistItem(WishlistDto wishlistDto) {
         wishlistDto.setPrice(wishlistDto.getPrice() * wishlistDto.getCount());
         Wishlist wishlist = mapper.wishlistDtoToEntity(wishlistDto);
-        wishListJpaRepository.save(wishlist);
+        wishlistJpaRepository.save(wishlist);
     }
 
     public void removeWishlistItem(WishlistDto wishListItem, Wishlist wishlistOptional) {
@@ -37,11 +47,11 @@ public class WishlistService {
         if (newCount > 0) {
             wishlistOptional.setCount(newCount);
             wishlistOptional.setPrice(wishlistOptional.getPrice() / wishListItem.getCount() * newCount);
-            wishListJpaRepository.save(wishlistOptional);
+            wishlistJpaRepository.save(wishlistOptional);
             return;
         }
 
-        wishListJpaRepository.delete(wishlistOptional);
+        wishlistJpaRepository.delete(wishlistOptional);
     }
 
 }
