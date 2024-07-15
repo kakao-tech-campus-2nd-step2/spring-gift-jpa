850ce361b8710b9b7efcb22069e7b0375f994a05
donghyeon <thornappl2@gmail.com>
2024-07-15 17:10:50 +0900

    sql: 데이터베이스 형식 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO products (name, price, image_url) VALUES
+INSERT INTO product (name, price, image_url) VALUES
     ('아이스 카페 아메리카노 T', 4500, 'https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg');
-INSERT INTO members (email, password) VALUES ('admin@email.com', 'cGFzc3dvcmQ='); -- Base64 encoded 'password'
+INSERT INTO member (email, password) VALUES ('admin@email.com', 'cGFzc3dvcmQ='); -- Base64 encoded 'password'
 
 
