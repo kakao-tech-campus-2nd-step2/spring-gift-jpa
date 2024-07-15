1aaf0b3a7277d9c33300a8089b2e6ce08837005c
seojiu <seojiu5598@gmail.com>
2024-07-15 15:20:30 +0900

    chore(MemberController.java): url 수정

--- src/main/java/gift/controller/MemberController.java
+++ src/main/java/gift/controller/MemberController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @RestController
-@RequestMapping("/members")
+@RequestMapping("/api/members")
 public class MemberController {
     private final MemberService memberService;
 
