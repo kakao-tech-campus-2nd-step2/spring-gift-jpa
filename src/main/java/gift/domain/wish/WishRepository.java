88480a05f83b7c17ae770828006458e3a4f77791
yoong-saks <wjdghtjd06@gmail.com>
2024-07-15 16:38:30 +0900

    fix: N + 1 문제 해결 ?? 일대다가 아닌 다대일 관계에서는 EntityGraph를 사용해도 pagination 문제가 발생하지 않는다..? 다만 일대다 상황에서(Product, Member) Wish의 컬렉선을 찾아오려면, Fetch Join, EntityGraph는 문제가 발생하므로 BatchSize를 통해 해결해야한다..?
    참고 :https://velog.io/@jinyoungchoi95/JPA-%EB%AA%A8%EB%93%A0-N1-%EB%B0%9C%EC%83%9D-%EC%BC%80%EC%9D%B4%EC%8A%A4%EA%B3%BC-%ED%95%B4%EA%B2%B0%EC%B1%85#pagination-%ED%95%B4%EA%B2%B0%EC%B1%85-1--toone-%EA%B4%80%EA%B3%84%EC%97%90%EC%84%9C-%ED%8E%98%EC%9D%B4%EC%A7%95-%EC%B2%98%EB%A6%AC
    https://wordbe.tistory.com/296
    https://velog.io/@idonymyeon/N1-Pageable-%ED%8A%B8%EB%9F%AC%EB%B8%94%EC%8A%88%ED%8C%85-%EC%97%AC%ED%96%89%EA%B8%B0-vobk675g
    https://s-y-130.tistory.com/184
    https://www.youtube.com/watch?v=ni92wUkAmQI

--- src/main/java/gift/domain/wish/WishRepository.java
+++ src/main/java/gift/domain/wish/WishRepository.java
@@ -1,14 +1,19 @@
 package gift.domain.wish;
 
+import gift.domain.product.Product;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WishRepository extends JpaRepository<Wish, Long> {
-
-    List<Wish> findAllByMember_Id(Long memberId);
+    @EntityGraph(attributePaths = {"member", "product"})
+    List<Wish> findAll();
+    @EntityGraph(attributePaths = {"member", "product"})
+    List<Wish> findAllByMember_Id(@Param("memberId") Long memberId);
     Optional<Wish> findByMember_IdAndProduct_Id(Long memberId, Long productId);
 }
