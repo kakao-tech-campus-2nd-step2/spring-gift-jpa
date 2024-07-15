e7eb8ff3d82fc3482c71fb94448ca10901596396
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2024-07-15 18:55:54 +0900

    refactor: AdminController 페이지네이션 로직 수정

--- src/main/java/gift/doamin/admin/controller/AdminController.java
+++ src/main/java/gift/doamin/admin/controller/AdminController.java
@@ -1,37 +1,34 @@
 package gift.doamin.admin.controller;
 
-import gift.doamin.product.entity.Product;
-import gift.doamin.product.repository.JpaProductRepository;
-import java.util.List;
-import org.springframework.http.HttpStatus;
+import gift.doamin.product.dto.ProductParam;
+import gift.doamin.product.service.ProductService;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class AdminController {
 
-    JpaProductRepository productRepository;
+    private final ProductService productService;
 
-    public AdminController(JpaProductRepository productRepository) {
-        this.productRepository = productRepository;
+    public AdminController(ProductService productService) {
+        this.productService = productService;
     }
 
     @GetMapping("/admin")
-    public ModelAndView showAdminPage(@RequestParam(defaultValue = "1") int page) {
-        List<Product> products = productRepository.findAll();
-        int lastPage = (products.size() - 1) / 5 + 1;
-        if (page < 1 || page > lastPage) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }
+    public ModelAndView showAdminPage(@RequestParam(defaultValue = "1", name = "page") int pageNum) {
+        Page<ProductParam> page = productService.getPage(pageNum - 1);
+        int lastPage = Math.max(1, page.getTotalPages());
+
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("admin/index");
-        modelAndView.addObject("products",
-            products.subList(Math.max(0, page * 5 - 5), Math.min(page * 5, products.size())));
-        modelAndView.addObject("productsCnt", products.size());
-        modelAndView.addObject("page", page);
-        modelAndView.addObject("startPage", Math.max(1, page - 2));
-        modelAndView.addObject("endPage", Math.max(lastPage, page + 2));
+        modelAndView.addObject("products", page.getContent());
+        modelAndView.addObject("productsCnt", page.getTotalElements());
+        modelAndView.addObject("page", pageNum);
+        modelAndView.addObject("startPage", Math.max(1, pageNum - 2));
+        modelAndView.addObject("endPage", Math.max(lastPage, pageNum + 2));
         modelAndView.addObject("lastPage", lastPage);
         return modelAndView;
     }
