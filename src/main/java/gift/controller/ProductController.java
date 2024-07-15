ed8b3172b18702743cf5db7ee148d4b43a435b09
HaegyeongKim01 <haegyeong.kim01@gmail.com>
2024-07-15 15:32:48 +0900

    refactor: imports - unused

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,17 +1,16 @@
 package gift.controller;
 
-import gift.service.ProductService;
 import gift.dto.ProductDto;
 import gift.dto.ProductUpdateDto;
-import jakarta.validation.Valid;
+import gift.service.ProductService;
 import gift.vo.Product;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
