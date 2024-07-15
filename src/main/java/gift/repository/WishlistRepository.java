4b149c77c1ca7ee87dddd307a73059487b0dda8b
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-15 11:46:05 +0900

    feat : WishlistRepository 수정 Page 사용을 위해 List로 만들었던 findByMember를 page로 바꿔줍니다.

--- src/main/java/gift/repository/WishlistRepository.java
+++ src/main/java/gift/repository/WishlistRepository.java
@@ -3,6 +3,8 @@ package gift.repository;
 import gift.model.Member;
 import gift.model.Product;
 import gift.model.Wishlist;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +13,7 @@ import java.util.List;
 @Repository
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
 
-    List<Wishlist> findByMember(Member member);
+    Page<Wishlist> findByMember(Member member, Pageable pageable);
 
     void deleteByMemberAndProduct(Member member, Product product);
 }
