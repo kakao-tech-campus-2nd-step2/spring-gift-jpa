b83fbf92516f1a895dbb33354e84467210f79e31
나제법 <skwpqjq@gmail.com>
2024-07-15 17:06:28 +0900

    feat: WishProductRepository.java 페이징 처리

--- src/main/java/gift/repository/WishProductRepository.java
+++ src/main/java/gift/repository/WishProductRepository.java
@@ -3,13 +3,14 @@ package gift.repository;
 import gift.domain.WishProduct;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WishProductRepository extends JpaRepository<WishProduct, Long> {
 
-    List<WishProduct> findByMemberId(Long memberId);
+    List<WishProduct> findByMemberId(Long memberId, Pageable pageable);
 
     Optional<WishProduct> findByMemberIdAndProductId(Long memberId, Long productId);
 }
