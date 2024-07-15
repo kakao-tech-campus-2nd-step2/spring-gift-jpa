3f3c0b6143de6aaf660fa2b173ab3a3afb56028d
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-15 17:45:22 +0900

    feat : N + 1 문제 해결

--- src/main/java/gift/repository/WishlistRepository.java
+++ src/main/java/gift/repository/WishlistRepository.java
@@ -5,12 +5,17 @@ import gift.model.Product;
 import gift.model.Wishlist;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
 
+    @EntityGraph(attributePaths = {"member", "product"})
+    Page<Wishlist> findAll(Pageable pageable);
+
+    @EntityGraph(attributePaths = {"member", "product"})
     Page<Wishlist> findByMember(Member member, Pageable pageable);
 
     void deleteByMemberAndProduct(Member member, Product product);
