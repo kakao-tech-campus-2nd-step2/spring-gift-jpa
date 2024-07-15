030a1fa69788875f872a66f5ab6d5039e15b9449
yoong-saks <wjdghtjd06@gmail.com>
2024-07-15 20:32:04 +0900

    feat: WishRepository 수정

--- src/main/java/gift/domain/wish/WishRepository.java
+++ src/main/java/gift/domain/wish/WishRepository.java
@@ -4,6 +4,8 @@ import gift.domain.product.Product;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,8 +14,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface WishRepository extends JpaRepository<Wish, Long> {
     @EntityGraph(attributePaths = {"member", "product"})
-    List<Wish> findAll();
+    Page<Wish> findAll(Pageable pageable);
     @EntityGraph(attributePaths = {"member", "product"})
-    List<Wish> findAllByMember_Id(@Param("memberId") Long memberId);
+    Page<Wish> findAllByMember_Id(@Param("memberId") Long memberId, Pageable pageable);
     Optional<Wish> findByMember_IdAndProduct_Id(Long memberId, Long productId);
 }
