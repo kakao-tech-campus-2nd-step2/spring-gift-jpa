eda432320eddade1b5e78ed746f784acbe955dec
yoong-saks <wjdghtjd06@gmail.com>
2024-07-15 09:43:05 +0900

    feat: N+1 문제 테스트

--- src/test/java/gift/service/member/MemberServiceTest.java
+++ src/test/java/gift/service/member/MemberServiceTest.java
@@ -5,7 +5,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import gift.domain.member.Member;
 import gift.domain.member.MemberRepository;
+import gift.domain.product.Product;
+import gift.domain.product.ProductReposiotory;
+import gift.domain.wish.Wish;
+import gift.domain.wish.WishRepository;
 import gift.web.dto.MemberDto;
+import jakarta.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +22,13 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 class MemberServiceTest {
     @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    private ProductReposiotory productReposiotory;
+    @Autowired
+    private WishRepository wishRepository;
+
+    @Autowired
+    private EntityManager entityManager;
 
     @BeforeEach
     void setUp() {
@@ -28,5 +42,34 @@ class MemberServiceTest {
         member.updateMember("ghtlr0607@naver.com", "5678");
         Member member2 = memberRepository.findByEmail("ghtlr0607@naver.com").get();
         assertThat(member2).isNotNull();
+    }
+
+    @Test
+    void test() {
+        List<Member> members = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            members.add(new Member("정호성" + i, "1234"));
+        }
+        memberRepository.saveAll(members);
+
+        List<Product> products = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            products.add(new Product("상품" + i, 2000L, "image.url"));
+        }
+        productReposiotory.saveAll(products);
+
+        List<Wish> wishess = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            Wish wish = new Wish(members.get(i), products.get(i), 2L);
+            wishess.add(wish);
+        }
+        wishRepository.saveAll(wishess);
+
+        entityManager.clear();
+
+        System.out.println("-----------N + 1 문제 테스트------------");
+        List<Wish> everyWishes = wishRepository.findAll();
+        assertFalse(everyWishes.isEmpty());
     }
 }
