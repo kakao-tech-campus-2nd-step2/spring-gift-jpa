ed2f999aff5b9ec351c9f855f5daa00442c2c071
bokyeong <wntjdals0412@gmail.com>
2024-07-17 12:37:01 +0900

    Refactor: WishController 분리

--- src/main/java/gift/controller/MemberController.java
+++ src/main/java/gift/controller/MemberController.java
@@ -1,28 +1,23 @@
 package gift.controller;
 
-import gift.authentication.LoginMember;
-import gift.authentication.UserDetails;
-import gift.dto.*;
+import gift.dto.MemberRegisterRequestDto;
+import gift.dto.MemberRegisterResponseDto;
+import gift.dto.MemberRequestDto;
+import gift.dto.TokenResponseDto;
 import gift.service.MemberService;
-import gift.service.WishService;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/members")
 public class MemberController {
     private final MemberService memberService;
-    private final WishService wishService;
 
-    public MemberController(MemberService memberService, WishService wishService) {
+    public MemberController(MemberService memberService) {
         this.memberService = memberService;
-        this.wishService = wishService;
     }
 
     @PostMapping("/signup")
@@ -34,39 +29,5 @@ public class MemberController {
     public ResponseEntity<TokenResponseDto> login(@RequestBody MemberRequestDto request) {
         String token = memberService.loginMember(request);
         return ResponseEntity.ok(new TokenResponseDto(token));
-    }
-
-    @PostMapping("/wishes")
-    public ResponseEntity<ApiResponse> addNewWish(@LoginMember UserDetails userDetails,
-                                                  @RequestBody WishAddRequestDto request) {
-        wishService.addWish(userDetails.id(), request);
-        return ResponseEntity.ok(new ApiResponse(HttpStatus.OK, "위시 리스트 등록에 성공하였습니다."));
-    }
-
-    @GetMapping("/wishes")
-    public ResponseEntity<List<WishResponseDto>> getMemberWishList(
-            @LoginMember UserDetails userDetails,
-            @RequestParam(required = false, defaultValue = "0", value = "page") int pageNum,
-            @RequestParam(required = false, defaultValue = "10", value = "size") int size,
-            @RequestParam(required = false, defaultValue = "id", value = "criteria") String criteria) {
-        Pageable pageable = PageRequest.of(pageNum, size, Sort.by(Sort.Direction.ASC, criteria));
-        return ResponseEntity.ok(wishService.getAllWishes(userDetails.id(), pageable));
-    }
-
-    @PutMapping("/wishes/{id}")
-    public ResponseEntity<ApiResponse> updateWish(
-            @LoginMember UserDetails userDetails,
-            @PathVariable("id") Long id,
-            @RequestBody WishUpdateRequestDto requestDto) {
-        wishService.updateWish(userDetails.id(), id, requestDto.quantity());
-        return ResponseEntity.ok(new ApiResponse(HttpStatus.OK, "수량이 성공적으로 수정되었습니다."));
-    }
-
-    @DeleteMapping("/wishes/{id}")
-    public ResponseEntity<ApiResponse> deleteWish(
-            @LoginMember UserDetails userDetails,
-            @PathVariable("id") Long productId) {
-        wishService.deleteWish(userDetails.id(), productId);
-        return ResponseEntity.ok(new ApiResponse(HttpStatus.NO_CONTENT, "성공적으로 삭제되었습니다."));
     }
 }
